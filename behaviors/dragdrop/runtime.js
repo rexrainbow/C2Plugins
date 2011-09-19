993cc87e98a42eaa33db30ea6164875d17dda2d0
hua <binhua.tein@gmail.com>
2011-09-20 07:53:17 +0800

    Add drag & drop behavior plugin

---
+++ behaviors/dragdrop/runtime.js
@@ -0,0 +1,67 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.DragDrop = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.DragDrop.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+		
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{   
+	};
+
+	behinstProto.tick = function ()
+	{
+
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+    
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+    
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+
+}());
