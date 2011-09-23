9bc354e2a350a1bc73a9420d9dbcc53cf8420a27
hua <binhua.tein@gmail.com>
2011-09-23 08:45:01 +0800

    Add touch+mouse plugin

---
+++ plugins/touchmouse/edittime.js
@@ -0,0 +1,127 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"TouchMouse",
+		"id":			"MyTouchMouse",
+		"description":	"Retrieve input from the mouse or touchscreen devices.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Input",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddComboParamOption("Left");
+AddComboParamOption("Middle");
+AddComboParamOption("Right");
+AddComboParam("Mouse button", "Select the mouse button to check.");
+AddComboParamOption("Clicked");
+AddComboParamOption("Double-clicked");
+//AddComboParamOption("Either single or double clicked");
+AddComboParam("Click type", "Select which kind of click to check for.");
+AddCondition(0,	cf_trigger, "On click", "Mouse", "On <b>{0}</b> button <b>{1}</b>", "Triggered when a mouse button clicked or double-clicked.", "OnClick");
+
+AddCondition(1,	cf_trigger, "On any click", "Mouse", "On any click", "Triggered when any mouse button clicked or double-clicked.", "OnAnyClick");
+
+AddComboParamOption("Left");
+AddComboParamOption("Middle");
+AddComboParamOption("Right");
+AddComboParam("Mouse button", "Select the mouse button to check.");
+AddCondition(2, 0, "Mouse button is down", "Mouse", "<b>{0}</b> button is down", "Test if a mouse button is currently held down.", "IsButtonDown");
+
+AddComboParamOption("Left");
+AddComboParamOption("Middle");
+AddComboParamOption("Right");
+AddComboParam("Mouse button", "Select the mouse button to check.");
+AddCondition(3, cf_trigger, "On button released", "Mouse", "On <b>{0}</b> button released", "Triggered when a mouse button released.", "OnRelease");
+
+//AddCondition(4,	cf_trigger, "On wheel up", "Mouse", "On wheel up", "Triggered when the mouse wheel rolled up.", "OnWheelUp");
+//AddCondition(5,	cf_trigger, "On wheel down", "Mouse", "On wheel down", "Triggered when the mouse wheel rolled down.", "OnWheelDown");
+
+AddComboParamOption("Left");
+AddComboParamOption("Middle");
+AddComboParamOption("Right");
+AddComboParam("Mouse button", "Select the mouse button to check.");
+AddComboParamOption("Clicked");
+AddComboParamOption("Double-clicked");
+//AddComboParamOption("Either single or double clicked");
+AddComboParam("Click type", "Select which kind of click to check for.");
+AddObjectParam("Object clicked", "Choose the object to check for a click on.");
+AddCondition(6,	cf_trigger, "On object clicked", "Mouse", "On <b>{0}</b> button <b>{1}</b> on {2}", "Triggered when a mouse button clicked or double-clicked on an object.", "OnObjectClicked");
+
+AddObjectParam("Object", "Choose the object to check for mouse over.");
+AddCondition(7,	0, "Cursor is over object", "Mouse", "Cursor is over {0}", "Check if the mouse cursor is over an object.", "IsOverObject");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number | ef_variadic_parameters, "Mouse X position", "Cursor", "X", "Get the mouse cursor X co-ordinate in the layout.");
+AddExpression(1, ef_return_number | ef_variadic_parameters, "Mouse Y position", "Cursor", "Y", "Get the mouse cursor Y co-ordinate in the layout.");
+
+AddExpression(2, ef_return_number, "Absolute mouse X", "Cursor", "AbsoluteX", "Get the mouse cursor X co-ordinate on the canvas.");
+AddExpression(3, ef_return_number, "Absolute mouse Y", "Cursor", "AbsoluteY", "Get the mouse cursor Y co-ordinate on the canvas.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/touchmouse/runtime.js
@@ -0,0 +1,535 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.MyTouchMouse = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.MyTouchMouse.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		
+		this.buttonMap = new Array(4);		// mouse down states
+		this.mouseXcanvas = 0;				// mouse position relative to canvas
+		this.mouseYcanvas = 0;
+		
+		this.triggerButton = 0;
+		this.triggerType = 0;
+        
+        // touch
+		this.touches = [];        
+        
+        // control
+        this.trigger_source = 1;  // 0=touch, 1=mouse
+	};
+
+	var instanceProto = pluginProto.Instance.prototype;
+
+    // touch
+    instanceProto.saveTouches = function (t)
+	{
+		this.touches.length = 0;
+		var offset = jQuery(this.runtime.canvas).offset();
+		
+		var i, len, touch;
+		for (i = 0, len = t.length; i < len; i++)
+		{
+			touch = t[i];
+			this.touches.push({ x: touch.pageX - offset.left, y: touch.pageY - offset.top });
+		}
+	};
+    
+	instanceProto.onCreate = function()
+	{
+		// Bind mouse events via jQuery
+		jQuery(document).mousemove(
+			(function (self) {
+				return function(info) {
+					self.onMouseMove(info);
+				};
+			})(this)
+		);
+		
+		jQuery(document).mousedown(
+			(function (self) {
+				return function(info) {
+					self.onMouseDown(info);
+				};
+			})(this)
+		);
+		
+		jQuery(document).mouseup(
+			(function (self) {
+				return function(info) {
+					self.onMouseUp(info);
+				};
+			})(this)
+		);
+		
+		jQuery(document).dblclick(
+			(function (self) {
+				return function(info) {
+					self.onDoubleClick(info);
+				};
+			})(this)
+		);
+        
+        // touch
+		this.runtime.canvas.addEventListener("touchstart",
+			(function (self) {
+				return function(info) {
+					self.onTouchStart(info);
+				};
+			})(this),
+			true
+		);
+		
+		this.runtime.canvas.addEventListener("touchmove",
+			(function (self) {
+				return function(info) {
+					self.onTouchMove(info);
+				};
+			})(this),
+			true
+		);
+		
+		this.runtime.canvas.addEventListener("touchend",
+			(function (self) {
+				return function(info) {
+					self.onTouchEnd(info);
+				};
+			})(this),
+			true
+		);        
+	};
+
+	instanceProto.onMouseMove = function(info)
+	{
+        this.trigger_source = 1;    
+		var offset = jQuery(this.runtime.canvas).offset();
+		this.mouseXcanvas = info.pageX - offset.left;
+		this.mouseYcanvas = info.pageY - offset.top;      
+	};
+
+	instanceProto.onMouseDown = function(info)
+	{
+        this.trigger_source = 1;
+		// Update button state
+		this.buttonMap[info.which] = true;
+		
+		// Trigger OnAnyClick
+		this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnAnyClick, this);
+		
+		// Trigger OnClick & OnObjectClicked
+		this.triggerButton = info.which - 1;	// 1-based
+		this.triggerType = 0;					// single click
+		this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnClick, this);
+		this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnObjectClicked, this);
+	};
+
+	instanceProto.onMouseUp = function(info)
+	{
+        this.trigger_source = 1;
+		// Update button state
+		this.buttonMap[info.which] = false;
+		
+		// Trigger OnRelease
+		this.triggerButton = info.which - 1;	// 1-based
+		this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnRelease, this);
+	};
+
+	instanceProto.onDoubleClick = function(info)
+	{
+        this.trigger_source = 1;
+		// Trigger OnClick & OnObjectClicked
+		this.triggerButton = info.which - 1;	// 1-based
+		this.triggerType = 1;					// double click
+		this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnClick, this);
+		this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnObjectClicked, this);
+	};
+    
+    // touch
+	instanceProto.onTouchMove = function (info)
+	{
+        this.trigger_source = 0;
+		info.preventDefault();
+		this.saveTouches(info.touches);
+	};
+
+	instanceProto.onTouchStart = function (info)
+	{
+        this.trigger_source = 0;
+		info.preventDefault();
+		this.saveTouches(info.touches);
+		
+		// Trigger OnTouchStart= OnAnyClick, OnClick
+        this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnAnyClick, this);
+        this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnClick, this);
+		//this.runtime.trigger(cr.plugins_.Touch.prototype.cnds.OnTouchStart, this);
+		
+		// Trigger OnTouchObject=OnObjectClicked for each touch started event
+		var offset = jQuery(this.runtime.canvas).offset();
+		
+		if (info.changedTouches)
+		{
+			var i, len;
+			for (i = 0, len = info.changedTouches.length; i < len; i++)
+			{
+				var touch = info.changedTouches[i];
+				
+				this.curTouchX = touch.pageX - offset.left;
+				this.curTouchY = touch.pageY - offset.top;
+                this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnObjectClicked, this);
+				//this.runtime.trigger(cr.plugins_.Touch.prototype.cnds.OnTouchObject, this);
+			}
+		}
+	};
+
+	instanceProto.onTouchEnd = function (info)
+	{
+        this.trigger_source = 0;
+		info.preventDefault();
+		this.saveTouches(info.touches);
+		
+		// Trigger OnTouchEnd=OnRelease
+        this.runtime.trigger(cr.plugins_.MyTouchMouse.prototype.cnds.OnRelease, this);
+		//this.runtime.trigger(cr.plugins_.Touch.prototype.cnds.OnTouchEnd, this);
+	};    
+
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;
+
+	cnds.OnClick = function (button, type)
+	{
+		return (this.trigger_source == 1)?
+               (button === this.triggerButton && type === this.triggerType):  // mouse
+               true;  //touch
+	};
+	
+	cnds.OnAnyClick = function ()
+	{
+		return true;
+	};
+	
+	cnds.IsButtonDown = function (button)
+	{
+		return (this.trigger_source==1)?
+               (this.buttonMap[button + 1]):   // mouse:jQuery uses 1-based buttons for some reason
+               (this.touches.length);    // touch
+	};
+	
+	cnds.OnRelease = function (button)
+	{
+		return (this.trigger_source == 1)?
+               (button === this.triggerButton):  // mouse
+               true;  //touch
+	};
+	
+	cnds.IsOverObject = function (obj)
+	{
+        if (this.trigger_source == 1)  // mouse
+        {    
+		    // We need to handle invert manually.  If inverted, turn invert off on the condition,
+		    // and instead pass it to testAndSelectCanvasPointOverlap() which does SOL picking
+		    // based on the invert status.
+		    var cnd = this.runtime.getCurrentCondition();
+		
+		    // Not run yet
+		    if (typeof (cnd.extra.mouseOverInverted) === "undefined")
+		    {
+			    // turn off invert as far as the event engine is concerned, and pass it
+			    // below instead
+			    cnd.extra.mouseOverInverted = cnd.inverted;
+			    cnd.inverted = false;
+		    }
+		
+		    var mx = this.mouseXcanvas;
+		    var my = this.mouseYcanvas;
+		
+		    return this.runtime.testAndSelectCanvasPointOverlap(obj, mx, my, cnd.extra.mouseOverInverted);
+        }
+        else    // touch
+        {
+		    if (!type)
+			    return false;
+			
+		    var sol = type.getCurrentSol();
+		    var instances = sol.getObjects();
+		    var px, py;
+		
+		    var touching = [];
+			
+		    // Check all touches for overlap with any instance
+		    var i, leni, j, lenj;
+		    for (i = 0, leni = instances.length; i < leni; i++)
+		    {
+			    var inst = instances[i];
+			    inst.update_bbox();
+			
+			    for (j = 0, lenj = this.touches.length; j < lenj; j++)
+			    {
+				    var touch = this.touches[j];
+				
+				    px = inst.layer.canvasToLayerX(touch.x);
+				    py = inst.layer.canvasToLayerY(touch.y);
+				
+				    if (inst.bquad.contains_pt(px, py))
+				    {
+					    touching.push(inst);
+					    break;
+				    }
+			    }
+		    }
+		
+		    if (touching.length)
+		    {
+			    sol.select_all = false;
+			    sol.instances = touching;
+			    return true;
+		    }
+		    else
+			    return false;        
+        }        
+	};
+	
+	cnds.OnObjectClicked = function (button, type, obj)
+	{
+        if (this.trigger_source == 1)  // mouse
+        {
+		    if (button !== this.triggerButton || type !== this.triggerType)
+			    return false;	// wrong click type
+		
+    		var mx = this.mouseXcanvas;
+	    	var my = this.mouseYcanvas;
+		
+		    // Can't be inverted
+		    return this.runtime.testAndSelectCanvasPointOverlap(obj, mx, my, false);
+        }
+        else    // touch
+        {
+		    if (!type)
+			    return false;
+			
+		    var sol = type.getCurrentSol();
+		    var instances = sol.getObjects();
+		    var px, py;
+		
+		    var touching = [];
+		
+		    // Check curTouchX/Y overlaps any instance
+		    var i, len;
+		    for (i = 0, len = instances.length; i < len; i++)
+		    {
+			    var inst = instances[i];
+			    inst.update_bbox();
+			
+			    px = inst.layer.canvasToLayerX(this.curTouchX);
+			    py = inst.layer.canvasToLayerY(this.curTouchY);
+			
+			    if (inst.bquad.contains_pt(px, py))
+				    touching.push(inst);
+		    }
+		
+		    if (touching.length)
+		    {
+			    sol.select_all = false;
+			    sol.instances = touching;
+			    return true;
+		    }
+		    else
+			    return false;        
+        }        
+	};
+
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+	exps.X = function (ret, layerparam)
+	{
+        if (this.trigger_source == 1)  // mouse
+        {
+		    var layer, oldScale;
+		
+		    if (typeof layerparam === "undefined")
+		    {
+			    // calculate X position on bottom layer as if its scale were 1.0
+			    layer = this.runtime.getLayerByNumber(0);
+			    oldScale = layer.scale;
+			    layer.scale = 1.0;
+			    ret.set_float(layer.canvasToLayerX(this.mouseXcanvas));
+			    layer.scale = oldScale;
+		    }
+		    else
+		    {
+			    // use given layer param
+			    if (typeof layerparam === "number")
+				    layer = this.runtime.getLayerByNumber(layerparam);
+			    else
+				    layer = this.runtime.getLayerByName(layerparam);
+				
+			    if (!layer)
+				    ret.set_float(0);
+				
+			    ret.set_float(layer.canvasToLayerX(this.mouseXcanvas));
+		    }
+        }
+        else    // touch
+        {
+		    if (this.touches.length)
+		    {
+			    var layer, oldScale;
+		
+			    if (typeof layerparam === "undefined")
+			    {
+				    // calculate X position on bottom layer as if its scale were 1.0
+				    layer = this.runtime.getLayerByNumber(0);
+				    oldScale = layer.scale;
+				    layer.scale = 1.0;
+				    ret.set_float(layer.canvasToLayerX(this.touches[0].x));
+				    layer.scale = oldScale;
+			    }
+			    else
+			    {
+				    // use given layer param
+				    if (typeof layerparam === "number")
+					    layer = this.runtime.getLayerByNumber(layerparam);
+				    else
+					    layer = this.runtime.getLayerByName(layerparam);
+					
+				    if (!layer)
+					    ret.set_float(0);
+					
+				    ret.set_float(layer.canvasToLayerX(this.touches[0].x));
+			    }
+		    }
+		    else
+			    ret.set_float(0);        
+        }        
+	};
+	
+	exps.Y = function (ret, layerparam)
+	{
+        if (this.trigger_source == 1)  // mouse
+        {
+            var layer, oldScale;
+            
+		    if (typeof layerparam === "undefined")
+		    {
+			    // calculate X position on bottom layer as if its scale were 1.0
+			    layer = this.runtime.getLayerByNumber(0);
+			    oldScale = layer.scale;
+			    layer.scale = 1.0;
+			    ret.set_float(layer.canvasToLayerY(this.mouseYcanvas));
+			    layer.scale = oldScale;
+		    }
+		    else
+		    {
+			    // use given layer param
+			    if (typeof layerparam === "number")
+				    layer = this.runtime.getLayerByNumber(layerparam);
+			    else
+				    layer = this.runtime.getLayerByName(layerparam);
+				
+			    if (!layer)
+				    ret.set_float(0);
+				
+			    ret.set_float(layer.canvasToLayerY(this.mouseYcanvas));
+		    }
+        
+        }
+        else    // touch
+        {
+		    if (this.touches.length)
+		    {
+			    var layer, oldScale;
+		
+			    if (typeof layerparam === "undefined")
+			    {
+				    // calculate X position on bottom layer as if its scale were 1.0
+				    layer = this.runtime.getLayerByNumber(0);
+				    oldScale = layer.scale;
+				    layer.scale = 1.0;
+				    ret.set_float(layer.canvasToLayerY(this.touches[0]));
+				    layer.scale = oldScale;
+			    }
+			    else
+			    {
+				    // use given layer param
+				    if (typeof layerparam === "number")
+					    layer = this.runtime.getLayerByNumber(layerparam);
+				    else
+					    layer = this.runtime.getLayerByName(layerparam);
+					
+				    if (!layer)
+					    ret.set_float(0);
+					
+				    ret.set_float(layer.canvasToLayerY(this.touches[0].y));
+			    }
+		    }
+		    else
+			    ret.set_float(0);        
+        }        
+	};
+	
+	exps.AbsoluteX = function (ret)
+	{
+        if (this.trigger_source == 1)  // mouse
+        {
+		    ret.set_float(this.mouseXcanvas);
+        }
+        else    // touch
+        {
+		    if (this.touches.length)
+			    ret.set_float(this.touches[0].x);
+		    else
+			    ret.set_float(0);        
+        }
+	};
+	
+	exps.AbsoluteY = function (ret)
+	{
+        if (this.trigger_source == 1)  // mouse
+        {
+		    ret.set_float(this.mouseYcanvas);
+        }
+        else    // touch
+        {
+		    if (this.touches.length)
+			    ret.set_float(this.touches[0].y);
+		    else
+			    ret.set_float(0);        
+        }
+	};
+	
+}());
