d82e734b73d6f4a12c9602cd1b666efb82c63e16
hua <binhua.tein@gmail.com>
2011-09-29 08:16:41 +0800

    add behavior plugin: fsm as a variable type

---
+++ behaviors/fsm/edittime.js
@@ -0,0 +1,134 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"state",
+		"id":			"MyFSM",
+		"description":	"Finite state machine",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Varaible",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(0, 0, "Clean all arguments", "Argument", 
+          "Clean {my} all arguments", 
+          "Clean all arguments.", 
+          "CleanArguments");
+AddAnyTypeParam("Index", "Index of argument, can be number of string", "0");
+AddAnyTypeParam("Value", "Value of argument", "0");
+AddAction(1, 0, "Add a argument", "Argument", 
+          "Set {my} arguments[<i>{0}</i>] = <i>{1}</i>", 
+          "Set a arguments pass into callback.", 
+          "SetArgument");
+AddAction(2, 0, "Request", "Request", 
+          "Request {my}", 
+          "input a request.", 
+          "Request");        
+AddStringParam("Name", "State name", '""');
+AddAction(3, 0, "Transit to state", "Request", 
+          "Transit {my} to <i>{0}</i>", 
+          "Transit to state.", 
+          "Transit");
+AddStringParam("Name", "State name", '""');
+AddAction(4, 0, "Force transit to state", "Request", 
+          "Force transit {my} to <i>{0}</i>", 
+          "Force transit to state.", 
+          "ForceTransit");
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddStringParam("Name", "State name", '""');
+AddCondition(0, cf_trigger, "On request", "Callback", 
+             "On {my} request at <i>{0}</i>", 
+			 "Triggered when request.", 
+			 "OnRequest");
+AddStringParam("Name", "State name", '""');
+AddCondition(1, cf_trigger, "On enter state", "Callback", 
+             "On {my} enter to <i>{0}</i>", 
+			 "Triggered when enter state.", 
+			 "OnEnter");
+AddStringParam("Name", "State name", '""');
+AddCondition(2, cf_trigger, "On exit state", "Callback", 
+             "On {my} exit from <i>{0}</i>", 
+			 "Triggered when exit state.", 
+			 "OnExit");
+AddStringParam("Name", "Exit from state", '""');
+AddStringParam("Name", "Enter to state", '""');
+AddCondition(3, cf_trigger, "On state transfer", "Callback", 
+             "On {my} exit from <i>{0}</i> and enter to <i>{1}</i>", 
+			 "Triggered when state transfer.", 
+			 "OnTransfer");
+AddCondition(4, cf_trigger, "On default request", "Callback", 
+             "On {my} default request", 
+			 "Triggered when no request callback.", 
+			 "OnDefaultRequest");
+AddCondition(5, cf_trigger, "On default enter", "Callback", 
+             "On {my} default enter", 
+			 "Triggered when no enter callback.", 
+			 "OnDefaultEnter");             
+AddCondition(6, cf_trigger, "On default exit", "Callback", 
+             "On {my} default exit", 
+			 "Triggered when no exit callback.", 
+			 "OnDefaultExit"); 
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_string, "Current state", "State", "CurState", "Get current state.");
+AddExpression(1, ef_return_string, "Previous state", "State", "PreState", "Get previous state.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+    new cr.Property(ept_text, "Start state", "Off", "State at the start of the layout."),
+    new cr.Property(ept_text, "Default transition", "", 'Set default transition, ex:"IDLE,RUN".'),	     
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/fsm/runtime.js
@@ -0,0 +1,189 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.MyFSM = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.MyFSM.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;        
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.activated = this.properties[0];
+        this.start_state = this.properties[1];
+		this.previous_state = "Off";	
+        this.current_state = "Off";	
+        this.args = {};
+        this.is_echo = false;	
+        
+        this._state_reset();        
+	};
+    
+	behinstProto._state_reset = function ()
+	{
+        this._state_transition("Off");
+        if (this.start_state != "Off")
+        {
+            this._state_transition(this.start_state);
+        }
+	};    
+    
+	behinstProto.tick = function ()
+	{
+	};
+	
+	behinstProto._state_transition = function (new_state)
+	{
+	    this.previous_state = this.current_state;
+		this.current_state = new_state;
+		this.is_echo = false;
+		this.runtime.trigger(cr.behaviors.MyFSM.prototype.cnds.OnTransfer, this);
+		if (!this.is_echo)
+		{
+            this.is_echo = false;
+		    this.runtime.trigger(cr.behaviors.MyFSM.prototype.cnds.OnExit, this);
+            if (!this.is_echo)
+            {
+                this.runtime.trigger(cr.behaviors.MyFSM.prototype.cnds.OnDefaultExit, this);
+            }
+            this.is_echo = false;
+			this.runtime.trigger(cr.behaviors.MyFSM.prototype.cnds.OnEnter, this);
+            if (!this.is_echo)
+            {
+                this.runtime.trigger(cr.behaviors.MyFSM.prototype.cnds.OnDefaultEntert, this);
+            }            
+		}
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+    
+	cnds.OnRequest = function (name)
+	{
+	    var is_my_call = (this.current_state == name);
+        this.is_echo |= is_my_call;
+		return is_my_call;
+	};   
+    
+	cnds.OnDefaultRequest = function ()
+	{
+		return true;
+	};   	
+    
+	cnds.OnEnter = function (name)
+	{
+		return (this.current_state == name);
+	};
+
+	cnds.OnDefaultEnter = function ()
+	{
+		return true;
+	}; 	
+	
+	cnds.OnExit = function (name)
+	{
+		return (this.previous_state == name);
+	};	
+    
+	cnds.OnDefaultExit = function ()
+	{
+		return true;
+	}; 	    
+
+	cnds.OnTransfer = function (name_from, name_to)
+	{
+	    var is_my_call = ((this.previous_state == name_from) && 
+		                  (this.current_state == name_to));
+        this.is_echo |= is_my_call;
+		return is_my_call;
+	};		
+    
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+ 
+ 	acts.CleanArguments = function ()
+	{
+        this.args = {};
+	};   
+    
+	acts.SetArgument = function (index, value)
+	{
+        this.args[index] = value;
+	};  
+
+    acts.Request = function ()
+	{
+		this.is_echo = false;
+		this.runtime.trigger(cr.behaviors.MyFSM.prototype.cnds.OnRequest, this);
+        if (!this.is_echo)
+        {
+		    this.runtime.trigger(cr.behaviors.MyFSM.prototype.cnds.OnDefaultRequest, this);        
+        }
+	};  
+    
+ 	acts.Transit = function (new_state)
+	{
+        if (this.default_transitions != null)
+        {
+        }
+        this._state_transition(new_state);
+	};
+    
+  	acts.ForceTransit = function (new_state)
+	{
+        this._state_transition(new_state);
+	};   
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+	exps.CurState = function (ret)
+	{
+	    ret.set_string( this.current_state );
+	};	
+	
+	exps.PreState = function (ret)
+	{
+	    ret.set_string( this.previous_state );
+	};
+}());
