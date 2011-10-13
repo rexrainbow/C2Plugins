e73cec3ff497f8c591a270fe585e2ea5fdf60527
hua <binhua.tein@gmail.com>
2011-10-13 09:10:26 +0800

    Add ninja behavior

---
+++ behaviors/ninja/runtime.js
@@ -0,0 +1,121 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Ninja = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Ninja.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+		// Bind mouse events via jQuery
+		jQuery(document).mousemove(
+			(function (self) {
+				return function(info) {
+					self.onMouseMove(info);
+				};
+			})(this)
+		);
+        
+		this.mouseXcanvas = 0;				// mouse position relative to canvas
+		this.mouseYcanvas = 0;    
+	};
+    
+	behtypeProto.onMouseMove = function(info)
+	{
+		var offset = jQuery(this.runtime.canvas).offset();
+		this.mouseXcanvas = info.pageX - offset.left;
+		this.mouseYcanvas = info.pageY - offset.top;           
+	};    
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.activated = this.properties[0];
+        this.is_over = false;
+        this.inst.visible = 0;
+	};
+
+	behinstProto.tick = function ()
+	{
+        if (this.activated==1)
+        {
+            var inst = this.inst;
+            inst.update_bbox();
+	        var lx = inst.layer.canvasToLayerX(this.type.mouseXcanvas);
+		    var ly = inst.layer.canvasToLayerY(this.type.mouseYcanvas);
+            this.is_over = inst.contains_pt(lx, ly);
+            inst.visible = this.is_over;
+            this.runtime.redraw = true;
+            
+            // Trigger OnOver
+            if (this.is_over)
+            {
+                this.runtime.trigger(cr.behaviors.Ninja.prototype.cnds.OnOver, this.inst);            
+            }
+        }
+	};
+    
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;   
+    
+	cnds.OnOver = function ()
+	{
+		return true;
+	};
+    
+	cnds.IsOver = function ()
+	{
+		return (this.is_over);
+	};    
+    
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+	acts.SetActivated = function (s)
+	{
+		this.activated = s;
+	};  
+    
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+}());
