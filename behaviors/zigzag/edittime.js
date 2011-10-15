34e4368f1eec579ed9f9884d15694426b6fb94d5
hua <binhua.tein@gmail.com>
2011-10-15 13:46:12 +0800

    Add zigzag

---
+++ behaviors/zigzag/edittime.js
@@ -0,0 +1,196 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Zigzag",
+		"id":			"Zigzag",
+		"description":	"Move sprite to specific position",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Movements",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On hit target position", "", 
+             "On {my} hit target", 
+			 "Triggered when hit target position.", 
+			 "OnHitTarget");             
+AddCmpParam("Comparison", "Choose the way to compare the current speed.");
+AddNumberParam("Speed", "The speed, in pixel per second, to compare the current speed to.");
+AddCondition(1, 0, "Compare speed", "Speed", 
+             "{my} speed {0} {1}", 
+             "Compare the current speed of the object.", 
+             "CompareSpeed");
+AddCondition(2,	cf_trigger, "On moving", "", "On {my} moving", "Triggered when object moving.", "OnMoving");                          
+AddCondition(3,	0, "Is moving", "", "Is {my} moving", "Is object moving.", "IsMoving");                
+
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "", 
+          "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's Zigzag behavior.", "SetActivated");
+AddAction(1, 0, "Trigger start", "", 
+          "Start {my} to execute command", 
+          "Start to execute command.", "Start"); 
+AddAction(2, 0, "Trigger stop", "", 
+          "Stop {my} to execute command", 
+          "Stop to execute command.", "Stop");  
+AddNumberParam("Max moving speed", "Maximum moving speed, in pixel per second.");
+AddAction(3, 0, "Set maximum moving speed", "Moving", 
+          "Set {my} maximum moving speed to <i>{0}</i>", 
+          "Set the object's maximum moving speed.", "SetMaxMovSpeed");
+AddNumberParam("Moving acceleration", "The moving acceleration setting, in pixel per second per second.");
+AddAction(4, 0, "Set moving acceleration", "Moving", 
+          "Set {my} moving acceleration to <i>{0}</i>", 
+          "Set the object's moving acceleration.", "SetMovAcceleration");
+AddNumberParam("Moving deceleration", "The moving deceleration setting, in pixels per second per second.");
+AddAction(5, 0, "Set moving deceleration", "Moving", 
+          "Set {my} moving deceleration to <i>{0}</i>", 
+          "Set the object's moving deceleration.", "SetMovDeceleration");
+AddNumberParam("Max rotation speed", "Maximum rotation speed, in degrees per second.");
+AddAction(6, 0, "Set maximum rotation speed", "Rotation", 
+          "Set {my} maximum rotation speed to <i>{0}</i>", 
+          "Set the object's maximum rotation speed.", "SetMaxRotSpeed");
+AddNumberParam("Rotation acceleration", "The rotation acceleration setting, in pixels per second per second.");
+AddAction(7, 0, "Set rotation acceleration", "Rotation", 
+          "Set {my} rotation acceleration to <i>{0}</i>", 
+          "Set the object's rotation acceleration.", "SetRotAcceleration");
+AddNumberParam("Rotation deceleration", "The rotation deceleration setting, in pixels per second per second.");
+AddAction(8, 0, "Set rotation deceleration", "Rotation", 
+          "Set {my} rotation deceleration to <i>{0}</i>", 
+          "Set the object's rotation deceleration.", "SetRotDeceleration");
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Repeat", "Executing command queue repeatly.",1);
+AddAction(9, 0, "Set repeat", "", 
+          "Set {my} repeat mode to <i>{0}</i>", 
+          "Enable to execute command queue repeatly.", "SetRepeat");          
+AddAction(10, 0, "Clean command queue", "Command queue", 
+          "Clean {my} command queue", 
+          "Clean command queue.", "CleanCmdQueue");
+AddComboParamOption("Move forward");
+AddComboParamOption("Move backward");
+AddComboParamOption("Turn left");
+AddComboParamOption("Turn Right");
+AddComboParamOption("Wait");
+AddComboParam("Command Type", "Command Types.",1);
+AddNumberParam("Parameter", "The parameter of command.");  
+AddAction(11, 0, "Add command", "Command queue", 
+          "Add <i>{0}</i> <i>{0}</i> into {my} command queue", 
+          "Add a command into command queue.", "AddCmd");          
+  
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "Get current activated state", "Current", "Activated", 
+              "The current activated state of behavior.");
+AddExpression(1, ef_return_number, "Get current moving speed", "Current", "MovSpeed", 
+              "The current moving speed, in pixel per second.");
+AddExpression(2, ef_return_number, "Get max moving speed", "Setting", "MaxMovSpeed", 
+              "The maximum moving speed setting, in pixel per second.");
+AddExpression(3, ef_return_number, "Get moving acceleration", "Setting", "MovAcc", 
+              "The moving acceleration setting, in pixel per second per second.");
+AddExpression(4, ef_return_number, "Get moving deceleration", "Setting", "MovDec", 
+              "The moving deceleration setting, in pixel per second per second.");              
+AddExpression(5, ef_return_number, "Get current rotation speed", "Current", "RotSpeed", 
+              "The current rotation speed, in degrees per second.");
+AddExpression(6, ef_return_number, "Get max rotation speed", "Setting", "MaxRotSpeed", 
+              "The maximum rotation speed setting, in degrees per second.");
+AddExpression(7, ef_return_number, "Get rotation acceleration", "Setting", "RotAcc", 
+              "The acceleration setting, in degrees per second per second.");
+AddExpression(8, ef_return_number, "Get rotation deceleration", "Setting", "RotDec", 
+              "The rotation deceleration setting, in degrees per second per second.");
+            
+
+         
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),                
+    new cr.Property(ept_combo, "Start", "Yes", "Enable if you wish this to start at the start of the layout.", "No|Yes"),
+    new cr.Property(ept_combo, "Rotatable", "Yes", "Enable to rotate sprite with command.", "No|Yes"),    
+    // Moving setup
+	new cr.Property(ept_float, "Max moving speed", 400, 
+                    "Maximum moving speed, in pixel per second."),
+	new cr.Property(ept_float, "Moving acceleration", 0, 
+                    "Moving acceleration, in pixel per second per second."),
+	new cr.Property(ept_float, "Moving deceleration", 0, 
+                    "Moving deceleration, in pixel per second per second."),
+    // Rotation setup                
+    new cr.Property(ept_float, "Max rotation speed", 180, 
+                    "Maximum rotation speed, in degrees per second."),
+	new cr.Property(ept_float, "Rotation acceleration", 0, 
+                    "Rotation acceleration, in degrees per second per second. 0 is using max speed directly."),
+	new cr.Property(ept_float, "Rotation deceleration", 0, 
+                    "Rotation deceleration, in degrees per second per second. 0 is ignored deceleration"),                               
+    // command queue
+    new cr.Property(ept_combo, "Repeat", "Yes", "Executing command queue repeatly.", "No|Yes"),                
+    new cr.Property(ept_text, "Command queue", "", "Set command queue."),
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Max moving speed"] < 0)
+		this.properties["Max moving speed"] = 0;
+		
+	if (this.properties["Moving acceleration"] < 0)
+		this.properties["Moving acceleration"] = 0;
+		
+	if (this.properties["Moving deceleration"] < 0)
+		this.properties["Moving deceleration"] = 0;
+        
+	if (this.properties["Max rotation speed"] < 0)
+		this.properties["Max rotation speed"] = 0;
+		
+	if (this.properties["Rotation acceleration"] < 0)
+		this.properties["Rotation acceleration"] = 0;
+		
+	if (this.properties["Rotation deceleration"] < 0)
+		this.properties["Rotation deceleration"] = 0;     
+}
---
+++ behaviors/zigzag/runtime.js
@@ -0,0 +1,396 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Zigzag = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Zigzag.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+		
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+    
+    
+    var _cmd_parsing = function(cmd_string)
+    {
+        var ret_cmds = [];
+        var cmds = cmd_string.split(";");
+        var i;
+        var cmd_length = cmds.length;
+        var cmd_slices, cmd_name, cmd_param;
+        for (i=0; i<cmd_length; i++)
+        {
+            cmd_slices = cmds[i].split(" ");
+            cmd_name = cmd_slices[0].toUpperCase();
+            cmd_param = parseFloat(cmd_slices[1]);
+            switch (cmd_name)
+            {
+            case "F":
+                cmd_name = "M";  // move
+                break;
+            case "B":
+                cmd_name = "M";  // move
+                cmd_param = -cmd_param;
+                break;
+            case "R":
+                cmd_name = "R";  // rotate
+                break;
+            case "L":
+                cmd_name = "R";  // rotate
+                cmd_param = -cmd_param;
+                break;
+            }
+            ret_cmds.push({ cmd: cmd_name,
+                            param: cmd_param });
+        }
+        return ret_cmds;
+    };
+
+	behinstProto.onCreate = function()
+	{
+        this.activated = this.properties[0];
+        this.is_run = this.properties[1];
+        this.rotatable = this.properties[2];
+        this.cur_cmd = null;
+              
+        this.pos_state = {x:this.inst.x, 
+                          y:this.inst.y, 
+                          angle:this.inst.angle};
+        this.CmdQueue = new cr.behaviors.Zigzag.CmdQueue(this.properties[9]);
+        this.CmdMove = new cr.behaviors.Zigzag.CmdMove(this.inst, 
+                                                       this.properties[3],
+                                                       this.properties[4],
+                                                       this.properties[5]);  
+        this.CmdRotate = new cr.behaviors.Zigzag.CmdRotate(this.inst, 
+                                                           this.rotatable,
+                                                           this.properties[6],
+                                                           this.properties[7],
+                                                           this.properties[8]);  
+        this.CmdWait = new cr.behaviors.Zigzag.CmdWait(); 
+        this.cmd_map = {M:this.CmdMove,
+                        R:this.CmdRotate,
+                        W:this.CmdWait};                                                      
+        this.CmdQueue.PushList(_cmd_parsing(this.properties[10]));
+	};
+
+	behinstProto.tick = function ()
+	{
+        //debugger;
+        if ( (this.activated==0) || (this.is_run==0) )
+            return;
+                          
+        var dt = this.runtime.getDt(this.inst);
+        var cmd;
+        while(dt)
+        {
+            if (this.cur_cmd == null) // try to get new cmd
+            {
+                this.cur_cmd = this.CmdQueue.GetCmd();
+                if (this.cur_cmd != null)
+                {
+                    cmd = this.cmd_map[this.cur_cmd.cmd]; 
+                    cmd.Start(this.pos_state, this.cur_cmd.param);    
+                }
+                else            
+                {
+                    this.is_run = false;
+                    break;
+                }
+            }
+            else
+            {
+                cmd = this.cmd_map[this.cur_cmd.cmd];
+            }
+            
+            dt = cmd.Tick(dt);
+            if (cmd.is_done)
+            {
+                this.cur_cmd = null;
+            }
+        }  
+	};   
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+    
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+	acts.SetActivated = function (s)
+	{
+		this.activated = s;
+	};  
+
+    
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+    
+	exps.Activated = function (ret)
+	{
+		ret.set_int(this.activated);
+	};    
+    
+}());
+
+(function ()
+{
+    // command queue
+    cr.behaviors.Zigzag.CmdQueue = function(is_repeat)
+    {
+        this.CleanAll();
+        this.is_repeat = is_repeat;
+    };
+    var CmdQueueProto = cr.behaviors.Zigzag.CmdQueue.prototype;
+    
+    CmdQueueProto.CleanAll = function()
+	{
+        this._queue_index = 0;    
+        this._queue = [];
+	};
+    
+    CmdQueueProto.Push = function(item)
+    {
+        this._queue.push(item);
+    };
+
+    CmdQueueProto.PushList = function(items)
+    {
+        var i;
+        var item_len = items.length;
+        for (i=0; i<item_len; i++)
+        {
+            this._queue.push(items[i]);
+        }
+    };    
+    CmdQueueProto.GetCmd = function()
+	{
+        var cmd;
+        if (this._queue_index >= 0)
+        {
+            cmd = this._queue[this._queue_index];
+            var index = this._queue_index+1;
+            if (index >= this._queue.length)         
+                this._queue_index = (this.is_repeat)? 0:(-1);
+            else
+                this._queue_index = index;            
+        } 
+        else        
+        {
+            cmd = null;
+        }
+        return cmd;
+	};
+     
+    // move
+    cr.behaviors.Zigzag.CmdMove = function(inst, max_speed, acc, dec)
+    {
+        this.inst = inst;
+        this._move = {max:max_speed, acc:acc, dec:dec};
+        this.is_done = true;
+    };
+    var CmdMoveProto = cr.behaviors.Zigzag.CmdMove.prototype;
+    
+    CmdMoveProto.Start = function(zigzag_state, distance)
+    {
+        this.target = zigzag_state;
+        this.dir = (distance >= 0);
+        this.remain_distance = Math.abs(distance);
+        _set_current_speed.call(this, null);
+        this.is_done = false;
+
+        var angle = zigzag_state.angle;
+        zigzag_state.x += (distance * Math.cos(angle));
+        zigzag_state.y += (distance * Math.sin(angle));  
+    };    
+    
+    CmdMoveProto.Tick = function(dt)
+    {
+        var remain_dt = 0;
+        var distance = _move_distance_get.call(this, dt);
+        this.remain_distance -= distance;   
+
+        // is hit to target at next tick?
+        if ( (this.remain_distance <= 0) || (this.current_speed <= 0) )
+        {
+            this.is_done = true;
+            this.inst.x = this.target.x;
+            this.inst.y = this.target.y;
+            if ( (this._move.acc>0) || (this._move.dec>0) )
+            {
+                this.SetCurrentSpeed(0);  // stop in point
+            }
+            else
+            {
+                //remain_dt = (-this.remain_distance)/this.current_speed;
+            }
+        }
+        else
+        {
+            var angle = this.target.angle;
+            if (!this.dir)
+                distance = -distance;
+            this.inst.x += (distance * Math.cos(angle));
+            this.inst.y += (distance * Math.sin(angle));
+        } 
+
+		this.inst.set_bbox_changed();
+        return remain_dt;    
+    };  
+    
+    // rotate
+    cr.behaviors.Zigzag.CmdRotate = function(inst, rotatable, max_speed, acc, dec)
+    {
+        this.inst = inst;
+        this.rotatable = rotatable;
+        this._move = {max:max_speed, acc:acc, dec:dec};
+        this.is_done = true;
+    };
+    var CmdRotateProto = cr.behaviors.Zigzag.CmdRotate.prototype;
+    
+    CmdRotateProto.Start = function(zigzag_state, distance)
+    {
+        this.target = zigzag_state;
+        this.current_angle = cr.to_clamped_degrees(zigzag_state.angle);
+        this.dir = (distance >= 0);
+        var angle = cr.to_clamped_radians(this.current_angle + distance);
+        this.remain_distance = Math.abs(distance);
+        _set_current_speed.call(this, null); 
+        this.is_done = false;
+        
+        zigzag_state.angle = angle;
+
+    };    
+    
+    CmdRotateProto.Tick = function(dt)
+    {
+        if (this.rotatable == 1)
+        var remain_dt = 0;
+        var distance = _move_distance_get.call(this, dt);
+        this.remain_distance -= distance;   
+
+        // is hit to target at next tick?
+        var target_angle;
+        if ( (this.remain_distance <= 0) || (this.current_speed <= 0) )
+        {
+            this.is_done = true;
+            target_angle = this.target.angle;
+        }
+        else
+        {
+            this.current_angle += ((this.dir)? distance:(-distance));
+            target_angle = cr.to_clamped_radians(this.current_angle);
+        } 
+
+        if (this.rotatable == 1)
+        {
+            this.inst.angle = target_angle;
+		    this.inst.set_bbox_changed();
+        }
+        return remain_dt;    
+    }; 
+    
+	var _set_current_speed = function(speed)
+	{
+        var move = this._move;
+        if (speed != null)
+        {
+            this.current_speed = (speed > move.max)? 
+                                 move.max: speed;
+        }        
+        else if (move.acc==0)
+        {
+            this.current_speed = move.max;
+        }
+	};  
+
+    var _move_distance_get = function(dt)
+    {
+        var move = this._move;
+        // assign speed
+        var is_slow_down = false;
+        if (move.dec != 0)
+        {
+            // is time to deceleration?                
+            var _speed = this.current_speed;
+            var _distance = (_speed*_speed)/(2*move.dec); // (v*v)/(2*a)
+            is_slow_down = (_distance >= this.remain_distance);
+        }
+        var acc = (is_slow_down)? (-move.dec):move.acc;
+        if (acc != 0)
+        {
+            _set_current_speed.call(this, this.current_speed + (acc * dt) );    
+        }
+
+		// Apply movement to the object     
+        var distance = this.current_speed * dt;
+        return distance;
+    };
+    
+    // wait
+    cr.behaviors.Zigzag.CmdWait = function()
+    {
+        this.is_done = true;
+    };
+    var CmdWaitProto = cr.behaviors.Zigzag.CmdWait.prototype;
+    
+    CmdWaitProto.Start = function(zigzag_state, distance)
+    {
+        this.remain_distance = distance;
+        this.is_done = false;
+    };    
+    
+    CmdWaitProto.Tick = function(dt)
+    {
+        this.remain_distance -= dt;
+        var remain_dt = 0;
+        if (this.remain_distance <= 0)
+        {
+            //remain_dt = -this.remain_distance;
+            this.is_done = true;
+        }
+        else
+        {
+            remain_dt = 0;
+        }
+        return remain_dt;    
+    };     
+}());
