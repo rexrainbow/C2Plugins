498b27bb93cab86fd544ddc7927b3672ccbd634c
hua <binhua.tein@gmail.com>
2011-10-18 08:11:28 +0800

    Add system extension plugin

---
+++ plugins/systemext/edittime.js
@@ -0,0 +1,109 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"System extension",
+		"id":			"MySysExt",
+		"description":	"System extension",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Utility",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+// SOL
+AddObjectParam("Object", "Object for picking");
+AddAction(0, 0, "Pick all objects", "SOL", 
+          "Pick all <i>{0}</i> objects", 
+          "Pick all objects.", "PickAll");
+AddObjectParam("Object", "Object for picking");
+AddNumberParam("UID", "UID of object", 0);
+AddComboParamOption("Current picked objects");
+AddComboParamOption("All objects");
+AddComboParam("All objects", "Pick from current picked objects or all objects", 1);
+AddAction(1, 0, "Pick object by UID", "SOL", 
+          "Pick <i>{0}</i> objects by UID is <i>{1}</i>, form <i>{2}</i>", 
+          "Pick object by UID.", "PickByUID");          
+AddObjectParam("Object", "Object for picking");
+AddStringParam("Property name", "The property name of sprite to be compared", 0);
+AddCmpParam("Comparison", "Choose the way to compare the variable.");
+AddAnyTypeParam("Value", "The target compare value", 0);
+AddComboParamOption("Current picked objects");
+AddComboParamOption("All objects");
+AddComboParam("All objects", "Pick from current picked objects or all objects", 1);
+AddAction(2, 0, "Pick object by property compare", "SOL", 
+          "Pick <i>{0}</i> objects by property compare <i>{1}</i> <i>{2}</i> <i>{3}</i>, form <i>{4}</i>", 
+          "Pick object by property name.", "PickByPropCmp");          
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/systemext/runtime.js
@@ -0,0 +1,132 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.MySysExt = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.MySysExt.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	};
+    
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+    acts.PickAll = function (objtype)
+	{
+        var sol = objtype.getCurrentSol();        
+        sol.select_all = true;
+	}; 
+    
+    acts.PickByUID = function (objtype, uid, is_pick_all)
+	{
+        var sol = objtype.getCurrentSol();  
+        if (is_pick_all==1)
+            sol.select_all = true;  
+            
+        var insts = sol.getObjects();
+        var insts_length = insts.length;
+        var i, inst;
+        var is_find = false;
+
+        for (i=0; i < insts_length; i++)
+        {
+            inst = insts[i];
+            if (inst.uid == uid)
+            {
+                is_find = true;
+                break;
+            }
+        }
+        
+        sol.instances.length = 0;   // clear contents
+        if (is_find)
+            sol.instances.push(inst);
+            
+        sol.select_all = false;
+	}; 
+    
+    acts.PickByPropCmp = function (objtype, var_name, cmp, y, is_pick_all)
+	{
+        var sol = objtype.getCurrentSol();  
+        if (is_pick_all==1)
+            sol.select_all = true;  
+            
+        var insts = sol.getObjects();
+        var insts_length = insts.length;
+        var i, inst;
+        var is_find = false;
+        var find_insts = [];
+
+        var_name = var_name.toLowerCase();
+        for (i=0; i < insts_length; i++)
+        {
+            inst = insts[i];
+            if (cr.do_cmp(inst[var_name], cmp, y))
+            {
+                find_insts.push(inst);
+            }
+        }
+        
+        sol.instances.length = 0;   // clear contents
+        if (find_insts.length > 0)
+        {
+            insts_length = find_insts.length;
+            for (i=0; i < insts_length; i++)
+            {            
+                sol.instances.push(find_insts[i]);
+            }
+        }
+            
+        sol.select_all = false;
+	};  
+    
+    
+
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+}());
