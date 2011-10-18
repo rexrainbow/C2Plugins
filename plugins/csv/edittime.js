07873d0eb8b2e77c940610da2a8aba1a035a9c7f
hua <binhua.tein@gmail.com>
2011-10-19 07:54:38 +0800

    Add csv table plugin

---
+++ plugins/csv/edittime.js
@@ -0,0 +1,114 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"CSV",
+		"id":			"MyCSV",
+		"description":	"Read a 2d table from cvs string.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Data & Storage",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+//AddCondition(0, cf_looping | cf_not_invertible, "For each col", "For Each", "For each col", "Repeat the event for each col in the table.", "ForEachCol");
+//AddStringParam("Col", "The col index.", '""');
+//AddCondition(1, cf_looping | cf_not_invertible, "For each row in col", "For Each", "For each row in a col", "Repeat the event for each row in a col.", "ForEachRowInCol");
+//AddStringParam("Row", "The row index.", '""');
+//AddCondition(3, cf_looping | cf_not_invertible, "For each col in row", "For Each", "For each col in a row", "Repeat the event for each col in a row.", "ForEachColInRow");
+
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("CSV string", "The csv string for loading.", '""');
+AddAction(1, 0, "Load table", "Table", "Load table from csv string",
+         "Load table from csv string.", "LoadCSV");
+AddStringParam("Col", "The col index.", '""');
+AddStringParam("Row", "The row index.", '""');
+AddAnyTypeParam("Value", "The value to store.", "0");
+AddAction(2, 0, "Set entry", "Table", "Set value at (<i>{0}</i>, <i>{1}</i>) to <i>{2}</i>", 
+          "Set the value in the table.", "SetEntry");
+AddAction(3, 0, "Clear", "Table", "Clear", "Clear all entries.", "Clear");
+AddStringParam("Row", "The row index.", '""');
+AddComboParamOption("Integer");
+AddComboParamOption("Float");
+AddComboParam("Type", "Conver type to numver.",0);
+AddAction(4, 0, "Convert type", "Table", "Convert entries type to <i>{1}</i> on row <i>{0}</i>",
+         "Convert entries type in a row.", "ConvertType");
+
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddStringParam("Col", "The col index.", '""');
+AddStringParam("Row", "The row index.", '""');
+AddExpression(0, ef_return_any | ef_variadic_parameters, "Get value at", "Table", "At", "Get value from the table.");
+//AddExpression(1, ef_return_number, "Current Col", "For Each", "CurCol", "Get the current col index in a For Each loop.");
+//AddExpression(2, ef_return_number, "Current Row", "For Each", "CurRow", "Get the current row index in a For Each loop.");
+//AddExpression(3, ef_return_number, "Current Value", "For Each", "CurValue", "Get the current value in a For Each loop.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [	     
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/csv/runtime.js
@@ -0,0 +1,185 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.MyCSV = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.MyCSV.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+		this.Clear();
+	};
+    
+	instanceProto.Clear = function()
+	{
+		this._table = {};
+        this.keys = [];
+        this.key_cnt = 0;
+        this.items = [];
+        this.item_cnt = 0;   
+	};    
+
+    instanceProto._create_keys = function(key_string)
+	{
+		var keys = key_string.split(",");
+        keys.shift();
+        var i, key;
+        var key_cnt = keys.length;
+        for (i=0; i<key_cnt; i++)
+        {
+            key = keys[i];
+            if (this._table[key] == null)
+                this._table[key] = {};         
+        }
+        this.keys = keys;
+        this.key_cnt = keys.length;
+	};
+    
+    instanceProto._create_items = function(item_string)
+	{
+        var values = item_string.split(",");
+        var item_name = values.shift();
+        var keys = this.keys;
+        var key_cnt = this.key_cnt;
+        var table = this._table;
+        var i;
+        for (i=0; i<key_cnt; i++)
+        {
+            table[keys[i]][item_name] = values[i];        
+        }
+        this.items.push(item_name);
+	}; 
+    
+	instanceProto._parsing = function(csv_string)
+	{
+        if (csv_string == "")
+            return;
+            
+		var lines = csv_string.split(/\r\n|\r|\n/);
+        var key_string = lines.shift();
+        this._create_keys(key_string);
+        var line_cnt = lines.length;
+        var i;
+        for (i=0; i<line_cnt; i++)
+        {
+            this._create_items(lines[i]);
+        }
+        this.item_cnt = this.items.length;
+	}; 
+
+    instanceProto.at = function(col, row)
+	{
+        if (this._table[col]==null)
+            return 0;
+        
+        var val = this._table[col][row];
+		if (val == null)
+            return 0;
+
+        return val;   
+	};
+	
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;
+
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+    
+	acts.LoadCSV = function (csv_string)
+	{         
+        this._parsing(csv_string);
+	};
+    
+	acts.SetEntry = function (col, row, val)
+	{
+        if (this._table[col] == null)
+        {
+            this._table[col] = {};
+        }
+        this._table[col][row] = val;        
+	};
+    
+	acts.Clear = function ()
+	{
+		this.Clear();
+	};    
+    
+	acts.ConvertType = function (row, to_type)
+	{
+        var handler = (to_type==0)? parseInt:
+                                    parseFloat;
+        var keys = this.keys;
+        var key_cnt = this.key_cnt;
+        var table = this._table;
+        var i, val;
+        for (i=0; i<key_cnt; i++)
+        {
+            val = table[keys[i]][row];
+            table[keys[i]][row] = handler(val);        
+        }                    
+	};   
+    
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+    
+	exps.At = function (ret, col, row)
+	{
+        ret.set_any(this.at(col,row));
+	}; 
+    
+	exps.CurCol = function (ret)
+	{
+		ret.set_int(this.forCol);
+	};
+	
+	exps.CurRow = function (ret)
+	{
+		ret.set_int(this.forRow);
+	};
+	
+	exps.CurValue = function (ret)
+	{
+		ret.set_any(this.at(this.forCol,this.forRow));
+	};    
+    
+}());
