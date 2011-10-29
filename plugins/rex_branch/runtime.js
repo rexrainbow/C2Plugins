f39cfc1b1a36634584192c4f633334f86d8f0840
hua <binhua.tein@gmail.com>
2011-10-30 07:01:47 +0800

    Add branch plugin

---
+++ plugins/rex_branch/runtime.js
@@ -0,0 +1,79 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Branch = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Branch.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.switch_branch = false;
+	};
+	
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+    
+	cnds.BranchIf = function ()
+	{
+        this.switch_branch = true;
+		return true;
+	};
+
+	cnds.BranchElseIf = function ()
+	{
+		return this.switch_branch;
+	};    
+
+	cnds.BranchBreak = function ()
+	{
+        this.switch_branch = false;
+		return true;
+	};         
+    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+}());
