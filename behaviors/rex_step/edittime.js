c536c9f52b1968e0f702abc5769a1bef6407afaf
hua <binhua.tein@gmail.com>
2011-11-05 12:15:41 +0800

    Add step behavior

---
+++ behaviors/rex_step/edittime.js
@@ -0,0 +1,96 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Step",
+		"id":			"Rex_Step",
+		"description":	"Insert x,y position.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Movements",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On step", "", "On {my} step", "Triggered when stepping the object for motion.", "OnCMStep");
+AddCondition(1, cf_trigger, "On horizontal step", "", "On {my} horizontal step", "Triggered when stepping the object horizontally for motion.", "OnCMHorizStep");
+AddCondition(2, cf_trigger, "On vertical step", "", "On {my} vertical step", "Triggered when stepping the object vertically for motion.", "OnCMVertStep");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "", "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's step behavior.", "SetActivated");
+AddComboParamOption("Go back a step");
+AddComboParamOption("Stay at current position");
+AddComboParam("Position", "Choose the resulting object position.");
+AddAction(1, 0, "Stop stepping", "Velocity", "Stop {my} stepping ({0})", 
+          "Stop the current stepping, preventing any more step triggers firing this tick.", "StopStepping");
+AddNumberParam("Pixels per step", "The pixels per step.", 5);
+AddAction(2, 0, "Set pixels per step", "", "Set {my} pixels per step to <i>{0}</i>", 
+          "Set pixels per step.", "SetPixelPerStep");             
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+	new cr.Property(ept_combo, "Stepping mode", "Linear", 
+                    "Move in increments while firing the step triggers.", 
+                    "Linear|Horizontal then vertical|Vertical then horizontal"),
+	new cr.Property(ept_integer, "Pixels per step",	5, 
+                    "When stepping enabled, the number of pixels to move each step.")
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Pixels per step"] < 1)
+		this.properties["Pixels per step"] = 1;
+}
---
+++ behaviors/rex_step/runtime.js
@@ -0,0 +1,188 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_Step = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_Step.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+
+		this.cancelStep = 0;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+		// Load properties
+        this.activated = this.properties[0]; 
+		this.step_mode = this.properties[1];	// 0=None, 1=Linear, 2=Horizontal then vertical, 3=Vertical then horizontal
+		this.pixel_per_step = this.properties[2];
+        
+		this.pre_x = this.inst.x;
+		this.pre_y = this.inst.y;        
+	};
+	
+	behinstProto.step = function (dx, dy, trigmethod)
+	{
+		var steps = Math.round(Math.sqrt(dx * dx + dy * dy) / this.pixel_per_step);
+        
+		if (steps === 0)
+			steps = 1;
+			
+		var i, prog;
+        var inst = this.inst;
+		var startx = this.pre_x;
+		var starty = this.pre_y;        
+		for (i = 1; i <= steps; i++)
+		{
+			prog = i / steps;
+			inst.x = startx + (dx * prog);
+			inst.y = starty + (dy * prog);
+			inst.set_bbox_changed();
+			
+			this.runtime.trigger(trigmethod, inst);
+			
+			if (this.cancelStep === 1)
+			{
+				// Go back a step and stop
+				i--;
+				prog = i / steps;
+			    inst.x = startx + (dx * prog);
+			    inst.y = starty + (dy * prog);
+				inst.set_bbox_changed();
+				return;
+			}
+			else if (this.cancelStep === 2)
+			{
+				// Stop and do nothing
+				return;
+			}
+		}
+	};
+
+	behinstProto.tick = function ()
+	{
+        if (this.activated == 0)
+            return;
+        
+        var cur_x = this.inst.x;
+        var cur_y = this.inst.y;
+        var dx = cur_x - this.pre_x;
+        var dy = cur_y - this.pre_y;
+		// Not moving, nothing to do        
+        if ((dx==0) && (dy==0))        
+            return;
+			          
+		this.cancelStep = 0;
+			
+		if (this.step_mode === 0)	// linear
+		{
+			this.step(dx, dy, cr.behaviors.Rex_Step.prototype.cnds.OnCMStep);
+		}
+		else if (this.step_mode === 1)	// horizontal then vertical
+		{
+			this.step(dx, 0, cr.behaviors.Rex_Step.prototype.cnds.OnCMHorizStep);
+			
+			if (this.cancelStep === 0)
+				this.step(0, dy, cr.behaviors.Rex_Step.prototype.cnds.OnCMHorizStep);
+		}
+		else if (this.step_mode === 2)	// vertical then horizontal
+		{
+			this.step(0, dy, cr.behaviors.Rex_Step.prototype.cnds.OnCMHorizStep);
+			
+			if (this.cancelStep === 0)
+				this.step(dx, 0, cr.behaviors.Rex_Step.prototype.cnds.OnCMHorizStep);
+		}
+		
+		this.pre_x = cur_x;
+		this.pre_y = cur_y; 
+        this.inst.x = cur_x;
+        this.inst.y = cur_y;
+		this.inst.set_bbox_changed();
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+
+	cnds.OnCMStep = function ()
+	{
+		return true;
+	};
+	
+	cnds.OnCMHorizStep = function ()
+	{
+		return true;
+	};
+	
+	cnds.OnCMVertStep = function ()
+	{
+		return true;
+	};
+
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+	acts.SetActivated = function (s)
+	{
+        if ( (this.activated==0) && (s==1) )
+        {
+            this.pre_x = this.inst.x;
+            this.pre_y = this.inst.y;
+        }
+		this.activated = s;
+	};
+    
+	acts.StopStepping = function (mode)
+	{
+		// set to 1 = go back a step, 2 = stay at current position
+		this.cancelStep = mode + 1;
+	};
+ 
+	acts.SetPixelPerStep = function (s)
+	{
+		this.pixel_per_step = s;
+	};
+
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+}());
