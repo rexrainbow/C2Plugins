1cbec98bb0564f501c4d068308e45e89c253c8bf
hua <binhua.tein@gmail.com>
2011-11-05 09:10:07 +0800

    Add touch direction behavior

---
+++ behaviors/rex_touchDirection/edittime.js
@@ -0,0 +1,95 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Touch direction",
+		"id":			"Rex_TouchDirection",
+		"description":	"Move sprite with mouse/touch moving",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Movements",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "", "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's touch moving behavior.", "SetActivated");
+AddNumberParam("Proportion", "The proportion of sprite moving followed mouse.", 10);
+AddAction(1, 0, "Set proportion", "", "Set {my} proportion to <i>{0}</i>", 
+          "Set proportionof sprite moving followed mouse.", "SetProportion");          
+
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0,	cf_trigger, "On move start", "", "On {my} move start", "Triggered when object move start.", "OnMoveStart");
+AddCondition(1,	cf_trigger, "On moving", "", "On {my} moving", "Triggered when object moving.", "OnMoving");             
+AddCondition(2,	cf_trigger, "On move stop", "", "On {my} move stop", "Triggered when object move stop.", "OnMoveStop"); 
+AddCondition(3,	0, "Is moving", "", "Is {my} moving", "Is object moving.", "IsMoving");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number | ef_variadic_parameters, "Mouse X position", "Position", "X", "Get the mouse cursor X co-ordinate in the layout.");
+AddExpression(1, ef_return_number | ef_variadic_parameters, "Mouse Y position", "Position", "Y", "Get the mouse cursor Y co-ordinate in the layout.");
+AddExpression(2, ef_return_number, "Absolute mouse X", "Position", "AbsoluteX", "Get the mouse cursor X co-ordinate on the canvas.");
+AddExpression(3, ef_return_number, "Absolute mouse Y", "Position", "AbsoluteY", "Get the mouse cursor Y co-ordinate on the canvas.");
+AddExpression(4, ef_return_number, "Get activated", "", "Activated", "The activated setting, 1 is activated.");
+AddExpression(5, ef_return_number, "Get Proportion", "", "Proportion", "The Proportion setting.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+    new cr.Property(ept_combo, "Mouse button", "Left", "Mouse button that can move objects.", "Left|Middle|Right"),
+    new cr.Property(ept_combo, "Axis", "Both", "The axis this object can move on.", "Both|Horizontal|Vertical"),
+    new cr.Property(ept_float, "Proportion", 1, "The proportion of sprite moving followed mouse."),
+    ];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_touchDirection/runtime.js
@@ -0,0 +1,412 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_TouchDirection = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_TouchDirection.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+		// Bind mouse events via jQuery
+		jQuery(document).mousedown(
+			(function (self) {
+				return function(info) {
+					self.onMouseDown(info);
+				};
+			})(this)
+		);
+		
+		jQuery(document).mouseup(
+			(function (self) {
+				return function(info) {
+					self.onMouseUp(info);
+				};
+			})(this)
+		);
+        
+		jQuery(document).mousemove(
+			(function (self) {
+				return function(info) {
+					self.onMouseMove(info);
+				};
+			})(this)
+		);   
+        
+        // touch
+		this.runtime.canvas.addEventListener("touchstart",
+			(function (self) {
+				return function(info) {
+					self.onTouchStart(info);
+				};
+			})(this),
+			true
+		);
+		
+		this.runtime.canvas.addEventListener("touchmove",
+			(function (self) {
+				return function(info) {
+					self.onTouchMove(info);
+				};
+			})(this),
+			true
+		);
+		
+		this.runtime.canvas.addEventListener("touchend",
+			(function (self) {
+				return function(info) {
+					self.onTouchEnd(info);
+				};
+			})(this),
+			true
+		);               
+
+        this.behavior_index = null;
+		this.triggerButton = { btn:0, press:false };
+        
+		this.mouseXcanvas = 0;				// mouse position relative to canvas
+		this.mouseYcanvas = 0;    
+        
+        // touch
+		this.touches = [];
+        this.is_on_touch = false;
+        
+        // control
+        this.trigger_source = 1;  // 0=touch, 1=mouse            
+	};
+    
+	behtypeProto.onMouseDown = function(info)
+	{
+        this.trigger_source = 1;    
+        this.triggerButton.btn = info.which - 1;	// 1-based 
+        this.triggerButton.press = true;
+
+        this.MovingStart(this.mouseXcanvas, this.mouseYcanvas);
+	};  
+
+	behtypeProto.onMouseUp = function(info)
+	{
+        this.trigger_source = 1;    
+        this.triggerButton.btn = info.which - 1;	// 1-based 
+        this.triggerButton.press = false;
+	};
+    
+	behtypeProto.onMouseMove = function(info)
+	{
+        this.trigger_source = 1;    
+		var offset = jQuery(this.runtime.canvas).offset();
+		this.mouseXcanvas = info.pageX - offset.left;
+		this.mouseYcanvas = info.pageY - offset.top;
+	};    
+        
+        
+    // touch
+    behtypeProto.saveTouches = function (t)
+	{
+		this.touches.length = 0;
+		var offset = jQuery(this.runtime.canvas).offset();
+		
+		var i, len, touch;
+		for (i = 0, len = t.length; i < len; i++)
+		{
+			touch = t[i];
+			this.touches.push({ x: touch.pageX - offset.left, y: touch.pageY - offset.top });
+		}
+	};
+    
+	behtypeProto.onTouchMove = function (info)
+	{
+        this.trigger_source = 0;
+		info.preventDefault();
+		this.saveTouches(info.touches);
+	};
+
+	behtypeProto.onTouchStart = function (info)
+	{
+        this.trigger_source = 0;
+		info.preventDefault();
+		this.saveTouches(info.touches);
+		
+		var offset = jQuery(this.runtime.canvas).offset();
+		var is_get_drag_inst;
+        
+		if (info.changedTouches)
+		{
+			var i, len;
+            var touch = info.changedTouches[0];
+			this.curTouchX = touch.pageX - offset.left;
+			this.curTouchY = touch.pageY - offset.top;
+            this.MovingStart(this.curTouchX, this.curTouchY);
+		}
+        
+        this.is_on_touch = true;
+	};
+
+
+	behtypeProto.onTouchEnd = function (info)
+	{
+        this.trigger_source = 0;
+		info.preventDefault();
+		//this.saveTouches(info.touches);  // do not update this.touches
+        
+        this.is_on_touch = false;
+	};    
+    
+    // drag detecting
+	behtypeProto.MovingStart = function(x, y)
+	{    
+        // 0. find out index of behavior instance
+        if (this.behavior_index == null )
+            this.behavior_index = this.objtype.getBehaviorIndexByName(this.name);
+            
+        var insts = this.objtype.instances;      
+        var i, cnt;
+        var inst;  
+        
+        cnt = insts.length; 
+        for (i=0; i<cnt; i++ )
+        {
+            inst = insts[i].behavior_insts[this.behavior_index];
+            if ( (this.trigger_source == 0) ||
+                 (inst.mouseButon == this.triggerButton.btn) ) 
+               )
+            {
+                inst.is_on_moving = true;
+                inst.pre_x = x;
+                inst.pre_y = y;      
+                this.runtime.trigger(cr.behaviors.Rex_TouchDirection.prototype.cnds.OnMoveStart, inst.inst); 
+            }
+        }
+	}; 
+        
+    // export
+	behtypeProto.GetABSX = function ()
+	{
+        var ret_x;
+        if (this.trigger_source == 1)  // mouse
+        {
+            ret_x = this.mouseXcanvas;
+        }
+        else    // touch
+        {
+		    if (this.touches.length)
+                ret_x = this.touches[0].x;
+		    else
+                ret_x = 0;        
+        }
+        return ret_x;
+	};  
+
+	behtypeProto.GetABSY = function ()
+	{
+        var ret_y;
+        if (this.trigger_source == 1)  // mouse
+        {
+            ret_y = this.mouseYcanvas;
+        }
+        else    // touch
+        {
+		    if (this.touches.length)
+                ret_y = this.touches[0].y;
+		    else
+                ret_x = 0;        
+        }
+        return ret_y;
+	};     
+    
+	behtypeProto.GetLayerX = function(inst)
+	{
+        return inst.layer.canvasToLayerX(this.GetABSX());
+	};
+    
+	behtypeProto.GetLayerY = function(inst)
+	{
+        return inst.layer.canvasToLayerY(this.GetABSY());
+	};
+    
+	behtypeProto._is_release = function(mouseButon)
+	{
+        var is_drop;
+        if (this.trigger_source == 1)  // mouse
+        {    
+             is_drop = (!this.triggerButton.press) && 
+                       (this.triggerButton.btn == mouseButon) ;          
+        }
+        else
+        {
+             //is_drop = (this.touches.length==0);        
+             is_drop = (!this.is_on_touch);
+        }
+        return is_drop;
+	};    
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+        
+		this.pre_x = 0;
+		this.pre_y = 0;           
+        this.is_on_moving = false;
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{   
+        this.activated = this.properties[0]; 
+        this.mouseButon = this.properties[1]; 
+        this.move_axis = this.properties[2]; 
+        this.move_proportion = this.properties[3];
+	};
+
+	behinstProto.tick = function ()
+	{        
+        if ( (this.activated == 0) ||
+             (!this.is_on_moving)      )
+        {
+            return;        
+        }
+        
+        // this.activated == 1 && this.is_on_moving        
+        var inst = this.inst;
+        var cur_x = this.type.GetABSX();
+        var cur_y = this.type.GetABSY();
+        var dx = cur_x - this.pre_x;
+        var dy = cur_y - this.pre_y;             
+        if ( (dx!=0) || (dy!=0) )
+        {
+            switch (this.move_axis)
+            {
+                case 1:
+                    inst.x += (this.move_proportion * dx);
+                    break;
+                case 2:
+                    inst.y += (this.move_proportion * dy);
+                    break;
+                default:
+                    inst.x += (this.move_proportion * dx);
+                    inst.y += (this.move_proportion * dy);
+                    break;
+            }
+            inst.set_bbox_changed();
+            this.pre_x = cur_x;
+            this.pre_y = cur_y;                    
+        }
+        this.runtime.trigger(cr.behaviors.Rex_TouchDirection.prototype.cnds.OnMoving, inst);
+                                
+        if ( this.type._is_release(this.mouseButon) )
+        {
+            this.is_on_moving = false;
+            this.runtime.trigger(cr.behaviors.Rex_TouchDirection.prototype.cnds.OnMoveStop, inst); 
+        }
+	};    
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;    
+    
+	cnds.OnMoveStart = function ()
+	{
+        return true;
+	};
+    
+	cnds.OnMoveStop = function ()
+	{
+		return true;
+	}; 
+
+ 	cnds.OnMoving = function ()
+	{   
+        return true;
+    }
+    
+ 	cnds.IsMoving = function ()
+	{   
+        return (this.is_on_moving);
+    }    
+    
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+	acts.SetActivated = function (s)
+	{
+        if ( (this.activated==0) && 
+             this.is_on_moving &&
+             (s==1)
+           )
+        {
+            this.pre_x = this.type.GetABSX();
+            this.pre_y = this.type.GetABSY();
+        }
+		this.activated = s;
+	}; 
+
+	acts.SetProportion = function (s)
+	{
+		this.move_proportion = s;
+	}; 
+    
+    
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+	exps.X = function (ret)
+	{
+        ret.set_float( this.type.GetLayerX(this.inst) );
+	};
+	
+	exps.Y = function (ret)
+	{
+	    ret.set_float( this.type.GetLayerY(this.inst) );
+	};
+	
+	exps.AbsoluteX = function (ret)
+	{
+        ret.set_float( this.type.GetABSX(this.inst) );
+	};
+	
+	exps.AbsoluteY = function (ret)
+	{
+        ret.set_float( this.type.GetABSY(this.inst) );
+	};
+    
+	exps.Activated = function (ret)
+	{
+		ret.set_int(this.activated);
+	}; 
+    
+	exps.Proportion = function (ret)
+	{
+		ret.set_float(this.move_proportion);
+	}; 
+    
+}());
