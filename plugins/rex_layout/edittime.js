284e5555c12c7a091168cd3924298561ea0f2b9a
hua <binhua.tein@gmail.com>
2011-11-11 22:18:25 +0800

    Add Layout plugin

---
+++ plugins/rex_layout/edittime.js
@@ -0,0 +1,93 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Layout",
+		"id":			"Rex_Layout",
+		"description":	"Layout instances",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Utility",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddObjectParam("Object", "Object to distribute");
+AddNumberParam("Start X", "X co-ordinate at start point", 0);
+AddNumberParam("Start Y", "Y co-ordinate at start point", 0);
+AddNumberParam("End X", "X co-ordinate at end point", 0);
+AddNumberParam("End Y", "Y co-ordinate at end point", 0);
+AddAction(0, 0, "Distribute instances", "Distribution", 
+          "Distribute <i>{0}</i> from (<i>{1}</i>,<i>{2}</i>) to (<i>{3}</i>,<i>{4}</i>)", 
+          "Distribute instances.", "DistributeInsts");          
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_layout/runtime.js
@@ -0,0 +1,82 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Layout = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Layout.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	};
+	
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+    
+    acts.DistributeInsts = function (objtype, start_x, start_y, end_x, end_y)
+	{
+        var sol = objtype.getCurrentSol();  
+            
+        var insts = sol.getObjects();
+        var insts_length = insts.length;
+        var seg_len = insts_length -1;
+        var i, prop, inst;
+        var dx = end_x - start_x;
+        var dy = end_y - start_y;
+        for (i=0; i < insts_length; i++)
+        {
+            inst = insts[i];
+            prop = i/seg_len;
+            inst.x = start_x + (dx * prop);
+            inst.y = start_y + (dy * prop);
+            inst.set_bbox_changed();
+        }
+        
+	}; 
+
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+    
+}());
