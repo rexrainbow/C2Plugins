30b88bf35bac5ad945d7bc3b7bb767c65692a94a
hua <binhua.tein@gmail.com>
2011-11-13 08:54:53 +0800

    Add text plus for chinese text

---
+++ plugins/rex_textplus/edittime.js
@@ -0,0 +1,196 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Text",
+		"id":			"Rex_TextPlus",
+		"description":	"Enhancement text object.",
+		"author":		"Scirra",
+		"help url":		"http://www.scirra.com",
+		"category":		"General",
+		"type":			"world",			// appears in layout
+		"rotatable":	false,				// cannot be rotated in layout
+		"flags":		pf_position_aces | pf_size_aces | pf_appearance_aces | pf_zorder_aces
+	};
+};
+
+// Conditions, actions and expressions
+AddStringParam("Text to compare", "Enter the text to compare with the object's content.", "\"\"");
+AddComboParamOption("Ignore case");
+AddComboParamOption("Case sensitive");
+AddComboParam("Case sensitivity", "Choose whether capital letters count as different to lowercase.  If ignoring case, \"ABC\" matches \"abc\".", 0);
+AddCondition(0, 0, "Compare text", "Text", "Text is <b>{0}</b> <i>({1})</i>", "Compare the text in this object.", "CompareText");
+
+AddAnyTypeParam("Text", "Enter the text to set the object's content to.", "\"\"");
+AddAction(0, 0, "Set text", "Text", "Set text to <i>{0}</i>", "Set the text of this object.", "SetText");
+
+AddAnyTypeParam("Text", "Enter the text to append to the object's content.", "\"\"");
+AddAction(1, 0, "Append text", "Text", "Append <i>{0}</i>", "Add text to the end of the existing text.", "AppendText");
+
+AddExpression(0,	ef_return_string,	"Get text",	"Text", "Text", "Get the object's text.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_text,		"Text",		"Text",			"Text to display."),
+	new cr.Property(ept_combo, "Initial visibility",	"Visible",	"Choose whether the object is visible when the layout starts.", "Visible|Invisible"),
+	new cr.Property(ept_font, 		"Font",		"Arial,-16",	"Choose the font to display.  This applies to all instances of this type."),
+	new cr.Property(ept_color,		"Color",	cr.RGB(0, 0, 0),	"Color of the text."),
+	new cr.Property(ept_combo,		"Horizontal alignment", "Left", "Horizontal alignment of the text.", "Left|Center|Right")
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+	
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Properties for font
+	this.font_str = "Arial,-16";		// default font string
+	this.old_font_str = "";				// last font string, in case not changed
+	this.recreate_font = true;			// font not yet created
+	this.font = null;					// handle to font in IDE
+}
+
+// Called when drawing the text and the font needs to be recreated
+IDEInstance.prototype.RecreateFont = function(renderer)
+{
+	// The font hasn't really changed: don't actually recreate
+	if (this.font_str == this.old_font_str)
+	{
+		this.recreate_font = false;
+		return;
+	}
+		
+	var had_font = false;
+	
+	// Release any existing font first
+	if (this.font != null)
+	{
+		renderer.ReleaseFont(this.font);
+		had_font = true;
+	}
+	
+	// Parse the font details out of the font string
+	var font_info = cr.ParseFontString(this.font_str);
+	
+	// Attempt to create the font as requested
+	this.font = renderer.CreateFont(font_info.face_name, font_info.face_size, font_info.bold, font_info.italic);
+	
+	// Creating the font failed: fall back to arial
+	if (this.font == null)
+	{
+		this.font = renderer.CreateFont("Arial", font_info.face_size, false, false);
+		
+		// Notify the user if the font has been changed via the property grid.  Don't notify
+		// if this error happens just loading a layout.
+		if (had_font)
+		{
+			BalloonTipLastProperty("Font not supported",
+								   "The font you chose does not appear to be supported by Construct 2, for technical reasons.  "
+								   + "The object has fallen back to 'Arial'.  Click the help link for more information.",
+								   bti_warning);
+		}
+	}
+	else if (!this.font.exact_match && had_font)
+	{
+		// The font was not an exact match.  Notify the user, but only when the font was changed,
+		// don't display this when loading a layout.
+		BalloonTipLastProperty("Font variation not supported",
+							   "The exact font you chose does not appear to be supported by Construct 2, for technical reasons.  "
+							   + "The object has fallen back to a different variation of the chosen font.  Click the help link for more information.",
+							   bti_warning);
+	}
+	
+	assert2(this.font != null, "Failed to create font or default Arial font");
+		
+	// Font has been created
+	this.recreate_font = false;
+	this.old_font_str = this.font_str;
+}
+
+IDEInstance.prototype.OnCreate = function()
+{
+	// Use upper-left hotspot
+	this.instance.SetHotspot(new cr.vector2(0, 0));
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnInserted = function()
+{
+	// Default to 200x30 with top left hotspot
+	this.instance.SetSize(new cr.vector2(200, 30));
+	this.instance.SetHotspot(new cr.vector2(0, 0));
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	// Recreate font if font property changed
+	if (property_name == "Font")
+	{
+		this.font_str = this.properties["Font"];
+		this.recreate_font = true;
+	}
+}
+
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+	// If the font is not yet created or needs recreating, recreate it
+	if (this.font == null || this.recreate_font)
+		this.RecreateFont(renderer);
+		
+	// If there is a font present, draw it
+	if (this.font != null)
+	{
+		var halign = ha_left;
+		
+		if (this.properties["Horizontal alignment"] == "Center")
+			halign = ha_center;
+		else if (this.properties["Horizontal alignment"] == "Right")
+			halign = ha_right;
+		
+		this.font.DrawText(this.properties["Text"],
+								this.instance.GetBoundingRect(),
+								this.properties["Color"],
+								halign,
+								this.instance.GetOpacity());
+	}
+}
+
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+	this.font = null;		// drop reference to created font
+}
---
+++ plugins/rex_textplus/runtime.js
@@ -0,0 +1,355 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_TextPlus = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_TextPlus.prototype;
+
+	pluginProto.onCreate = function ()
+	{
+		// Override the 'set width' action
+		pluginProto.acts.SetWidth = function (w)
+		{
+			if (this.width !== w)
+			{
+				this.width = w;
+				this.text_changed = true;	// also recalculate text wrapping
+				this.set_bbox_changed();
+			}
+		};
+	};
+
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		this.lines = [];		// for word wrapping
+		this.text_changed = true;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+		this.text = this.properties[0];
+		this.visible = (this.properties[1] === 0);	// 0=visible, 1=invisible
+		this.font = this.properties[2];
+		this.color = this.properties[3];
+		this.halign = this.properties[4];			// 0=left, 1=center, 2=right
+		
+		// Get the font height in pixels.
+		// Look for token ending "NNpt" in font string (e.g. "bold 12pt Arial").
+		var arr = this.font.split(" ");
+		var ptSize = 0;
+		
+		var i;
+		for (i = 0; i < arr.length; i++)
+		{
+			// Ends with 'pt'
+			if (arr[i].substr(arr[i].length - 2, 2) === "pt")
+			{
+				ptSize = parseInt(arr[i].substr(0, arr[i].length - 2));
+				this.pxHeight = Math.ceil((ptSize / 72.0) * 96.0) + 4;	// assume 96dpi...
+				break;
+			}
+		}
+		
+		assert2(this.pxHeight, "Could not determine font text height");
+	};
+
+	instanceProto.draw = function(ctx)
+	{
+		ctx.font = this.font;
+		ctx.textBaseline = "top";
+		ctx.fillStyle = this.color;
+		
+		// Set global alpha if opacity not 100%
+		if (this.opacity !== 1.0)
+			ctx.globalAlpha = this.opacity;
+		
+		// If text has changed, run the word wrap.
+		if (this.text_changed)
+		{
+			this.type.plugin.WordWrap(this.text, this.lines, ctx, this.width);
+			this.text_changed = false;
+		}
+		
+		// Draw each line after word wrap
+		var penX = this.x - (this.hotspotX * this.width);
+		var penY = this.y - (this.hotspotY * this.height);
+		
+		if (this.runtime.pixel_rounding)
+		{
+			penX = Math.round(penX);
+			penY = Math.round(penY);
+		}
+		
+		var endY = penY + this.height;
+		var line_height = this.pxHeight;
+		var drawX;
+		var i;
+		
+		for (i = 0; i < this.lines.length; i++)
+		{
+			// Adjust the line draw position depending on alignment
+			drawX = penX;
+			
+			if (this.halign === 1)		// center
+				drawX = penX + (this.width - this.lines[i].width) / 2;
+			else if (this.halign === 2)	// right
+				drawX = penX + (this.width - this.lines[i].width);
+				
+			ctx.fillText(this.lines[i].text, drawX, penY);
+			penY += line_height;
+			
+			if (penY >= endY - line_height)
+				break;
+		}
+		
+		// Restore global alpha if opacity was not 100%
+		if (this.opacity !== 1.0)
+			ctx.globalAlpha = 1.0;
+	};
+	
+	var wordsCache = [];
+
+	pluginProto.TokeniseWords = function (text)
+	{
+		wordsCache.length = 0;
+		var cur_word = "";
+		var ch;
+		
+		// Loop every char
+		var i = 0;
+		
+		while (i < text.length)
+		{
+			ch = text.charAt(i);
+			
+			if (ch === "\n")
+			{
+				// Dump current word if any
+				if (cur_word.length)
+				{
+					wordsCache.push(cur_word);
+					cur_word = "";
+				}
+				
+				// Add newline word
+				wordsCache.push("\n");
+				
+				++i;
+			}
+			// Whitespace or hyphen: swallow rest of whitespace and include in word
+			else if ( (ch === " ") || (ch === "\t") || (ch === "-") ||
+                      (ch.charCodeAt(0) >= 128) )
+			{
+				do {
+					cur_word += text.charAt(i);
+					i++;
+				}
+				while (i < text.length && (text.charAt(i) === " " || text.charAt(i) === "\t"));
+				
+				wordsCache.push(cur_word);
+				cur_word = "";
+			}
+			else if (i < text.length)
+			{
+				cur_word += ch;
+				i++;
+			}
+		}
+		
+		// Append leftover word if any
+		if (cur_word.length)
+			wordsCache.push(cur_word);
+	};
+
+	pluginProto.WordWrap = function (text, lines, ctx, width)
+	{
+		if (!text || !text.length)
+		{
+			lines.length = 0;
+			return;
+		}
+			
+		if (width <= 2.0)
+		{
+			lines.length = 0;
+			return;
+		}
+		
+		// If under 100 characters (i.e. a fairly short string), try a short string optimisation: just measure the text
+		// and see if it fits on one line, without going through the tokenise/wrap (which tends to create garbage)
+		// Text musn't contain a linebreak!
+		if (text.length <= 100 && text.indexOf("\n") === -1)
+		{
+			var all_width = ctx.measureText(text).width;
+			
+			if (all_width <= width)
+			{
+				// fits on one line
+				if (lines.length)
+					lines.length = 1;
+				else
+					lines.push({});
+					
+				lines[0].text = text;
+				lines[0].width = all_width;
+				return;
+			}
+		}
+			
+		this.WordWrapByWord(text, lines, ctx, width);
+	};
+
+	pluginProto.WordWrapByWord = function (text, lines, ctx, width)
+	{
+		this.TokeniseWords(text);	// writes to wordsCache
+		var cur_line = "";
+		var prev_line;
+		var line_width;
+		var i;
+		var lineIndex = 0;
+		var line;
+		
+		for (i = 0; i < wordsCache.length; i++)
+		{
+			// Look for newline
+			if (wordsCache[i] === "\n")
+			{
+				// Flush line.  Recycle a line if possible
+				if (lineIndex >= lines.length)
+					lines.push({});
+					
+				line = lines[lineIndex];
+				line.text = cur_line;
+				line.width = ctx.measureText(cur_line).width;
+				lineIndex++;
+				cur_line = "";
+				continue;
+			}
+			
+			// Otherwise add to line
+			prev_line = cur_line;
+			cur_line += wordsCache[i];
+			
+			// Measure line
+			line_width = ctx.measureText(cur_line).width;
+			
+			// Line too long: wrap the line before this word was added
+			if (line_width >= width)
+			{
+				// Append the last line's width to the string object
+				if (lineIndex >= lines.length)
+					lines.push({});
+					
+				line = lines[lineIndex];
+				line.text = prev_line;
+				line.width = ctx.measureText(prev_line).width;
+				lineIndex++;
+				cur_line = wordsCache[i];
+			}
+		}
+		
+		// Add any leftover line
+		if (cur_line.length)
+		{
+			if (lineIndex >= lines.length)
+				lines.push({});
+				
+			line = lines[lineIndex];
+			line.text = cur_line;
+			line.width = ctx.measureText(cur_line).width;
+			lineIndex++;
+		}
+		
+		// truncate lines to the number that were used
+		lines.length = lineIndex;
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;
+
+	cnds.CompareText = function(text_to_compare, case_sensitive)
+	{
+		if (case_sensitive)
+			return this.text == text_to_compare;
+		else
+			return this.text.toLowerCase() == text_to_compare.toLowerCase();
+	};
+
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+	acts.SetText = function(param)
+	{
+		if (typeof param === "number")
+			param = Math.round(param * 1e10) / 1e10;	// round to nearest ten billionth - hides floating point errors
+		
+		var text_to_set = param.toString();
+		
+		if (this.text !== text_to_set)
+		{
+			this.text = text_to_set;
+			this.text_changed = true;
+			this.runtime.redraw = true;
+		}
+	};
+	
+	acts.AppendText = function(param)
+	{
+		if (typeof param === "number")
+			param = Math.round(param * 1e10) / 1e10;	// round to nearest ten billionth - hides floating point errors
+			
+		var text_to_append = param.toString();
+		
+		if (text_to_append)	// not empty
+		{
+			this.text += text_to_append;
+			this.text_changed = true;
+			this.runtime.redraw = true;
+		}
+	};
+
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+	exps.Text = function(ret)
+	{
+		ret.set_string(this.text);
+	};
+		
+}());
