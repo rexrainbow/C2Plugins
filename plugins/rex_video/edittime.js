cbd848888e22e67c2277d25ab9a96caa11475157
hua <binhua.tein@gmail.com>
2011-11-19 12:48:42 +0800

    Add video plugin

---
+++ plugins/rex_video/edittime.js
@@ -0,0 +1,99 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Video",
+		"id":			"Rex_Video",
+		"description":	"Video.",
+		"author":		"",
+		"help url":		"",
+		"category":		"Html5",
+		"type":			"world",			// appears in layout
+		"rotatable":	false,
+		"flags":		pf_position_aces | pf_size_aces
+	};
+};
+
+////////////////////////////////////////
+// Conditions
+
+////////////////////////////////////////
+// Actions
+
+////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_text, "Source", "", "Source of video."),    
+];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	this.just_inserted = false;
+}
+
+IDEInstance.prototype.OnCreate = function()
+{
+	this.instance.SetHotspot(new cr.vector2(0, 0));
+}
+
+IDEInstance.prototype.OnInserted = function()
+{
+	this.instance.SetSize(new cr.vector2(150, 22));
+}
+
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+	
+// Called to draw self in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
---
+++ plugins/rex_video/runtime.js
@@ -0,0 +1,122 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Video = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	var pluginProto = cr.plugins_.Rex_Video.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+		this.elem = document.createElement("video");
+        this.elem.autoplay = true;
+        this.elem.src = this.properties[0];     
+		jQuery(this.elem).appendTo("body");        
+        
+
+		this.updatePosition();
+		
+		this.runtime.tickMe(this);
+	};
+	
+	instanceProto.onDestroy = function ()
+	{
+		jQuery(this.elem).remove();
+		this.elem = null;
+	};
+	
+	instanceProto.tick = function ()
+	{
+		this.updatePosition();
+	};
+	
+	instanceProto.updatePosition = function ()
+	{
+		var left = this.layer.layerToCanvas(this.x, this.y, true);
+		var top = this.layer.layerToCanvas(this.x, this.y, false);
+		var right = this.layer.layerToCanvas(this.x + this.width, this.y + this.height, true);
+		var bottom = this.layer.layerToCanvas(this.x + this.width, this.y + this.height, false);
+		
+		// Is entirely offscreen or invisible: hide
+		if (!this.visible || right <= 0 || bottom <= 0 || left >= this.runtime.width || top >= this.runtime.height)
+		{
+			jQuery(this.elem).hide();
+			return;
+		}
+		
+		// Truncate to canvas size
+		if (left < 1)
+			left = 1;
+		if (top < 1)
+			top = 1;
+		if (right >= this.runtime.width)
+			right = this.runtime.width - 1;
+		if (bottom >= this.runtime.height)
+			bottom = this.runtime.height - 1;
+			
+		jQuery(this.elem).show();
+		
+		var offx = left + jQuery(this.runtime.canvas).offset().left;
+		var offy = top + jQuery(this.runtime.canvas).offset().top;
+		jQuery(this.elem).offset({left: offx, top: offy});
+		jQuery(this.elem).width(right - left);
+		jQuery(this.elem).height(bottom - top);
+	};
+	
+	// only called if a layout object
+	instanceProto.draw = function(ctx)
+	{
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;
+	
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+}());
