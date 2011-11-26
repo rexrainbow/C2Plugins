dec6836a09a7ebcd56c0755e3fc0c252c7c3ece5
hua <binhua.tein@gmail.com>
2011-11-26 08:10:50 +0800

    Support touch

--- behaviors/rex_cursor/runtime.js
+++ behaviors/rex_cursor/runtime.js
@@ -35,17 +35,159 @@ cr.behaviors.Rex_Cursor = function(runtime)
 					self.onMouseMove(info);
 				};
 			})(this)
+		);   
+        
+        // touch
+		this.runtime.canvas.addEventListener("touchstart",
+			(function (self) {
+				return function(info) {
+					self.onTouchStart(info);
+				};
+			})(this),
+			true
 		);
+		
+		this.runtime.canvas.addEventListener("touchmove",
+			(function (self) {
+				return function(info) {
+					self.onTouchMove(info);
+				};
+			})(this),
+			true
+		);
+		
+		this.runtime.canvas.addEventListener("touchend",
+			(function (self) {
+				return function(info) {
+					self.onTouchEnd(info);
+				};
+			})(this),
+			true
+		);               
+
+        this.behavior_index = null;
         
 		this.mouseXcanvas = 0;				// mouse position relative to canvas
 		this.mouseYcanvas = 0;    
+        
+        // touch
+		this.touches = [];
+        this.is_on_touch = false;
+        this.last_touch_x = 0;
+        this.last_touch_y = 0;
+        
+        // control
+        this.trigger_source = 1;  // 0=touch, 1=mouse            
 	};
     
 	behtypeProto.onMouseMove = function(info)
 	{
+        this.trigger_source = 1;    
 		var offset = jQuery(this.runtime.canvas).offset();
 		this.mouseXcanvas = info.pageX - offset.left;
-		this.mouseYcanvas = info.pageY - offset.top;           
+		this.mouseYcanvas = info.pageY - offset.top;
+	};    
+        
+    // touch
+    behtypeProto.saveTouches = function (t)
+	{
+		this.touches.length = 0;
+		var offset = jQuery(this.runtime.canvas).offset();
+		
+		var i, len, touch;
+		for (i = 0, len = t.length; i < len; i++)
+		{
+			touch = t[i];
+			this.touches.push({ x: touch.pageX - offset.left, y: touch.pageY - offset.top });
+		}
+	};
+    
+	behtypeProto.onTouchMove = function (info)
+	{
+        this.trigger_source = 0;
+		info.preventDefault();
+		this.saveTouches(info.touches);
+	};
+
+	behtypeProto.onTouchStart = function (info)
+	{
+        this.trigger_source = 0;
+		info.preventDefault();
+		this.saveTouches(info.touches);
+		
+		var offset = jQuery(this.runtime.canvas).offset();
+		var is_get_drag_inst;
+        
+		if (info.changedTouches)
+		{
+			var i, len;
+			for (i = 0, len = info.changedTouches.length; i < len; i++)
+			{
+				var touch = info.changedTouches[i];
+				
+				this.curTouchX = touch.pageX - offset.left;
+				this.curTouchY = touch.pageY - offset.top;                
+			}
+            
+		}
+        
+        this.is_on_touch = true;
+	};
+
+	behtypeProto.onTouchEnd = function (info)
+	{
+        this.trigger_source = 0;
+		info.preventDefault();
+		//this.saveTouches(info.touches);  // do not update this.touches
+        
+        this.is_on_touch = false;
+        this.last_touch_x = this.touches[0].x;
+        this.last_touch_y = this.touches[0].y;        
+	};    
+           
+    // export
+	behtypeProto.GetABSX = function ()
+	{
+        var ret_x;
+        if (this.trigger_source == 1)  // mouse
+        {
+            ret_x = this.mouseXcanvas;
+        }
+        else    // touch
+        {
+		    if (this.is_on_touch)
+                ret_x = this.touches[0].x;
+		    else
+                ret_x = this.last_touch_x;        
+        }
+        return ret_x;
+	};  
+
+	behtypeProto.GetABSY = function ()
+	{
+        var ret_y;
+        if (this.trigger_source == 1)  // mouse
+        {
+            ret_y = this.mouseYcanvas;
+        }
+        else    // touch
+        {
+		    if (this.is_on_touch)
+                ret_y = this.touches[0].y;
+		    else
+                ret_x = this.last_touch_y;      
+        }
+        return ret_y;
+	};     
+    
+	behtypeProto.GetLayerX = function(inst)
+	{
+        return inst.layer.canvasToLayer(this.GetABSX(), this.GetABSY(), true);
+	};
+    
+	behtypeProto.GetLayerY = function(inst)
+	{
+        return inst.layer.canvasToLayer(this.GetABSX(), this.GetABSY(), false);
 	};    
 
 	/////////////////////////////////////
@@ -57,8 +199,8 @@ cr.behaviors.Rex_Cursor = function(runtime)
 		this.inst = inst;				// associated object instance to modify
 		this.runtime = type.runtime;
         
-		this.pre_mouseXcanvas = type.mouseXcanvas;
-		this.pre_mouseYcanvas = type.mouseYcanvas; 
+		this.pre_x = type.GetABSX();
+		this.pre_y = type.GetABSY(); 
         this.is_moving = false;
 	};
 
@@ -71,29 +213,21 @@ cr.behaviors.Rex_Cursor = function(runtime)
 
 	behinstProto.tick = function ()
 	{
-        this.is_moving = (this.pre_mouseXcanvas != this.type.mouseXcanvas) ||
-                        (this.pre_mouseYcanvas != this.type.mouseYcanvas);
+        var curr_x = this.type.GetABSX();
+        var curr_y = this.type.GetABSY();
+        this.is_moving = (this.pre_x != curr_x) ||
+                         (this.pre_y != curr_y);
         if ( (this.activated== 1) && this.is_moving) {
-           this.inst.x = this.GetLayerX();
-           this.inst.y = this.GetLayerY();
-           this.inst.set_bbox_changed();
-           // Trigger OnMoving
-           this.runtime.trigger(cr.behaviors.Rex_Cursor.prototype.cnds.OnMoving, this.inst);
-           this.pre_mouseXcanvas = this.type.mouseXcanvas;
-           this.pre_mouseYcanvas = this.type.mouseYcanvas;
+            var inst = this.inst;
+            inst.x = this.type.GetLayerX(inst);
+            inst.y = this.type.GetLayerY(inst);
+            inst.set_bbox_changed();
+            // Trigger OnMoving
+            this.runtime.trigger(cr.behaviors.Rex_Cursor.prototype.cnds.OnMoving, inst);
+            this.pre_x = curr_x;
+            this.pre_y = curr_y;
         }
 	};
-    
-	behinstProto.GetLayerX = function()
-	{        
-        return this.inst.layer.canvasToLayer(this.type.mouseXcanvas, this.type.mouseYcanvas, true);
-	};
-    
-	behinstProto.GetLayerY = function()
-	{
-        return this.inst.layer.canvasToLayer(this.type.mouseXcanvas, this.type.mouseYcanvas, false);      
-    }
-
 	//////////////////////////////////////
 	// Conditions
 	behaviorProto.cnds = {};
@@ -126,26 +260,26 @@ cr.behaviors.Rex_Cursor = function(runtime)
 
 	exps.X = function (ret)
 	{
-        ret.set_float( this.GetLayerX() );
+        ret.set_float( this.type.GetLayerX(this.inst) );
 	};
 	
 	exps.Y = function (ret)
 	{
-	    ret.set_float( this.GetLayerY() );
+	    ret.set_float( this.type.GetLayerY(this.inst) );
 	};
 	
 	exps.AbsoluteX = function (ret)
 	{
-		ret.set_float(this.type.mouseXcanvas);
+        ret.set_float( this.type.GetABSX(this.inst) );
 	};
 	
 	exps.AbsoluteY = function (ret)
 	{
-		ret.set_float(this.type.mouseYcanvas);
+        ret.set_float( this.type.GetABSY(this.inst) );
 	};
     
 	exps.Activated = function (ret)
 	{
 		ret.set_int(this.activated);
-	};    
+	};     
 }());
