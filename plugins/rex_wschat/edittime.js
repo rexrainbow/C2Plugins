671d78dcb94925222adc0716dd80c8c6f3676c73
hua <binhua.tein@gmail.com>
2011-11-28 22:49:05 +0800

    Prototype of WS chat-room client plugin

---
+++ plugins/rex_wschat/edittime.js
@@ -0,0 +1,103 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"WS Chat",
+		"id":			"Rex_WSChat",
+		"description":	"WasabiStudio Chatroom protocol",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Server",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On system message", "System", "On system message", 
+             "Trigger when system message occured", "OnSysMsg");
+AddCondition(1, cf_trigger, "On system error", "System", "On system error", 
+             "Trigger when system error occured", "OnSysError");
+AddCondition(2, cf_trigger, "On user message received", "Send/Receive", "On user message received", 
+             "Trigger when user message received", "OnUsrMsgReceived");
+
+//////////////////////////////////////////////////////////////
+// Actions  
+AddAction(1, 0, "Connect to server", "System", "Connect to server", "Connect to server.", "Connect"); 
+AddStringParam("Message", "Message", '""');
+AddAction(2, 0, "Send message", "Send/Receive", "Send message <i>{0}</i>", "Send message.", "SendMsg");
+AddStringParam("Url", "url of server", '""');
+AddAction(3, 0, "Set server", "System", "Set url of server to <i>{0}</i>", "Set server.", "SetServerUrl");
+  
+          
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_string, "System message", "System", "SysMsg", "Get the system message/error.");
+AddExpression(1, ef_return_string, "User message", "Send/Receive", "Msg", "Get the user message.");
+AddExpression(2, ef_return_string, "Chatroom url", "System", "URL", "Get the chatroom url.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish connect to server at the start of the layout.", "No|Yes"),
+    new cr.Property(ept_text, "Chatroom url", "http://chat.wasabistudio.ca", 
+                    "Url of WasabiStudio chatroom."),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_wschat/runtime.js
@@ -0,0 +1,267 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_WSChat = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_WSChat.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this._sysMsg = "";
+        this._userMsg = "";
+        this._chatroom = new cr.plugins_.Rex_WSChat.ChatroomKlass(this.properties[1], this);
+        if (this.properties[0] == 1)
+            this._chatroom.Connect();
+	};
+
+	instanceProto.onReceived = function()
+	{
+        this.runtime.trigger(cr.plugins_.Rex_WSChat.prototype.cnds.OnUsrMsgReceived, this);
+	};
+    
+	instanceProto._PostSysMsg = function(msg, is_error_msg)
+	{
+        this._sysMsg = msg;
+        var trig_method = (is_error_msg)? 
+                          cr.plugins_.Rex_WSChat.prototype.cnds.OnSysError:
+                          cr.plugins_.Rex_WSChat.prototype.cnds.OnSysMsg;
+        this.runtime.trigger(trig_method, this);
+        this._sysMsg = "";
+	};    
+      
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;         
+    
+	cnds.OnSysMsg = function ()
+	{
+		return true;
+	};	
+    
+	cnds.OnSysError = function ()
+	{
+		return true;
+	};	
+    
+	cnds.OnUsrMsgReceived = function ()
+	{
+		return true;
+	};
+    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+       
+	acts.SendMsg = function (msg)
+	{     
+        this._chatroom.Send(msg);
+	}; 
+       
+	acts.Connect = function ()
+	{       
+        this._chatroom.Connect();  
+	};     
+       
+	acts.SetServerUrl = function (url)
+	{       
+        this._chatroom.server_url = url;
+	};  
+    
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+    exps.SysMsg = function (ret)
+	{
+	    ret.set_string(this._sysMsg);
+	};	
+
+    exps.Msg = function (ret)
+	{
+	    ret.set_string(this._userMsg);
+	};	 
+
+    exps.URL = function (ret)
+	{
+	    ret.set_string( );
+	};	 
+    
+}());
+
+(function ()
+{
+    cr.plugins_.Rex_WSChat.ChatroomKlass = function(url, plugin)
+    {
+        this.server_url = url;    
+        this.sid = "";
+        this.session = "";
+        this.username = "";
+        this.currentRoom = "";
+        this.plugin = plugin;         
+    };
+    
+    var ChatroomKlassProto = cr.plugins_.Rex_WSChat.ChatroomKlass.prototype;
+    
+    ChatroomKlassProto.Connect = function()
+    {
+        this.sid = "6de3437bc0f0fa98248eb8834285e65b";
+        this._setupServerSession();
+    };
+    
+    ChatroomKlassProto.Send = function(message)
+    {
+    };
+  
+    ChatroomKlassProto.Receive = function()
+    {
+    };
+    
+    ChatroomKlassProto.Tick = function()
+    {
+    }; 
+
+    ChatroomKlassProto.SysMsg = function(msg, is_error_msg)
+    {        
+        this.plugin._PostSysMsg(msg, is_error_msg);
+    }; 
+    
+    ChatroomKlassProto._getSessionId = function() 
+    {
+        debugger;        
+        var context_obj = { inst: this };
+        var _url = this.server_url + "/sid";
+        jQuery.ajax({
+            context: context_obj,
+            type: "POST",
+            url: _url,
+            success: function(data) 
+            {
+                debugger;
+                this.inst.sid = data;
+                //setupServerSession();
+            },
+            error: function(xhr) 
+            {
+                debugger;
+                var message = "無法登入聊天室，請確定您已登入論壇。";             
+            },
+            dataType: 'text'
+        });
+    };  
+
+    ChatroomKlassProto._setupServerSession = function() 
+    {
+        debugger;    
+        var context_obj = { inst: this };
+        var _url = this.server_url + "/client/session";
+        jQuery.ajax({
+            context: context_obj,
+            type: "GET",
+            url: _url,
+            success: function(data) 
+            {
+                debugger;    
+                this.inst.session = data;
+                //setInterval(pollUpdate, pollInterval);
+                //this.inst._userInit();
+            },
+            error: function(xhr) 
+            {
+                debugger;    
+                var message = '登入聊天室失敗： ' + xhr.status;
+            },
+            dataType: 'text'
+        });
+    }; 
+    
+    /*
+    ChatroomKlassProto._userInit = function() 
+    {
+        this._enter(
+            function(data) 
+            {
+                this.username = data;
+
+                var roomKey = "大廳";
+                this._joinRoom(roomKey, function() {
+                    var message = '成功加入聊天室「' + roomKey + '」';               
+
+                    this.currentRoom = roomKey;
+                });
+            };
+        );
+    };*/
+    
+    ChatroomKlassProto._enter = function(success) 
+    {
+        var context_obj = { inst: this };
+        jQuery.ajax({
+            context: context_obj,
+            contentType: 'application/json',
+            type: 'POST',
+            url: _appendSession(this.server_url + 'client/enter/' + this.sid),
+            success: success,
+            error: function(xhr, textStatus) 
+            {
+                var message = '登入聊天室失敗： ' + xhr.status;
+                
+            }
+        });
+    };
+    
+    ChatroomKlassProto._joinRoom = function(roomKey, success) 
+    {
+        var key = encodeURIComponent(roomKey);
+        jQuery.ajax({
+            type: 'POST',
+            url: this._appendSession(this.server_url + 'room/join/' + key),
+            success: success,
+            error: function(xhr, textStatus) 
+            {
+                var message = '加入聊天室失敗： ' + xhr.status;
+            }
+        });
+    };
+    
+    ChatroomKlassProto._appendSession = function(url) 
+    {
+        return this.server_url + ';jsessionid=' + this.session;
+    }        
+}());
