5d115701cbd00354dc76b791745e095c7cc7ca2c
hua <binhua.tein@gmail.com>
2011-12-03 09:43:09 +0800

    Add pause plugin

---
+++ plugins/rex_pause/edittime.js
@@ -0,0 +1,94 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Pause",
+		"id":			"Rex_Pause",
+		"description":	"Pause timescale",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Utility",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On pause", "Event", "On pause", "", "OnPause");
+AddCondition(1, cf_trigger, "On resume", "Event", "On resume", "", "OnResume");
+AddCondition(2, 0, "Is pause", "If", "Is pause", "", "IsPause");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(0, 0, "Toggle pause", "Pause/Resume", 
+          "Toggle pause", 
+          "Toggle pause.", "TooglePause");     
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "Previous timescale", "Timescale", "PreTimescale", "Get previous timescale.");
+
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_pause/runtime.js
@@ -0,0 +1,101 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Pause = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Pause.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.is_pause = false;
+        this.previous_timescale = 0;
+	};
+    
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+
+	cnds.OnPause = function ()
+	{
+		return true;
+	};
+
+	cnds.OnResume = function ()
+	{
+		return true;
+	};   
+
+	cnds.IsPause = function ()
+	{
+		return this.is_pause;
+	};
+    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+    acts.TooglePause = function ()
+	{
+        var trig_method;
+        if (!this.is_pause)  // run -> pause
+        {
+            this.previous_timescale = this.runtime.timescale;
+            this.runtime.timescale = 0;
+            trig_method = cr.plugins_.Rex_Pause.prototype.cnds.OnPause;
+        }
+        else  // pause -> run
+        {
+            this.runtime.timescale = this.previous_timescale;
+            trig_method = cr.plugins_.Rex_Pause.prototype.cnds.OnResume;
+        }
+        this.is_pause = (!this.is_pause);
+        this.runtime.trigger(trig_method, this);        
+	}; 
+    
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+    exps.Timescale = function (ret)
+	{
+	    ret.set_float( this.previous_timescale );
+	};
+    
+}());
