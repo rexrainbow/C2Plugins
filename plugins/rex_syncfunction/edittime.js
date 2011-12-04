a8f8152bc4240d4440113ee6a30cd4fe17404772
hua <binhua.tein@gmail.com>
2011-12-04 18:29:41 +0800

    Add sync function plugin

---
+++ plugins/rex_syncfunction/edittime.js
@@ -0,0 +1,94 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Sync-Function",
+		"id":			"Rex_SyncFn",
+		"description":	"Sync function for network",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Utility",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions     
+AddObjectParam("Network", "Network object for synchronization");
+AddObjectParam("Function", "Function object for callback");
+AddAction(0, 0, "Setup Sync Function", "Setup", 
+          "Get connection from <i>{0}</i>, callback to <i>{1}</i>", 
+          "Setup Sync Function.", "Setup");
+AddStringParam("Commands", "Commands", '""');
+AddAction(1, 0, "Execute commands", "Function", "Execute commands <i>{0}</i>", "Execute commands.", "ExecuteCommands");
+
+          
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_syncfunction/runtime.js
@@ -0,0 +1,91 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_SyncFn = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_SyncFn.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.branch = null;
+        this.callback = null;   
+        this.usr_id = "";        
+	};
+    
+    instanceProto.on_message = function(usr_id, msg)
+	{
+        this.usr_id = usr_id;  
+        this.callback.ExecuteCommands(msg);
+	};      
+	
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+    acts.Setup = function (network_objs, fn_objs)
+	{  
+        var network = network_objs.instances[0];
+        if (network.check_name == "NETWORK")
+            this.branch = network.CreateBranch(this, this.on_message);
+        else
+            alert ("Sync-Function should connect to a network object");          
+        
+        var callback = fn_objs.instances[0];
+        if (callback.check_name == "FUNCTION")
+            this.callback = callback;        
+        else
+            alert ("Sync-Function should connect to a function object");
+	};  
+    
+	acts.ExecuteCommands = function (command_string)
+	{
+        debugger;
+        this.branch.send(command_string);
+	};     
+
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+}());
