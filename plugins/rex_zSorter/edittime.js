272e93aa3b1141c199b56354d05f27b2f478a5b6
hua <binhua.tein@gmail.com>
2011-12-25 10:38:38 +0800

    Add z sort plugin

---
+++ plugins/rex_zSorter/edittime.js
@@ -0,0 +1,99 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Z Sorter",
+		"id":			"Rex_ZSorter",
+		"description":	"Sorts Objects in Layer By Y",
+		"author":		"Juan Pablo Tarquino",
+		"help url":		"",
+		"category":		"General",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+////////////////////////////////////////
+// Conditions
+
+////////////////////////////////////////
+// Actions
+AddAnyTypeParam("Layer", "Layer name of number.", 0);
+AddAction(0, 0, "Sort All Objects in Layer By Y", "Depth", 
+          "Sort All Objects in Layer <i>{0}</i> By Y", "Sort All Objects in Layer By Y", "SortObjsLayerByY");
+
+////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+var property_list = [
+
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	// this.myValue = 0...
+}
+
+// Called when inserted via Insert Object Dialog for the first time
+IDEInstance.prototype.OnInserted = function()
+{
+}
+
+// Called when double clicked in layout
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called after a property has been changed in the properties bar
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+
+// For rendered objects to load fonts or textures
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+
+// Called to draw self in the editor if a layout object
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// For rendered objects to release fonts or textures
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
---
+++ plugins/rex_zSorter/runtime.js
@@ -0,0 +1,100 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_ZSorter = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	var pluginProto = cr.plugins_.Rex_ZSorter.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+
+	};
+	
+	// only called if a layout object
+	instanceProto.draw = function(ctx)
+	{
+	};
+
+	//////////////////////////////////////
+	// Conditions
+
+	
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+    
+    var ZSORT = function(instance_a, instance_b)
+    {
+        if ( (instance_a.y > instance_b.y) ||
+             ((instance_a.y == instance_b.y) && 
+              (instance_a.x > instance_b.x))
+           )
+            return 1;
+        else if ( (instance_a.y == instance_b.y) && 
+                  (instance_a.x == instance_b.x)   )
+            return 0;
+        else
+            return -1;
+    }
+
+    //Z-Sort all objects in current layer by their Y position
+	acts.SortObjsLayerByY = function (layerparam)
+	{
+        var layer = (typeof layerparam == "number")?
+                    this.runtime.getLayerByNumber(layerparam):
+                    this.runtime.getLayerByName(layerparam);
+        if (layer == null)
+        {
+            alert("Z Sort: Can not find layer  " + layerparam);
+            return;
+        }
+	    layer.instances.sort(ZSORT);
+	    layer.zindices_stale = true;
+	    this.runtime.redraw = true;
+	};
+	
+	//////////////////////////////////////
+	// Expressions
+	// pluginProto.exps = {};
+	// var exps = pluginProto.exps;
+
+}());
