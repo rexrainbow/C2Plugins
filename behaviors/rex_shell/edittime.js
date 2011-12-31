2025f17319256ad77feb1c00c44aaa234e988c79
hua <binhua.tein@gmail.com>
2011-12-31 11:01:35 +0800

    Add shell behavior

---
+++ behaviors/rex_shell/edittime.js
@@ -0,0 +1,107 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"shell",
+		"id":			"Rex_Shell",
+		"description":	"Shell behavior allowed inserted javascript",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"General",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "", "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's tick behavior.", "SetActivated");
+AddAction(1, 0, "Clean all memory", "Memory", 
+          "Clean {my} all memory", 
+          "Clean all memory.", 
+          "CleanMemory");
+AddAnyTypeParam("Index", "Index of memory, can be number of string", 0);
+AddAnyTypeParam("Value", "Value of memory", 0);
+AddAction(2, 0, "Set a memory value", "Memory", 
+          "Set {my} Mem[<i>{0}</i>] to <i>{1}</i>", 
+          "Set the value stored in memory in fsm.", 
+          "SetMemory");          
+AddStringParam("Code", "JS function code", '""');
+AddAction(3, 0, "Inject JS function objects", "JS Function", 
+          "Inject JS <i>{0}</i> into {my}", "Inject JS function objects.", "InjectJSFunctionObjects");
+AddObjectParam("Function", "Function object for controlling the game world");
+AddAction(4, 0, "Connect to function object", "Advance: Setup", 
+          "Connect to function object <i>{0}</i>", 
+          "Connect to function object.", "ConnectFn");
+AddObjectParam("CSV", "CSV object for accessing global variables");
+AddAction(5, 0, "Connect to CSV object", "Advance: Setup", 
+          "Connect to CSV object <i>{0}</i>", 
+          "Connect to CSV object.", "ConnectCSV");     
+
+          
+          
+//////////////////////////////////////////////////////////////
+// Conditions
+             
+//////////////////////////////////////////////////////////////
+// Expressions
+AddAnyTypeParam(0, "The index of memory to get, can be number of string.", 0);
+AddExpression(0, ef_return_any | ef_variadic_parameters, 
+              "Get memory", "Memory", "Mem", 
+              "Get the value from memory by index.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Debug mode", "Off", "Enable to show error message.", "Off|On"),
+	new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+	new cr.Property(ept_text, "Default memory", "", 'Set initial value of memory, ex:"{"x":10, "y":20}".'),     
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_shell/runtime.js
@@ -0,0 +1,228 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_Shell = function(runtime)
+{
+	this.runtime = runtime;
+    this.uid2inst = {};     // mapping uid to behavior instance
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_Shell.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+        this.create_cb = null;
+        this.destroy_cb = null;
+        this.tick_cb = null;
+        this.fn_obj = null;
+        this.csv_obj = null;
+
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime; 
+        
+        this.uid2inst = this.behavior.uid2inst;   // mapping uid to behavior instance          
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.activated = (this.properties[1]==1);
+                                                                       
+        this.uid2inst[this.inst.uid] = this;
+        
+		// initial memory
+		var mem = this.properties[2];
+        try
+        {
+            mem = (mem!="")? jQuery.parseJSON(mem):{};      
+        }
+        catch(err)
+        {
+            alert(err);
+            mem = {};            
+        }
+        this.shell_obj = new cr.behaviors.Rex_Shell.ShellKlass(this,
+                                                               mem);   
+        
+        var create_cb = this.type.create_cb;
+        if (create_cb != null)
+        {
+            create_cb(this.shell_obj, this.type.fn_obj, this.type.csv_obj);
+        }
+	};  
+	behinstProto.onDestroy = function()
+	{
+        var destroy_cb = this.type.destroy_cb;
+        if (destroy_cb != null)
+        {
+            destroy_cb(this.shell_obj, this.type.fn_obj, this.type.csv_obj);
+        }    
+		delete this.uid2inst[this.inst.uid];
+	};    
+    
+	behinstProto.tick = function ()
+	{
+        var tick_cb = this.type.tick_cb;
+        if (this.activated && 
+            (tick_cb!= null))
+        {
+            tick_cb(this.shell_obj, this.type.fn_obj, this.type.csv_obj);
+        }
+	};
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+    
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+    
+	acts.SetActivated = function (s)
+	{
+		this.activated = (s==1);
+	};   
+    
+	acts.CleanMemory = function ()
+	{
+        this.shell_obj["Mem"] = {};
+	};  
+        
+	acts.SetMemory = function (index, value)
+	{
+        this.shell_obj["Mem"][index] = value;
+	};
+    
+
+	acts.InjectJSFunctionObjects = function (code_string)
+	{
+        var fn = eval("("+code_string+")");
+        fn(this.shell_obj, this.type.fn_obj, this.type.csv_obj);
+	};      
+
+    acts.ConnectFn = function (fn_objs)
+	{  
+        var fn_obj = fn_objs.instances[0];
+        if (fn_obj.check_name == "FUNCTION")
+            this.type.fn_obj = fn_obj.adapter;        
+        else
+            alert ("Can not connect to a function object");
+	};    
+    
+    acts.ConnectCSV = function (csv_objs)
+	{  
+        var csv_obj = csv_objs.instances[0];
+        if (csv_obj.check_name == "CSV")
+            this.type.csv_obj = csv_obj.adapter;        
+        else
+            alert ("Can not connect to a csv object");
+	};   
+  
+    
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+    exps.Mem = function (ret, index)
+	{
+        var value = this.fsm["Mem"][index];
+        if (value == null) 
+        {
+            value = 0;
+            if (this.is_debug_mode) 
+                alert ("Can not find index in memory '" + index + "'");
+                
+        }
+	    ret.set_any(value);
+	};	
+}());
+
+(function ()
+{
+    cr.behaviors.Rex_Shell.ShellKlass = function(plugin,
+                                                 mem)
+    {
+        this["_plugin"] = plugin;
+        this["_type"] = plugin.type;
+        this["_runtime"] = plugin.runtime;
+        this["inst"] = plugin.inst;
+
+        this["Mem"] = mem;
+        this["uid2inst"] = plugin.uid2inst;
+    };
+    var ShellKlassProto = cr.behaviors.Rex_Shell.ShellKlass.prototype;
+    
+    ShellKlassProto["GetDt"] = function()
+    {
+        return this["_runtime"].getDt(this["inst"]);
+    };
+    
+    ShellKlassProto["GetX"] = function(inst)
+    {
+        return this["inst"].x;
+    }; 
+
+    ShellKlassProto["GetY"] = function(inst)
+    {
+        return this["inst"].y;
+    };
+    
+    ShellKlassProto["SetX"] = function(inst, value)
+    {
+        this["inst"].x = value;
+    }; 
+
+    ShellKlassProto["SetY"] = function(inst, value)
+    {
+        this["inst"].y = value;
+    };   
+
+    ShellKlassProto["set_bbox_changed"] = function(inst)
+    {
+        inst.set_bbox_changed();
+    };     
+    
+    ShellKlassProto["InjectCreateCB"] = function(fn)
+    {
+        this["_type"].create_cb = fn;
+    };  
+    
+    ShellKlassProto["InjectDestroyCB"] = function(fn)
+    {
+        this["_type"].destroy_cb = fn;
+    };      
+    
+    ShellKlassProto["InjectTickCB"] = function(fn)
+    {
+        this["_type"].tick_cb = fn;
+    };  
+}());
