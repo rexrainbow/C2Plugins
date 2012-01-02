c9e7295f890242671e9c77d091f42f3d94f1aca5
hua <binhua.tein@gmail.com>
2012-01-02 10:22:18 +0800

    Add bone behavior extended from pin behavior

---
+++ behaviors/rex_bone/edittime.js
@@ -0,0 +1,93 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Bone",					// as appears in 'add behavior' dialog, can be changed as long as "id" stays the same
+		"id":			"rex_Bone",					// this is used to identify this behavior and is saved to the project; never change it
+		"description":	"Stick to another object maintaining a relative distance and angle.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"General",				// Prefer to re-use existing categories, but you can set anything here
+		"flags":		0						// uncomment lines to enable flags...
+						| bf_onlyone			// can only be added once to an object, e.g. solid
+	};
+};
+
+////////////////////////////////////////
+// Conditions		
+AddCondition(0, cf_none, "Is pinned", "", "{my} is pinned", "Object is currently pinned to another object.", "IsPinned");
+
+////////////////////////////////////////
+// Actions
+AddObjectParam("Pin to", "Choose the object to pin to.");
+AddAction(0, af_none, "Pin to object", "", "{my} Pin to {0}", "Pin the object to another object.", "Pin");
+AddAction(1, af_none, "Unpin", "", "{my} Unpin", "Unpin the object.", "Unpin");
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(2, 0, "Set activated", "", "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's bone behavior.", "SetActivated");
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Lock angle", "Lock angle.",1);
+AddAction(3, 0, "Lock angle", "", "Lock {my} angle to <i>{0}</i>", 
+          "Enable to lock the angle.", "SetEnableLockAngle");    
+
+////////////////////////////////////////
+// Expressions
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+
+var property_list = [
+    new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+    new cr.Property(ept_combo, "Lock angle", "Yes", "Enable to lock the angle.", "No|Yes"),
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// any other properties here, e.g...
+	// this.myValue = 0;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_bone/runtime.js
@@ -0,0 +1,163 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.rex_Bone = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.rex_Bone.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.activated = (this.properties[0]==1);
+        this.is_lock_angle = (this.properties[1]==1);
+        
+		this.pinObject = null;
+		this.pinAngle = 0;
+		this.pinDist = 0;
+		this.myStartAngle = 0;
+		this.theirStartAngle = 0;
+		this.lastKnownAngle = 0;
+		
+		// Need to know if pinned object gets destroyed
+		this.myDestroyCallback = (function (self) {
+											return function(inst) {
+												self.onInstanceDestroyed(inst);
+											};
+										})(this);
+										
+		this.runtime.addDestroyCallback(this.myDestroyCallback);
+	};
+	
+	behinstProto.onInstanceDestroyed = function (inst)
+	{
+		// Pinned object being destroyed
+		if (this.pinObject == inst)
+			this.pinObject = null;
+	};
+	
+	behinstProto.onDestroy = function()
+	{
+		this.pinObject = null;
+		this.runtime.removeDestroyCallback(this.myDestroyCallback);
+	};
+	
+	behinstProto.tick = function ()
+	{
+		// do work in tick2 instead, after events to get latest object position
+	};
+
+	behinstProto.tick2 = function ()
+	{
+		if ((!this.pinObject) || (!this.activated))
+			return;
+			
+		// Instance angle has changed by events/something else
+		if (this.lastKnownAngle !== this.inst.angle)
+			this.myStartAngle = cr.clamp_angle(this.myStartAngle + (this.inst.angle - this.lastKnownAngle));
+			
+		var newx = this.pinObject.x + Math.cos(this.pinObject.angle + this.pinAngle) * this.pinDist;
+		var newy = this.pinObject.y + Math.sin(this.pinObject.angle + this.pinAngle) * this.pinDist;
+		var newangle = cr.clamp_angle(this.myStartAngle + (this.pinObject.angle - this.theirStartAngle));
+		this.lastKnownAngle = newangle;
+		
+		if (this.inst.x !== newx || this.inst.y !== newy || this.inst.angle !== newangle)
+		{
+			this.inst.x = newx;
+			this.inst.y = newy;
+			if (this.is_lock_angle)
+                this.inst.angle = newangle;
+			this.inst.set_bbox_changed();
+		}
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+
+	cnds.IsPinned = function ()
+	{
+		return !!this.pinObject;
+	};
+
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+	acts.Pin = function (obj)
+	{
+		if (!obj)
+			return;
+			
+		var otherinst = obj.getFirstPicked();
+		
+		if (!otherinst)
+			return;
+			
+		this.pinObject = otherinst;
+		this.pinAngle = cr.angleTo(otherinst.x, otherinst.y, this.inst.x, this.inst.y) - otherinst.angle;
+		this.pinDist = cr.distanceTo(otherinst.x, otherinst.y, this.inst.x, this.inst.y);
+		this.myStartAngle = this.inst.angle;
+		this.lastKnownAngle = this.inst.angle;
+		this.theirStartAngle = otherinst.angle;
+	};
+	
+	acts.Unpin = function ()
+	{
+		this.pinObject = null;
+	};
+	
+	acts.SetActivated = function (s)
+	{
+		this.activated = (s==1);
+	};
+	
+	acts.SetEnableLockAngle = function (e)
+	{
+		this.is_lock_angle = (e==1);
+	};    
+    
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+	
+}());
