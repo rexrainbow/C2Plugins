6400bd4c2bbb5eb9a60bd4da7259648d648eab06
hua <binhua.tein@gmail.com>
2012-01-08 16:06:59 +0800

    Add sprite extension

---
+++ behaviors/rex_spriteext/edittime.js
@@ -0,0 +1,75 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Sprite Ext",
+		"id":			"Rex_SpriteExt",
+		"description":	"Extension of sprite",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"General",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddNumberParam("Visible", "1 = visible, 0 = invisible", 0);          
+AddAction(1, 0, "Set visible", "Setting", 
+          "Set {my} visible to <i>{0}</i>", 
+          "Set visible.", "SetVisible"); 
+          
+//////////////////////////////////////////////////////////////
+// Conditions
+             
+//////////////////////////////////////////////////////////////
+// Expressions
+   
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_spriteext/runtime.js
@@ -0,0 +1,78 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_SpriteExt = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_SpriteExt.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime; 
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{                   
+	};  
+	behinstProto.onDestroy = function()
+	{
+	};    
+    
+	behinstProto.tick = function ()
+	{
+	};
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+    
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+	acts.SetVisible = function (val)
+	{
+        if (val == this.inst.visible)
+            return;
+		this.inst.visible = val;
+        this.runtime.redraw = true;
+	};    
+    
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+}());
