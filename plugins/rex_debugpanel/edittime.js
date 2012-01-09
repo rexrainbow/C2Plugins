a5077baa11f46995230f33e3064288ae6ce81241
hua <binhua.tein@gmail.com>
2012-01-10 00:14:12 +0800

    Add debug panel plugin

---
+++ plugins/rex_debugpanel/edittime.js
@@ -0,0 +1,88 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Debug Panel",
+		"id":			"Rex_DebugPanel",
+		"description":	"Dump debug message to panel.",
+		"author":		"",
+		"help url":		"",
+		"category":		"Utility",
+		"type":			"object",			// appears in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+////////////////////////////////////////
+// Conditions        
+
+////////////////////////////////////////
+// Actions
+//AddAction(1, 0, "Clean all messages", "Message", "Clean all message", "Clean all messages.", "CleanMessages");
+AddStringParam("Message", "Debug message", '""');
+AddAction(2, 0, "Append a message", "Message", "Append message <i>{0}</i>", "Append a message.", "AppendMessage");
+
+////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [   
+];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_debugpanel/runtime.js
@@ -0,0 +1,92 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_DebugPanel = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	var pluginProto = cr.plugins_.Rex_DebugPanel.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+        this.panel = window.open("", "", "width=800, height=600, location=no, menubar=no, status=no");
+	};
+	
+	instanceProto.onDestroy = function ()
+	{
+	};
+	
+	instanceProto.tick = function ()
+	{
+	};
+	
+	// only called if a layout object
+	instanceProto.draw = function(ctx)
+	{
+	};
+	
+	instanceProto.drawGL = function(glw)
+	{
+	};
+    
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;
+    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+	acts.CleanMessages = function ()
+	{
+		this.panel.document.clear();
+	};      
+    
+	acts.AppendMessage = function (message)
+	{
+        this.panel.document.write(message+ "<br>");
+	};
+   
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+}());
