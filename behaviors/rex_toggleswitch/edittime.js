ae70c1d058c4bc594e51f9f6ec4339b234caaef5
hua <binhua.tein@gmail.com>
2012-01-10 00:14:34 +0800

    Add toggle switch behavior

---
+++ behaviors/rex_toggleswitch/edittime.js
@@ -0,0 +1,84 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Toggle switch",
+		"id":			"Rex_ToggleSwitch",
+		"description":	"A switch for toggling between on and off",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Varaible",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On {my} turn on", "Event", "On turn on", "", "OnTurnOn");
+AddCondition(1, cf_trigger, "On {my} turn off", "Event", "On turn off", "", "OnTurnOff");
+AddCondition(2, 0, "Is {my} turn on", "If", "Is turn on", "", "IsTurnOn");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(0, 0, "Toggle switch", "Toggle", 
+          "Toggle {my}","Toggle switch between on and off.", "ToogleValue");  
+AddComboParamOption("Off");
+AddComboParamOption("On");
+AddComboParam("Value", "Set value to.",0);
+AddAction(1, 0, "Set value", "Set", 
+          "Set {my} value to <i>{0}</i>", "Set value of switch.", "SetValue");
+          
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "Get value of switch", "Value", "Value", "Get value of switch.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_combo, "Initial value", "Off", "Set the initialize value of switch.", "Off|On"),
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_toggleswitch/runtime.js
@@ -0,0 +1,113 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_ToggleSwitch = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_ToggleSwitch.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+		
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.value = (this.properties[0]==1);
+        this.is_my_call = false;
+	};
+
+	behinstProto.tick = function ()
+	{   
+	}; 
+    
+	behinstProto._toogle_value = function (value)
+	{
+        var cur_value = this.value;
+        if (value == cur_value)
+            return;
+        this.value = (!cur_value);
+        var trig_method = (this.value)?
+                          cr.behaviors.Rex_ToggleSwitch.prototype.cnds.OnTurnOn:
+                          cr.behaviors.Rex_ToggleSwitch.prototype.cnds.OnTurnOff;
+        this.is_my_call = true;
+        this.runtime.trigger(trig_method, this.inst);   
+        this.is_my_call = false;
+	};           
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+
+	cnds.OnTurnOn = function ()
+	{
+		return (this.is_my_call);
+	};
+
+	cnds.OnTurnOff = function ()
+	{
+		return (this.is_my_call);
+	};    
+
+	cnds.IsTurnOn = function ()
+	{
+		return (this.value);
+	};       
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+	acts.ToogleValue = function ()
+	{
+		this._toogle_value();
+	};  
+
+	acts.SetValue = function (s)
+	{
+		this._toogle_value(s);
+	};
+    
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+    
+	exps.Value = function (ret)
+	{        
+		ret.set_int((this.value)? 1:0);
+	};  
+}());
