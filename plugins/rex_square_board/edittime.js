5d2a79d3b571f3b0e08bd59a37bf4b247b4360f8
hua <binhua.tein@gmail.com>
2012-02-14 21:59:18 +0800

    Add square board plugin

---
+++ plugins/rex_square_board/edittime.js
@@ -0,0 +1,140 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Square board",
+		"id":			"Rex_SquareBoard",
+		"description":	"A square board container",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Utility",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "Get moveable brick", "Command", 
+             "Get moveable brick", 
+             'Callback of "Action:Get moveable bricks."', "GetMoveableBrick"); 
+             
+//////////////////////////////////////////////////////////////
+// Actions   
+AddNumberParam("X", "Initial number of elements on the X axis. 0 is unchanged.", 0);
+AddNumberParam("Y", "Initial number of elements on the Y axis. 0 is unchanged.", 0);
+AddNumberParam("Z", "Initial number of elements on the Z axis. 0 is unchanged.", 0)
+AddAction(0, 0, "Clean board", "Initialize", "Clean board", 
+          "Clean board to empty.", "CleanBoard");
+AddNumberParam("UID", "The uid of chess", 0);
+AddNumberParam("X", "The X index (0-based) of the chess to set.", 0);
+AddNumberParam("Y", "The Y index (0-based) of the chess to set.", 0);
+AddAction(1, 0, "Add brick", "Board", "Add brick <i>{0}</i> to [<i>{1}</i>, <i>{2}</i>, 0]", 
+          "Add brick on the board.", "AddBrick");
+AddNumberParam("UID", "The uid of chess", 0);
+AddNumberParam("X", "The X index (0-based) of the chess to set.", 0);
+AddNumberParam("Y", "The Y index (0-based) of the chess to set.", 0);
+AddNumberParam("Z", "The Z index (0-based) of the chess to set. 0 is brick.", 0);
+AddAction(2, 0, "Add chess", "Board", "Add chess <i>{0}</i> to [<i>{1}</i>, <i>{2}</i>, <i>{3}</i>]", 
+          "Add chess on the board.", "AddChess"); 
+AddObjectParam("Function", "Function object for command's callback");
+AddAction(3, 0, "Setup callback", "Setup", 
+          "Set command's callback to <i>{0}</i>", 
+          "Setup callback.", "Setup");
+AddNumberParam("UID", "The uid of chess", 0);
+AddNumberParam("Total cost", "Total cost of this moving.", 0);
+AddStringParam("Moving cost", "Callback name to get moving cost of brick.", '""');
+AddAction(4, 0, "Get moveable bricks", "Command", 
+          "Get chess <i>{0}</i>'s moveable bricks by total cost to <i>{1}</i> and cost function name to <i>{2}</i>", 
+          "Get moveable bricks.", "GetMoveableBricks");
+                    
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number | ef_variadic_parameters, 
+              "Get UID of selected chess", "Chess", "SelectedUID", 
+              "Get UID of selected chess.");
+AddExpression(2, ef_return_number | ef_variadic_parameters, 
+              "Get X index of selected chess", "Chess", "SelectedX", 
+              "Get X index of selected chess on the board.");
+AddExpression(3, ef_return_number | ef_variadic_parameters, 
+              "Get Y index of selected chess", "Chess", "SelectedY", 
+              "Get Y index of selected chess on the board.");
+AddExpression(4, ef_return_number | ef_variadic_parameters, 
+              "Get Z index of selected chess", "Chess", "SelectedZ", 
+              "Get Z index of selected chess on the board.");
+AddExpression(5, ef_return_number, 
+              "Get UID of brick", "Command", "BrickUID", 
+              'Get UID of brick. Used in "Condition:Get moveable brick" ');
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+		new cr.Property(ept_integer, "Width", 64, "Initial number of elements on the X axis."),
+		new cr.Property(ept_integer, "Height", 64, "Initial number of elements on the Y axis."),
+		new cr.Property(ept_integer, "Depth", 2, "Initial number of elements on the Z axis."),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Width"] < 1)
+		this.properties["Width"] = 1;
+		
+	if (this.properties["Height"] < 1)
+		this.properties["Height"] = 1;
+		
+	if (this.properties["Depth"] < 1)
+		this.properties["Depth"] = 1;    
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_square_board/runtime.js
@@ -0,0 +1,260 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_SquareBoard = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_SquareBoard.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{ 
+	};
+	
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this.exp_SelectedUID =0;
+	    this.exp_BrickUID =0;
+	    
+	    this.board = [];
+	    this.clean_board(this.properties[0]-1,
+	                     this.properties[1]-1,
+	                     this.properties[2]-1);
+	                     
+        this.callback = null;    
+        this._bricks = {};
+        this._chess_xyz = null;
+	};
+	
+	instanceProto.clean_board = function(x_max, y_max, z_max)
+	{
+	    if (x_max>=0)
+	        this.x_max = x_max;
+	    if (y_max>=0)    
+	        this.y_max = y_max;
+	    if (z_max>=0)
+	        this.z_max = z_max;
+	    
+		this.board.length = x_max;
+		var x, y, z;
+		for (x=0;x<=x_max;x++)
+		{
+		    this.board[x] = [];
+		    this.board[x].length = y_max;
+		    for(y=0;y<=y_max;y++)
+		    {
+		        this.board[x][y] = [];
+		        this.board[x][y].length = z_max;
+		        for(z=0;z<=z_max;z++)
+		        {
+		            this.board[x][y][z] = null;
+		        }
+		    }
+		}
+		
+		this.chesses = {};
+	};
+	
+	instanceProto.add_item = function(uid, _x, _y, _z)
+	{
+	    if ((_x<=this.x_max) && (_y<=this.y_max) && (_z<=this.z_max))
+	    {
+	        this.board[_x][_y][_z] = uid;
+	        this.chesses[uid] = {x:_x, y:_y, z:_z};
+	    }
+	};	
+	
+	instanceProto.xyz2uid = function(x, y, z)
+	{
+	    return this.board[x][y][z];
+	};
+	
+	instanceProto.uid2xyz = function(uid)
+	{
+	    return this.chesses[uid];
+	};
+	
+	instanceProto._get_moveable_brick = function(brick_uid, _total_cost, cost_fn_name, pre_brick_uid)
+	{
+	    if (brick_uid == null)
+	        return;
+	        	 
+	    var brick_xyz = this.uid2xyz(brick_uid);
+	    var at_chess_xy = ((brick_xyz.x==this._chess_xyz.x) && (brick_xyz.y==this._chess_xyz.y));
+	    var result;
+
+        if (!at_chess_xy)
+        {
+	        this.exp_BrickUID = brick_uid;
+	        var brick_cost = this.callback.CallFn(cost_fn_name);
+	        if (brick_cost == null)
+	        {
+	            // maybe an error?
+	            return;
+	        }
+	            
+	        result = _total_cost - brick_cost;
+	        if ((result >= 0) && (this._bricks[brick_uid] == null)) 
+	        {
+	            this.runtime.trigger(cr.plugins_.Rex_SquareBoard.prototype.cnds.GetMoveableBrick, this);
+	            this._bricks[brick_uid] = 0;
+	        }
+      	}
+      	else
+      	{
+      	    result = _total_cost;
+      	}
+      	
+	    if (result > 0)
+	    {
+	        var next_brick_uid;
+	        next_brick_uid = this.xyz2uid(brick_xyz.x-1, brick_xyz.y, 0);
+	        if (pre_brick_uid != next_brick_uid)
+	            this._get_moveable_brick(next_brick_uid, result, cost_fn_name, brick_uid);
+	        
+	        next_brick_uid = this.xyz2uid(brick_xyz.x+1, brick_xyz.y, 0);
+	        if (pre_brick_uid != next_brick_uid)
+	            this._get_moveable_brick(next_brick_uid, result, cost_fn_name, brick_uid);
+	        
+	        next_brick_uid = this.xyz2uid(brick_xyz.x, brick_xyz.y-1, 0);
+	        if (pre_brick_uid != next_brick_uid)
+	            this._get_moveable_brick(next_brick_uid, result, cost_fn_name, brick_uid);
+	        
+	        next_brick_uid = this.xyz2uid(brick_xyz.x, brick_xyz.y+1, 0);
+	        if (pre_brick_uid != next_brick_uid)
+	            this._get_moveable_brick(next_brick_uid, result, cost_fn_name, brick_uid);
+	    }
+	};
+	
+	instanceProto.get_moveable_bricks = function(chess_uid, _total_cost, cost_fn_name)
+	{
+	    this.exp_SelectedUID = chess_uid;	    
+	    this._bricks = {};
+	    this._chess_xyz = this.uid2xyz(chess_uid);
+	    this._get_moveable_brick(this.xyz2uid(this._chess_xyz.x, this._chess_xyz.y, 0),
+	                             _total_cost, cost_fn_name, null);
+	};
+	
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;        
+
+	cnds.GetMoveableBrick = function ()
+	{
+        return true;
+	};    
+	
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+	
+	acts.CleanBoard = function (x_max,y_max,z_max)
+	{
+		this.clean_board(x_max-1, y_max-1, z_max-1);
+	};
+	
+	acts.AddBrick = function (uid,x,y)
+	{
+		this.add_item(uid,x,y,0);
+	};
+		
+	acts.AddChess = function (uid,x,y,z)
+	{
+		this.add_item(uid,x,y,z);
+	};	
+    
+    acts.Setup = function (fn_objs)
+	{
+        var callback = fn_objs.instances[0];
+        if (callback.check_name == "FUNCTION")
+            this.callback = callback;        
+        else
+            alert ("Square board should connect to a function object");
+	};      
+		
+	acts.GetMoveableBricks = function (chess_uid, totoal_cost, cost_fn_name)
+	{
+	    var _xyz = this.uid2xyz(chess_uid);
+	    if ((_xyz == null) || (this.callback == null) || (totoal_cost<=0))
+	        return;
+
+		this.get_moveable_bricks(chess_uid, totoal_cost, cost_fn_name);
+	};    	
+		
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+	
+	exps.SelectedUID = function (ret,_x,_y,_z)
+	{
+	    var uid;
+	    if ((_x != null) && (_y != null) && (_z != null))
+	    {
+	        uid = this.xyz2uid(_x,_y,_z);
+	        if (uid == null)
+	            uid = -1;
+	    }
+	    else
+	        uid = this.exp_SelectedUID;
+	    
+		ret.set_int(uid);
+	};
+	
+	exps.SelectedX = function (ret, uid)
+	{
+	    var chess_xyz = this.uid2xyz(uid);
+	    var x = (chess_xyz==null)? (-1):chess_xyz.x;
+		ret.set_int(x);
+	};	
+	
+	exps.SelectedY = function (ret, uid)
+	{
+	    var chess_xyz = this.uid2xyz(uid);
+	    var y = (chess_xyz==null)? (-1):chess_xyz.y;
+		ret.set_int(y);
+	};
+	
+	exps.SelectedZ = function (ret, uid)
+	{
+	    var chess_xyz = this.uid2xyz(uid);
+	    var z = (chess_xyz==null)? (-1):chess_xyz.z;
+		ret.set_int(z);
+	};
+	
+	exps.BrickUID = function (ret)
+	{
+		ret.set_int(this.exp_BrickUID);
+	};	
+}());
