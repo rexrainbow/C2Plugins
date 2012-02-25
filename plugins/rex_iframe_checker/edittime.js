eda723d1dc558e778361f63183bb3c4c22e8d4b6
hua <binhua.tein@gmail.com>
2012-02-25 19:42:07 +0800

    Add iframe checker

---
+++ plugins/rex_iframe_checker/edittime.js
@@ -0,0 +1,92 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Iframe checker",
+		"id":			"Rex_IframeChecker",
+		"description":	"Check if current page is not in iframe, or in acceptable iframe.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Utility",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, 0, "Is acceptable", "Check", "Is not in iframe or in acceptable iframe", 
+             "Check If current page is not in iframe or in acceptable iframe.", "Check");
+
+//////////////////////////////////////////////////////////////
+// Actions     
+AddStringParam("URL", "The URL in white-list.", '""');
+AddAction(1, 0, "Append", "White-list", "Append <i>{0}</i> into white-list", "Append a url into white-list.", "Append");
+AddStringParam("JSON string", "JSON string.", '""');
+AddAction(2, 0, "Set", "White-list", "Set white-list to JSON string <i>{0}</i>", "Set white-list to JSON string.", "SetJSON");
+   
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_iframe_checker/runtime.js
@@ -0,0 +1,88 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_IframeChecker = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_IframeChecker.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.white_list = [];
+	};
+	
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+    
+	cnds.Check = function ()
+	{
+        var ref = document.referrer;
+		if (ref == "")    // not in iframe
+		    return true;
+	    
+		var list = this.white_list;
+		var list_len = list.length;
+		var i;
+		for (i=0;i<list_len;i++)
+		{
+		    if (ref.indexOf(list[i])!=-1)			
+			    return true;			
+		}
+		return false;
+	};
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+    
+	acts.Append = function (url)
+	{
+	    this.white_list.push(url);
+	}; 
+    
+	acts.SetJSON = function (JSON_string)
+	{
+	    this.white_list = JSON.parse(JSON_string);
+	}; 	
+	
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+}());
