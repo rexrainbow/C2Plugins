e8a546420a33ced72f7f9d8dbcf38c20ee74670f
hua <binhua.tein@gmail.com>
2012-02-29 22:43:30 +0800

    Add instances group plugin

---
+++ plugins/rex_ginstgroup/edittime.js
@@ -0,0 +1,166 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Inst Group",
+		"id":			"Rex_gInstGroup",
+		"description":	"Set/list of instances stored by uid",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Data & Storage",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddStringParam("Name", "Sorting function name.", '""');
+AddCondition(1, cf_trigger, "On sorting", "List: Sort", 
+             "On sorting function <i>{0}</i>", "Triggered when sorting by function.", "OnSortingFn");
+AddStringParam("Name", "Group name.", '""');
+AddCondition(2, cf_looping | cf_not_invertible, "For each UID", "List", 
+             "For each UID in group <i>{0}</i>", "Repeat the event for each UID in a group.", "ForEachUID");
+
+//////////////////////////////////////////////////////////////
+// Actions      
+AddStringParam("Name", "Group name.", '""');
+AddAction(1, 0, "Clean group", "Group", "Clean group <i>{0}</i>", 
+          "Clean group to empty.", "Clean");
+AddStringParam("Name", "Group A.", '""');
+AddStringParam("Name", "Group result.", '""');
+AddAction(2, 0, "Copy", "Group", "Copy group <i>{0}</i> to group <i>{1}</i>", 
+          "Copy group to another group.", "Copy");   
+AddObjectParam("Instances", "Instances to be added into group.");
+AddStringParam("Name", "Group name.", '""');
+AddAction(6, 0, "Add instances", "Instance: Add", "Add <i>{0}</i> into group <i>{1}</i>", 
+          "Add instances into group.", "PushInsts");
+AddNumberParam("UID", "The UID of instance to be added into group.", 0);
+AddStringParam("Name", "Group name.", '""');
+AddAction(7, 0, "Add instances by UID", "Instance: Add", "Add instance UID:<i>{0}</i> into group <i>{1}</i>", 
+          "Add instances into group by UID.", "PushInst");          
+AddObjectParam("Instances", "Instances to be removed from group.");
+AddStringParam("Name", "Group name.", '""');
+AddAction(8, 0, "Remove instances", "Instance: Reomve", "Remove <i>{0}</i> from group <i>{1}</i>", 
+          "Remove instances from group.", "RemoveInsts"); 
+AddNumberParam("UID", "The UID of instance to be removed from group.", 0);
+AddStringParam("Name", "Group name.", '""');
+AddAction(9, 0, "Remove instances by UID", "Instance: Reomve", "Remove instance UID:<i>{0}</i> from group <i>{1}</i>", 
+          "Remove instances from group by UID.", "RemoveInst");
+AddStringParam("Name", "Group A.", '""');
+AddStringParam("Name", "Group B.", '""');
+AddStringParam("Name", "Group result.", '""');
+AddAction(10, 0, "Union", "Group: Set operation", "Set group <i>{2}</i> to group <i>{0}</i> union group <i>{1}</i>", 
+          "Set group by Union operation.", "Union");        
+AddStringParam("Name", "Group A.", '""');
+AddStringParam("Name", "Group B.", '""');
+AddStringParam("Name", "Group result.", '""');
+AddAction(11, 0, "Complement", "Group: Set operation", "Set group <i>{2}</i> to group <i>{0}</i> complement group <i>{1}</i>", 
+          "Set group by complement operation.", "Complement");  
+AddStringParam("Name", "Group A.", '""');
+AddStringParam("Name", "Group B.", '""');
+AddStringParam("Name", "Group result.", '""');
+AddAction(12, 0, "Intersection", "Group: Set operation", "Set group <i>{2}</i> to group <i>{0}</i> intersection group <i>{1}</i>", 
+          "Set group by intersection operation.", "Intersection");            
+AddStringParam("Name", "Group name.", '""');
+AddAction(13, 0, "Sort group by shuffling", "List: Sort", "Sort group <i>{0}</i> by shuffling", 
+          "Sort group by shuffling.", "Shuffle"); 
+AddStringParam("Name", "Group name.", '""');
+AddStringParam("Sorting function", "Sorting function of group", '""');
+AddAction(14, 0, "Sort group by function", "List: Sort", "Sort group <i>{0}</i> by function <i>{1}</i>", 
+          "Sort group by function.", "SortByFn"); 
+AddNumberParam("Result", "Compared result. (-1) is (A < B), 0 is (A == B), 1 is (A > B)", 0);
+AddAction(15, 0, "Set compared result", "List: Sort", "Set compare result to <i>{0}</i>", 
+          'Set compared result. Used in callback of "Action: Sort group by function"', "SetCmpResultDirectly");
+AddComboParamOption("<");
+AddComboParamOption("=");
+AddComboParamOption(">");
+AddComboParam("Result", "Compared result", 0);   
+AddAction(16, 0, "Set compared result", "List: Sort", "Set compare result to CmpUIDA <i>{0}</i> CmpUIDB", 
+          'Set compared result. Used in callback of "Action: Sort group by function"', "SetCmpResultCombo");           
+AddStringParam("Name", "Group name.", '""');
+AddAction(17, 0, "Create iterator", "List: Iterator", "Create iterator from group <i>{0}</i>", 
+          "Create iterator from group.", "CreateIterator");                     
+AddStringParam("Name", "Group name.", '""');          
+AddObjectParam("Object", "Object for picking");
+AddComboParamOption("Keep");
+AddComboParamOption("Pop");
+AddComboParam("Operation", "Keep or pop UID", 0);
+AddAction(18, 0, "Pick instances", "SOL", 
+          "Pick and <i>{2}</i> <i>{0}</i> from group <i>{1}</i>", "Pick instances from group.", "PickInsts");    
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, 
+              "Get UID A of sorting function", "List: Sort", "CmpUIDA", 'Get Instance UID A of sorting function. Used in "Action: Sort group by function"');
+AddExpression(2, ef_return_number, 
+              "Get UID B of sorting function", "List: Sort", "CmpUIDB", 'Get Instance UID B of sorting function. Used in "Action: Sort group by function"');              
+AddExpression(4, ef_return_number | ef_variadic_parameters, 
+              "Get item count", "Group", "InstCnt", "Get item count of group.");
+AddExpression(5, ef_return_number | ef_variadic_parameters,
+              'Get UID from "For each"', "List", "ForEachUID", 'Get UID in a group. Used in "Condition:For each UID"');                         
+AddExpression(6, ef_return_number | ef_variadic_parameters,
+              "Get UID from iterator", "List", "IterUID", 'Get UID in a group. Used with "Action:Create iterator"');                         
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_ginstgroup/runtime.js
@@ -0,0 +1,355 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_gInstGroup = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_gInstGroup.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this.groups = {};
+	    this._cmp_uidA = 0;
+	    this._cmp_uidB = 0;
+	    this._sort_fn_name = "";	    
+	    this._compared_result = 0;
+	    this._foreach_UID = 0;
+	};
+
+	instanceProto.get_group = function(name)
+	{
+	    var group = this.groups[name];
+	    if (group == null)
+	    {
+	        group = new cr.plugins_.Rex_gInstGroup.GroupKlass();
+	        this.groups[name] = group;
+	    }
+	    return group;
+	};
+	
+    
+    instanceProto._sets_operation_target_get = function (group_a, group_b, group_result)
+    {
+	    if ((group_a != group_result) && (group_b != group_result))
+	    {
+	        this.get_group(group_result).Copy(this.get_group(group_a));
+            group_a = group_result;   
+	    }
+	    else if (group_result == group_b)
+	    {
+	        group_b = group_a;
+	        group_a = group_result;
+        }
+        return {"a":group_a, "b":group_b};
+    };
+
+    var _thisArg  = null;
+	var _sort_fn = function(uid_a, uid_b)
+	{
+	    _thisArg._cmp_uidA = uid_a;
+	    _thisArg._cmp_uidB = uid_b;	    
+	    _thisArg.runtime.trigger(cr.plugins_.Rex_gInstGroup.prototype.cnds.OnSortingFn, this);
+	    return _thisArg._compared_result;	    
+	};		
+	
+	var _shuffle = function (arr)
+	{
+        var i = arr.length, j, temp;
+        if ( i == 0 ) return;
+        while ( --i ) 
+        {
+            j = Math.floor( Math.random() * (i+1) );
+            temp = arr[i]; 
+            arr[i] = arr[j]; 
+            arr[j] = temp;
+        }
+    };	
+
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds; 
+	  
+	cnds.OnSortingFn = function (name)
+	{
+	    _thisArg = this;
+		return (this._sort_fn_name == name);
+	};	 
+	
+	cnds.ForEachUID = function (name)
+	{
+	    var uids = this.get_group(name).GetList();
+	    var uids_len = uids.length;
+	    var i;
+        var current_event = this.runtime.getCurrentEventStack().current_event;		
+		for (i=0; i<uids_len; i++)
+	    {
+	        this._foreach_UID = uids[i];
+		    this.runtime.pushCopySol(current_event.solModifiers);
+			current_event.retrigger();
+			this.runtime.popSol(current_event.solModifiers);
+		}
+		
+        return false;
+	};
+	    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts; 
+	
+    acts.Clean = function (name)
+	{
+	    this.get_group(name).Clean();
+	};  	
+	
+    acts.Copy = function (group_a, group_result)
+	{
+	    this.get_group(group_result).Copy(this.get_group(group_a));
+	};	
+	
+    acts.PushInsts = function (objtype, name)
+	{
+	    var insts = objtype.getCurrentSol().getObjects();
+	    var insts_length = insts.length;
+	    var i;
+	    
+	    var group = this.get_group(name);
+	    for (i=0; i<insts_length; i++)
+	        group.AddUID(insts[i].uid);
+	};		
+	
+    acts.PushInst = function (uid, name)
+	{
+	    this.get_group(name).AddUID(uid);	    
+	};		
+	
+    acts.RemoveInsts = function (objtype, name)
+	{
+	    var insts = objtype.getCurrentSol().getObjects();
+	    var insts_length = insts.length;
+	    var i;
+	    
+	    var group = this.get_group(name);
+	    for (i=0; i<insts_length; i++)
+	        group.RemoveUID(insts[i].uid);	    
+	};		
+	
+    acts.RemoveInst = function (uid, name)
+	{
+	    this.get_group(name).RemoveUID(uid);		    
+	};		
+	
+    acts.Union = function (group_a, group_b, group_result)
+	{
+	    var groups = this._sets_operation_target_get(group_a, group_b, group_result);
+	    this.get_group(groups["a"]).Union(this.get_group(groups["b"]).GetSet());
+	};
+	
+    acts.Complement = function (group_a, group_b, group_result)
+	{
+	    var groups = this._sets_operation_target_get(group_a, group_b, group_result);
+	    this.get_group(groups["a"]).Complement(this.get_group(groups["b"]).GetSet());	    
+	};	
+	
+    acts.Intersection = function (group_a, group_b, group_result)
+	{
+	    var groups = this._sets_operation_target_get(group_a, group_b, group_result);	
+	    this.get_group(groups["a"]).Intersection(this.get_group(groups["b"]).GetSet());	      
+	};		
+	
+    acts.Shuffle = function (name)
+	{
+	    _shuffle(this.get_group(name).GetList());
+	};	
+	
+    acts.SortByFn = function (name, fn_name)
+	{
+	    this._sort_fn_name = fn_name;
+	    this.get_group(name).GetList().sort(this._sort_fn);
+	};		
+	
+    acts.SetCmpResultDirectly = function (result)
+	{
+	    this._compared_result = result;
+	};		
+	
+    acts.SetCmpResultCombo = function (result)
+	{
+	    this._compared_result = result -1;
+	};
+	
+    acts.CreateIterator = function (name)
+	{
+	};	
+		
+    acts.PickInsts = function (name, objtype, is_pop)
+	{
+	    var group = this.get_group(name);
+	    var group_uids = group.GetSet();	    
+        var sol = objtype.getCurrentSol();  
+        sol.select_all = true;   
+        var insts = sol.getObjects();
+        var insts_length = insts.length;
+        var i, inst;
+        sol.instances.length = 0;   // clear contents
+        for (i=0; i < insts_length; i++)
+        {
+           inst = insts[i];
+           if (group_uids[ inst.uid ] != null)
+           {
+               sol.instances.push(inst);
+               if (is_pop == 1)
+                   group.RemoveUID(inst.uid);
+           }
+        }
+        sol.select_all = false;
+	};
+				
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+	
+	exps.CmpUIDA = function (ret)
+	{   
+	    ret.set_int(this._cmp_uidA);
+	};    
+	
+	exps.CmpUIDB = function (ret)
+	{   
+	    ret.set_int(this._cmp_uidB);
+	};    	
+	
+	exps.InstCnt = function (ret, name)
+	{   
+	    ret.set_int(this.get_group(name).GetList().length);
+	};
+    
+	exps.ForEachUID = function (ret, name)
+	{   
+	    ret.set_int(this._foreach_UID);
+	};	
+    
+	exps.IterUID = function (ret, name)
+	{   
+	    ret.set_int(0);
+	};		
+}());
+
+(function ()
+{
+    cr.plugins_.Rex_gInstGroup.GroupKlass = function()
+    {
+        this.Clean();
+    };
+    var GroupKlassProto = cr.plugins_.Rex_gInstGroup.GroupKlass.prototype;
+    
+	GroupKlassProto.Clean = function()
+	{
+		this._set = {};
+        this._list = [];
+	};
+    
+	GroupKlassProto.Copy = function(group)
+	{
+		this._set = jQuery.extend({}, group._set);
+        this._list.length =0;
+        var uid;
+        for (uid in this._set)
+            this._list.push(parseInt(uid));
+	};
+	
+	GroupKlassProto.AddUID = function(uid)
+	{
+	    if (this._set[uid] == null)
+	    {
+	        this._set[uid] = 0;
+	        this._list.push(uid);
+	    }
+	};
+	
+	GroupKlassProto.RemoveUID = function(uid)
+	{
+	    if (this._set[uid] != null)
+	    {
+	        delete this._set[uid];
+	        var i = this._list.indexOf(uid);
+	        this._list.splice(i,1);
+	    }
+	};
+		
+	GroupKlassProto.Union = function(uids)
+	{	    		    
+        var uid;        
+        for (uid in uids)        
+            this.AddUID(parseInt(uid));    
+	};	
+		
+	GroupKlassProto.Complement = function(uids)
+	{	        
+        var uid;        
+        for (uid in uids)        
+            this.RemoveUID(parseInt(uid));                
+	};
+		
+	GroupKlassProto.Intersection = function(uids)
+	{	    
+        var uid;
+        var remove_uids = [];        
+        for (uid in this._set)
+        {
+            if (uids[uid] == null)
+                remove_uids.push(parseInt(uid));
+        }
+        
+        var remove_len = remove_uids.length;
+        var i;
+        for (i=0; i<remove_len; i++)
+            this.RemoveUID(remove_uids[i]);
+	};	
+	
+	GroupKlassProto.GetSet = function()
+	{
+	    return this._set;
+	};
+	
+	GroupKlassProto.GetList = function()
+	{
+	    return this._list;
+	};
+}());    
+    
