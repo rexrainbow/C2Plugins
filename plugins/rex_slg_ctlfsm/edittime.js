84175504e34edc2e8b872bdfda5398fcbe56b685
hua <binhua.tein@gmail.com>
2012-03-17 01:16:04 +0800

    Add slg control fsm

---
+++ plugins/rex_slg_ctlfsm/edittime.js
@@ -0,0 +1,156 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Control FSM",
+		"id":			"Rex_SLGCTLFSM",
+		"description":	"Control FSM of SLG game",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"SLG",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, 'On enter "Idle"', "Enter", 
+             'On enter "Idle" state', 'Trigger when state enter to "Idle".', "OnEnterIdle");
+AddCondition(2, cf_trigger, 'On exit "Idle"', "Exit", 
+             'On exit "Idle" state', 'Trigger when state exit from "Idle".', "OnExitIdle");   
+AddCondition(3, cf_trigger, 'On enter "GetSource"', "Enter", 
+             'On enter "GetSource" state', 'Trigger when state enter to "GetSource".', "OnEnterGetSource");
+AddCondition(4, cf_trigger, 'On exit "GetSource"', "Exit", 
+             'On exit "GetSource" state', 'Trigger when state exit from "GetSource".', "OnExitGetSource");   
+AddCondition(5, cf_trigger, 'On enter "GetCommand"', "Enter", 
+             'On enter "GetCommand" state', 'Trigger when state enter to "GetCommand".', "OnEnterGetCommand");
+AddCondition(6, cf_trigger, 'On exit "GetCommand"', "Exit", 
+             'On exit "GetCommand" state', 'Trigger when state exit from "GetCommand".', "OnExitGetCommand");                        
+AddCondition(7, cf_trigger, 'On enter "GetTarget"', "Enter", 
+             'On enter "GetTarget" state', 'Trigger when state enter to "GetTarget".', "OnEnterGetTarget");
+AddCondition(8, cf_trigger, 'On exit "GetTarget"', "Exit", 
+             'On exit "GetTarget" state', 'Trigger when state exit from "GetTarget".', "OnExitGetTarget");                        
+AddCondition(9, cf_trigger, 'On enter "AcceptCommand"', "Enter", 
+             'On enter "AcceptCommand" state', 'Trigger when state enter to "AcceptCommand".', "OnEnterAcceptCommand");
+AddCondition(10, cf_trigger, 'On exit "AcceptCommand"', "Exit", 
+             'On exit "AcceptCommand" state', 'Trigger when state exit from "AcceptCommand".', "OnExitAcceptCommand"); 
+AddCondition(11, cf_trigger, 'On enter "RunCommand"', "Enter", 
+             'On enter "RunCommand" state', 'Trigger when state enter to "RunCommand".', "OnEnterRunCommand");
+AddCondition(12, cf_trigger, 'On exit "RunCommand"', "Exit", 
+             'On exit "RunCommand" state', 'Trigger when state exit from "RunCommand".', "OnExitRunCommand"); 
+             
+//////////////////////////////////////////////////////////////
+// Actions
+AddObjectParam("Group", "Instance group object");
+AddAction(0, 0, "Setup", "Setup", 
+          "Set instance group object to <i>{1}</i>", 
+          "Set instance group object.", "Setup"); 
+AddStringParam("Available", "Group name of available instances", '""');
+AddAction(3, 0, "Get available source group", "Request: Source", 
+          "Get available source group to <i>{0}</i>", 
+          "Get available source group.", "GetAvailableSourceGroup");     
+AddStringParam("Source", "Group name of target instances", '""');
+AddAction(4, 0, "Get source group", "Request: Source", 
+          "Get source group to <i>{0}</i>", 
+          "Get source group.", "GetSourceGroup");
+AddStringParam("Commands", "Available commands", '""');
+AddAction(5, 0, "Get available commands", "Request: Command", 
+          "Get available commands to <i>{0}</i>", 
+          "Get available commands.", "GetAvailableCommands");
+AddStringParam("Commands", "Executed command", '""');
+AddAction(6, 0, "Get command", "Request: Command", 
+          "Get executed command to <i>{0}</i>", 
+          "Get executed command.", "GetCommand");   
+AddStringParam("Available", "Group name of available instances", '""');
+AddAction(7, 0, "Get available target group", "Request: Target", 
+          "Get available target group to <i>{0}</i>", 
+          "Get available target group.", "GetAvailableTargetGroup");         
+AddStringParam("Target", "Group name of target instances", '""');
+AddAction(8, 0, "Get target group", "Request: Target", 
+          "Get target group to <i>{0}</i>", 
+          "Get target group.", "GetTargetGroup"); 
+AddAction(9, 0, "Accept", "Request: Run command", 
+          "Accept command", 
+          "Accept command.", "AcceptCommand");   
+AddAction(10, 0, "Finish", "Request: Run command", 
+          "Finish execution", 
+          "Finish execution.", "FinishExecution");          
+AddAction(15, 0, "Cancel", "Request: Cancel", 
+          "Cancel", 
+          "Cancel operation.", "Cancel");   
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(3, ef_return_string, 
+              "Get new state", "State", "NewState", "Get new state.");
+AddExpression(4, ef_return_string, 
+              "Get current state", "State", "CurState", "Get current state.");              
+AddExpression(5, ef_return_string, 
+              "Get source group", "Command", "Source", "Get source group.");
+AddExpression(6, ef_return_string, 
+              "Get command", "Command", "command", "Get command.");
+AddExpression(7, ef_return_string, 
+              "Get target group", "Command", "Target", "Get target group.");              
+
+              
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_slg_ctlfsm/runtime.js
@@ -0,0 +1,243 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_SLGCTLFSM = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_SLGCTLFSM.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this._available_source = "";
+        this.exp_Source = "";
+        this._available_command = "";
+        this.exp_Command = "";
+        this._available_target = "";
+        this.exp_Target = "";
+        this._pre_state = "";
+        this._cur_state = "Off";
+        this._request_handler = {"Off":this._request_Off,
+                                 "Idle":this._request_Idle,
+                                 "GetSource":this._request_GetSource,
+                                 "GetCommand":this._request_GetCommand,
+                                 "GetTarget":this._request_GetTarget,
+                                 "AcceptCommand":this._request_AcceptCommand,
+                                 "RunCommand":this._request_RunCommand       };  
+        this._enterTrigs = {"Idle":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnEnterIdle,
+                            "GetSource":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnEnterGetSource,
+                            "GetCommand":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnEnterGetCommand,
+                            "GetTarget":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnEnterGetTarget,
+                            "AcceptCommand":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnEnterAcceptCommand,
+                            "RunCommand":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnEnterRunCommand       };   
+        this._exitTrigs =  {"Idle":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnExitIdle,
+                            "GetSource":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnExitGetSource,
+                            "GetCommand":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnExitGetCommand,
+                            "GetTarget":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnExitGetTarget,
+                            "AcceptCommand":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnExitAcceptCommand,
+                            "RunCommand":cr.plugins_.Rex_SLGCTLFSM.prototype.cnds.OnExitRunCommand       };   
+
+        this._request();  // "Off" -> "Idle"                    
+	};
+   	
+	instanceProto._request = function(cmd)
+	{
+        return this._request_handler[this._cur_state](cmd);
+	};
+	instanceProto._request_Off = function(cmd)
+	{
+        return "Idle";
+	};    
+	instanceProto._request_Idle = function(cmd)
+	{
+        if (cmd == "GetAvailableSource")
+        {
+            if (this._has_instances(this._available_source))   
+                return "GetSource";
+            //else
+            // TODO
+                
+        }
+	};
+	instanceProto._request_GetSource = function(cmd)
+	{
+        if (cmd == "GetSource")
+           return "GetCommand";
+        else if (cmd == "Cancel")
+           return "Idle";        
+	}; 
+	instanceProto._request_GetCommand = function(cmd)
+	{
+        if (cmd == "GetCommand")
+           return "GetTarget";
+        else if (cmd == "Cancel")
+           return "Idle";             
+	};
+	instanceProto._request_GetTarget = function(cmd)
+	{
+        if (cmd == "GetTarget")
+           return "AcceptCommand";
+        else if (cmd == "Cancel")
+           return "Idle"; 
+	};  
+	instanceProto._request_AcceptCommand = function(cmd)
+	{
+        if (cmd == "AcceptCommand")
+           return "RunCommand";
+        else if (cmd == "Cancel")
+           return "Idle"; 
+	};
+	instanceProto._request_RunCommand = function(cmd)
+	{
+        if (cmd == "FinishExecution")    
+            return "Idle"; 
+	};     
+	instanceProto._on_state_transfer = function(new_state)
+	{     
+        if (new_state == null)
+            return;
+        var old_state = this._cur_state;
+        this._pre_state = this._cur_state;
+        this._cur_state = new_state;
+        var trig;
+        trig = this._exitTrigs[old_state];
+        if (trig != null)
+            this.runtime.trigger(this._exitTrigs[old_state],this);
+        trig = this._enterTrigs[new_state];
+        if (trig != null)            
+            this.runtime.trigger(this._enterTrigs[new_state],this);
+	};
+	instanceProto._has_instances = function(group_name)
+	{
+        return (this.group.GetGroup(group_name).length > 0);
+	};
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;
+	    
+	cnds.OnEnterIdle = function (name)  { return true; };    
+	cnds.OnExitIdle = function (name)  { return true; };  	
+	cnds.OnEnterGetSource = function (name)  { return true; };    
+	cnds.OnExitGetSource = function (name)  { return true; };
+	cnds.OnEnterGetCommand = function (name)  { return true; };    
+	cnds.OnExitGetCommand = function (name)  { return true; };	
+	cnds.OnEnterGetTarget = function (name)  { return true; };    
+	cnds.OnExitGetTarget = function (name)  { return true; };  	
+	cnds.OnEnterAcceptCommand = function (name)  { return true; };    
+	cnds.OnExitAcceptCommand = function (name)  { return true; };
+	cnds.OnEnterRunCommand = function (name)  { return true; };    
+	cnds.OnExitRunCommand = function (name)  { return true; };		
+	 
+	 
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+    
+    acts.Setup = function (group_objs)
+	{
+        var group = group_objs.instances[0];
+        if (group.check_name == "INSTGROUP")
+            this.group = group;        
+        else
+            alert ("SLG CTLFSM should connect to a instance group object");            
+	};     
+    acts.GetAvailableSourceGroup = function (group_name)
+    {        
+        this._available_source = group_name;
+        this._on_state_transfer(this._request("GetAvailableSource"));    
+	};
+    acts.GetSourceGroup = function (group_name)
+    {        
+        this.exp_Source = group_name;  
+        this._on_state_transfer(this._request("GetSource"));
+	};  
+    acts.GetAvailableCommands = function (command)
+    {
+        this._available_command = command;
+	};
+    acts.GetCommand = function (command)
+    {        
+        this.exp_Command = command;
+        this._on_state_transfer(this._request("GetCommand"));
+	}; 
+    acts.GetAvailableTargetGroup = function (group_name)
+    {
+        this._available_target = group_name;
+	};
+    acts.GetTargetGroup = function (group_name)
+    {
+        this.exp_Target = group_name;
+        this._on_state_transfer(this._request("GetTarget"));
+	}; 
+    acts.AcceptCommand = function ()
+    {        
+        this._on_state_transfer(this._request("AcceptCommand"));
+	};
+    acts.FinishExecution = function ()
+    {        
+        this._on_state_transfer(this._request("FinishExecution"));
+	};    
+    acts.Cancel = function ()
+    {        
+        this._on_state_transfer(this._request("Cancel"));
+	};     
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+    
+	exps.NewState = function (ret)
+	{   
+	    ret.set_string(this._cur_state);
+	};  
+	exps.CurState = function (ret)
+	{   
+	    ret.set_string(this._pre_state);
+	};    
+	exps.Source = function (ret)
+	{   
+	    ret.set_string(this.exp_Source);
+	};
+	exps.Command = function (ret)
+	{   
+	    ret.set_string(this.exp_Command);
+	};   
+	exps.Target = function (ret)
+	{   
+	    ret.set_string(this.exp_Target);
+	};      
+}());
