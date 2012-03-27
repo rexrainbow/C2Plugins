ecf3253a106d68b987d01f1ae9fd6c24cc7bb079
hua <binhua.tein@gmail.com>
2012-03-28 07:49:33 +0800

    Add token plugin

---
+++ plugins/rex_token/edittime.js
@@ -0,0 +1,131 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Token",
+		"id":			"Rex_Token",
+		"version":		"0.1",   		
+		"description":	"Round-robin player index, used in turn based game.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"General",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, 0, "First index", "Index", 
+             "First index", "Testing if current index is first.", "IsFirst");
+AddCondition(2, 0, "Last index", "Index", 
+             "Last index", "Testing if current index is the last.", "IsLast");
+             
+//////////////////////////////////////////////////////////////
+// Actions          
+AddAction(2, 0, "Invert", "Order", 
+          "Invert token order", 
+          "Invert token order.", "InvertOrder");            
+AddAction(3, 0, "Next", "Index", 
+          "Turn to next player", 
+          "Turn to next player.", "NextIndex");
+AddNumberParam("Index", "Player index", 0);
+AddAction(4, 0, "Set", "Index", 
+          "Set next player index to <i>{0}</i>", 
+          "Set next player index.", "SetIndex"); 
+AddAction(5, 0, "Turn off", "Index", 
+          "Turn off token", 
+          "Turn off token.", "TurnOff");                 
+AddStringParam("ID", "Player ID string", "1,2");
+AddAction(10, 0, "Set ID list", "ID", 
+          "Set ID list to [ <i>{0}</i> ]", 
+          "Set ID list.", "SetIDList");  
+AddAnyTypeParam("ID", "Player ID", 1);
+AddAction(11, 0, "Append ID", "ID", 
+          "Append <i>{0}</i> to ID list", 
+          "Append ID to list.", "AppendIDList");  
+//AddAnyTypeParam("ID", "Player ID", 1);
+//AddAction(12, 0, "Remove ID", "ID", 
+//          "Remove <i>{0}</i> from ID list", 
+//          "Remove ID from list.", "RemoveIDList");                   
+       
+//////////////////////////////////////////////////////////////
+// Expressions
+AddNumberParam("Index", "Player index.", 0);   
+AddExpression(1, ef_return_any | ef_variadic_parameters,
+              "Get ID by index", "Look up", "Index2ID",
+              "Get player ID by player index.");
+AddStringParam("ID", "Player ID.", "1");   
+AddExpression(2, ef_return_number | ef_variadic_parameters,
+              "Get index by ID", "Look up", "ID2Index",
+              "Get player index by player ID.");            
+AddExpression(3, ef_return_number, 
+              "Get current index", "Index", "CurrIndex", "Get current player index.");
+AddExpression(4, ef_return_any, 
+              "Get current ID", "ID", "CurrID", "Get current player ID.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_text, "Player ID", "1,2", 'Player ID. Seprate by","'),
+    new cr.Property(ept_integer, "Initial index", (-1), "Initial index. The next index of (-1) is 0."),
+    new cr.Property(ept_combo, "Order", "Increasing", "Order of transfering player index.", "Increasing|Decreasing"),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_token/runtime.js
@@ -0,0 +1,141 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Token = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Token.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this._set_id_list(this.properties[0]);
+        this.index = this.properties[1];
+        this.is_inc_order = (this.properties[2] == 0);
+	};
+	
+	instanceProto._set_id_list = function(id_list_string)
+	{
+	    this.player_id_list = JSON.parse("["+id_list_string+"]");
+	};	
+	
+	instanceProto._set_next_index = function()
+	{
+	    var last_index = this.player_id_list.length-1;
+	    if (this.index == (-1))
+	        this.index = (this.is_inc_order)? 0: last_index;
+	    else
+	    {
+	        if (this.is_inc_order)
+	            this.index = (this.index == last_index)? 0: (this.index+1);
+	        else
+	            this.index = (this.index == 0)? last_index: (this.index-1);
+	    }
+	};	
+
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+    
+	cnds.IsFirst = function ()
+	{
+		return (this.index == 0);        
+	}; 	    
+	cnds.IsLast = function ()
+	{
+		return (this.index == (this.player_id_list.length-1));        
+	};
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+		
+	acts.NextIndex = function ()
+	{        
+	    this._set_next_index();
+	};	
+	
+	acts.SetIndex = function (_index)
+	{        
+        if (_index < this.player_id_list.length)
+            this.index = _index;
+	};
+	
+	acts.TurnOff = function ()
+	{        
+        this.index = (-1);
+	};
+		
+	acts.InvertOrder = function ()
+	{        
+        this.is_inc_order = !this.is_inc_order;
+	};
+	
+	acts.SetIDList = function (id_list_string)
+	{        
+        this._set_id_list(id_list_string);
+	};	
+	acts.AppendIDList = function (_id)
+	{        
+        this.player_id_list.push(_id);
+	};	
+	acts.RemoveIDList = function (_id)
+	{        
+        //
+	};		
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+    
+	exps.Index2ID = function (ret, _index, default_value)
+	{
+	    ret.set_any(this.player_id_list[_index]);
+	};    
+	exps.ID2Index = function (ret, _id)
+	{
+	    ret.set_int(this.player_id_list.indexOf(_id));
+	};   
+	exps.CurrIndex = function (ret)
+	{
+	    ret.set_int(this.index);
+	};
+	exps.CurrID = function (ret)
+	{
+	    ret.set_any(this.player_id_list[this.index]);
+	};	
+	
+}());
