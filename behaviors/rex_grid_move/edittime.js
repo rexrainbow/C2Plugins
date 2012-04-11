1dd7b8e9b5c2cb40c8ca652e10716d402f4dd0e2
hua <binhua.tein@gmail.com>
2012-04-11 08:17:13 +0800

    Add hex grid move

---
+++ behaviors/rex_grid_move/edittime.js
@@ -0,0 +1,172 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Grid Move",
+		"id":			"Rex_GridMove",
+		"version":		"0.1",        
+		"description":	"Move sprite to neighbor on board object",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Movements",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On hit target position", "", 
+             "On {my} hit target", 
+			 "Triggered when hit target position.", 
+			 "OnHitTarget");
+AddCondition(2,	cf_trigger, "On moving", "", "On {my} moving", 
+             "Triggered when object moving.", "OnMoving");                          
+AddCondition(3,	0, "Is moving", "", "Is {my} moving", "Test if object is moving.", "IsMoving");  
+AddCondition(4,	cf_trigger, "On moving accepted", "Request", 
+             "On {my} moving request accepted", "Triggered when moving request accepted.", "OnMovingRequestAccepted");                
+AddCondition(5,	cf_trigger, "On moving rejected", "Request", 
+             "On {my} moving request rejected", "Triggered when moving request rejected.", "OnMovingRequestRejected");                          
+AddCondition(6,	0, "Moving accepted", "Request", "Is {my} moving request accepted", "Test if moving request accepted.", "IsMovingRequestAccepted");  
+AddNumberParam("X offset", "Relatived X offset.",0);
+AddNumberParam("Y offset", "Relatived Y offset.",0);
+AddCondition(7,	0, "Can move to", "Test", "{my} can move to offset [<i>{0}</i>, <i>{1}</i>]", 
+             "Test if object can move to relatived offset target.", "TestMoveToOffset");  
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "", 
+          "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's Square-grid Move behavior.", "SetActivated");
+AddComboParamOption("Right");		  
+AddComboParamOption("Down");
+AddComboParamOption("Left");
+AddComboParamOption("Up");
+AddComboParam("Direction", "Moving direction.", 0);
+AddAction(1, 0, "Move to neighbor", "Request: Square grid", "{my} move to <i>{0}</i>", 
+          "Move to neighbor.", "MoveToNeighbor"); 
+AddNumberParam("X offset", "Relatived X offset.",0);
+AddNumberParam("Y offset", "Relatived Y offset.",0);
+AddAction(2, 0, "Move to offset", "Request", "{my} move to offset [<i>{0}</i>, <i>{1}</i>]", 
+          "Move to relatived offset target.", "MoveToOffset");        
+AddNumberParam("Max speed", "Maximum speed, in pixel per second.", 400);
+AddAction(3, 0, "Set maximum speed", "Speed", 
+          "Set {my} maximum speed to <i>{0}</i>", 
+          "Set the object's maximum speed.", "SetMaxSpeed");
+AddNumberParam("Acceleration", "The acceleration setting, in pixel per second per second.", 0);
+AddAction(4, 0, "Set acceleration", "Speed", 
+          "Set {my} acceleration to <i>{0}</i>", 
+          "Set the object's acceleration.", "SetAcceleration");
+AddNumberParam("Deceleration", "The deceleration setting, in pixels per second per second.", 0);
+AddAction(5, 0, "Set deceleration", "Speed", 
+          "Set {my} deceleration to <i>{0}</i>", 
+          "Set the object's deceleration.", "SetDeceleration");    
+AddNumberParam("Current speed", "Current speed, in pixel per second.", 400);
+AddAction(6, 0, "Set current speed", "Speed", 
+          "Set {my} current speed to <i>{0}</i>", 
+          "Set the object's Current speed.", "SetCurrentSpeed");                     
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Solid", "Solid.", 0);
+AddAction(7, 0, "Set solid", "Solid", "Set {my} solid to <i>{0}</i>", 
+          "Set solid.", "SetSolid");    
+AddNumberParam("Solid", "Set solid. 0=No, 1=Yes", 0);
+AddAction(8, 0, "Set solid by number", "Solid", "Set {my} solid to <i>{0}</i>", 
+          "Set solid by number.", "SetSolid"); 
+AddComboParamOption("Right");
+AddComboParamOption("Down-right");	  
+AddComboParamOption("Down-left");	 
+AddComboParamOption("Left");
+AddComboParamOption("Up-left");
+AddComboParamOption("Up-right");
+AddComboParam("Direction", "Moving direction.", 0);
+AddAction(9, 0, "Move to neighbor", "Request: Hexagon grid", "{my} move to <i>{0}</i>", 
+          "Move to neighbor.", "MoveToNeighbor"); 
+AddNumberParam("Direction", "The direction of neighbor.", 0);		  
+AddAction(10, 0, "Move to neighbor", "Request", "{my} move to direction <i>{0}</i>", 
+          "Move to neighbor.", "MoveToNeighbor"); 
+		  
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "Get current activated state", "Current", "Activated", 
+              "The current activated state of behavior.");
+AddExpression(1, ef_return_number, "Get current speed", "Current", "Speed", 
+              "The current object speed, in pixel per second.");
+AddExpression(2, ef_return_number, "Get max speed", "Setting", "MaxSpeed", 
+              "The maximum speed setting, in pixel per second.");
+AddExpression(3, ef_return_number, "Get acceleration", "Setting", "Acc", 
+              "The acceleration setting, in pixel per second per second.");
+AddExpression(4, ef_return_number, "Get deceleration", "Setting", "Dec", 
+              "The deceleration setting, in pixel per second per second.");
+AddExpression(5, ef_return_number, "Get target position X", "Target", "TargetX", 
+              "The X co-ordinate of target position to move toward.");
+AddExpression(6, ef_return_number, "Get target position Y", "Target", "TargetY", 
+              "The Y co-ordinate of target position to move toward."); 
+AddExpression(7, ef_return_number, "Get solid", "Setting", "Solid", 
+              "Solid property. 0=No, 1=Yes.");
+                          
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Solid", "No", "Enable if you do not allow other chess move over.", "No|Yes"),
+    new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),                    
+	new cr.Property(ept_float, "Max speed", 400, "Maximum speed, in pixel per second."),
+	new cr.Property(ept_float, "Acceleration", 0, 
+                    "Acceleration, in pixel per second per second."),
+	new cr.Property(ept_float, "Deceleration", 0, 
+                    "Deceleration, in pixel per second per second."),    
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Max speed"] < 0)
+		this.properties["Max speed"] = 0;
+		
+	if (this.properties["Acceleration"] < 0)
+		this.properties["Acceleration"] = 0;
+		
+	if (this.properties["Deceleration"] < 0)
+		this.properties["Deceleration"] = 0;
+}
---
+++ behaviors/rex_grid_move/runtime.js
@@ -0,0 +1,414 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_GridMove = function(runtime)
+{
+	this.runtime = runtime;
+    this.uid2solid = {};     // mapping uid to solid property of behavior instance
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_GridMove.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+        
+        this.uid2solid = this.behavior.uid2solid;   // mapping uid to behavior instance
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.board = null;
+        this.is_solid = (this.properties[0] == 1);
+        this._cmd_move_to = new cr.behaviors.Rex_GridMove.CmdMoveTo(this);
+        this.uid2solid[this.inst.uid] = this.is_solid; 
+        		
+        this._target_xyz = {x:0,y:0,z:0};        
+        this._is_moving_request_accepted = false;
+        this.is_my_call = false;
+	};
+	behinstProto.onDestroy = function()
+	{
+		delete this.uid2solid[this.inst.uid];
+	};        
+
+    behinstProto.tick = function ()
+	{
+	    this._cmd_move_to.tick();
+	};
+	
+	behinstProto._board_get = function ()
+	{
+        var _xyz;
+        if (this.board != null)
+        {
+            _xyz = this.board.uid2xyz(this.inst.uid);
+            if (_xyz != null)
+                return this.board;  // find out xyz on board
+            else  // chess no longer at board
+                this.board = null;
+        }
+            
+        var plugins = this.runtime.types;
+        var name, obj;
+        for (name in plugins)
+        {
+            obj = plugins[name].instances[0];
+            if ((obj != null) && (obj.check_name == "BOARD"))
+            {
+                _xyz = obj.uid2xyz(this.inst.uid)
+                if (_xyz != null)
+                {
+                    this.board = obj;
+                    return this.board;
+                }
+            }
+        }
+        return null;	
+	};
+	
+    behinstProto._xyz_get = function ()
+    {
+	    var board = this._board_get();
+		if (board != null)
+		    return board.uid2xyz(this.inst.uid);
+	    else
+            return null;
+    };
+
+    behinstProto._test_move_to = function (target_x, target_y, target_z)
+    {
+        var _target_uid = this.board.xyz2uid(target_x, target_y, target_z);
+        if (_target_uid == null)  // no overlap at the same z
+        {
+            var z_max = this.board.z_max;
+            var z;
+            for (z=0; z<=z_max; z++)    // find out if neighbors have solid property
+            {
+                _target_uid = this.board.xyz2uid(target_x, target_y, z);
+                if ((z==0) && (_target_uid == null))  // tile does not exist
+                    return null;
+                else if ((_target_uid != null) && (this.uid2solid[_target_uid]))  // solid
+                    return (-1);
+            }           
+            return 1; // can move to target
+        }
+        else        
+            return (-1);
+    };
+    behinstProto._move_to_target = function (target_x, target_y, target_z)
+    {
+        var can_move = this._test_move_to(target_x, target_y, target_z);
+        if (can_move == 1)
+        {
+            // can move to neighbor
+            this.board.move_item(this.inst.uid, target_x, target_y, target_z);
+            // set moveTo
+            var layout = this.board.layout;
+            this._cmd_move_to._set_target_pos(layout.GetX(target_x, target_y), 
+                                              layout.GetY(target_x, target_y));
+            this._is_moving_request_accepted = true;           
+            this.is_my_call = true;                          
+            this.runtime.trigger(cr.behaviors.Rex_GridMove.prototype.cnds.OnMovingRequestAccepted, this.inst);                                           
+            this.is_my_call = false; 
+        } 
+        else if (can_move == (-1))
+        {
+            this._is_moving_request_accepted = false;
+            this.is_my_call = true;                             
+            this.runtime.trigger(cr.behaviors.Rex_GridMove.prototype.cnds.OnMovingRequestRejected, this.inst);                                           
+            this.is_my_call = false;            
+        }    
+        else
+        {
+            this._is_moving_request_accepted = false;
+        }
+    };
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+
+	cnds.OnHitTarget = function ()
+	{
+		return (this._cmd_move_to.is_my_call);
+	};
+	
+    cnds.OnMoving = function ()
+	{
+		return (this._cmd_move_to.is_my_call);
+	};
+    
+	cnds.IsMoving = function ()
+	{
+		return (this._cmd_move_to.is_moving);
+	};
+	
+    cnds.OnMovingRequestAccepted = function ()
+	{
+		return (this.is_my_call);
+	};	
+    cnds.OnMovingRequestRejected = function ()
+	{
+		return (this.is_my_call);
+	};
+    cnds.IsMovingRequestAccepted = function ()
+	{
+		return (this.is_my_call && this._is_moving_request_accepted);
+	};
+    cnds.TestMoveToOffset = function (dx, dy)
+	{
+		var _xyz = this._xyz_get();
+		if (_xyz == null)
+		    return false;
+
+        var can_move = this._test_move_to(_xyz.x+dx, _xyz.y+dy, _xyz.z);	    
+		return (can_move==1);
+	};			
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+    
+	acts.SetActivated = function (s)
+	{
+		this._cmd_move_to.activated = (s==1);
+	};
+
+	acts.SetMaxSpeed = function (s)
+	{
+		this._cmd_move_to.move.max = s;
+        this._cmd_move_to._set_current_speed(null);
+	};      
+    
+	acts.SetAcceleration = function (a)
+	{
+		this._cmd_move_to.move.acc = a;
+        this._cmd_move_to._set_current_speed(null);
+	};
+	
+	acts.SetDeceleration = function (a)
+	{
+		this._cmd_move_to.move.dec = a;
+	};
+    
+	acts.SetCurrentSpeed = function (s)
+	{
+        this._cmd_move_to._set_current_speed(s);
+	}; 
+	
+	acts.MoveToNeighbor = function (dir)
+	{
+	    if (this._cmd_move_to.activated)
+		{
+		    var _board = this._board_get();
+			if (_board != null)
+			{
+	            var _xyz = this._xyz_get();
+		        this._move_to_target(_board.layout.GetNeighborLX(_xyz.x, _xyz.y, dir), 
+				                     _board.layout.GetNeighborLY(_xyz.x, _xyz.y, dir),
+									 _xyz.z);
+	        }
+        }
+	};
+	
+	acts.MoveToOffset = function (dx, dy)
+	{
+	    if (this._cmd_move_to.activated)
+		{
+		    var _xyz = this._xyz_get();
+			if (_xyz != null)
+		        this._move_to_target(_xyz.x+dx, _xyz.y+dy, _xyz.z);
+	    }
+	};	
+	
+	acts.SetSolid = function (enable)
+	{
+		this.is_solid = (enable == 1);
+        this.uid2solid[this.inst.uid] = this.is_solid;
+	};    
+	
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+    
+	exps.Activated = function (ret)
+	{
+		ret.set_int((this._cmd_move_to.activated)? 1:0);
+	};    
+    
+	exps.Speed = function (ret)
+	{
+		ret.set_float(this._cmd_move_to.current_speed);
+	};
+    
+	exps.MaxSpeed = function (ret)
+	{
+		ret.set_float(this._cmd_move_to.move.max);
+	}; 
+
+	exps.Acc = function (ret)
+	{
+		ret.set_float(this._cmd_move_to.move.acc);
+	};  
+
+ 	exps.Dec = function (ret)
+	{
+		ret.set_float(this._cmd_move_to.move.dec);
+	}; 
+
+	exps.TargetX = function (ret)
+	{
+        var x = (this._cmd_move_to.is_moving)? this.target.x:0;
+		ret.set_float(x);
+	};  
+
+ 	exps.TargetY = function (ret)
+	{
+        var y = (this._cmd_move_to.is_moving)? this.target.y:0;
+		ret.set_float(y);
+	};     
+
+ 	exps.Solid = function (ret)
+	{
+        ret.set_int((this.is_solid)? 1:0);		
+	};     
+	
+}());
+
+(function ()
+{
+    cr.behaviors.Rex_GridMove.CmdMoveTo = function(plugin)
+    {
+        this.activated = plugin.properties[1];
+        this.move = {max:plugin.properties[2],
+                     acc:plugin.properties[3],
+                     dec:plugin.properties[4]};
+        this.target = {x:0 , y:0, angle:0};
+        this.is_moving = false;  
+        this.current_speed = 0;       
+        this.remain_distance = 0;
+        this.is_hit_target = false;
+        this.is_my_call = false; 
+        
+        this.inst = plugin.inst;
+        this.runtime = plugin.runtime;
+    };
+    var CmdMoveToProto = cr.behaviors.Rex_GridMove.CmdMoveTo.prototype;
+    
+    CmdMoveToProto.tick = function ()
+	{
+        if (this.is_hit_target)
+        {
+            this.is_my_call = true;
+            this.runtime.trigger(cr.behaviors.Rex_GridMove.prototype.cnds.OnHitTarget, this.inst); 
+            this.is_my_call = false;
+            this.is_hit_target = false;
+        }
+        
+        if ( (!this.activated) || (!this.is_moving) ) 
+        {
+            return;
+        }
+        
+		var dt = this.runtime.getDt(this.inst);
+        if (dt==0)   // can not move if dt == 0
+            return;
+		
+        // assign speed
+        var is_slow_down = false;
+        if (this.move.dec != 0)
+        {
+            // is time to deceleration?                
+            var _speed = this.current_speed;
+            var _distance = (_speed*_speed)/(2*this.move.dec); // (v*v)/(2*a)
+            is_slow_down = (_distance >= this.remain_distance);
+        }
+        var acc = (is_slow_down)? (-this.move.dec):this.move.acc;
+        if (acc != 0)
+        {
+            this._set_current_speed( this.current_speed + (acc * dt) );    
+        }
+
+		// Apply movement to the object     
+        var distance = this.current_speed * dt;
+        this.remain_distance -= distance;   
+
+        // is hit to target at next tick?
+        if ( (this.remain_distance <= 0) || (this.current_speed <= 0) )
+        {
+            this.is_moving = false;
+            this.inst.x = this.target.x;
+            this.inst.y = this.target.y;
+            this._set_current_speed(0);
+            this.is_hit_target = true;
+        }
+        else
+        {
+            var angle = this.target.angle;
+            this.inst.x += (distance * Math.cos(angle));
+            this.inst.y += (distance * Math.sin(angle));
+        } 
+
+		this.inst.set_bbox_changed();
+        this.is_my_call = true;
+        this.runtime.trigger(cr.behaviors.Rex_GridMove.prototype.cnds.OnMoving, this.inst);
+        this.is_my_call = false;
+	};
+	
+	CmdMoveToProto._set_current_speed = function(speed)
+	{
+        if (speed != null)
+        {
+            this.current_speed = (speed > this.move.max)? 
+                                 this.move.max: speed;
+        }        
+        else if (this.move.acc==0)
+        {
+            this.current_speed = this.move.max;
+        }
+	};  
+    
+	CmdMoveToProto._set_target_pos = function (_x, _y)
+	{
+        var dx = _x - this.inst.x;
+        var dy = _y - this.inst.y;
+        
+        this.is_moving = true;         
+		this.target.x = _x;
+        this.target.y = _y; 
+        this.target.angle = Math.atan2(dy, dx);
+        this.remain_distance = Math.sqrt( (dx*dx) + (dy*dy) );
+        this._set_current_speed(null);
+	}; 
+}());    
