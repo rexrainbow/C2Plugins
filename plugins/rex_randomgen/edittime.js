9f44b618a8a589775c9311223f7b5cf059d18e87
hua <binhua.tein@gmail.com>
2012-05-01 22:26:27 +0800

    Add plugin of random value generator with seed

---
+++ plugins/rex_randomgen/edittime.js
@@ -0,0 +1,91 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Random gen",
+		"id":			"Rex_Random",
+		"version":		"0.1",   		
+		"description":	"Generate random number with a seed.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"General",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions     
+AddNumberParam("Seed", "Set seed", 0);
+AddAction(2, 0, "Create", "Random generator", "Create a random value generator with a seed to <i>{0}</i>", 
+          "Create a random value generator with a seed.", "SetSeed");
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(2, ef_return_number, 
+              "Get random value", "Random", "random", 
+              "Get random value from random generator.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
