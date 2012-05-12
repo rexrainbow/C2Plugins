fbc72a47c5b86f5476099440c7673c26ced4340b
hua <binhua.tein@gmail.com>
2012-05-12 22:54:38 +0800

    Add drag drop 2

---
+++ behaviors/rex_dragdrop2/edittime.js
@@ -0,0 +1,92 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Drag & Drop2",
+		"id":			"Rex_DragDrop2",
+		"version":		"1.0",        
+		"description":	"Use the mouse to drag and drop an object",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Movements",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "", "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's drag&drop behavior.", "SetActivated");
+AddAction(1, 0, "Force to drop", "", "Force {my} to drop", 
+          "Force the dragged object to drop.", "ForceDropp");          
+
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0,	cf_trigger, "On drag start", "", "On {my} drag start", "Triggered when object drag start.", "OnDragStart");
+AddCondition(1,	cf_trigger, "On dragging", "", "On {my} dragging", "Triggered when object dragging.", "OnDragging");             
+AddCondition(2,	cf_trigger, "On drop", "", "On {my} drop", "Triggered when object drop.", "OnDrop"); 
+AddCondition(3,	0, "Is dragging", "", "Is {my} dragging", "Is object dragging.", "IsDragging");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number | ef_variadic_parameters, "Mouse X position", "Position", "X", "Get the mouse cursor X co-ordinate in the layout.");
+AddExpression(1, ef_return_number | ef_variadic_parameters, "Mouse Y position", "Position", "Y", "Get the mouse cursor Y co-ordinate in the layout.");
+AddExpression(2, ef_return_number, "Absolute mouse X", "Position", "AbsoluteX", "Get the mouse cursor X co-ordinate on the canvas.");
+AddExpression(3, ef_return_number, "Absolute mouse Y", "Position", "AbsoluteY", "Get the mouse cursor Y co-ordinate on the canvas.");
+AddExpression(4, ef_return_number, "Get activated", "", "Activated", "The activated setting, 1 is activated.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+    new cr.Property(ept_combo, "Axis", "Both", "The axis this object can drag on.", "Both|Horizontal|Vertical"),
+    ];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_dragdrop2/runtime.js
@@ -0,0 +1,288 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_DragDrop2 = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_DragDrop2.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+        this.touchwrap = null;
+        this.behavior_index = null;
+        this._behavior_insts = [];
+	};
+    
+	behtypeProto.TouchWrapGet = function ()
+	{
+        if (this.touchwrap != null)
+            return;
+            
+        var plugins = this.runtime.types;
+        var name, obj;
+        for (name in plugins)
+        {
+            obj = plugins[name].instances[0];
+            if ((obj != null) && (obj.check_name == "TOUCHWRAP"))
+            {
+                this.touchwrap = obj;
+                this.touchwrap.HookMe(this);
+                break;
+            }
+        }
+        assert2(this.touchwrap, "You need put a Touchwrap object for Cursor behavior");
+	};  
+    
+    behtypeProto.OnTouchStart = function ()
+    {
+        this.DragDetecting(this.GetABSX(), this.GetABSY());
+    };
+    
+    behtypeProto.OnTouchEnd = function ()
+    {
+    };
+    
+    // drag detecting
+	behtypeProto.DragDetecting = function(x, y)
+	{
+        var sol = this.objtype.getCurrentSol(); 
+        var select_all_save = sol.select_all;
+        sol.select_all = true;
+        var overlap_cnt = this.runtime.testAndSelectCanvasPointOverlap(this.objtype, x, y, false);
+        if (overlap_cnt == 0)
+        {
+            // recover to select_all_save
+            sol.select_all = select_all_save;        
+            return false;
+        }
+        
+        // overlap_cnt > 0
+        // 0. find out index of behavior instance
+        if (this.behavior_index == null )
+            this.behavior_index = this.objtype.getBehaviorIndexByName(this.name);
+            
+            
+        // 1. get all valid behavior instances            
+        var ovl_insts = sol.getObjects();
+        var i, cnt;
+        var inst;            
+        cnt = ovl_insts.length;   
+        this._behavior_insts.length = 0;          
+        for (i=0; i<cnt; i++ )
+        {
+            inst = ovl_insts[i].behavior_insts[this.behavior_index];
+            if (inst.activated)
+                this._behavior_insts.push(inst);
+        }
+            
+        // 2. get the max z-order inst
+        cnt = this._behavior_insts.length;
+        var target_inst = this._behavior_insts[0];
+        for (i=1; i<cnt; i++ )
+        {
+            inst = this._behavior_insts[i];
+            if ( inst.inst.zindex > target_inst.inst.zindex )
+                target_inst = inst;
+        }
+        target_inst.drag_info.is_on_drag = true;
+        var inst_x = target_inst.inst.x;
+        var inst_y = target_inst.inst.y;
+        target_inst.drag_info.drag_dx = inst_x - x;
+        target_inst.drag_info.drag_dy = inst_y - y;
+        this.runtime.trigger(cr.behaviors.Rex_DragDrop2.prototype.cnds.OnDragStart, target_inst.inst);     
+
+        // recover to select_all_save
+        sol.select_all = select_all_save;
+        this._behavior_insts.length = 0; 
+        
+        return true;  // get drag inst  
+	}; 
+        
+    // export     
+	behtypeProto.GetABSX = function ()
+	{
+        return this.touchwrap.GetAbsoluteX();
+	};  
+
+	behtypeProto.GetABSY = function ()
+	{
+        return this.touchwrap.GetAbsoluteY();
+	};     
+        
+	behtypeProto.GetLayerX = function(inst)
+	{
+        return this.touchwrap.GetX(inst.layer);
+	};
+    
+	behtypeProto.GetLayerY = function(inst)
+	{
+        return this.touchwrap.GetY(inst.layer);
+	};  
+        
+	behtypeProto._is_release = function()
+	{
+        return (!this.touchwrap.IsInTouch());
+	};    
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+        
+        type.TouchWrapGet();            
+        this.drag_info = {pre_x:this.type.GetLayerX(inst),
+                          pre_y:this.type.GetLayerY(inst),
+                          drag_dx:0,
+                          drag_dy:0,
+                          is_on_drag:false};                       
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{   
+        this.activated = this.properties[0]; 
+        this.dragButton = this.properties[1]; 
+        this.move_axis = this.properties[2];  
+	};
+
+	behinstProto.tick = function ()
+	{        
+        if ( (this.activated == 0) ||
+             (!this.drag_info.is_on_drag)      )
+        {
+            return;        
+        }
+        
+        // this.activated == 1 && this.is_on_drag        
+        var inst = this.inst;
+        var cur_x = this.type.GetLayerX(inst);
+        var cur_y = this.type.GetLayerY(inst);
+        var is_mouse_moved = (this.drag_info.pre_x != cur_x) ||
+                             (this.drag_info.pre_y != cur_y);      
+        if ( is_mouse_moved )
+        {
+            var drag_x = cur_x + this.drag_info.drag_dx;
+            var drag_y = cur_y + this.drag_info.drag_dy;
+            switch (this.move_axis)
+            {
+                case 1:
+                    inst.x = drag_x;
+                    break;
+                case 2:
+                    inst.y = drag_y;
+                    break;
+                default:
+                    inst.x = drag_x;
+                    inst.y = drag_y;
+                    break;
+            }
+            inst.set_bbox_changed();
+            this.drag_info.pre_x = cur_x;
+            this.drag_info.pre_y = cur_y;                    
+        }
+        this.runtime.trigger(cr.behaviors.Rex_DragDrop2.prototype.cnds.OnDragging, inst);
+                                
+        if ( this.type._is_release() )
+        {
+            this.drag_info.is_on_drag = false;
+            this.runtime.trigger(cr.behaviors.Rex_DragDrop2.prototype.cnds.OnDrop, inst); 
+        }
+	};    
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;    
+    
+	cnds.OnDragStart = function ()
+	{
+        return true;
+	};
+    
+	cnds.OnDrop = function ()
+	{
+		return true;
+	}; 
+
+ 	cnds.OnDragging = function ()
+	{   
+        return true;
+    }
+    
+ 	cnds.IsDragging = function ()
+	{   
+        return (this.is_on_drag);
+    }    
+    
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+	acts.SetActivated = function (s)
+	{
+		this.activated = s;
+	};  
+
+	acts.ForceDropp = function ()
+	{
+        if (this.drag_info.is_on_drag)
+        {
+		    this.drag_info.is_on_drag = false;            
+            this.runtime.trigger(cr.behaviors.Rex_DragDrop2.prototype.cnds.OnDrop, this.inst); 
+        }
+	};      
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+	exps.X = function (ret)
+	{
+        ret.set_float( this.type.GetLayerX(this.inst) );
+	};
+	
+	exps.Y = function (ret)
+	{
+	    ret.set_float( this.type.GetLayerY(this.inst) );
+	};
+	
+	exps.AbsoluteX = function (ret)
+	{
+        ret.set_float( this.type.GetABSX(this.inst) );
+	};
+	
+	exps.AbsoluteY = function (ret)
+	{
+        ret.set_float( this.type.GetABSY(this.inst) );
+	};
+    
+	exps.Activated = function (ret)
+	{
+		ret.set_int(this.activated);
+	};    
+}());
