59e9de3f8beef44d96c35a19ce3e2f772b84d47d
hua <binhua.tein@gmail.com>
2012-05-20 11:58:22 +0800

    Add arrow key plugin

---
+++ plugins/rex_arrowkey/edittime.js
@@ -0,0 +1,103 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"ArrowKey",
+		"id":			"Rex_ArrowKey",
+		"version":		"0.1",   		
+		"description":	"Get arrow key event from keyboard or touch/mouse input.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Input",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On UP key pressed", "Pressd", "On UP pressed", "Triggered when UP key is pressed.", "OnUPKey");
+AddCondition(2, cf_trigger, "On DOWN key pressed", "Pressd", "On DOWN pressed", "Triggered when DOWN key is pressed.", "OnDOWNKey");
+AddCondition(3, cf_trigger, "On LEFT key pressed", "Pressd", "On LEFT pressed", "Triggered when LEFT key is pressed.", "OnLEFTKey");
+AddCondition(4, cf_trigger, "On RIGHT key pressed", "Pressd", "On RIGHT pressed", "Triggered when RIGHT key is pressed.", "OnRIGHTKey");
+AddCondition(5, cf_trigger, "On any key pressed", "Pressd", "On any key pressed", "Triggered when any keyboard key is pressed.", "OnAnyKey");
+
+AddCondition(11,	0,	"UP Key is down",	"Is down", "UP is down", "Test if UP key is currently held down.", "IsUPDown");
+AddCondition(12,	0,	"DOWN Key is down",	"Is down", "DOWN is down", "Test if DOWN key is currently held down.", "IsDOWNDown");
+AddCondition(13,	0,	"LEFT Key is down",	"Is down", "LEFT is down", "Test if LEFT key is currently held down.", "IsLEFTDown");
+AddCondition(14,	0,	"RIGHT Key is down",	"Is down", "RIGHT is down", "Test if RIGHT key is currently held down.", "IsRIGHTDown");
+
+AddCondition(21,	cf_trigger,	"On UP key released",	"Released", "On UP released", "Triggered when UP key is released.", "OnUPKeyReleased");
+AddCondition(22,	cf_trigger,	"On DOWN key released",	"Released", "On DOWN released", "Triggered when DOWN key is released.", "OnDOWNKeyReleased");
+AddCondition(23,	cf_trigger,	"On LEFT key released",	"Released", "On LEFT released", "Triggered when LEFT key is released.", "OnLEFTKeyReleased");
+AddCondition(24,	cf_trigger,	"On RIGHT key released",	"Released", "On RIGHT released", "Triggered when RIGHT key is released.", "OnRIGHTKeyReleased");
+
+//////////////////////////////////////////////////////////////
+// Actions
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Directions", "8 directions", "The number of directions of movement available.", "Up & down|Left & right|4 directions|8 directions"),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_arrowkey/runtime.js
@@ -0,0 +1,272 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_ArrowKey = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_ArrowKey.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{  
+	};
+    
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this._directions = this.properties[0]; 
+        this.runtime.tickMe(this);
+     
+        this.setup_stage = true;
+        // touch   
+        this.touchwrap = null;
+		this.pre_x = 0;
+		this.pre_y = 0;           
+        this.is_on_moving = false;  
+        this.keyMap = [false, false, false, false];
+        
+        this.press_handlers =   [cr.plugins_.Rex_ArrowKey.prototype.cnds.OnRIGHTKey,
+                                 cr.plugins_.Rex_ArrowKey.prototype.cnds.OnDOWNKey,
+                                 cr.plugins_.Rex_ArrowKey.prototype.cnds.OnLEFTKey,
+                                 cr.plugins_.Rex_ArrowKey.prototype.cnds.OnUPKey            ];   
+        this.release_handlers = [cr.plugins_.Rex_ArrowKey.prototype.cnds.OnRIGHTKeyReleased,
+                                 cr.plugins_.Rex_ArrowKey.prototype.cnds.OnDOWNKeyReleased,
+                                 cr.plugins_.Rex_ArrowKey.prototype.cnds.OnLEFTKeyReleased,
+                                 cr.plugins_.Rex_ArrowKey.prototype.cnds.OnUPKeyReleased    ];
+	};
+
+	instanceProto.TouchWrapGet = function ()
+	{  
+        var plugins = this.runtime.types;
+        var name, obj;
+        for (name in plugins)
+        {
+            obj = plugins[name].instances[0];
+            if ((obj != null) && (obj.check_name == "TOUCHWRAP"))
+            {
+                this.touchwrap = obj;
+                this.touchwrap.HookMe(this);
+                break;
+            }
+        }
+	}; 
+    
+    instanceProto.OnTouchStart = function ()
+    {
+        this.is_on_moving = true;
+        this.pre_x = this.get_touch_x();
+        this.pre_y = this.get_touch_y(); 
+    };
+    
+    instanceProto.OnTouchEnd = function ()
+    {
+        this.is_on_moving = false;
+        var i;
+        for (i=0; i<4; i++)
+        {
+            if (this.keyMap[i])
+            {
+                this.runtime.trigger(this.release_handlers[i], this);
+                this.keyMap[i] = false;
+            }
+        }
+    };
+    
+	instanceProto.get_touch_x = function ()
+	{
+        return this.touchwrap.GetAbsoluteX();
+	};  
+
+	instanceProto.get_touch_y = function ()
+	{
+        return this.touchwrap.GetAbsoluteY();
+	};
+    
+    instanceProto.tick = function()
+    {
+        this._setup();
+        this._touch_arrow();
+    };
+
+	instanceProto._setup = function ()
+	{
+        if (!this.setup_stage)
+            return;
+        
+        this.TouchWrapGet();  
+        this.setup_stage = false;
+        if (this.touchwrap == null)
+            assert("Arrow key object need at least one input signal.");
+	};
+    
+    var RIGHTKEY = 0x1;
+    var DOWNKEY = 0x2;
+    var LEFTKEY = 0x4;
+    var UPKEY = 0x8;
+	instanceProto._touch_arrow = function ()
+	{
+        if ((this.touchwrap == null) || (!this.is_on_moving))
+            return;
+        
+        var cur_x = this.get_touch_x();
+        var cur_y = this.get_touch_y();
+        var dx = cur_x - this.pre_x;
+        var dy = cur_y - this.pre_y;
+        if ( (dx!=0) || (dy!=0) )
+        {
+            var angle = cr.to_clamped_degrees(Math.atan2(dy,dx));
+            switch (this._directions)
+            {
+            case 0:
+                var key_id = (angle <180)? DOWNKEY:UPKEY;
+                this._keydown(key_id);
+                break;
+            case 1:
+                var key_id = ((angle >90) && (angle <=270))? LEFTKEY:RIGHTKEY;
+                this._keydown(key_id);
+                break;  
+            case 2:
+                var key_id = ((angle>45) && (angle<=135))?  DOWNKEY:
+                             ((angle>135) && (angle<=225))? LEFTKEY:
+                             ((angle>225) && (angle<=315))? UPKEY:RIGHTKEY;                          
+                this._keydown(key_id);
+                break;
+            case 3:
+                var key_id = ((angle>22.5) && (angle<=67.5))?   (DOWNKEY | RIGHTKEY):
+                             ((angle>67.5) && (angle<=112.5))?  DOWNKEY:
+                             ((angle>112.5) && (angle<=157.5))? (DOWNKEY | LEFTKEY):
+                             ((angle>157.5) && (angle<=202.5))? LEFTKEY:
+                             ((angle>202.5) && (angle<=247.5))? (LEFTKEY | UPKEY):
+                             ((angle>247.5) && (angle<=292.5))? UPKEY:
+                             ((angle>292.5) && (angle<=337.5))? (UPKEY | RIGHTKEY): RIGHTKEY;                          
+                this._keydown(key_id);
+                break;                
+            }         
+            this.pre_x = cur_x;
+            this.pre_y = cur_y;                    
+        }
+	}; 
+    
+    var _keyid_list = [RIGHTKEY,DOWNKEY,LEFTKEY,UPKEY];       
+    instanceProto._keydown = function(key_id)
+    {
+        var i;
+        for (i=0; i<4; i++)
+        {
+            var is_key_down = ((_keyid_list[i] & key_id) != 0);            
+            if (this.keyMap[i] && (!is_key_down))
+                this.runtime.trigger(this.release_handlers[i], this);
+        }
+        var pressed_any_key = false;
+        for (i=0; i<4; i++)
+        {
+            var is_key_down = ((_keyid_list[i] & key_id) != 0); 
+            if ((!this.keyMap[i]) && is_key_down)
+            {
+                this.runtime.trigger(this.press_handlers[i], this);
+                pressed_any_key = true;
+            }
+            this.keyMap[i] = is_key_down;
+        }         
+        if (pressed_any_key)
+            this.runtime.trigger(cr.plugins_.Rex_ArrowKey.prototype.cnds.OnAnyKey, this);
+    };
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+
+	cnds.IsUPDown = function()
+	{
+        return this.keyMap[3];
+	};
+	cnds.IsDOWNDown = function()
+	{
+        return this.keyMap[1];    
+	};	
+	cnds.IsLEFTDown = function()
+	{
+        return this.keyMap[2];
+	};
+	cnds.IsRIGHDown = function()
+	{
+        return this.keyMap[0];
+	};    
+    
+	cnds.OnUPKey = function()
+	{
+        return true;
+	};
+	cnds.OnDOWNKey = function()
+	{
+        return true;    
+	};    
+	cnds.OnLEFTKey = function()
+	{
+        return true;    
+	};
+	cnds.OnRIGHTKey = function()
+	{
+        return true;    
+	};      
+	
+	cnds.OnAnyKey = function()
+	{
+        return true;    
+	};
+	
+	cnds.OnUPKeyReleased = function()
+	{
+        return true;
+	};
+	cnds.OnDOWNKeyReleased = function()
+	{
+        return true;    
+	};  
+	cnds.OnLEFTKeyReleased = function()
+	{
+        return true;    
+	};
+	cnds.OnRIGHTKeyReleased = function()
+	{
+        return true;    
+	};   
+    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+}());
