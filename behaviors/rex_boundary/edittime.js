a440012d5368e1dd70c21cc15908ce93673d3fc3
hua <binhua.tein@gmail.com>
2012-05-23 23:14:05 +0800

    Add boundary behavior

---
+++ behaviors/rex_boundary/edittime.js
@@ -0,0 +1,97 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Boundary",
+		"id":			"Rex_boundary",
+		"description":	"Limit position of object in the boundary.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"General",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Horizontal", "Enable the horizontal boundary.",1);
+AddAction(3, 0, "Horizontal boundary enable", "Enable boundary", 
+          "Set {my} horizontal boundary enable to <i>{0}</i>", "Enable the object's horizontal boundary.", "EnableHorizontal");
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Vertical", "Enable the vertical boundary.",1);
+AddAction(4, 0, "Vertical boundary enable", "Enable boundary", 
+          "Set {my} vertical boundary enable to <i>{0}</i>", "Enable the object's vertical boundary.", "EnableVertical");		  
+AddNumberParam("Left", "Left boundary.");
+AddNumberParam("Right", "Right boundary.");
+AddAction(5, 0, "Set horizontal boundary", "Set boundary", 
+          "Set {my} horizontal boundary to [<i>{0}</i>, <i>{1}</i>]", "Set the object's horizontal boundary.", "SetHorizontalBoundary");
+AddNumberParam("Up", "Up boundary.");
+AddNumberParam("Down", "Down boundary.");
+AddAction(5, 0, "Set vertical boundary", "Set boundary", 
+          "Set {my} vertical boundary to [<i>{0}</i>, <i>{1}</i>]", "Set the object's vertical boundary.", "SetVerticalBoundary");
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [        
+    new cr.Property(ept_combo, "Horizontal", "No", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),	
+    new cr.Property(ept_float, "Left", 0, "Left boundary."),	
+	new cr.Property(ept_float, "Right", 0, "Right boundary."),	
+	new cr.Property(ept_combo, "Vertical", "No", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+	new cr.Property(ept_float, "Up", 0, "Up boundary."),	
+	new cr.Property(ept_float, "Down", 0, "Down boundary."),	
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Pixels per step"] < 1)
+		this.properties["Pixels per step"] = 1;
+}
---
+++ behaviors/rex_boundary/runtime.js
@@ -0,0 +1,130 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_boundary = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_boundary.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	var _sort_fn = function(a,b)
+	{   
+	    return (a>b)? 1:
+               (a<b)? (-1):0;    
+	};    	
+	behinstProto.onCreate = function()
+	{    
+        this.horizontal_enable = (this.properties[0]==1);
+        this.horizontal_boundary = [this.properties[1], this.properties[2]];
+        this.vertical_enable = (this.properties[3]==1);
+        this.vertical_boundary = [this.properties[4], this.properties[5]];	
+		this.horizontal_boundary.sort(_sort_fn);
+		this.vertical_boundary.sort(_sort_fn);		
+	};
+	
+	behinstProto.tick = function ()
+	{
+	    var has_updated = false;
+	    has_updated |= this.horizontal_boundary_check();
+	    has_updated |= this.vertical_boundary_check();		
+		if (has_updated)
+		    this.inst.set_bbox_changed();  
+	};
+	
+	behinstProto.horizontal_boundary_check = function ()
+	{
+	    if (!this.horizontal_enable)
+		    return false;
+		var curr_x = this.inst.x;
+		if (this.inst.x < this.horizontal_boundary[0])
+		    this.inst.x = this.horizontal_boundary[0];
+        else if (this.inst.x > this.horizontal_boundary[1])
+		    this.inst.x = this.horizontal_boundary[1];
+	    return (curr_x != this.inst.x);
+	};
+	
+	behinstProto.vertical_boundary_check = function ()
+	{
+	    if (!this.vertical_enable)
+		    return false;
+	    var curr_y = this.inst.y;
+		if (this.inst.y < this.vertical_boundary[0])
+		    this.inst.y = this.vertical_boundary[0];
+        else if (this.inst.y > this.vertical_boundary[1])
+		    this.inst.y = this.vertical_boundary[1];
+	    return (curr_y != this.inst.y);			
+	};
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+	acts.EnableHorizontal = function (s)
+	{
+		this.horizontal_enable = (s==1);
+	};  
+
+	acts.EnableVertical = function (s)
+	{
+		this.vertical_enable = (s==1);
+	};
+
+	acts.SetHorizontalBoundary = function (l, r)
+	{
+		this.horizontal_boundary[0] = l;
+		this.horizontal_boundary[1] = r;
+		this.horizontal_boundary.sort(_sort_fn);
+	};
+
+	acts.SetVerticalBoundary = function (u, d)
+	{
+		this.vertical_boundary[0] = l;
+		this.vertical_boundary[1] = r;
+		this.vertical_boundary.sort(_sort_fn);
+	};
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+}());
