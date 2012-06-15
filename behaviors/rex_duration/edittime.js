168c3c5ec0f63c13c5bb648eae228814eec567fb
hua <binhua.tein@gmail.com>
2012-06-15 22:02:07 +0800

    Add duration behavior

---
+++ behaviors/rex_duration/edittime.js
@@ -0,0 +1,129 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Duration",
+		"id":			"Rex_Duration",
+		"version":		"1.0",          
+		"description":	"Timer, to execute function at time-out",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Timer",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddStringParam("Duration", "Duration name", '""');
+AddCondition(0, 0, "Is duration running", "Durations", "Is {my} <i>{0}</i> running", "", "IsRunning");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddObjectParam("Timeline", "Timeline object for getting timer");
+AddObjectParam("Function", "Function object for callback");
+AddAction(0, 0, "Setup", "Setup", 
+          "Get {my} timer from <i>{0}</i>, callback to <i>{1}</i>", 
+          "Setup.", "Setup");          
+AddStringParam("Duration", "Duration name", '""');
+AddNumberParam("Duration", "Duration time", 1);
+AddNumberParam("Interval", "Interval time", 0.1);
+AddStringParam("On start", "Callback at duration starting", '""');
+AddStringParam("On interval", 'Callback for each interval', '""');
+AddStringParam("On end", "Callback at duration finished", '""');
+AddAction(2, 0, "Start duration", "Control", 
+          "Start {my} <i>{0}</i> with duration to <i>{1}</i> second for each <i>{2}</i> second. Callback of 'On start' to <i>{3}</i>, 'On interval' to <i>{4}</i>, and 'On end' to <i>{5}</i>", 
+          "Start duration.", "Start");
+AddStringParam("Duration", "Duration name", '""');      
+AddAction(3, 0, "Pause duration", "Control", 
+          "Pause {my} <i>{0}</i>", 
+          "Pause duration.", "Pause"); 
+AddStringParam("Duration", "Duration name", '""');          
+AddAction(4, 0, "Resume duration", "Control", 
+          "Resume {my} <i>{0}</i>", 
+          "Resume duration.", "Resume"); 
+AddStringParam("Duration", "Duration name", '""');          
+AddAction(5, 0, "Stop duration", "Control", 
+          "Stop {my} <i>{0}</i>", 
+          "Stop duration.", "Stop");
+AddAction(6, 0, "Pause all durations", "Control", 
+          "Pause {my} all durations", 
+          "Pause all durations.", "PauseAll");                    
+AddAction(7, 0, "Resume all durations", "Control", 
+          "Resume {my} all durations", 
+          "Resume all durations.", "ResumeAll"); 
+AddAction(8, 0, "Stop all durations", "Control", 
+          "Stop {my} all durations", 
+          "Stop all durations.", "StopAll");          
+
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number | ef_variadic_parameters, "Get remainder duration", 
+              "Duration", "Remainder", 
+              "Get remainder duration. Add second parameter to specify duration.");
+AddExpression(1, ef_return_number | ef_variadic_parameters, "Get elapsed time of duration", 
+              "Duration", "Elapsed", 
+              "Get elapsed time of duration. Add second parameter to specify duration.");              
+AddExpression(2, ef_return_number | ef_variadic_parameters, "Get remainder time percentage of duration", 
+              "Percentage", "RemainderPercent", 
+              "Get remainder time percentage of duration. Add second parameter to specify duration.");
+AddExpression(3, ef_return_number | ef_variadic_parameters, "Get elapsed time percentage of duration", 
+              "Percentage", "ElapsedPercent", 
+              "Get elapsed time percentage of duration. Add second parameter to specify duration.");  
+AddExpression(4, ef_return_number | ef_variadic_parameters, "Get interval time", 
+              "Duration", "Interval", 
+              "Get interval time. Add second parameter to specify duration.");
+AddExpression(5, ef_return_number | ef_variadic_parameters, "Get duration time", 
+              "Duration", "Duration", 
+              "Get duration time. Add second parameter to specify duration.");              
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_duration/runtime.js
@@ -0,0 +1,286 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_Duration = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_Duration.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+        this.timeline = null;    
+        this.callback = null;        
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.timers = {};
+        this._trigger_timer = null;        
+	};
+    
+	behinstProto.onDestroy = function()
+	{
+        var name, timer;
+        for (name in this.timers)
+            this.timers[name].Remove();
+	};    
+    
+	behinstProto.tick = function ()
+	{
+	};
+
+    behinstProto._exec_callback = function(callback)
+    {
+        if (callback == "")
+            return;
+            
+        // setup sol
+        var sol = this.type.objtype.getCurrentSol();
+        sol.select_all = false;
+	    sol.instances.length = 1;        
+        sol.instances[0] = this.inst;
+        // call function object
+        this.type.callback.ExecuteCommands(callback); 
+    };
+        
+    behinstProto._timer_handle = function(duration_name)
+    {
+        var timer = this.timers[duration_name];
+        this._trigger_timer = timer;
+        var duration_remain = timer.__duration_duration_remain;
+        var interval = timer.__duration_interval;
+        if (duration_remain <= interval)
+        {
+            timer.__duration_duration_remain = 0;
+            if (duration_remain == interval)
+                this._exec_callback(timer.__duration_cb_on_interval);
+            this._exec_callback(timer.__duration_cb_on_end); 
+            timer.__duration_is_alive = false;
+        }
+        else
+        {
+            duration_remain -= interval;         
+            timer.__duration_duration_remain = duration_remain;        
+            this._exec_callback(timer.__duration_cb_on_interval);     
+            timer.Start(Math.min(duration_remain, interval));  
+        }
+    };
+
+	//////////////////////////////////////
+	// Conditions
+	behaviorProto.cnds = {};
+	var cnds = behaviorProto.cnds;
+    
+	cnds.IsRunning = function (name)
+	{  
+        var timer = this.timers[duration_name];        
+        return ((timer != null) && timer.__duration_is_alive);
+	};
+
+	//////////////////////////////////////
+	// Actions
+	behaviorProto.acts = {};
+	var acts = behaviorProto.acts;
+
+    acts.Setup = function (timeline_objs, fn_objs)
+	{
+        var timeline = timeline_objs.instances[0];
+        if (timeline.check_name == "TIMELINE")
+            this.type.timeline = timeline;        
+        else
+            alert ("Duration behavior should connect to a timeline object");          
+        
+        var callback = fn_objs.instances[0];
+        if (callback.check_name == "FUNCTION")
+            this.type.callback = callback;        
+        else
+            alert ("Duration behavior should connect to a function object");
+	};      
+    
+
+    acts.Start = function (duration_name, duration_time, interval_time,
+                           cb_on_start, cb_on_interval, cb_on_end)
+	{
+        var args = [duration_name];
+        var timer = this.timers[duration_name];
+        if (timer != null)     
+            timer.Remove(); 
+        else
+        {
+            timer = this.type.timeline.CreateTimer(this, this._timer_handle);          
+            this.timers[duration_name] = timer;
+        }
+        timer.SetCallbackArgs(args);
+        timer.__duration_duration = duration_time;
+        timer.__duration_interval = interval_time;
+        timer.__duration_duration_remain = duration_time;
+        timer.__duration_cb_on_interval = cb_on_interval;
+        timer.__duration_cb_on_end = cb_on_end;
+        timer.__duration_is_alive = true;
+        timer.Start(interval_time);
+        this._trigger_timer = timer;
+        this._exec_callback(cb_on_start);        
+	};
+
+    acts.Pause = function ()
+	{
+        var timer = this.timers[duration_name];
+        if (timer != null)
+            timer.Suspend();
+	};   
+
+    acts.Resume = function ()
+	{
+        var timer = this.timers[duration_name];
+        if (timer != null)
+            this.timer.Resume();
+	};       
+    
+    acts.Stop = function ()
+	{
+        var timer = this.timers[duration_name];
+        if (timer != null)
+            this.timer.Remove();
+	};
+
+    acts.PauseAll = function ()
+	{
+        var name, timer;
+        for (name in this.timers)
+        {
+            timer = this.timers[name];
+            if (timer.IsActive());
+                timer.Suspend();
+        }
+	};   
+
+    acts.ResumeAll = function ()
+	{
+        var name, timer;
+        for (name in this.timers)
+        {
+            timer = this.timers[name];
+            if (timer.IsActive());
+                timer.Resume();
+        }
+	};       
+    
+    acts.StopAll = function ()
+	{
+        var name, timer;
+        for (name in this.timers)
+        {
+            timer = this.timers[name];
+            if (timer.IsActive());
+                timer.Remove();
+        }
+	};
+	//////////////////////////////////////
+	// Expressions
+	behaviorProto.exps = {};
+	var exps = behaviorProto.exps;
+
+    exps.Remainder = function (ret, duration_name)
+	{
+	    var timer = (duration_name != null)? 
+	                this.timers[duration_name]:this._trigger_timer;
+	    var val;
+	    if ((timer == null) || (!timer.__duration_is_alive))
+	        val = 0;
+	    else
+	        val = timer.__duration_duration_remain;
+	    ret.set_float(val);
+	};
+    
+	exps.Elapsed = function (ret, duration_name)
+	{
+	    var timer = (duration_name != null)? 
+	                this.timers[duration_name]:this._trigger_timer;
+	    var val;
+	    if ((timer == null) || (!timer.__duration_is_alive))
+	        val = 0;
+	    else
+	        val = timer.__duration_duration - timer.__duration_duration_remain;   
+	    ret.set_float(val);
+	};  
+
+    exps.RemainderPercent = function (ret, duration_name)
+	{
+	    var timer = (duration_name != null)? 
+	                this.timers[duration_name]:this._trigger_timer;
+	    var val;
+	    if ((timer == null) || (!timer.__duration_is_alive))
+	        val = 0;
+	    else
+	        val = timer.__duration_duration_remain/timer.__duration_duration;   
+	    ret.set_float(val);
+	};
+    
+	exps.ElapsedPercent = function (ret, duration_name)
+	{
+	    var timer = (duration_name != null)? 
+	                this.timers[duration_name]:this._trigger_timer;
+	    var val;
+	    if ((timer == null) || (!timer.__duration_is_alive))
+	        val = 0;
+	    else
+	        val = (timer.__duration_duration - timer.__duration_duration_remain)/timer.__duration_duration;    
+	    ret.set_float(val);
+	};  
+
+    exps.Interval = function (ret, duration_name)
+	{
+	    var timer = (duration_name != null)? 
+	                this.timers[duration_name]:this._trigger_timer;
+	    var val;
+	    if ((timer == null) || (!timer.__duration_is_alive))
+	        val = 0;
+	    else
+	        val = timer.__duration_interval;
+	    ret.set_float(val);
+	};
+    
+	exps.Duration = function (ret, duration_name)
+	{
+	    var timer = (duration_name != null)? 
+	                this.timers[duration_name]:this._trigger_timer;
+	    var val;
+	    if ((timer == null) || (!timer.__duration_is_alive))
+	        val = 0;
+	    else
+	        val = timer.__duration_duration;   
+	    ret.set_float(val);
+	}; 
+}());
