66d6b0832c09651f42686e185d13c7046a7be4b7
hua <binhua.tein@gmail.com>
2012-06-23 22:38:03 +0800

    Add fuzzy plugin

---
+++ plugins/rex_fuzzy/edittime.js
@@ -0,0 +1,133 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Fuzzy",
+		"id":			"Rex_Fuzzy",
+		"version":		"0.1",   		
+		"description":	"Fuzzy logic",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"AI",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+                     
+//////////////////////////////////////////////////////////////
+// Actions     
+AddStringParam("Name", "Input variable name.", '""');
+AddStringParam("Negative Big", 'Range setting of Negative Big. Empty string is ignored.', '"0, 17"');
+AddStringParam("Negative Medium", 'Range setting of Negative Medium. Empty string is ignored.', '"0, 17, 33"');
+AddStringParam("Negative Small", 'Range setting of Negative Small. Empty string is ignored.', '"17, 33, 50"');
+AddStringParam("Zero", 'Range setting of Zero. Empty string is ignored.', '"33, 50, 66"');
+AddStringParam("Positive Small", 'Range setting of Positive Small. Empty string is ignored.', '"50, 66, 83"');
+AddStringParam("Positive Medium", 'Range setting of Positive Medium. Empty string is ignored.', '"66, 83, 100"');
+AddStringParam("Positive Big", 'Range setting of Positive Big. Empty string is ignored.', '"83, 100"');
+AddAction(1, 0, "Define membership", "Membership", 
+          "Define membership <i>{0}</i>: NB to <i>{1}</i>, NM to <i>{2}</i>, NS to <i>{3}</i>, ZO to <i>{4}</i>, PS to <i>{5}</i>, PM to <i>{6}</i>, PB to <i>{7}</i>", 
+          "Define membership.", "DefineMembership");
+AddStringParam("Condition", "Condition name.", '""');         
+AddStringParam("Variable", "Variable name.", '""');
+AddComboParamOption("Negative Big");
+AddComboParamOption("Negative Medium");
+AddComboParamOption("Negative Small");
+AddComboParamOption("Zero");
+AddComboParamOption("Positive Small");
+AddComboParamOption("Positive Medium");
+AddComboParamOption("Positive Big");
+AddComboParam("Membership", "Membership of variable", 3);
+AddAction(2, 0, "Add membership condition", "Condition", 
+          "Add condition <i>{0}</i>: <i>{1}</i> is <i>{2}</i>", "Add condition from membership.", "AddMembershipCond"); 
+AddStringParam("Condition", "Condition name.", '""');
+AddStringParam("Condition from", "Condition name.", '""');
+AddAction(3, 0, "Add invert condition", "Condition", 
+          "Add invert condition <i>{0}</i> from condition <i>{1}</i>", "Add invert condition.", "AddInvertCond");            
+AddStringParam("Condition", "Condition name.", '""');
+AddStringParam("Condition A", "Sub-condition name.", '""');
+AddComboParamOption("And");
+AddComboParamOption("Or");
+AddComboParam("Logic", "Logic", 1);
+AddStringParam("Condition B", "Sub-condition name.", '""');
+AddAction(4, 0, "Add combination condition", "Condition", 
+          "Add condition <i>{0}</i> to '<i>{1}</i> <i>{2}</i> <i>{3}</i>'", "Add combination condition.", "AddCombinationCond");    
+AddStringParam("Rule", "Rule name.", '""');
+AddStringParam("Condition", "Condition name.", '""');
+AddStringParam("Variable", "Variable name.", '""');
+AddAction(5, 0, "Add rule", "Rule", 
+          "Add rule <i>{0}</i>: <i>{1}</i> -> <i>{2}</i>", "Add rule.", "AddRule");  
+AddAction(6, 0, "Execute rules", "Rule", 
+          "Execute rules", "Execute rules.", "ExecuteRules");          
+AddStringParam("Name", "Variable name.", '""');
+AddNumberParam("Value", "Variable value.", 0);
+AddAction(10, 0, "Set variable value", "Input", 
+          "Set variable <i>{0}</i> value to <i>{1}</i>", "Set variable value.", "SetVarValue");      
+//////////////////////////////////////////////////////////////
+// Expressions
+AddStringParam("Output", "Output name.", '""');
+AddExpression(1, ef_return_number | ef_variadic_parameters, "Get output grade", "Output", "Grade", "Get output grade.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_fuzzy/runtime.js
@@ -0,0 +1,333 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Fuzzy = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Fuzzy.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this.rule_bank = new cr.plugins_.Rex_Fuzzy.FRuleBank();
+	};
+   
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+	
+    var _get_points = function (list_string)
+    {
+        if (list_string == null)
+            return null;
+        var points = JSON.parse("["+list_string+"]");
+        if (points.length>3)
+            points.length = 3;
+        return points;
+    };    
+	acts.DefineMembership = function (var_name, nb, nm, ns, zo, ps, pm, pb)
+	{     
+		this.rule_bank.add_variable(var_name, _get_points(nb).reverse(), _get_points(nm), _get_points(ns), 
+                                    _get_points(zo), _get_points(ps), _get_points(pm), _get_points(pb));
+	};  
+    
+    var membership_map = ["nb", "nm", "ns", "zo", "ps", "pm", "pb"];
+	acts.AddMembershipCond = function (cond_name, var_name, membership)
+	{
+		this.rule_bank.add_membership_cond(cond_name, var_name, membership_map[membership]);
+	};  
+    
+	acts.AddInvertCond = function (cond_name, cond_from)
+	{
+		this.rule_bank.add_invert_cond(cond_name, cond_from);
+	};        
+    
+    var logic_op_map = ["and", "or"];
+	acts.AddCombinationCond = function (cond_name, cond_A, logic_op, cond_B)
+	{
+		this.rule_bank.add_combination_cond(cond_name, cond_A, logic_op_map[logic_op], cond_B);
+	}; 
+    
+	acts.AddRule = function (rule_name, cond_name, var_name)
+	{
+		this.rule_bank.add_rule(rule_name, cond_name, var_name);
+	};  
+    
+	acts.SetVarValue = function (var_name, value)
+	{
+		this.rule_bank.set_variable_value(var_name, value);
+	};     
+    
+	acts.ExecuteRules = function ()
+	{
+		this.rule_bank.execute();
+	};     
+    
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+	
+	exps.Grade = function (ret, var_name)
+	{
+        var grade_out = this.rule_bank.out_vars[var_name];
+        if (grade_out == null)
+            grade_out = 0;
+		ret.set_float(grade_out);
+	}; 
+}());
+
+(function ()
+{
+    cr.plugins_.Rex_Fuzzy.FRuleBank = function()
+    {
+        this.rules = {};
+        this.conds = {};
+        this.in_vars = {};
+        this.out_vars = {};
+    };
+    var FRuleBankProto = cr.plugins_.Rex_Fuzzy.FRuleBank.prototype;
+    
+    FRuleBankProto.add_variable = function (var_name, nb, nm, ns, zo, ps, pm, pb)
+    {
+        this.in_vars[var_name] = new FMembership(nb, nm, ns, zo, ps, pm, pb);
+    };
+    
+    FRuleBankProto.add_membership_cond = function (cond_name, var_name, membership)
+    {
+        if (this.conds[cond_name] != null)
+            alert("Fuzzy: condition "+ cond_name + " had existed");
+        var var_obj = this.in_vars[var_name];
+        if (var_obj == null)
+            alert("Fuzzy: can not find membership "+ var_name);
+        this.conds[cond_name] = new FCond(membership, var_obj);
+    };
+    
+    FRuleBankProto.add_invert_cond = function (cond_name, cond_from)
+    {
+        if (this.conds[cond_name] != null)
+            alert("Fuzzy: condition "+ cond_name + " had existed");    
+        var cond_obj = this.conds[cond_from];
+        if (cond_obj == null)
+            alert("Fuzzy: can not find condition "+ cond_from);
+        this.conds[cond_name] = new FCond("not", cond_obj );
+    }; 
+    
+    FRuleBankProto.add_combination_cond = function (cond_name, cond_A, logic_op, cond_B)
+    {
+        if (this.conds[cond_name] != null)
+            alert("Fuzzy: condition "+ cond_name + " had existed");        
+        var cond_A_obj = this.conds[cond_A], cond_B_obj = this.conds[cond_B];
+        if (cond_A_obj == null)
+            alert("Fuzzy: can not find condition "+ cond_A);    
+        if (cond_B_obj == null)
+            alert("Fuzzy: can not find condition "+ cond_B);              
+        this.conds[cond_name] = new FCond(logic_op, cond_A_obj, cond_B_obj );
+    }; 
+
+	FRuleBankProto.add_rule = function (rule_name, cond_name, var_name)
+	{
+        if (this.rules[rule_name] != null)
+            alert("Fuzzy: rule "+ rule_name + " had existed");    
+        var cond_obj = this.conds[cond_name];            
+        if (cond_obj == null)
+            alert("Fuzzy: can not find condition "+ cond_name); 
+        if (this.out_vars[var_name] != null)
+            alert("Fuzzy: Output "+ var_name + " had been defined");       
+		this.rules[rule_name] = new FRule(cond_obj, var_name, this.out_vars);
+	}; 
+    
+    FRuleBankProto.set_variable_value = function (var_name, value)
+    {    
+        this.in_vars[var_name].value = value;
+    };    
+    
+    FRuleBankProto.execute = function ()
+    {
+        var name;
+        for (name in this.rules)
+            this.rules[name].execute();
+    }; 
+    
+    var FRule = function(cond, out_var_name, out_vars)
+    {
+        this.cond = cond;
+        this.out_var_name = out_var_name;
+        this.out_vars = out_vars;        
+    };
+    var FRuleProto = FRule.prototype;
+    
+    FRuleProto.execute = function()
+    {
+        this.out_vars[this.out_var_name] = this.cond.get_grade();
+    };
+    
+    var FCond = function(op, opA, opB)
+    {
+        this._get_grade = {"not":this.get_NOT_value,
+                           "and":this.get_AND_value,
+                           "or":this.get_OR_value}[op];
+        if (this._get_grade == null)
+            this._get_grade = this.get_membership_value;
+        this.op = op;
+        this.opA = opA;
+        this.opB = opB;
+    };
+    var FCondProto = FCond.prototype;
+    
+    FCondProto.get_NOT_value = function()
+    {
+        return (1 - this.opA.get_grade());
+    };
+    
+    FCondProto.get_AND_value = function()
+    {
+        return Math.min(this.opA.get_grade(), this.opB.get_grade());
+    };  
+
+    FCondProto.get_OR_value = function()
+    {
+        return Math.max(this.opA.get_grade(), this.opB.get_grade());
+    }; 
+
+    FCondProto.get_membership_value = function()
+    {
+        return this.opA.get_grade(this.op);
+    }; 
+    
+    FCondProto.get_grade = function()
+    {
+        return this._get_grade();
+    }; 
+
+    var FMembership = function(nb, nm, ns, zo, ps, pm, pb)
+    {
+        this._membership_cb = {"nb":new FGrade(nb),
+                               "nm":new FGrade(nm),
+                               "ns":new FGrade(ns),
+                               "zo":new FGrade(zo),
+                               "ps":new FGrade(ps),
+                               "pm":new FGrade(pm),
+                               "pb":new FGrade(pb), };
+        this.value = 0;        
+    };
+    var FMembershipProto = FMembership.prototype;    
+    
+    FMembershipProto.get_grade = function(membership)
+    {
+        return this._membership_cb[membership].get_grade(this.value);
+    }; 
+    
+    var FGrade = function(points)
+    {
+        if (points == null)
+        {
+            this._get_grade = null;
+            this.points = null;
+            return;
+        }
+        this._get_grade = {2:this.get_2p_grade,
+                           3:this.get_3p_grade,
+                           4:this.get_4p_grade}[points.length];
+        this.points = points;
+    };
+    var FGradeProto = FGrade.prototype;    
+    
+    FGradeProto.get_4p_grade = function(x)
+    {
+        var p0=this.points[0],  p1=this.points[1], p2=this.points[2], p3=this.points[3];
+        
+        if ((x>=p1) && (x<=p2))
+            return 1;
+        else if ((x<=p0) || (x>=p3))
+            return 0;
+        else if (x<p1)
+            return (x-p0)/(p1-p0);
+        else // if (x>p2)
+            return (x-p2)/(p3-p2);
+    }; 
+    
+    FGradeProto.get_3p_grade = function(x)
+    {
+        var p0=this.points[0], p1=this.points[1], p2=this.points[2];
+        
+        if (x==p1)
+            return 1;
+        else if ((x<p0) || (x>p2))
+            return 0;
+        else if (x<p1)
+            return (x-p0)/(p1-p0);
+        else // if (x>p2)
+            return (x-p2)/(p3-p2);    
+    }; 
+    
+    FGradeProto.get_2p_grade = function(x)
+    {
+        var p0=this.points[0], p1=this.points[1];
+        var is_inorder = (p0 < p1);
+        
+        if (p0 < p1)
+        {
+            if (x<=p0)
+                return 0;
+            else if (x>=p1)
+                return 1;
+            else
+                return (x-p0)/(p1-p0);
+               
+        }
+        else
+        {
+            if (x<=p1)
+                return 0;
+            else if (x>=p0)
+                return 1;    
+            else
+                return (x-p1)/(p0-p1);        
+        }
+    }; 
+    
+    FGradeProto.get_grade = function(x)
+    {
+        return this._get_grade(x);
+    }; 
+    
+}());   
