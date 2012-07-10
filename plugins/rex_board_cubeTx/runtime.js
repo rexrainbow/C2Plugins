0c9e94af11d2f0d48ec6a150e8795a3d0370edf5
hua <binhua.tein@gmail.com>
2012-07-11 07:45:01 +0800

    Add cubeTx plugin

---
+++ plugins/rex_board_cubeTx/runtime.js
@@ -0,0 +1,123 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_SLGCubeTx = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_SLGCubeTx.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{        
+        this.check_name = "LAYOUT";
+        this.is_isometric = (this.properties[0]==1);
+        this.PositionOX = this.properties[1];
+        this.PositionOY = this.properties[2];
+        this.SetWidth(this.properties[3]);
+        this.SetHeight(this.properties[4]);
+        this.deep = this.properties[5];
+	};
+   
+	instanceProto.SetWidth = function(width)
+	{
+        this.width = width;
+        this.half_width = width/2;        
+	}; 
+	instanceProto.SetHeight = function(height)
+	{
+        this.height = height;
+        this.half_height = height/2;        
+	};     	
+	instanceProto.GetX = function(logic_x, logic_y, logic_z)
+	{
+        var x = (this.is_isometric)? ((logic_x - logic_y)*this.half_width):
+                                     (logic_x*this.width);
+        return x+this.PositionOX;
+	};
+	instanceProto.GetY = function(logic_x, logic_y, logic_z)
+	{
+        var y = (this.is_isometric)? ((logic_x + logic_y)*this.half_height):
+                                     (logic_y*this.height);
+        y -= (logic_z*this.deep);
+        return y+this.PositionOY;
+	};   
+	instanceProto.CreateItem = function(obj_type,x,y,z,layer)
+	{
+        return this.runtime.createInstance(obj_type, layer,this.GetX(x,y,z),this.GetY(x,y,z) );        
+	};
+	instanceProto.GetNeighborLX = function(x, y, dir)
+	{
+        var dx = (dir==0)? 1:
+		         (dir==2)? (-1):
+				          0;
+		return (x+dx);
+	};
+	instanceProto.GetNeighborLY = function(x, y, dir)
+	{
+        var dy = (dir==1)? 1:
+		        (dir==3)? (-1):
+				          0;       
+        return (y+dy);
+	};
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+    
+    acts.SetOrientation = function (orientation)
+    {        
+        this.is_isometric = (orientation == 1);
+	};
+    acts.SetCellSize = function (width, height)
+    {        
+        this.SetWidth(width);
+        this.SetHeight(height);
+	};
+    acts.SetOffset = function (x, y)
+    {        
+        this.PositionOX = x;
+        this.PositionOY = y;
+	};    
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+}());
