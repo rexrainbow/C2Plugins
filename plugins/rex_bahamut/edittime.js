9e94d0f50d948386e64eae4c22e17a2a0620c69b
hua <binhua.tein@gmail.com>
2012-07-13 23:05:55 +0800

    Add bahamut plugin

---
+++ plugins/rex_bahamut/edittime.js
@@ -0,0 +1,109 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Bahamut",
+		"id":			"Rex_Bahamut",
+		"version":		"0.1",   		
+		"description":	"Get user data from Bahamut website.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Web",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal,
+		"dependency":	"jquery.xdomainajax.js"
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1,cf_trigger,"On user data received","User data","On user data received",
+             "Triggered when receives user's data.","OnGetUserData");
+AddCondition(2,cf_trigger,"On user data received failed","User data","On user data received failed",
+             "Triggered when receives user's data failed.","OnGetUserDataFailed");
+
+                          
+//////////////////////////////////////////////////////////////
+// Actions     
+AddStringParam("Name","User name.","");
+AddAction(1,0,"Get user data","User data",
+          "Get user data <b>{0}</b>",
+          "Get user data.","GetUserData");
+AddAction(2,0,"Clean all users data","Clean",
+          "Clean all users data",
+          "Clean all users data.","CleanUserData");          
+                    
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1,ef_return_string,"Get user name","User data","Name","Get user name.");
+AddExpression(2,ef_return_number,"Get user's STR","User data","STR","Get user's STR.");
+AddExpression(3,ef_return_number,"Get user's DEX","User data","DEX","Get user's DEX.");
+AddExpression(4,ef_return_number,"Get user's INT","User data","INT","Get user's INT.");
+AddExpression(5,ef_return_number,"Get user's LUK","User data","LUK","Get user's LUK.");
+AddExpression(6,ef_return_number,"Get user's VIT","User data","VIT","Get user's VIT.");
+AddExpression(7,ef_return_number,"Get user's AGI","User data","AGI","Get user's AGI.");
+AddExpression(8,ef_return_number,"Get user's MND","User data","MND","Get user's MND.");
+AddExpression(9,ef_return_string,"Get url of user image","User data","ImageURL","Get url of user image.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_bahamut/runtime.js
@@ -0,0 +1,202 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+// load jquery.xdomainajax.js
+document.write('<script src="jquery.xdomainajax.js"></script>');
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Bahamut = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Bahamut.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this._user_data = {};
+	    this._current_user_name = "";
+	};
+		
+    var _is_vaild_html = function(content)
+    {
+        return content.indexOf("查詢失敗") == (-1);
+    };
+    
+	var _get_user_property = function(content, property_name)
+	{	    
+	    property_name += "：";
+	    var key = "<p>"+property_name;
+	    var start_index = content.indexOf(key);
+	    if (start_index == (-1))
+	    {
+	        key = "<li>"+property_name;
+	        start_index = content.indexOf(key);
+	    }
+	    if (start_index == (-1))
+	        return null;
+	    start_index += key.length;
+	    var end_index = content.indexOf("<", start_index);
+	    var value_string = content.substring(start_index, end_index);
+	    return parseInt(value_string);
+	};
+   
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;    
+
+	cnds.OnGetUserData = function()
+	{    
+		return true;
+	};
+	cnds.OnGetUserDataFailed = function()
+	{
+		return true;
+	};    
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+	acts.GetUserData = function(user_name)
+	{       
+	    this._current_user_name = user_name;	    
+	    var inst = this;
+        jQuery.ajax({
+            "url": "http://home.gamer.com.tw/homeindex.php?owner="+user_name,
+            "type": 'GET',
+            "success": function(res) {
+                var content = res.responseText;
+                content = content.replace(/\r\n/g, "\n");
+                var usr_name = user_name;
+                inst._current_user_name = user_name;
+                if (_is_vaild_html(content))
+                {
+                    if (inst._user_data[usr_name] == null)
+                        inst._user_data[usr_name] = {}; 
+                    var properties = inst._user_data[usr_name];         
+                    properties.STR = _get_user_property(content, "STR");
+                    properties.DEX = _get_user_property(content, "DEX");
+                    properties.INT = _get_user_property(content, "INT");
+                    properties.LUK = _get_user_property(content, "LUK");
+                    properties.VIT = _get_user_property(content, "VIT");
+                    properties.AGI = _get_user_property(content, "AGI");
+                    properties.MND = _get_user_property(content, "MND"); 
+                    inst.runtime.trigger(cr.plugins_.Rex_Bahamut.prototype.cnds.OnGetUserData, inst);                                                     
+                }
+                else
+                    inst.runtime.trigger(cr.plugins_.Rex_Bahamut.prototype.cnds.OnGetUserDataFailed, inst);
+            },
+            "error": function()
+			{
+				inst._current_user_name = user_name;
+				inst.runtime.trigger(cr.plugins_.Rex_Bahamut.prototype.cnds.OnGetUserDataFailed, inst);
+			},
+        });        
+	};  
+	
+	acts.CleanUserData = function()
+	{
+	    var name;
+	    for (name in this._user_data)
+	        delete this._user_data[name];
+	};
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+	
+	exps.Name = function(ret)
+	{   
+		ret.set_string(this._current_user_name);         
+	}; 
+		
+	exps.STR = function(ret, user_name)
+	{   
+	    if (user_name == null)
+	       user_name = this._current_user_name;
+		ret.set_int(this._user_data[user_name].STR);         
+	}; 
+
+	exps.DEX = function(ret, user_name)
+	{   
+	    if (user_name == null)
+	       user_name = this._current_user_name;
+		ret.set_int(this._user_data[user_name].DEX);           
+	}; 	
+	
+	exps.INT = function(ret, user_name)
+	{   
+	    if (user_name == null)
+	       user_name = this._current_user_name;
+		ret.set_int(this._user_data[user_name].INT);         
+	}; 
+
+	exps.LUK = function(ret, user_name)
+	{   
+	    if (user_name == null)
+	       user_name = this._current_user_name;
+		ret.set_int(this._user_data[user_name].LUK);      
+	}; 	
+	
+	exps.VIT = function(ret, user_name)
+	{   
+	    if (user_name == null)
+	       user_name = this._current_user_name;
+		ret.set_int(this._user_data[user_name].VIT);           
+	}; 	
+	
+	exps.AGI = function(ret, user_name)
+	{   
+	    if (user_name == null)
+	       user_name = this._current_user_name;
+		ret.set_int(this._user_data[user_name].AGI);           
+	}; 
+
+	exps.MND = function(ret, user_name)
+	{   
+	    if (user_name == null)
+	       user_name = this._current_user_name;
+		ret.set_int(this._user_data[user_name].MND);          
+	};
+
+	exps.ImageURL = function(ret, user_name)
+	{   
+	    if (user_name == null)
+	       user_name = this._current_user_name;	    
+	    var img_url = "http://avatar2.bahamut.com.tw/avataruserpic/"+
+	                  user_name.charAt(0) +"/"+ user_name.charAt(1)+ "/"+ user_name + "/"+user_name+".png";
+		ret.set_string(img_url);          
+	}; 		
+}());
