92b20856f38700894ee59c853e269ea04e6787ad
hua <binhua.tein@gmail.com>
2012-07-14 21:38:49 +0800

    Add wait event plugin

---
+++ plugins/rex_waitevent/edittime.js
@@ -0,0 +1,105 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Wait Event",
+		"id":			"Rex_WaitEvent",
+		"version":		"0.1",   		
+		"description":	"Wait events finished, used with ajax",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Control flow",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddStringParam("Tag","Event tag.","");
+AddCondition(1,cf_trigger,"On all events finished","Finsihed",
+             "On all events with tag <b>{0}</b> has finished",
+             "Triggered when all events with tag has finished.","OnAllEventsFinished");
+AddStringParam("Tag","Event tag.","");
+AddCondition(2,cf_trigger,"On any event finished","Finsihed",
+             "On any event with tag <b>{0}</b> has finished",
+             "Triggered when any event with tag has finished.","OnAnyEventFinished");
+			 
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("Event name","Event name.","");     
+AddStringParam("Tag","Event tag.","");
+AddAction(1,0,"Wait event","Wait",
+          "Wait event <b>{0}</b> with tag <b>{1}</b>",
+          "Wait event.","WaitEvent");
+AddStringParam("Event name","Event name.","");     
+AddStringParam("Tag","Event tag.","");
+AddAction(2,0,"Event has finished","Finished",
+          "Event <b>{0}</b> with tag <b>{1}</b> has finished",
+          "Event has finished.","EventFinished");		  
+		  
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1,ef_return_string,"Get current finished event name","Event name","CurEventName","Get current finished event name.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_waitevent/runtime.js
@@ -0,0 +1,108 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_WaitEvent = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_WaitEvent.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this.events = {};
+		this._current_finished_event_name = "";
+	};   
+	
+	var is_hash_empty = function(hash_obj)
+	{
+	    var k;
+		var is_empty = true;
+		for (k in hash_obj)
+		{
+		    is_empty = false;
+			break;
+		}
+		return is_empty;
+	};
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();      
+
+	Cnds.prototype.OnAllEventsFinished = function()
+	{    
+		return true;
+	};
+
+	Cnds.prototype.OnAnyEventFinished = function()
+	{    
+		return true;
+	};
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+	Acts.prototype.WaitEvent = function(event_name, tag)
+	{       
+	    if (this.events[tag] == null)
+		    this.events[tag] = {};
+	    this.events[tag][event_name] = true;
+	};  
+
+	Acts.prototype.EventFinished = function(event_name, tag)
+	{       
+	    if ((this.events[tag] == null) || (this.events[tag][event_name] == null))
+		    return;
+	    delete this.events[tag][event_name];
+		this._current_finished_event_name = event_name;
+		this.runtime.trigger(cr.plugins_.Rex_WaitEvent.prototype.cnds.OnAnyEventFinished, this); 
+		if (is_hash_empty(this.events[tag]))
+		{
+		    this.runtime.trigger(cr.plugins_.Rex_WaitEvent.prototype.cnds.OnAllEventsFinished, this); 
+			delete this.events[tag];
+        }
+	};  	
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+	Exps.prototype.CurEventName = function(ret)
+	{   
+		ret.set_string(this._current_finished_event_name);         
+	}; 
+}());
