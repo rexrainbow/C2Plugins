7cf98dd13d76b7f983781bcde7b681b4de5b66e8
hua <binhua.tein@gmail.com>
2012-07-25 21:44:00 +0800

    Add cool down mask behavior

---
+++ behaviors/rex_cdmask/edittime.js
@@ -0,0 +1,90 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"CD Mask",
+		"id":			"Rex_cdmask",
+		"description":	"Cool down mask.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Mask",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+             
+//////////////////////////////////////////////////////////////
+// Actions
+AddObjectParam("Canvas", "Canvas object");
+AddAction(0, 0, "Setup canvas", "Setup", 
+          "{my} get canvas from <i>{0}</i>", 
+          "Setup canvas.", "SetupCanvas");
+AddNumberParam("percentage", "Cool down percentage, 1-0", 0);
+AddAction(1, 0, "Set percentage", "Mask", 
+          "Set cool down percentage to <i>{0}</i>", 
+          "Set cool down percentage.", "SetCoolDownPercentage"); 
+AddStringParam("Color", "Use color name, hex \"#FFA500\", \"rgb(0-255,0-255,0-255)\", \"rgba(0-255,0-255,0-255,0-1)\", \"hsl(0-360,0-100%,0-100%)\", or \"hsla(0-360,0-100%,0-100%,0-1)\" ", "\"black\"");
+AddAction(2, 0, "Set mask color", "Mask", "Set mask color to <i>{0}</i>", "Set mask color.", "SetMaskColor");
+                   
+ 
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [           
+    new cr.Property(ept_text, "Color", "black", 
+                   "Use color name, hex \"#FFA500\", \"rgb(0-255,0-255,0-255)\", \"rgba(0-255,0-255,0-255,0-1)\", \"hsl(0-360,0-100%,0-100%)\", or \"hsla(0-360,0-100%,0-100%,0-1)\" "),	       
+    new cr.Property(ept_combo, "Shape", "Rectangle", "Mask shape.", "Rectangle|Circle"),                                   
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Pixels per step"] < 1)
+		this.properties["Pixels per step"] = 1;
+}
---
+++ behaviors/rex_cdmask/runtime.js
@@ -0,0 +1,160 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_cdmask = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_cdmask.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{ 
+        this.canvas_type = null;   	    
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+    
+	behinstProto.onCreate = function()
+	{        
+	    this.mask_color = this.properties[0];
+	    this.is_circle = (this.properties[1] == 1);
+	    this.canvas_inst = null;
+	};  
+    
+	behinstProto.onDestroy = function()
+	{
+        if (this.canvas_inst != null)
+        {
+            this.runtime.DestroyInstance(this.canvas_inst);
+            this.canvas_inst = null;
+        }
+	};  
+	behinstProto.tick = function ()
+	{
+	};
+	
+	behinstProto._create_canvas = function ()
+	{
+	    if (this.canvas_inst != null)
+	        return;
+	        
+	    var canvas_type = this.type.canvas_type;
+	    var _layer = this.runtime.getLayerByNumber(this.inst.layer.index);
+	    var _x = this.inst.x;
+	    var _y = this.inst.y;	    
+        this.canvas_inst = this.runtime.createInstance(canvas_type,_layer,_x,_y);
+        this.canvas_inst.width = this.inst.width;
+        this.canvas_inst.height = this.inst.height;
+        this.canvas_inst.hotspotX = this.inst.hotspotX;
+        this.canvas_inst.hotspotY = this.inst.hotspotY;
+        this.canvas_inst.ctx.clearRect(0,0,this.canvas_inst.width,this.canvas_inst.height);
+        this.canvas_inst.runtime.redraw = true; 
+	};
+	
+    var start_radians = cr.to_radians(-90);
+	behinstProto._cd_mask = function (percentage)
+	{
+	    if (percentage > 1)
+	        percentage = 1;	      
+	    var inst = this.canvas_inst;
+	    var ctx = inst.ctx;
+	    var width = inst.canvas.width;
+	    var height = inst.canvas.height;
+	    ctx.clearRect(0,0,width,height);
+	    
+	    var center_x = width/2;
+	    var center_y = height/2; 
+	    var radius;
+	    if (this.is_circle)
+	        radius = Math.min(center_x, center_y);
+	    else
+	        radius = Math.max(width, height) *2 ;	    
+	    if (percentage == 1)
+	    {
+	        ctx.fillStyle = this.mask_color;
+	        if (this.is_circle)
+	        {
+		        ctx.beginPath();
+		        ctx.arc(center_x, center_y, radius, 0, cr.to_radians(360), true);
+		        ctx.fill();          
+	        }
+	        else 	       
+	            ctx.fillRect(0,0,width,height);
+	    }	   
+	    else if (percentage > 0)
+	    {
+	        var end_angle = (360*(1-percentage)) -90;
+	        ctx.beginPath();
+	        ctx.moveTo(center_x, center_y);
+	        ctx.lineTo(center_x, center_y - radius);
+	        ctx.arc(center_x, center_y, radius, 
+	                start_radians, cr.to_radians(end_angle), 
+	                true);
+		    ctx.fillStyle = this.mask_color;
+		    ctx.fill();
+	    }
+	    
+	    inst.runtime.redraw = true;    
+	};	
+	
+	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+    Acts.prototype.SetupCanvas = function (canvas_type)
+	{
+	    this.type.canvas_type = canvas_type;   
+	}; 
+
+	Acts.prototype.SetCoolDownPercentage = function(percentage)
+	{
+	    this._create_canvas();
+	    this._cd_mask(percentage);
+	};
+
+	Acts.prototype.SetMaskColor = function(color)
+	{
+	    this.mask_color = color;
+	};  
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+}());
