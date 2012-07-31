aa6736ca9ef96ffc6d65ea4b245cd27033818e93
hua <binhua.tein@gmail.com>
2012-07-31 21:32:14 +0800

    Add push out solid behavior

---
+++ behaviors/rex_pushoutsolid/edittime.js
@@ -0,0 +1,81 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Push Out Solid",
+		"id":			"Rex_pushOutSolid",
+		"description":	"Push out from solid object.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Movements",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "", "Set {my} activated to <i>{0}</i>", 
+         "Enable the object's push out solid behavior.", "SetActivated");
+
+
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [           
+    new cr.Property(ept_combo, "Activated", "Yes", 
+                    "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_pushoutsolid/runtime.js
@@ -0,0 +1,89 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_pushOutSolid = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_pushOutSolid.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{ 
+        this.canvas_type = null;   	    
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+    
+	behinstProto.onCreate = function()
+	{        
+	    this.activated = (this.properties[0] == 1);
+	};  
+    
+	behinstProto.onDestroy = function()
+	{
+	};  
+    
+	behinstProto.tick = function ()
+	{
+	    if (!this.activated)
+	        return;
+	        
+		// Is already overlapping solid: must have moved itself in (e.g. by rotating or being crushed),
+		// so push out
+		var collobj = this.runtime.testOverlapSolid(this.inst);
+		if (collobj)
+		{
+			this.runtime.registerCollision(this.inst, collobj);
+			this.runtime.pushOutSolidNearest(this.inst);
+		}	    
+	};
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	  
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+	
+	Acts.prototype.SetActivated = function (s)
+	{
+		this.activated = (s==1);
+	}; 
+    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+}());
