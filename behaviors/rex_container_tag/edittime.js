3bbb76a382e5a04a657e58d096908cc6dd88826e
hua <binhua.tein@gmail.com>
2012-09-09 16:20:01 +0800

    Add container tag behavior

---
+++ behaviors/rex_container_tag/edittime.js
@@ -0,0 +1,77 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Container tag",
+		"id":			"Rex_container_tag",
+		"description":	"Container tag to add sprite into container at layout editor.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Container",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+	  			 		 
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("Tag", "Container tag.", '""');
+AddAction(3, 0, "Set tag", "Tag", "Set {my} tag to <i>{0}</i>",
+         "Set tag.", "SetTag");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(3, ef_return_string, "Get tag", "Tag", "Tag", "Get tag.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [ 
+    new cr.Property(ept_text, "Tag", "", "Tag for adding sprite into container at layout editor."),	
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{	
+}
---
+++ behaviors/rex_container_tag/runtime.js
@@ -0,0 +1,88 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_container_tag = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_container_tag.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{     
+	};
+    
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;       
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{     
+        this.set_tag(this.properties[0]); 
+	};
+
+	behinstProto.tick = function ()
+	{
+	};  
+
+	behinstProto.set_tag = function (tag)
+	{
+	    this.tag = tag
+	    if (cr.plugins_.Rex_Container == null)
+		    return;
+		var container = cr.plugins_.Rex_Container.tag2container[tag];
+		if (container == null)
+		    return;
+	    container.add_insts([this.inst])
+	};  	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetTag = function (tag)
+	{
+		this.set_tag(tag); 
+	}; 			
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+	
+	Exps.prototype.Tag = function (ret)
+	{
+		ret.set_string(this.tag);
+	};			
+			
+}());
