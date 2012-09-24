779872accd4afac29d226bd03d4c5206e9256d83
hua <binhua.tein@gmail.com>
2012-09-25 06:34:04 +0800

    Add text properties behavior

---
+++ behaviors/rex_text_properties/edittime.js
@@ -0,0 +1,94 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Text properties",
+		"id":			"Rex_text_properties",
+		"description":	"Support actions to change properties of text object.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Text",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("Left");
+AddComboParamOption("Center");
+AddComboParamOption("Right");
+AddComboParam("Horizontal alignment", "Horizontal alignment of the text", 0);          
+AddAction(1, 0, "Set horizontal alignment", "Properties", 
+          "Set horizontal alignment to <i>{0}</i>", 
+          "Set horizontal alignment.", "SetHorizontalAlignment"); 
+AddComboParamOption("Top");
+AddComboParamOption("Center");
+AddComboParamOption("Bottom");
+AddComboParam("Vertical alignment", "Horizontal alignment of the text", 0);          
+AddAction(2, 0, "Set Vertical alignment", "Properties", 
+          "Set vertical alignment to <i>{0}</i>", 
+          "Set vertical alignment.", "SetVerticalAlignment");              
+AddComboParamOption("Word");
+AddComboParamOption("Character");
+AddComboParam("Wrapping", "Wrap text by space-separated words or nearest character", 0);          
+AddAction(3, 0, "Set wrapping", "Properties", 
+          "Set wrapping to <i>{0}</i>", 
+          "Set wrapping.", "SetWrapping");   
+AddNumberParam("Line height", "Offset to the default line height, in pixels. 0 is default line height", 0);      
+AddAction(4, 0, "Set line height", "Properties", 
+          "Set line height to <i>{0}</i>", 
+          "Set line height.", "SetLineHeight"); 
+//////////////////////////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [                  
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_text_properties/runtime.js
@@ -0,0 +1,89 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_text_properties = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_text_properties.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{  
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{         
+	};  
+	
+	behinstProto.tick = function ()
+	{
+	};
+ 
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetHorizontalAlignment = function(align)
+	{
+        this.inst.halign = align;   // 0=left, 1=center, 2=right
+	};
+
+	Acts.prototype.SetVerticalAlignment = function(align)
+	{
+        this.inst.valign = align;   // 0=top, 1=center, 2=bottom
+	};	
+
+	Acts.prototype.SetWrapping = function(wrap_mode)
+	{
+        this.inst.wrapbyword = (wrap_mode === 0);;   // 0=word, 1=character
+	};
+
+	Acts.prototype.SetLineHeight = function(line_height_offset)
+	{
+        this.inst.line_height_offset = line_height_offset;
+	};	
+	
+	    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+}());
