0937e10389b7f8b7894e9a7c2d62ba9c829256e0
hua <binhua.tein@gmail.com>
2012-10-04 22:48:08 +0800

    Add nickname behavior

---
+++ behaviors/rex_bnickname/edittime.js
@@ -0,0 +1,77 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Nickname",
+		"id":			"Rex_bNickname",
+		"description":	"Assign a nickname to this object.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Nickname",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+	  			 		 
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("Nickname", "Nickname of object.", '""');
+AddAction(3, 0, "Set nickname", "Nickname", "Set {my} nickname to <i>{0}</i>",
+         "Set nickname.", "SetNickname");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(3, ef_return_string, "Get nickname", "Nickname", "Nickname", "Get nickname.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [ 
+    new cr.Property(ept_text, "Nickname", "", "Nickname of this object."),	
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{	
+}
---
+++ behaviors/rex_bnickname/runtime.js
@@ -0,0 +1,87 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_bNickname = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_bNickname.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{     
+	    this.nickname = null;
+	};
+    
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;       
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{     
+	    if (this.type.nickname == null)
+            this.set_nickname(this.properties[0]); 
+	};
+
+	behinstProto.tick = function ()
+	{
+	};  
+
+	behinstProto.set_nickname = function (nickname)
+	{
+	    this.type.nickname = nickname;
+	    if (cr.plugins_.Rex_Nickname == null)
+		    return;
+		cr.plugins_.Rex_Nickname.nickname2objtype[nickname] = this.inst.type;
+	};  	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetNickname = function (nickname)
+	{
+		this.set_nickname(nickname); 
+	}; 			
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+	
+	Exps.prototype.Nickname = function (ret)
+	{
+		ret.set_string(this.type.nickname);
+	};			
+			
+}());
