4c44a5079f899620e60b735e538df1f4cea77005
hua <binhua.tein@gmail.com>
2012-10-04 22:47:38 +0800

    Add nickname plugin

---
+++ plugins/rex_nickname/edittime.js
@@ -0,0 +1,177 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Nickname",
+		"id":			"Rex_Nickname",
+		"version":		"0.1",
+		"description":	"Create object by nickname.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"General",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+////////////////////////////////////////
+// Conditions        
+  
+////////////////////////////////////////
+// Actions
+AddStringParam("Nickname", "Nickname of object.", '""');
+AddObjectParam("Object", "Object."); 
+AddAction(1, 0, "Assign nickname", "Assign", "Assign nickname <i>{0}</i> to <i>{1}</i>", 
+          "Assign nickname.", "AssignNickname");
+AddStringParam("Nickname", "Nickname of object.", '""');   
+AddNumberParam("X", "X co-ordinate.", 0);
+AddNumberParam("Y", "Y co-ordinate.", 0);
+AddAnyTypeParam("Layer", "Layer name of number.", 0);
+AddAction(2, 0, "Create instance", "Create", "Create <i>{0}</i> at (<i>{1}</i>,<i>{2}</i>) on layer <i>{3}</i>", 
+          "Create instance by nickname.", "CreateInsts");              
+////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [				
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	// this.myValue = 0...
+}
+
+IDEInstance.prototype.OnCreate = function()
+{
+
+    switch (this.properties["Hotspot"])
+    {
+        case "Top-left" :
+            this.instance.SetHotspot(new cr.vector2(0, 0));
+            break;
+        case "Top" :
+            this.instance.SetHotspot(new cr.vector2(0.5, 0));
+            break;
+        case "Top-right" :
+            this.instance.SetHotspot(new cr.vector2(1, 0));
+            break;
+        case "Left" :
+            this.instance.SetHotspot(new cr.vector2(0, 0.5));
+            break;
+        case "Center" :
+            this.instance.SetHotspot(new cr.vector2(0.5, 0.5));
+            break;
+        case "Right" :
+            this.instance.SetHotspot(new cr.vector2(1, 0.5));
+            break;
+        case "Bottom-left" :
+            this.instance.SetHotspot(new cr.vector2(0, 1));
+            break;
+        case "Bottom" :
+            this.instance.SetHotspot(new cr.vector2(0.5, 1));
+            break;
+        case "Bottom-right" :
+            this.instance.SetHotspot(new cr.vector2(1, 1));
+            break;
+    }
+}
+
+IDEInstance.prototype.OnInserted = function()
+{
+}
+
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	// Edit image link
+	if (property_name === "Hotspot")
+	{
+        switch (this.properties["Hotspot"])
+        {
+            case "Top-left" :
+                this.instance.SetHotspot(new cr.vector2(0, 0));
+                break;
+            case "Top" :
+                this.instance.SetHotspot(new cr.vector2(0.5, 0));
+                break;
+            case "Top-right" :
+                this.instance.SetHotspot(new cr.vector2(1, 0));
+                break;
+            case "Left" :
+                this.instance.SetHotspot(new cr.vector2(0, 0.5));
+                break;
+            case "Center" :
+                this.instance.SetHotspot(new cr.vector2(0.5, 0.5));
+                break;
+            case "Right" :
+                this.instance.SetHotspot(new cr.vector2(1, 0.5));
+                break;
+            case "Bottom-left" :
+                this.instance.SetHotspot(new cr.vector2(0, 1));
+                break;
+            case "Bottom" :
+                this.instance.SetHotspot(new cr.vector2(0.5, 1));
+                break;
+            case "Bottom-right" :
+                this.instance.SetHotspot(new cr.vector2(1, 1));
+                break;
+        }
+	}
+}
+
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+	
+// Called to draw self in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+    var quad = this.instance.GetBoundingQuad();
+    renderer.Fill(quad, cr.RGB(255, 130, 122));
+}
+
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
+
+IDEInstance.prototype.OnTextureEdited = function ()
+{
+}
---
+++ plugins/rex_nickname/runtime.js
@@ -0,0 +1,97 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Nickname = function(runtime)
+{
+	this.runtime = runtime;
+};
+cr.plugins_.Rex_Nickname.nickname2objtype = {};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Nickname.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{	    
+	    this.nickname2objtype = cr.plugins_.Rex_Nickname.nickname2objtype;
+	};
+	
+	instanceProto.create_insts = function (nickname,x,y,_layer)
+	{
+	    var obj_type = this.nickname2objtype[nickname];
+        if (obj_type == null)
+            return;
+        var layer = (typeof _layer == "number")?
+                    this.runtime.getLayerByNumber(_layer):
+                    this.runtime.getLayerByName(_layer);  
+        var inst = this.runtime.createInstance(obj_type, layer, x, y ); 
+        // Pick just this instance
+        var sol = inst.type.getCurrentSol();
+        sol.select_all = false;
+		sol.instances.length = 1;
+		sol.instances[0] = inst;
+	    return inst;
+	}; 	
+
+	instanceProto.draw = function(ctx)
+	{
+	};
+	
+	instanceProto.drawGL = function(glw)
+	{
+	};
+    
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+		
+	Acts.prototype.AssignNickname = function (nickname, objtype)
+	{
+        this.nickname2objtype[nickname] = objtype;
+	};
+	
+	Acts.prototype.CreateInsts = function (nickname,x,y,_layer)
+	{
+        this.create_insts(nickname,x,y,_layer);
+	};
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+}());
