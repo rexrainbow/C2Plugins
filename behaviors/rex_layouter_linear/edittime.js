ab3d5b4d4e66e8aaf433b798c1eff5818e80a675
hua <binhua.tein@gmail.com>
2012-10-21 07:04:35 +0800

    Add linear layout

---
+++ behaviors/rex_layouter_linear/edittime.js
@@ -0,0 +1,77 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Linear layout",
+		"id":			"Rex_layouter_linear",
+		"description":	"Put sprite linear on layouter.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Layouter",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+	  			 		 
+//////////////////////////////////////////////////////////////
+// Actions
+
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [ 
+new cr.Property(ept_combo, "Mode", "Average", "Average mode: layout instances in range averagely, Fix mode: layout instances with fix angle, Compact mode: layout instances compactly.", "Average|Fix|Compact"),
+    new cr.Property(ept_combo, "Direction", "Left to right", "Direction from start to end.", "Left to right|Right to left|Top to bottom|Bottom to top"),
+    new cr.Property(ept_combo, "Alignment", "Start", "Alignment. It only uses in Fix or Compact mode.", "Center|Start|End"),        
+    new cr.Property(ept_float, "Delta distance", 40, "Delta distance of each instance, in pixel. It only uses in Fix mode."),
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{	
+}
---
+++ behaviors/rex_layouter_linear/runtime.js
@@ -0,0 +1,184 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_layouter_linear = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_layouter_linear.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{     
+	};
+    
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;       
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+	    this.check_name = "LAYOUTER";
+        this.mode = this.properties[0];        
+        this.direction = this.properties[1];
+        this.aligment = this.properties[2];
+        this.delta_distance = this.properties[3];        
+        this._points = {start:{x:0,y:0},end:{x:0,y:0}};
+        
+        // implement handlers
+        this.on_add_insts = this._on_update;
+        this.on_remove_insts = this._on_update; 
+        this.mode_handler = [this._update_avarage_mode,
+                             this._update_fix_mode,
+                             this._update_compact_mode];
+	};
+
+	behinstProto.tick = function ()
+	{
+	}; 
+
+	behinstProto._get_start_end_points = function ()
+	{     
+        var layouter =  this.inst;   
+	    layouter.update_bbox();
+	    var quad = layouter.bquad;       
+        switch (this.direction)
+        {
+        case 0:  // Left to right
+            this._points.start.x = (quad.tlx + quad.blx)/2;
+            this._points.start.y = (quad.tly + quad.bly)/2;          
+            this._points.end.x = (quad.trx + quad.brx)/2;
+            this._points.end.y = (quad.try_ + quad.bry)/2;
+            break;
+        case 1:  // Right to left
+            this._points.start.x = (quad.trx + quad.brx)/2;
+            this._points.start.y = (quad.try_ + quad.bry)/2;
+            this._points.end.x = (quad.tlx + quad.blx)/2;
+            this._points.end.y = (quad.tly + quad.bly)/2;      
+            break;   
+        case 2:  // Top to bottom
+            this._points.start.x = (quad.tlx + quad.trx)/2;    
+            this._points.start.y = (quad.tly + quad.try_)/2;         
+            this._points.end.x = (quad.blx + quad.brx)/2; 
+            this._points.end.y = (quad.bly + quad.bry)/2;    
+            break;
+        case 3:  // Bottom to top
+            this._points.start.x = (quad.blx + quad.brx)/2;   
+            this._points.start.y = (quad.bly + quad.bry)/2;     
+            this._points.end.x = (quad.tlx + quad.trx)/2;      
+            this._points.end.y = (quad.tly + quad.try_)/2;           
+            break;            
+        }
+        console.log(this._points);
+	};  
+    
+ 	behinstProto._on_update = function ()
+	{             
+        this.mode_handler[this.mode].apply(this);
+    };
+
+	behinstProto._update_avarage_mode = function ()
+	{    
+        this._get_start_end_points();
+        var layouter =  this.inst;   
+        var a = layouter.angle;
+        var sprites = layouter.sprites;
+        var i, cnt = sprites.length, params;
+        var dx, dy;
+        var seg = (cnt==1)? 1: (cnt-1);
+        dx = (this._points.end.x - this._points.start.x)/seg;
+        dy = (this._points.end.y - this._points.start.y)/seg;
+        var start_x = this._points.start.x;
+        var start_y = this._points.start.y;
+	    for (i=0;i<cnt;i++)
+	    {
+	        params = {x:start_x + (dx*i),
+	                  y:start_y + (dy*i),
+	                  angle:cr.to_degrees(a)};
+	        layouter.layout_inst(sprites[i], params);
+	    }        
+	};
+    
+	behinstProto._update_fix_mode = function ()
+	{    
+        this._get_start_end_points();
+        var layouter =  this.inst;   
+        var a = layouter.angle;
+        var sprites = layouter.sprites;
+        var i, cnt = sprites.length, params;
+        var dx, dy;
+        dx = this.delta_distance * Math.cos(a);
+        dy = this.delta_distance * Math.sin(a);
+        var start_x = this._points.start.x;
+        var start_y = this._points.start.y;
+	    for (i=0;i<cnt;i++)
+	    {
+	        params = {x:start_x + (dx*i),
+	                  y:start_y + (dy*i),
+	                  angle:cr.to_degrees(a)};
+	        layouter.layout_inst(sprites[i], params);
+	    }        
+	};
+
+	behinstProto._update_compact_mode = function ()
+	{    
+        this._get_start_end_points();
+        var layouter =  this.inst;   
+        var a = layouter.angle;
+        var sprites = layouter.sprites;
+        var i, cnt = sprites.length, params;
+        var dx, dy;
+        dx = this.delta_distance * Math.cos(a);
+        dy = this.delta_distance * Math.sin(a);
+        var start_x = this._points.start.x;
+        var start_y = this._points.start.y;
+	    for (i=0;i<cnt;i++)
+	    {
+	        params = {x:start_x + (dx*i),
+	                  y:start_y + (dy*i),
+	                  angle:cr.to_degrees(a)};
+	        layouter.layout_inst(sprites[i], params);
+	    }        
+	};    
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+		
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+	
+}());
