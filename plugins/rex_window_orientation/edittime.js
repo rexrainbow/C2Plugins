23778748915efa8781b8254a0e0c2312f7eb3376
hua <binhua.tein@gmail.com>
2012-10-30 20:44:27 +0800

    Add window orientation detection plugin

---
+++ plugins/rex_window_orientation/edittime.js
@@ -0,0 +1,97 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Orientation",
+		"id":			"Rex_WindowOrientation",
+		"version":		"0.1",   		
+		"description":	"Get orientation of window. (landspcape or portrait)",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Input",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(2, cf_trigger, "On orientation changed", "Orientation", "On orientation changed", 
+             "Triggered when orientation of window changed.", "OnChanged");
+AddCondition(3, cf_trigger, "On changing to landspcape view", "Orientation", "On changing to landspcape view", 
+             "Triggered when changing to landspcape view .", "OnLandspcape");
+AddCondition(4, cf_trigger, "On changing to portrait view", "Orientation", "On changing to portrait view", 
+             "Triggered when changing to portrait view .", "OnPortrait");
+AddCondition(5, 0, "Is landspcape view", "Orientation", "Is landspcape view", 
+             "True if window is at landspcape view .", "IsLandspcape");
+AddCondition(6, 0, "Is portrait view", "Orientation", "Is portrait view", 
+             "True if window is at portrait view .", "IsPortrait"); 
+             
+//////////////////////////////////////////////////////////////
+// Actions
+   
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, "Orientation", "Orientation", "Orientation", "Get orientation value of window.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{	
+}
---
+++ plugins/rex_window_orientation/runtime.js
@@ -0,0 +1,116 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_WindowOrientation = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_WindowOrientation.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{  
+	};
+    
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this.pre_is_landspcape = is_landspcape();
+	    this.runtime.tickMe(this);
+	};
+	
+    instanceProto.tick = function()
+    {
+        var cur_is_landspcape = is_landspcape();
+        if (this.pre_is_landspcape == cur_is_landspcape)
+            return;
+        
+        this.runtime.trigger(cr.plugins_.Rex_WindowOrientation.prototype.cnds.OnChanged, this);
+        if (cur_is_landspcape)
+            this.runtime.trigger(cr.plugins_.Rex_WindowOrientation.prototype.cnds.OnLandspcape, this);
+        else
+            this.runtime.trigger(cr.plugins_.Rex_WindowOrientation.prototype.cnds.OnPortrait, this);
+        this.pre_is_landspcape = cur_is_landspcape;
+    };
+    
+    var is_landspcape = function()
+    {
+        var ret;
+        var _orientation = window["orientation"];
+        if (_orientation != null)
+            ret = (Math.abs(_orientation) == 90);
+        else        
+            ret = (window["innerHeight"] > window["innerWidth"]);
+        return ret;
+    };
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();    
+
+	Cnds.prototype.OnChanged = function()
+	{
+        return true;
+	};
+	Cnds.prototype.OnLandspcape = function()
+	{
+        return true;
+	};	
+	Cnds.prototype.OnPortrait = function()
+	{
+        return true;
+	};     
+	Cnds.prototype.IsLandspcape = function()
+	{
+	    return is_landspcape();   
+	};
+	Cnds.prototype.IsPortrait = function()
+	{
+	    return (!is_landspcape());
+	}; 	
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+    
+	Exps.prototype.Orientation = function (ret)
+	{
+	    var _orientation = window["orientation"];
+	    if (_orientation == null)
+	        _orientation = 0;
+		ret.set_int(_orientation);
+	};
+}());
