53c5ce4374ce9026c606b699bf6fc76c42448ab6
hua <binhua.tein@gmail.com>
2012-11-02 08:15:49 +0800

    Add tilt to arrowkey plugin

---
+++ plugins/rex_tilt2ArrowKey/edittime.js
@@ -0,0 +1,122 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Tilt to arrow key",
+		"id":			"Rex_Tilt2ArrowKey",
+		"version":		"0.1",   		
+		"description":	"Get arrow key event from tilt's beta and gamma input.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Input",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On UP key pressed", "Pressd", "On UP pressed", "Triggered when UP key is pressed.", "OnUPKey");
+AddCondition(2, cf_trigger, "On DOWN key pressed", "Pressd", "On DOWN pressed", "Triggered when DOWN key is pressed.", "OnDOWNKey");
+AddCondition(3, cf_trigger, "On LEFT key pressed", "Pressd", "On LEFT pressed", "Triggered when LEFT key is pressed.", "OnLEFTKey");
+AddCondition(4, cf_trigger, "On RIGHT key pressed", "Pressd", "On RIGHT pressed", "Triggered when RIGHT key is pressed.", "OnRIGHTKey");
+AddCondition(5, cf_trigger, "On any key pressed", "Pressd", "On any key pressed", "Triggered when any keyboard key is pressed.", "OnAnyKey");
+
+AddCondition(11,	0,	"UP Key is down",	"Is down", "UP is down", "Test if UP key is currently held down.", "IsUPDown");
+AddCondition(12,	0,	"DOWN Key is down",	"Is down", "DOWN is down", "Test if DOWN key is currently held down.", "IsDOWNDown");
+AddCondition(13,	0,	"LEFT Key is down",	"Is down", "LEFT is down", "Test if LEFT key is currently held down.", "IsLEFTDown");
+AddCondition(14,	0,	"RIGHT Key is down",	"Is down", "RIGHT is down", "Test if RIGHT key is currently held down.", "IsRIGHTDown");
+
+AddCondition(21,	cf_trigger,	"On UP key released",	"Released", "On UP released", "Triggered when UP key is released.", "OnUPKeyReleased");
+AddCondition(22,	cf_trigger,	"On DOWN key released",	"Released", "On DOWN released", "Triggered when DOWN key is released.", "OnDOWNKeyReleased");
+AddCondition(23,	cf_trigger,	"On LEFT key released",	"Released", "On LEFT released", "Triggered when LEFT key is released.", "OnLEFTKeyReleased");
+AddCondition(24,	cf_trigger,	"On RIGHT key released",	"Released", "On RIGHT released", "Triggered when RIGHT key is released.", "OnRIGHTKeyReleased");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("0");
+AddComboParamOption("current angle");
+AddComboParam("ZERO angle", "ZERO angle of Up-Bottom direction.");
+AddComboParamOption("0");
+AddComboParamOption("current angle");
+AddComboParam("ZERO angle", "ZERO angle of Left-Right direction.");
+AddAction(2, 0, "Calibration", "calibration", "Calibration zero angle of Up-Bottom to <i>{0}</i>, zero angle of Left-Right to <i>{1}</i>", 
+          "Calibration zero angle.", "Calibration");
+AddNumberParam("Sensitivity angle", "Sensitivity angle.");          
+AddAction(3, 0, "Set sensitivity angle", "Sensitivity", "Set sensitivity angle to <i>{0}</i>", 
+          "Set sensitivity angle of turning direction detection.", "SetSensitivity");
+          
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, "Zero of Up-Bottom", "Angle", "ZEROUD", "Get zero of Up-Bottom.");
+AddExpression(2, ef_return_number, "Zero of Left-Right", "Angle", "ZEROLR", "Get zero of Left-Right.");
+AddExpression(3, ef_return_number, "Rotate angle of Up-Bottom", "Angle", "RotateUD", "Get rotate angle of Up-Bottom.");
+AddExpression(4, ef_return_number, "Rotate angle of Left-Right", "Angle", "RotateLR", "Get rotate angle of Left-Right.");
+AddExpression(5, ef_return_number, "Sensitivity angle", "Angle", "SensitivityAngle", "Get sensitivity angle of turning direction detection.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Directions", "8 directions", "The number of directions of movement available.", "Up & down|Left & right|4 directions|8 directions"),
+    new cr.Property(ept_float, "Sensitivity", 5, "Sensitivity of tilt angle, in degree."),   
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Sensitivity"] < 0)
+		this.properties["Sensitivity"] = 1;    
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{	
+}
---
+++ plugins/rex_tilt2ArrowKey/runtime.js
@@ -0,0 +1,324 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Tilt2ArrowKey = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Tilt2ArrowKey.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{  
+	};
+    
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this._directions = this.properties[0];
+        this._sensitivity = this.properties[1]; 
+        this.runtime.tickMe(this);
+     
+        this.setup_stage = true;
+        this.touchwrap = null;
+        this.degree_ZEROUD = 0;
+        this.degree_ZEROLR = 0;
+        this.degree_diffUD = 0;
+        this.degree_diffLR = 0;
+        this.keyUD = 0; // 0=no key, 1=up key, 2=down key 
+        this.keyLR = 0; // 0=no key, 1=left key, 2=right key 
+	};
+
+	instanceProto.TouchWrapGet = function ()
+	{  
+        var plugins = this.runtime.types;
+        var name, obj;
+        for (name in plugins)
+        {
+            obj = plugins[name].instances[0];
+            if ((obj != null) && (obj.check_name == "TOUCHWRAP"))
+            {
+                this.touchwrap = obj;
+                this.touchwrap.HookMe(this);
+                break;
+            }
+        }
+	}; 
+    
+    instanceProto.tick = function()
+    {
+        this._setup();
+        this._tilt2arrowkey();
+    };
+	
+	var orientation_get = function()
+	{
+	    var ret = window["orientation"];
+	    if (ret == null)
+	        ret = 0;
+	    return ret;
+	}
+	
+	instanceProto._setup = function ()
+	{
+        if (!this.setup_stage)
+            return;
+        
+        this.TouchWrapGet();  
+        this.setup_stage = false;
+        if (this.touchwrap == null)
+            assert("Tilt to Arrowkey: please put touchwrap object into project file.");
+        else        
+            this._degree_ZERO_set();        
+	};
+	
+	instanceProto._degree_ZERO_set = function (ZERO_UD, ZERO_LR)
+	{
+        var is_landspcape = (Math.abs(orientation_get()) == 90);  
+        if (ZERO_UD == null)
+        {
+            ZERO_UD = (is_landspcape)?
+                      this.touchwrap.GetGamma():this.touchwrap.GetBeta();  
+        }
+        if (ZERO_LR == null)
+        {
+            ZERO_LR = (is_landspcape)?
+                      this.touchwrap.GetBeta():this.touchwrap.GetGamma();  
+        }
+        this.degree_ZEROUD = ZERO_UD;               
+        this.degree_ZEROLR = ZERO_LR;  
+	};	
+
+    instanceProto._diffUD_get = function(orientation)
+    {
+        var diff;
+        switch (orientation)
+        {
+        case 0:    // U:b+ , D:b-
+            diff = -this.touchwrap.GetBeta() + this.degree_ZEROUD;  
+            break;
+        case 90:   // U:g- , D:g+
+            diff = this.touchwrap.GetGamma() - this.degree_ZEROUD;
+            break;
+        case 180:  // U:b- , D:b+
+            diff = this.touchwrap.GetBeta() - this.degree_ZEROUD;  
+            break;
+        case -90:  // U:g+ , D:g-
+            diff = -this.touchwrap.GetGamma() + this.degree_ZEROUD;
+            break;
+        }
+        return diff;
+    };    
+    instanceProto._diffLR_get = function(orientation)
+    {
+        var diff;
+        switch (orientation)
+        {
+        case 0:    // L:g+ , R:g-
+            diff = -this.touchwrap.GetGamma() + this.degree_ZEROLR;
+            break;
+        case 90:   // L:b+ , R:b-
+            diff = -this.touchwrap.GetBeta() + this.degree_ZEROLR;            
+            break;
+        case 180:  // L:g- , R:g+
+            diff = this.touchwrap.GetGamma() - this.degree_ZEROLR;
+            break;
+        case -90:  // L:b- , R:b+
+            diff = this.touchwrap.GetBeta() - this.degree_ZEROLR;         
+            break;
+        }
+        return diff;
+    };   
+	instanceProto._tilt2arrowkey = function ()
+	{
+        if (this.touchwrap == null)
+            return;
+          
+        var orientation = orientation_get();
+        var dir = this._directions; //0=UD, 1=LR, 2=4dir, 3=8dir
+        // key UD
+        if ((dir==0) || (dir==2) || (dir==3))
+            this._update_keyUD(orientation);
+            
+        // key LR
+        if ((dir==1) || (dir==3) || ((dir==2) && (this.keyUD==0)))
+            this._update_keyLR(orientation);
+	}; 
+
+	instanceProto._update_keyUD = function (orientation)
+	{
+        this.degree_diffUD = this._diffUD_get(orientation);
+        var current_keyUD;
+        if (Math.abs(this.degree_diffUD) >= this._sensitivity)
+            current_keyUD = (this.degree_diffUD>0)? 1:2; // 1=up, 2=bottom
+        else  // no key
+            current_keyUD = 0;
+        if (current_keyUD != this.keyUD)
+        {
+            if (this.keyUD == 1)  // release up key
+                this.runtime.trigger(cr.plugins_.Rex_Tilt2ArrowKey.prototype.cnds.OnUPKeyReleased, this);        
+            else if (this.keyUD == 2)  // release bottom key
+                this.runtime.trigger(cr.plugins_.Rex_Tilt2ArrowKey.prototype.cnds.OnDOWNKeyReleased, this);   
+            if (current_keyUD == 1)  // press up key
+                this.runtime.trigger(cr.plugins_.Rex_Tilt2ArrowKey.prototype.cnds.OnUPKey, this);        
+            else if (current_keyUD == 2)  // press bottom key
+                this.runtime.trigger(cr.plugins_.Rex_Tilt2ArrowKey.prototype.cnds.OnDOWNKey, this); 
+            this.keyUD = current_keyUD;
+        }  
+	};	
+	instanceProto._update_keyLR = function (orientation)
+	{
+        // key LR
+        this.degree_diffLR = this._diffLR_get(orientation);        
+        var current_keyLR;
+        if (Math.abs(this.degree_diffLR) >= this._sensitivity)
+            current_keyLR = (this.degree_diffLR>0)? 1:2; // 1=left, 2=right
+        else  // no key
+            current_keyLR = 0;           
+        if (current_keyLR != this.keyLR)
+        {
+            if (this.keyLR == 1)  // release left key
+                this.runtime.trigger(cr.plugins_.Rex_Tilt2ArrowKey.prototype.cnds.OnLEFTKeyReleased, this);        
+            else if (this.keyLR == 2)  // release right key
+                this.runtime.trigger(cr.plugins_.Rex_Tilt2ArrowKey.prototype.cnds.OnRIGHTKeyReleased, this);   
+            if (current_keyLR == 1)  // press left key
+                this.runtime.trigger(cr.plugins_.Rex_Tilt2ArrowKey.prototype.cnds.OnLEFTKey, this);        
+            else if (current_keyLR == 2)  // press right key
+                this.runtime.trigger(cr.plugins_.Rex_Tilt2ArrowKey.prototype.cnds.OnRIGHTKey, this); 
+            this.keyLR = current_keyLR;
+        }  
+	};	
+		
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();    
+
+	Cnds.prototype.IsUPDown = function()
+	{
+        return (this.keyUD == 1);
+	};
+	Cnds.prototype.IsDOWNDown = function()
+	{
+        return (this.keyUD == 2);
+	};	
+	Cnds.prototype.IsLEFTDown = function()
+	{
+        return (this.keyLR == 1);
+	};
+	Cnds.prototype.IsRIGHTDown = function()
+	{
+        return (this.keyLR == 2)
+	};    
+    
+	Cnds.prototype.OnUPKey = function()
+	{
+        return true;
+	};
+	Cnds.prototype.OnDOWNKey = function()
+	{
+        return true;    
+	};    
+	Cnds.prototype.OnLEFTKey = function()
+	{
+        return true;    
+	};
+	Cnds.prototype.OnRIGHTKey = function()
+	{
+        return true;    
+	};      
+	
+	Cnds.prototype.OnAnyKey = function()
+	{
+        return true;    
+	};
+	
+	Cnds.prototype.OnUPKeyReleased = function()
+	{
+        return true;
+	};
+	Cnds.prototype.OnDOWNKeyReleased = function()
+	{
+        return true;    
+	};  
+	Cnds.prototype.OnLEFTKeyReleased = function()
+	{
+        return true;    
+	};
+	Cnds.prototype.OnRIGHTKeyReleased = function()
+	{
+        return true;    
+	};   
+    
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+	
+    Acts.prototype.Calibration = function (ZERO_UD, ZERO_LR)
+	{	     
+        if (ZERO_UD==1)
+            ZERO_UD = null;
+        if (ZERO_LR==1)
+            ZERO_LR = null;
+        this._setup();        
+        this._degree_ZERO_set(ZERO_UD, ZERO_LR);
+	};
+	
+    Acts.prototype.SetSensitivity = function (a)
+	{	     
+        this._sensitivity = a; 
+	}; 
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+    
+	Exps.prototype.ZEROUD = function (ret)
+	{
+		ret.set_float(this.degree_ZEROUD);
+	};
+	Exps.prototype.ZEROLR = function (ret)
+	{
+		ret.set_float(this.degree_ZEROLR);
+	};   
+	Exps.prototype.RotateUD = function (ret)
+	{
+		ret.set_float(this.degree_diffUD);
+	};
+	Exps.prototype.RotateLR = function (ret)
+	{
+		ret.set_float(this.degree_diffLR);
+	};
+	Exps.prototype.SensitivityAngle = function (ret)
+	{
+		ret.set_float(this._sensitivity);
+	};	
+}());
