018f95adc6bca51968bedd46fde625ac6486c2ee
hua <binhua.tein@gmail.com>
2012-11-04 17:07:55 +0800

    Add sprite to arrow key plugin

---
+++ plugins/rex_spriteArrowKey/edittime.js
@@ -0,0 +1,127 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Sprite to ArrowKey",
+		"id":			"Rex_Sprite2ArrowKey",
+		"version":		"0.1",   		
+		"description":	'Get arrow key event from sprites\' "is in touch".',
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Input",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On Up arrow pressed", "Pressd", "On Up arrow pressed", 
+             "Triggered when Up arrow is pressed.", "OnUPPressed");
+AddCondition(2, cf_trigger, "On Down arrow pressed", "Pressd", "On Down arrow pressed", 
+             "Triggered when Down arrow is pressed.", "OnDOWNPressed");
+AddCondition(3, cf_trigger, "On Left arrow pressed", "Pressd", "On Left arrow pressed", 
+             "Triggered when Left arrow is pressed.", "OnLEFTPressed");
+AddCondition(4, cf_trigger, "On Right arrow pressed", "Pressd", "On Right arrow pressed", 
+             "Triggered when Right arrow is pressed.", "OnRIGHTPressed");
+AddCondition(5, cf_trigger, "On any arrow pressed", "Pressd", "On any arrow pressed", 
+             "Triggered when any arrow is pressed.", "OnAnyPressed");
+
+AddCondition(11, 0,	"Up arrow is down",	"Is down", "Up arrow is down", 
+             "Return true if Up arrow is currently held down.", "IsUPDown");
+AddCondition(12, 0,	"Down arrow is down", "Is down", "Down arrow is down", 
+             "Return true if Down arrow is currently held down.", "IsDOWNDown");
+AddCondition(13, 0,	"Left arrow is down", "Is down", "Left arrow is down", 
+             "Return true if Left arrow is currently held down.", "IsLEFTDown");
+AddCondition(14, 0,	"Right arrow is down", "Is down", "Right arrow is down", 
+             "Return true if Right arrow is currently held down.", "IsRIGHTDown");
+
+AddCondition(21, cf_trigger, "On Up arrow released", "Released", "On Up arrow released", 
+             "Triggered when Up arrow is released.", "OnUPReleased");
+AddCondition(22, cf_trigger, "On Down arrow released", "Released", "On Down arrow released", 
+             "Triggered when Down arrow is released.", "OnDOWNReleased");
+AddCondition(23, cf_trigger, "On Left arrow released", "Released", "On Left arrow released", 
+             "Triggered when Left arrow is released.", "OnLEFTReleased");
+AddCondition(24, cf_trigger, "On Right arrow released", "Released", "On Right arrow released", 
+             "Triggered when Right arrow is released.", "OnRIGHTReleased");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("Right arrow");
+AddComboParamOption("Down arrow");
+AddComboParamOption("Left arrow");
+AddComboParamOption("Up arrow");
+AddComboParam("Arrow key", "Arrow key", 0);
+AddObjectParam("Sprite", "Arrow key of sprite");
+AddAction(2, 0, "Set arrow key sprite", "Sprite", 
+          "Set <i>{0}</i> to <i>{1}</i>", 
+          "Set arrow key sprite.", "SetArrowkeySprite");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Directions", "8 directions", "The number of directions of movement available.", "Up & down|Left & right|4 directions|8 directions"),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+	if (this.properties["Sensitivity"] < 0)
+		this.properties["Sensitivity"] = 1;
+		
+}
---
+++ plugins/rex_spriteArrowKey/runtime.js
@@ -0,0 +1,275 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Sprite2ArrowKey = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Sprite2ArrowKey.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{  
+	};
+    
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this._directions = this.properties[0];
+        this.runtime.tickMe(this);
+     
+        this.setup_stage = true;
+        // touch   
+        this.touchwrap = null;
+        this.pre_key_id = 0;
+        this.keyMap = [{inst:null, state:false, is_in_touch:false}, 
+                       {inst:null, state:false, is_in_touch:false}, 
+                       {inst:null, state:false, is_in_touch:false}, 
+                       {inst:null, state:false, is_in_touch:false}  ];
+        
+        this.press_handlers =   [cr.plugins_.Rex_Sprite2ArrowKey.prototype.cnds.OnRIGHTPresse,
+                                 cr.plugins_.Rex_Sprite2ArrowKey.prototype.cnds.OnDOWNPresse,
+                                 cr.plugins_.Rex_Sprite2ArrowKey.prototype.cnds.OnLEFTPresse,
+                                 cr.plugins_.Rex_Sprite2ArrowKey.prototype.cnds.OnUPPressed     ];   
+        this.release_handlers = [cr.plugins_.Rex_Sprite2ArrowKey.prototype.cnds.OnRIGHTReleased,
+                                 cr.plugins_.Rex_Sprite2ArrowKey.prototype.cnds.OnDOWNReleased,
+                                 cr.plugins_.Rex_Sprite2ArrowKey.prototype.cnds.OnLEFTReleased,
+                                 cr.plugins_.Rex_Sprite2ArrowKey.prototype.cnds.OnUPReleased    ];
+	};
+
+	instanceProto.TouchWrapGet = function ()
+	{  
+        var plugins = this.runtime.types;
+        var name, obj;
+        for (name in plugins)
+        {
+            obj = plugins[name].instances[0];
+            if ((obj != null) && (obj.check_name == "TOUCHWRAP"))
+            {
+                this.touchwrap = obj;
+                this.touchwrap.HookMe(this);
+                break;
+            }
+        }
+	}; 
+    
+    instanceProto.OnTouchStart = function (_NthTouch, _TouchX, _TouchY)
+    {
+    };
+    
+    instanceProto.OnTouchEnd = function (_NthTouch)
+    {
+    };
+
+    instanceProto.tick = function()
+    {
+        this._setup();
+        this._touch_arrow();
+    };
+
+	instanceProto._setup = function ()
+	{
+        if (!this.setup_stage)
+            return;
+        
+        this.TouchWrapGet();  
+        this.setup_stage = false;
+        if (this.touchwrap == null)
+            assert("Sprite Arrow key object need at least one input signal.");
+	};
+    
+    instanceProto._update_is_in_touch = function()
+    {
+        var touchs = this.touchwrap.touches, tx, ty;    
+        var i, px, py, inst, keyMap;    
+        var j, lenj = touchs.length;        
+        for (i=0; i<4; i++)
+        {
+            keyMap = this.keyMap[i];
+            keyMap.is_in_touch = false;        
+            inst = keyMap.inst;
+            if (inst == null)
+                continue;
+ 			for (j=0; j<lenj; j++)
+			{
+				tx = touchs[j].x;
+				ty = touchs[j].y;
+				px = inst.layer.canvasToLayer(tx, ty, true);
+				py = inst.layer.canvasToLayer(tx, ty, false);
+                if (inst.contains_pt(px, py))
+				    keyMap.is_in_touch = true;
+			}
+        }
+    };    
+    
+    var RIGHTKEY = 0x1;
+    var DOWNKEY = 0x2;
+    var LEFTKEY = 0x4;
+    var UPKEY = 0x8;
+	instanceProto._touch_arrow = function ()
+	{    
+        if (this.touchwrap == null)       
+            return;
+        
+        this._update_is_in_touch();
+
+        switch (this._directions)
+        {
+        case 0:  // Up & down
+            var key_id = (this.keyMap[3].is_in_touch)?  UPKEY:
+                         (this.keyMap[1].is_in_touch)?  DOWNKEY:0;
+            this._keydown(key_id);
+            break;
+        case 1:  // Left & right
+            var key_id = (this.keyMap[0].is_in_touch)?  RIGHTKEY:
+                         (this.keyMap[2].is_in_touch)?  LEFTKEY:0;
+            this._keydown(key_id);
+            break;  
+        case 2:  // 4 directions
+            var key_id = (this.keyMap[0].is_in_touch)?  RIGHTKEY:
+                         (this.keyMap[1].is_in_touch)?  DOWNKEY:
+                         (this.keyMap[2].is_in_touch)?  LEFTKEY:
+                         (this.keyMap[3].is_in_touch)?  UPKEY:0;                          
+            this._keydown(key_id);
+            break;
+        case 3:  // 8 directions
+            var key_UD = (this.keyMap[3].is_in_touch)?  UPKEY:
+                         (this.keyMap[1].is_in_touch)?  DOWNKEY:0;
+            var key_LR = (this.keyMap[0].is_in_touch)?  RIGHTKEY:
+                         (this.keyMap[2].is_in_touch)?  LEFTKEY:0;                       
+            this._keydown(key_UD + key_LR);
+            break;                
+        }
+	}; 
+    
+    var _keyid_list = [RIGHTKEY,DOWNKEY,LEFTKEY,UPKEY];       
+    instanceProto._keydown = function(key_id)
+    {
+        if (this.pre_key_id == key_id)
+            return;
+            
+        var i;
+        for (i=0; i<4; i++)
+        {
+            var is_key_down = ((_keyid_list[i] & key_id) != 0);            
+            if (this.keyMap[i].state && (!is_key_down))
+                this.runtime.trigger(this.release_handlers[i], this);
+        }
+        var pressed_any_key = false;
+        for (i=0; i<4; i++)
+        {
+            var is_key_down = ((_keyid_list[i] & key_id) != 0); 
+            if ((!this.keyMap[i].state) && is_key_down)
+            {
+                this.runtime.trigger(this.press_handlers[i], this);
+                pressed_any_key = true;
+            }
+            this.keyMap[i].state = is_key_down;
+        }         
+        if (pressed_any_key)
+            this.runtime.trigger(cr.plugins_.Rex_Sprite2ArrowKey.prototype.cnds.OnAnyKey, this);
+        
+        this.pre_key_id = key_id;            
+    };
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();    
+
+	Cnds.prototype.IsUPDown = function()
+	{
+        return this.keyMap[3].state;
+	};
+	Cnds.prototype.IsDOWNDown = function()
+	{
+        return this.keyMap[1].state;    
+	};	
+	Cnds.prototype.IsLEFTDown = function()
+	{
+        return this.keyMap[2].state;
+	};
+	Cnds.prototype.IsRIGHTDown = function()
+	{
+        return this.keyMap[0].state;
+	};    
+    
+	Cnds.prototype.OnUPPressed = function()
+	{
+        return true;
+	};
+	Cnds.prototype.OnDOWNPressed = function()
+	{
+        return true;    
+	};    
+	Cnds.prototype.OnLEFTPressed = function()
+	{
+        return true;    
+	};
+	Cnds.prototype.OnRIGHTPressed = function()
+	{
+        return true;    
+	};      
+
+	Cnds.prototype.OnAnyPressed = function()
+	{
+        return true;    
+	};
+	
+	Cnds.prototype.OnUPReleased = function()
+	{
+        return true;
+	};
+	Cnds.prototype.OnDOWNReleased = function()
+	{
+        return true;    
+	};  
+	Cnds.prototype.OnLEFTReleased = function()
+	{
+        return true;    
+	};
+	Cnds.prototype.OnRIGHTReleased = function()
+	{
+        return true;    
+	};     
+    
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+    
+    Acts.prototype.SetArrowkeySprite = function (arrowkey, objs)
+	{
+        this.keyMap[arrowkey].inst = objs.getFirstPicked();
+	}; 
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+
+}());
