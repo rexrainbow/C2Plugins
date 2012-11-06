3ece994ad3c41ddd2e61818fd6ce78ed7d1838bd
hua <binhua.tein@gmail.com>
2012-11-07 07:28:46 +0800

    Add sequence matcher plugin

---
+++ plugins/rex_sequence_matcher/edittime.js
@@ -0,0 +1,102 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Sequence matcher",
+		"id":			"Rex_SequenceMatcher",
+		"version":		"0.1",   		
+		"description":	"To find sequence pattern.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Game logic",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddStringParam("Pattern", "Pattern.", '""');
+AddCondition(2, cf_trigger, "On matching", "Patern", 
+             "On matching <i>{0}</i>", 'Trigger by "Action:Put symbol" when matching pattern.', "OnMatchPattern");    
+AddCondition(3, cf_trigger, "On no matching pattern", "Patern", 
+             "On no matching pattern", 'Trigger by "Action:Put symbol" when matching pattern.', "OnNoMatchPattern");   
+//////////////////////////////////////////////////////////////
+// Actions 
+AddAction(1, 0, "Clean buffer", "Symbol buffer", 
+          "Clean symbol buffer", 
+          "Clean symbol buffer.", "CleanSymbolBuffer");
+AddNumberParam("Length", "length of symbol buffer ", 10);           
+AddAction(2, 0, "Set buffer length", "Symbol buffer", 
+          "Set symbol buffer length to <i>{0}</i>", 
+          "Set symbol buffer length.", "SetSymbolBufferLength");
+AddStringParam("Symbol", "Symbol to push", '""');
+AddAction(5, 0, "Push symbol", "Request: Matching", 
+          "Push symbol <i>{0}</i>", 
+          "Push symbol into buffer then do matching.", "PushSymbol");
+             
+//////////////////////////////////////////////////////////////
+// Expressions             
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_integer, "Buffer length", 10, "Buffer length of saved symbols."),   
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_sequence_matcher/runtime.js
@@ -0,0 +1,148 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_SequenceMatcher = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_SequenceMatcher.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this._symbol_buffer = new cr.plugins_.Rex_SequenceMatcher.BufferKlass(this.properties[0]);
+        this._has_matched_pattern = false;
+	};
+	
+	instanceProto.push_symbol = function (s)	
+	{
+	    this._symbol_buffer.push_data(s);
+	    this._has_matched_pattern = false;
+	    this.runtime.trigger(cr.plugins_.Rex_SequenceMatcher.prototype.cnds.OnMatchPattern, this);
+	    if (!this._has_matched_pattern)
+            this.runtime.trigger(cr.plugins_.Rex_SequenceMatcher.prototype.cnds.OnNoMatchPattern, this);
+	};
+    
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();    
+
+	Cnds.prototype.OnMatchPattern = function (pattern)
+	{       
+	    var is_matched = this._symbol_buffer.is_matched(pattern);
+	    this._has_matched_pattern |= is_matched;
+        return is_matched;
+	};
+	
+	Cnds.prototype.OnNoMatchPattern = function ()
+	{
+        return true;
+	};
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+    
+	Acts.prototype.CleanSymbolBuffer = function ()	
+	{
+	    this._symbol_buffer.clean();
+	};
+	Acts.prototype.SetSymbolBufferLength = function (max_len)	
+	{
+	    this._symbol_buffer.set_max_length(max_len);
+	};    
+	Acts.prototype.PushSymbol = function (s)	
+	{
+	    this.push_symbol(s);
+	};
+    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+}());
+
+(function ()
+{
+    cr.plugins_.Rex_SequenceMatcher.BufferKlass = function(max_len)
+    {    
+        this._buf = [];
+        this.set_max_length(max_len);
+    };
+    var BufferKlassProto = cr.plugins_.Rex_SequenceMatcher.BufferKlass.prototype;
+    
+	BufferKlassProto.clean = function()
+	{
+	    this._buf.length = 0;
+	};
+	
+	BufferKlassProto.set_max_length = function(max_len)
+	{
+	    this.max_len = max_len;
+        if (max_len < this._buf.length)
+            this._buf.length = max_len;
+	};
+	
+	BufferKlassProto.push_data = function(data)
+	{
+	    this._buf.push(data);
+	    if (this._buf.length > this.max_len)
+	        this._buf.shift()
+	};
+	
+	BufferKlassProto.is_matched = function(pattern)
+	{
+	    if (pattern == "")
+	        return false;
+	        
+	    var pattern_len=pattern.length;
+	    var buf_len=this._buf.length;
+	    if (pattern_len > buf_len)
+	        return false;
+	    
+	    var i,is_matched=true;
+	    for (i=0; i<pattern_len; i++)
+	    {
+	        if (pattern[pattern_len-1-i] != this._buf[buf_len-1-i])
+	        {
+	            is_matched = false;
+	            break;
+	        }
+	    }
+	    return is_matched;
+	};
+}());    
