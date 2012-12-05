bb0696084353e5f4fb5c3d3d21f0aa1c42937d3b
hua <binhua.tein@gmail.com>
2012-12-05 23:28:15 +0800

    Add layer plugin to add behavior to control layer.

---
+++ plugins/rex_layer/edittime.js
@@ -0,0 +1,167 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Layer",
+		"id":			"Rex_LayerObj",
+		"version":		"0.1",
+		"description":	"Adding behavior to change the scale/anlge/opacity/visible of layer.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"General",
+		"type":			"world",			// appears in layout
+		"rotatable":	true,
+		"flags":	    pf_position_aces | pf_size_aces | pf_angle_aces
+	};
+};
+
+////////////////////////////////////////
+// Conditions
+           
+////////////////////////////////////////
+// Actions
+           
+////////////////////////////////////////
+// Expressions
+             
+ACESDone();
+
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	// this.myValue = 0...
+}
+
+IDEInstance.prototype.OnCreate = function()
+{
+
+    switch (this.properties["Hotspot"])
+    {
+        case "Top-left" :
+            this.instance.SetHotspot(new cr.vector2(0, 0));
+            break;
+        case "Top" :
+            this.instance.SetHotspot(new cr.vector2(0.5, 0));
+            break;
+        case "Top-right" :
+            this.instance.SetHotspot(new cr.vector2(1, 0));
+            break;
+        case "Left" :
+            this.instance.SetHotspot(new cr.vector2(0, 0.5));
+            break;
+        case "Center" :
+            this.instance.SetHotspot(new cr.vector2(0.5, 0.5));
+            break;
+        case "Right" :
+            this.instance.SetHotspot(new cr.vector2(1, 0.5));
+            break;
+        case "Bottom-left" :
+            this.instance.SetHotspot(new cr.vector2(0, 1));
+            break;
+        case "Bottom" :
+            this.instance.SetHotspot(new cr.vector2(0.5, 1));
+            break;
+        case "Bottom-right" :
+            this.instance.SetHotspot(new cr.vector2(1, 1));
+            break;
+    }
+}
+
+IDEInstance.prototype.OnInserted = function()
+{
+}
+
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	// Edit image link
+	if (property_name === "Hotspot")
+	{
+        switch (this.properties["Hotspot"])
+        {
+            case "Top-left" :
+                this.instance.SetHotspot(new cr.vector2(0, 0));
+                break;
+            case "Top" :
+                this.instance.SetHotspot(new cr.vector2(0.5, 0));
+                break;
+            case "Top-right" :
+                this.instance.SetHotspot(new cr.vector2(1, 0));
+                break;
+            case "Left" :
+                this.instance.SetHotspot(new cr.vector2(0, 0.5));
+                break;
+            case "Center" :
+                this.instance.SetHotspot(new cr.vector2(0.5, 0.5));
+                break;
+            case "Right" :
+                this.instance.SetHotspot(new cr.vector2(1, 0.5));
+                break;
+            case "Bottom-left" :
+                this.instance.SetHotspot(new cr.vector2(0, 1));
+                break;
+            case "Bottom" :
+                this.instance.SetHotspot(new cr.vector2(0.5, 1));
+                break;
+            case "Bottom-right" :
+                this.instance.SetHotspot(new cr.vector2(1, 1));
+                break;
+        }
+	}
+}
+
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+	
+// Called to draw self in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+    var quad = this.instance.GetBoundingQuad();
+    renderer.Fill(quad, cr.RGB(255, 130, 122));
+}
+
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
+
+IDEInstance.prototype.OnTextureEdited = function ()
+{
+}
---
+++ plugins/rex_layer/runtime.js
@@ -0,0 +1,119 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_LayerObj = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	var pluginProto = cr.plugins_.Rex_LayerObj.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created    
+	instanceProto.onCreate = function()
+	{
+	    this._width_init = this.width;
+	    this._height_init = this.height;
+ 
+	    this._width_save = this.width;
+	    this._height_save = this.height;
+	    this._angle_save = this.angle;
+	    this._opactiy_save = this.opacity;
+	    this._visible_save = this.visible;	       
+	    
+	    this.runtime.tick2Me(this);        
+	};
+
+	instanceProto.tick2 = function()
+	{
+	    if ((this.width != this._width_save) || (this.height != this._height_save))
+	    {
+	        var wscale = this.width/this._width_init;
+	        var hscale = this.height/this._height_init;
+	        var scale = Math.min(wscale, hscale);
+            this.layer.scale = scale;
+            this.runtime.redraw = true;	    
+	        this._width_save = this.width;
+	        this._height_save = this.height;
+	    }
+	    
+	    if (this._angle_save != this.angle)
+	    {
+	        this.layer.angle = this.angle;
+	        this.runtime.redraw = true;
+	        this._angle_save = this.angle;
+	    }
+	    
+	    if (this._opactiy_save != this.opacity)
+	    {
+	        var opacity_ = cr.clamp(this.opacity / 100, 0, 1);
+	        this.layer.opacity = opacity_;
+	        this.runtime.redraw = true;
+	        this._opactiy_save = opacity_; 
+	    }
+	    
+	    if (this._visible_save != this.visible)
+	    {
+	        this.layer.visible = this.visible;
+	        this.runtime.redraw = true;
+	        this._visible_save = this.visible;	  
+	    }	    
+	};	
+	
+	// only called if a layout object
+	instanceProto.draw = function(ctx)
+	{
+	};  
+
+	instanceProto.drawGL = function(glw)
+	{
+	};
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds(); 
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+}());
