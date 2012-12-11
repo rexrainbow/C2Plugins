df4d56751a6915903186866468991f854990aabf
hua <binhua.tein@gmail.com>
2012-12-11 23:40:05 +0800

    Add chess bank extended from inst bank

---
+++ plugins/rex_chessbank/edittime.js
@@ -0,0 +1,128 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Chess Bank",
+		"id":			"Rex_ChessBank",
+		"version":		"0.8",            
+		"description":	"Save/Load chess on a board.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Board",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddObjectParam("Object", "Object for saving");
+AddCondition(1, cf_trigger, "On saving", "Save chess", 
+             "On saving <i>{0}</i>", 'Saving handler. Triggered by "action:Save chess"', "OnSave");
+AddObjectParam("Object", "Object for loading");
+AddCondition(2, cf_trigger, "On loading", "Load chess", 
+             "On loading <i>{0}</i>", 'Loading handler. Triggered by "action:Load all instances"', "OnLoad");
+AddObjectParam("Object", "Object for picking");             
+AddNumberParam("UID", "UID of object", 0);
+AddCondition(3, 0, "Pick by saved UID", "Load chess", 
+             "Pick <i>{0}</i> by saved UID to <i>{1}</i>", 
+              "Pick instance by saved UID.", "PickBySavedUID");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(0, 0, "Clean bank", "Bank", 
+          "Clean bank to empty", 
+          "Clean bank to empty.", "CleanBank");  
+AddObjectParam("Board", "The board object for saving");
+AddComboParamOption("Tiles");
+AddComboParamOption("Chess");
+AddComboParamOption("Tiles and Chess");
+AddComboParam("Target", "Save tiles, or chess, or both", 2);
+AddAction(1, 0, "Save chess", "Save chess", 
+          "Save <i>{1}</i> on <i>{0}</i>", 
+          "Save chess on board.", "SaveInstances");
+AddObjectParam("Board", "The board object for saving");          
+AddAction(2, 0, "Load all chess", "Load chess", 
+          "Load all saved chess to <i>{0}</i>", 
+          "Load all saved chess.", "LoadInstances");
+AddStringParam("JSON string", "JSON string.", '""');
+AddAction(3, 0, "Load bank from JSON string", "Bank", "Load bank from JSON string <i>{0}</i>",
+         "Load bank from JSON string.", "StringToBank");
+AddAnyTypeParam("Index", "Index of info, can be number of string", "0");
+AddAnyTypeParam("Value", "Value of info", 0);
+AddAction(4, 0, "Save info", "Save", 
+          "Save custom info[<i>{0}</i>] to <i>{1}</i>", 'Save custom info. Used under "Condition:On saving"', "SaveInfo"); 
+AddObjectParam("Object", "Object for picking");          
+AddNumberParam("UID", "UID of object", 0);
+AddAction(5, 0, "Pick by saved UID", "Load instance", 
+          "Pick <i>{0}</i> by saved UID to <i>{1}</i>", 
+          "Pick instance by saved UID.", "PickBySavedUID");      
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_string, "Transfer bank to string", "Bank", "BankToString", "Transfer current bank to JSON string.");
+AddAnyTypeParam("0", "The index of info to get, can be number of string.", "0");
+AddExpression(1, ef_return_any | ef_variadic_parameters, "Get info", "Load", "SavedInfo", 
+             'Get saved custom info for instances loading. Used under "Condition:On loading"');
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [	    
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_chessbank/runtime.js
@@ -0,0 +1,160 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_ChessBank = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_ChessBank.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.bank = null;    
+        this._target_inst = null;
+        this._info = {};
+	};
+	
+    instanceProto.instbank_get = function()
+    {     
+        if (this.bank != null)
+            return;
+        assert2(cr.plugins_.Rex_InstanceBank, "Chess bank: please put a instance bank object into project");
+        this.bank = new cr.plugins_.Rex_InstanceBank.InstBankKlass(this);   
+    }; 		
+	// handlers
+    instanceProto.OnSaving = function(inst, ret_info)
+    {     
+        this._target_inst = inst;
+        this._info = ret_info;
+        this.runtime.trigger(cr.plugins_.Rex_ChessBank.prototype.cnds.OnSave, this);
+    }; 	
+    instanceProto.OnLoading = function(inst, info)
+    {
+        this._target_inst = inst;
+        this._info = info;    
+        this.runtime.trigger(cr.plugins_.Rex_ChessBank.prototype.cnds.OnLoad, this);
+    };     
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();
+    
+	Cnds.prototype.OnSave = function (obj_type)
+	{
+		if (!obj_type)
+			return;    
+	    return this.bank.SOLPickOne(obj_type, this._target_inst);
+	};
+    
+	Cnds.prototype.OnLoad = function (obj_type)
+	{
+		if (!obj_type)
+			return;      
+		return this.bank.SOLPickOne(obj_type, this._target_inst);
+	}; 
+     
+	Cnds.prototype.PickBySavedUID = function (obj_type, saved_uid)
+	{
+		if (!obj_type)
+			return;      
+		return this.bank.SOLPickBySavedUID(obj_type, saved_uid);
+	};  
+    
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+    
+    Acts.prototype.CleanBank = function ()
+	{
+	    this.instbank_get();
+        this.bank.CleanBank();
+	};
+    
+    Acts.prototype.SaveInstances = function (board_obj, save_target_type)
+	{
+        this.instbank_get();	    
+		if (!board_obj)
+			return;      
+        this.bank.SaveInstances(obj_type, is_pick_all);
+	};
+
+    Acts.prototype.LoadInstances = function (board_obj)
+	{  
+        this.instbank_get();	    
+        this.bank.LoadAllInstances();
+	};
+
+    Acts.prototype.StringToBank = function (JSON_string)
+	{  
+        this.instbank_get();	    
+        this.bank.JSON2Bank(JSON_string);
+	};  
+
+    Acts.prototype.SaveInfo = function (index, value)
+	{  
+        this.instbank_get();	    
+        this._info[index] = value;
+	};
+    
+	Acts.prototype.PickBySavedUID = function (obj_type, saved_uid)
+	{
+        this.instbank_get();	    
+		if (!obj_type)
+			return;      
+		this.bank.SOLPickBySavedUID(obj_type, saved_uid);
+	};  
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+	Exps.prototype.BankToString = function (ret)
+	{
+        this.instbank_get();	    
+        var json_string = this.bank.ToString();
+		ret.set_string(json_string);
+	}; 
+
+    Exps.prototype.SavedInfo = function (ret, index, default_value)
+	{
+        this.instbank_get();	    
+        var val = this.bank._info[index];
+        if (val == null)
+            val = default_value;
+	    ret.set_any(val);
+	};     
+}());
