bd7e025f86545832285956f91ece1b0047737591
hua <binhua.tein@gmail.com>
2012-12-18 01:57:36 +0800

    Add layout to board plugin

---
+++ plugins/rex_layout2board/edittime.js
@@ -0,0 +1,99 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Layout to Board",
+		"id":			"Rex_layout2board",
+		"version":		"0.1",   		
+		"description":	"Put layout's instances into square grid board.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Board",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions     
+AddObjectParam("Chess", "Chess object");
+AddObjectParam("Board", "Board object");
+AddAnyTypeParam("Logic Z", "The logic Z index (0-based).", 0);
+AddAction(1, 0, "Put chess on board", "Chess", 
+          "Put <i>{0}</i> on <i>{1}</i> with logic Z index to <i>{2}</i>", 
+          "Put chess on board.", "Layout2Board");
+AddNumberParam("Width", "Cell width in pixels.", 0);
+AddNumberParam("Height", "Cell height in pixels.", 0);
+AddAction(3, 0, "Set cell size", "Size", 
+          "Set cell width to <i>{0}</i>, height to <i>{1}</i>", 
+          "Set cell size.", "SetCellSize");          
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_float, "Width", 32, "Cell width in pixels."),
+	new cr.Property(ept_float, "Height", 32, "Cell height in pixels.")
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_layout2board/runtime.js
@@ -0,0 +1,112 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_layout2board = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_layout2board.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.cell_width = this.properties[0];
+        this.cell_height = this.properties[1];  
+        this.px0 = null;
+        this.py0 = null;
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();      
+    
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+    Acts.prototype.Layout2Board = function (chess_objs, board_objs, lz)
+	{  
+		if ((!chess_objs) || (!board_objs))
+			return;    
+            
+        var board = board_objs.getFirstPicked();
+        var instances = chess_objs.getCurrentSol().getObjects();
+        var i, cnt=instances.length, chess; 
+        var pxmax=null, pymax=null;
+        for (i=0; i<cnt; i++)
+        {
+            chess = instances[i];
+            if ((this.px0==null) || (chess.x < this.px0))
+                this.px0 = chess.x;
+            if ((this.py0==null) || (chess.y < this.py0))
+                this.py0 = chess.y;  
+            if (lz==0)                
+            {
+                if ((pxmax==null) || (chess.x > pxmax))
+                    pxmax = chess.x;
+                if ((pymax==null) || (chess.y > pymax))
+                    pymax = chess.y;                   
+            }
+        }
+        if (lz==0)
+        {
+            var logic_width=Math.ceil((pxmax-this.px0)/this.cell_width)+1;
+            var logic_height=Math.ceil((pymax-this.py0)/this.cell_height)+1;
+            board.reset_board(logic_width, logic_height);
+            board.layout.PositionOX = this.px0;
+            board.layout.PositionOY = this.py0;
+        }
+        var lx, ly;            
+        for (i=0; i<cnt; i++)
+        {
+            chess = instances[i];
+            lx = (chess.x - this.px0)/this.cell_width;
+            ly = (chess.y - this.py0)/this.cell_height;
+            board.add_item(chess, Math.ceil(lx), Math.ceil(ly), lz);
+        }
+	};
+    
+    Acts.prototype.SetCellSize = function (width, height)
+    {
+        this.cell_width = width;
+        this.cell_height = height; 
+	};
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+
+}());
