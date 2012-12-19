c7710993651ddd2119dccffb1d3319e7b6b51f1a
hua <binhua.tein@gmail.com>
2012-12-19 23:55:50 +0800

    Add fps monitor plugin

---
+++ behaviors/rex_text_fpsmonitor/edittime.js
@@ -0,0 +1,82 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"FPS monitor",
+		"id":			"Rex_text_fpsmonitor",
+		"description":	"Dump fps infomation.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Text",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParamOption("Toggle");
+AddComboParam("Activated", "Enable the rotation behavior.",1);
+AddAction(0, 0, "Set activated", "", "Set {my} activated to <i>{0}</i>", "Enable the fps moniter.", "SetActivated");
+              
+//////////////////////////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [ 
+    new cr.Property(ept_combo, "Activated", "Yes", 
+                    "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+    new cr.Property(ept_combo, "Current fps", "Yes", "Display current fps.", "No|Yes"),      
+    new cr.Property(ept_combo, "Minimum fps", "Yes", "Display minimum fps.", "No|Yes"),   
+    new cr.Property(ept_combo, "Maximum fps", "Yes", "Display maximum fps.", "No|Yes"),
+    new cr.Property(ept_combo, "Average fps", "Yes", "Display average fps.", "No|Yes"),                   
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_text_fpsmonitor/runtime.js
@@ -0,0 +1,129 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_text_fpsmonitor = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_text_fpsmonitor.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{  
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{         
+	    this.activated = (this.properties[0] == 1);
+	    this.current_fps_enable = (this.properties[1] == 1);
+	    this.minimum_fps_enable = (this.properties[2] == 1);
+	    this.maximum_fps_enable = (this.properties[3] == 1);	
+	    this.average_fps_enable = (this.properties[4] == 1);	
+	    this._reset();       
+	};  
+	
+	behinstProto._reset = function()
+	{         	    
+	    this.min_fps = null;
+	    this.max_fps = null;	 
+	    this.acc_fps = 0; 
+	    this.tick_cnt = 0;        
+	};
+	 
+	behinstProto.SetText = function (param)
+	{	    
+        cr.plugins_.Text.prototype.acts.SetText.apply(this.inst, [param]);
+	}; 
+	
+	behinstProto.tick = function ()
+	{
+	    if (!this.activated)
+	        return;
+	        
+	    var content = "";
+	    var cur_fps = this.runtime.fps;
+	    if (this.current_fps_enable)
+	        content += (Math.floor(cur_fps).toString() + "  ");
+	    if ((this.minimum_fps_enable) && (cur_fps != 0))
+	    {
+	        if ((this.min_fps == null) || (cur_fps < this.min_fps))
+	            this.min_fps = this.runtime.fps;
+	        content += ("min:" + Math.floor(this.min_fps).toString() + "  ");
+	    }
+	    if (this.maximum_fps_enable)
+	    {
+	        if ((this.max_fps == null) || (cur_fps > this.max_fps))
+	            this.max_fps = this.runtime.fps;
+	        content += ("max:" + Math.floor(this.max_fps).toString() + "  ");
+	    }
+	    if (this.average_fps_enable)
+	    {
+	        this.acc_fps += cur_fps;
+	        this.tick_cnt += 1;
+	        var avg_fps = this.acc_fps/this.tick_cnt;
+	        content += ("avg:" + Math.floor(avg_fps).toString());
+	    }	
+	    
+	    if (content != "")
+	        this.SetText(content);  
+	};
+ 
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetActivated = function (s)
+	{
+	    if (s == 2)
+	        this.activated = (!this.activated);
+	    else
+		    this.activated = (s==1);
+		    
+		if (!this.activated)
+		{
+		    this._reset();
+		    this.SetText(""); 
+		}
+	};  
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+}());
