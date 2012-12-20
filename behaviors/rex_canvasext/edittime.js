3790319045bee7c6c24f78a2dba716d8bd486ea3
hua <binhua.tein@gmail.com>
2012-12-20 23:28:14 +0800

    Add canvas ext to put image into

---
+++ behaviors/rex_canvasext/edittime.js
@@ -0,0 +1,77 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Canvas Ext",
+		"id":			"Rex_CanvasExt",
+		"description":	"Extension of canvas.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Canvas",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(10, cf_trigger, "On image URL loaded", "Web", "On image URL loaded", "Triggered after 'Load image from URL' when the image has finished loading.", "OnURLLoaded");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("URI", "Enter the URL on the web, or data URI, of an image to load.", "\"http://\"");
+AddComboParamOption("Resize to image size");
+AddComboParamOption("Keep current size");
+AddComboParam("Size", "Whether to resize the sprite to the size of the loaded image, or stretch it to the current size.");
+AddAction(10, 0, "Load image from URL", "Web", "Load image from <i>{0}</i> ({1})", "Replace the currently displaying animation frame with an image loaded from a web address or data URI.", "LoadURL");
+              
+//////////////////////////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [                 
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_canvasext/runtime.js
@@ -0,0 +1,113 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_CanvasExt = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_CanvasExt.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{  
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{               
+	    this.webGL_texture = null;
+	};  
+	
+	behinstProto.tick = function ()
+	{
+	};
+ 
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	
+	Cnds.prototype.OnURLLoaded = function ()
+	{
+		return true;
+	};
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+	
+	Acts.prototype.LoadURL = function (url_, resize_)
+	{
+		var img = new Image();
+		var self = this;
+		var curFrame_ = this.curFrame;
+		
+		img.onload = function ()
+		{
+		    var inst = self.inst;
+		    inst.ctx.drawImage(img, 0, 0, img.width, img.height);
+			
+			// WebGL renderer: need to create texture (canvas2D just draws with img directly)
+			if (self.runtime.glwrap)
+			{
+				if (self.webGL_texture)
+					self.runtime.glwrap.deleteTexture(self.webGL_texture);
+					
+				self.webGL_texture = self.runtime.glwrap.loadTexture(img, false, self.runtime.linearSampling);
+			}
+			
+			// Set size if necessary
+			if ((resize_ === 0) && 
+			    (inst.width != img.width) || (inst.height != img.height))
+			{
+				inst.width = img.width;
+				inst.height = img.height;
+				inst.set_bbox_changed();
+			}
+			
+			self.runtime.redraw = true;
+			self.runtime.trigger(cr.behaviors.Rex_CanvasExt.prototype.cnds.OnURLLoaded, inst);
+		};
+		
+		if (url_.substr(0, 5) !== "data:")
+			img.crossOrigin = 'anonymous';
+		
+		img.src = url_;
+	};
+	 
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+}());
