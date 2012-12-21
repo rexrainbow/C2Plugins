f3dba8b24e211ee52ef552bfe9c472d09c6560a4
hua <binhua.tein@gmail.com>
2012-12-21 22:14:16 +0800

    Add UndoRedo plugin

---
+++ plugins/rex_undoredo/edittime.js
@@ -0,0 +1,117 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Undo & Redo",
+		"id":			"Rex_UndoRedo",
+		"version":		"0.1",   		
+		"description":	"A data structure to support undo&redo steps/states.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Data & Storage",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, 0, "Can undo", "Undo", 
+             "Can undo", "Retrun true if can undo to previous step.", "CanUndo");
+AddCondition(2, 0, "Can redo", "Redo", 
+             "Can redo", "Retrun true if can redo to next step.", "CanRedo");              
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(1, 0, "Clean", "Recorder", 
+          "Clean all", 
+          "Clean all steps.", "CleanAll");   
+AddAnyTypeParam("Data", "Data of step", 0);
+AddAction(2, 0, "Push", "Recorder", 
+          "Push step to <i>{0}</i>", 
+          "Push step.", "Push"); 
+AddStringParam("JSON string", "JSON string.", '""');
+AddAction(10, 0, "Load steps from JSON string", "JSON", 
+          "Load steps from JSON string <i>{0}</i>",
+          "Load steps from JSON string.", "StringToSteps");          
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_any,
+              "Undo", "Undo", "Undo",
+              "Get previosu step.");
+AddExpression(2, ef_return_any,
+              "Redo", "Redo", "Redo",
+              "Get next step.");             
+AddExpression(3, ef_return_number, 
+              "Get count", "Recorder", "StepsCnt", "Get steps count.");    
+AddExpression(4, ef_return_any, 
+              "Get current step", "Recorder", "CurStep", "Get current step.");              
+AddExpression(5, ef_return_number, 
+              "Get current index", "Recorder", "CurIndex", "Get current step index.");               
+AddExpression(10, ef_return_string, "Get JSON from recorder", 
+              "JSON", "ToString", "Get JSON from recorder.");  
+              
+              
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Mode", "Steps", 
+                    "Use steps or states mode. They will have different undo/redo behaviors.", "Steps|states"),
+	new cr.Property(ept_integer, "Max count", 0, "Max count of steps. 0 is infinite."),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_undoredo/runtime.js
@@ -0,0 +1,248 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_UndoRedo = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_UndoRedo.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this._handler = (this.properties[0] == 0)? new steps_handler(this):
+                                                   new states_handler(this)
+	    this._is_states_mode = (this.properties[0] == 0);
+        this.max_record_length = this.properties[1];
+        this._recorder = [];
+        this._clean_all();
+	};
+
+	instanceProto._clean_all = function()
+	{
+        this._recorder.length = 0;
+        this._current_index = (-1);
+	};
+
+	instanceProto._push = function(data)
+	{
+	    if ((this._recorder.length-1) == this._current_index)
+	    {
+            this._current_index += 1;	        
+            this._recorder.push(data);
+        }
+        else
+        {
+            this._current_index += 1;
+            this._recorder[this._current_index] = data;
+            this._recorder.length = this._current_index+1;
+        }
+            
+        if ((this.max_record_length > 0) && 
+            (this._recorder.length > this.max_record_length))  
+        {      
+            this._recorder.shift();
+            this._current_index -= 1;
+        }          
+	};
+
+    var states_handler = function(plugin)
+    {
+        this.plugin = plugin;
+    };
+    var states_handlerProto = states_handler.prototype;
+    // undo
+	states_handlerProto.CanUndo = function()
+	{
+	    return (this.plugin._current_index > 0);
+	};
+	states_handlerProto.Undo = function()
+	{	    
+	    if (!this.CanUndo())
+	        return 0;
+	    
+	    var plugin = this.plugin;
+	    plugin._current_index -=1;
+	    return plugin._recorder[plugin._current_index];
+	};
+	// redo
+	states_handlerProto.CanRedo = function()
+	{
+	    var plugin = this.plugin;
+	    return (plugin._current_index < (plugin._recorder.length-1));
+	};
+	states_handlerProto.Redo = function()
+	{
+	    if (!this.CanRedo())
+	        return 0;
+	    
+	    var plugin = this.plugin;
+	    plugin._current_index +=1 ;
+	    return plugin._recorder[plugin._current_index];
+	};	
+	
+    var steps_handler = function(plugin)
+    {
+        this.plugin = plugin;
+    };
+    var steps_handlerProto = steps_handler.prototype;
+    // undo
+	steps_handlerProto.CanUndo = function()
+	{
+	    return (this.plugin._current_index >= 0);
+	};
+	steps_handlerProto.Undo = function()
+	{	    
+	    if (!this.CanUndo())
+	        return 0;
+	    
+	    var plugin = this.plugin;
+	    var val = plugin._recorder[plugin._current_index];
+	    plugin._current_index -=1;
+	    return val;
+	};
+	// redo
+	steps_handlerProto.CanRedo = function()
+	{
+	    var plugin = this.plugin;
+	    return (plugin._current_index <= (plugin._recorder.length-1));
+	};
+	steps_handlerProto.Redo = function()
+	{
+	    if (!this._can_redo())
+	        return 0;
+	    
+	    var plugin = this.plugin;
+	    var val = plugin._recorder[plugin._current_index];
+	    plugin._current_index +=1 ;
+	    return val;
+	};
+	
+	
+	// current step
+	instanceProto._has_steps = function()
+	{
+	    return (this._current_index != -1);
+	};
+	instanceProto._cur_step = function()
+	{
+	    if (!this._has_steps())
+	        return 0;
+	    
+	    return this._recorder[this._current_index];
+	};
+	
+	// JSON
+	instanceProto._to_string = function()
+	{
+	    return JSON.stringify({"data":this._recorder,
+	                           "curIndex":this._current_index,
+	                           });
+	};
+	instanceProto._to_steps = function(JSON_string)
+	{
+	    if (JSON_string == "")
+	        return;
+	    var o = JSON.parse(JSON_string);
+	    this._recorder = o["data"];
+	    this._current_index = o["curIndex"];
+	};
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds(); 
+     
+	Cnds.prototype.CanUndo = function ()
+	{
+		return this._handler.CanUndo();      
+	};
+     
+	Cnds.prototype.CanRedo = function ()
+	{
+		return this._handler.CanRedo();        
+	};
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+		
+	Acts.prototype.CleanAll = function ()
+	{        
+	    this._clean_all();
+	};	
+		
+	Acts.prototype.Push = function (data)
+	{        
+	    this._push(data);
+	};	
+		
+	Acts.prototype.StringToSteps = function (JSON_string)
+	{        
+	    this._to_steps(JSON_string);
+	};		
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+	Exps.prototype.Undo = function (ret)
+	{
+	    ret.set_any(this._handler.Undo());
+	};
+	
+	Exps.prototype.Redo = function (ret)
+	{
+	    ret.set_any(this._handler.Redo());
+	};	
+	
+	Exps.prototype.CurStep = function (ret)
+	{
+	    ret.set_any(this._cur_step());
+	};
+		
+	Exps.prototype.StepsCnt = function (ret)
+	{
+	    ret.set_int(this._recorder.length);
+	};
+		
+	Exps.prototype.CurIndex = function (ret)
+	{
+	    ret.set_int(this._current_index);
+	};		
+	
+	Exps.prototype.ToString = function (ret)
+	{
+	    ret.set_string(this._to_string());
+	};
+}());
