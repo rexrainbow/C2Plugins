454c4f052472f7b274ff1298a2c85be75c8e1660
hua <binhua.tein@gmail.com>
2012-12-24 21:34:40 +0800

    Add drag arrowkey behavior

---
+++ behaviors/rex_dragArrowkey2/edittime.js
@@ -0,0 +1,105 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Drag-Arrowkey2",
+		"id":			"Rex_DragArrowkey2",
+		"version":		"0.1",        
+		"description":	"Get arrow key event from dragging input.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Input",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Actions
+
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On Up arrow pressed", "Pressd", "On {my} Up arrow pressed", 
+             "Triggered when Up arrow is pressed.", "OnUPPressed");
+AddCondition(2, cf_trigger, "On Down arrow pressed", "Pressd", "On {my} Down arrow pressed", 
+             "Triggered when Down arrow is pressed.", "OnDOWNPressed");
+AddCondition(3, cf_trigger, "On Left arrow pressed", "Pressd", "On {my} Left arrow pressed", 
+             "Triggered when Left arrow is pressed.", "OnLEFTPressed");
+AddCondition(4, cf_trigger, "On Right arrow pressed", "Pressd", "On {my} Right arrow pressed", 
+             "Triggered when Right arrow is pressed.", "OnRIGHTPressed");
+AddCondition(5, cf_trigger, "On any arrow pressed", "Pressd", "On {my} any arrow pressed", 
+             "Triggered when any arrow is pressed.", "OnAnyPressed");
+
+AddCondition(11, 0,	"Up arrow is down",	"Is down", "{my} Up arrow is down", 
+             "Return true if Up arrow is currently held down.", "IsUPDown");
+AddCondition(12, 0,	"Down arrow is down", "Is down", "{my} Down arrow is down", 
+             "Return true if Down arrow is currently held down.", "IsDOWNDown");
+AddCondition(13, 0,	"Left arrow is down", "Is down", "{my} Left arrow is down", 
+             "Return true if Left arrow is currently held down.", "IsLEFTDown");
+AddCondition(14, 0,	"Right arrow is down", "Is down", "{my} Right arrow is down", 
+             "Return true if Right arrow is currently held down.", "IsRIGHTDown");
+
+AddCondition(21, cf_trigger, "On Up arrow released", "Released", "On {my} Up arrow released", 
+             "Triggered when Up arrow is released.", "OnUPReleased");
+AddCondition(22, cf_trigger, "On Down arrow released", "Released", "On {my} Down arrow released", 
+             "Triggered when Down arrow is released.", "OnDOWNReleased");
+AddCondition(23, cf_trigger, "On Left arrow released", "Released", "On {my} Left arrow released", 
+             "Triggered when Left arrow is released.", "OnLEFTReleased");
+AddCondition(24, cf_trigger, "On Right arrow released", "Released", "On {my} Right arrow released", 
+             "Triggered when Right arrow is released.", "OnRIGHTReleased");
+             
+//////////////////////////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+    new cr.Property(ept_combo, "Directions", "8 directions", "The number of directions of movement available.", "Up & down|Left & right|4 directions|8 directions"),
+    new cr.Property(ept_float, "Sensitivity", 10, "Sensitivity of touch movment, in pixel."),    
+    new cr.Property(ept_combo, "Right direction", "Right", "The absolute direction of relate right direction.", "Right|Left|Up|Down"),    
+    ];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_dragArrowkey2/runtime.js
@@ -0,0 +1,346 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_DragArrowkey2 = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_DragArrowkey2.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+        this.touchwrap = null;
+        this.GetX = null;
+        this.GetY = null;
+        this.behavior_index = null;
+        this._behavior_insts = [];
+	};
+    
+	behtypeProto.TouchWrapGet = function ()
+	{
+        if (this.touchwrap != null)
+            return;
+            
+        var plugins = this.runtime.types;
+        var name, obj;
+        for (name in plugins)
+        {
+            obj = plugins[name].instances[0];
+            if ((obj != null) && (obj.check_name == "TOUCHWRAP"))
+            {
+                this.touchwrap = obj;
+                this.GetX = cr.plugins_.rex_TouchWrap.prototype.exps.XForID;
+                this.GetY = cr.plugins_.rex_TouchWrap.prototype.exps.YForID;
+                this.touchwrap.HookMe(this);
+                break;
+            }
+        }
+        assert2(this.touchwrap, "You need put a Touchwrap object for Cursor behavior");
+	};  
+    
+    behtypeProto.OnTouchStart = function (touch_src, touchX, touchY)
+    {
+        var sol = this.objtype.getCurrentSol(); 
+        var select_all_save = sol.select_all;
+        sol.select_all = true;
+        var overlap_cnt = this.runtime.testAndSelectCanvasPointOverlap(this.objtype, touchX, touchY, false);
+        if (overlap_cnt == 0)
+        {
+            // recover to select_all_save
+            sol.select_all = select_all_save;        
+            return false;
+        }
+        
+        // overlap_cnt > 0
+        // 0. find out index of behavior instance
+        if (this.behavior_index == null )
+            this.behavior_index = this.objtype.getBehaviorIndexByName(this.name);
+            
+            
+        // 1. get all valid behavior instances            
+        var ovl_insts = sol.getObjects();
+        var i, cnt, inst, behavior_inst;          
+        cnt = ovl_insts.length;           
+        for (i=0; i<cnt; i++ )
+        {
+		    inst = ovl_insts[i];
+            behavior_inst = inst.behavior_insts[this.behavior_index];
+            if (behavior_inst.activated && (!behavior_inst.is_on_dragging))
+            {
+                behavior_inst.on_control_start(touch_src);  
+                break;
+            }
+        }
+        
+        // recover to select_all_save
+        sol.select_all = select_all_save;
+    };
+    
+    behtypeProto.OnTouchEnd = function (touch_src)
+    {
+        if (this.behavior_index == null )
+            return;
+			
+	    var sol = this.objtype.getCurrentSol();
+        var select_all_save = sol.select_all;	
+		sol.select_all = true;
+		var insts = sol.getObjects();
+        var i, cnt=insts.length, inst, behavior_inst;
+        for (i=0; i<cnt; i++ )
+        {
+		    inst = insts[i];
+            behavior_inst = inst.behavior_insts[this.behavior_index];
+            if (behavior_inst.is_on_dragging && (behavior_inst.touch_src == touch_src))
+            {
+                behavior_inst.on_control_end();  
+                break;
+            }          
+        }	
+		sol.select_all = select_all_save;     
+    };  
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+        
+        type.TouchWrapGet();                                 
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{   
+        this.activated = (this.properties[0]==1);  
+        this._directions = this.properties[1]; 
+        this._sensitivity = this.properties[2];
+        this._angle_offset = (this.properties[3]==0)? 0:
+                             (this.properties[3]==1)? 180:
+                             (this.properties[3]==2)? 270:
+                                                      90;
+
+        this.is_on_dragging = false;         
+        this.touch_src = null;
+		this.pre_x = 0;
+		this.pre_y = 0; 
+        this.keyMap = [false, false, false, false];
+        this.pre_key_id = 0;  
+        
+        this.press_handlers =   [cr.behaviors.Rex_DragArrowkey2.prototype.cnds.OnRIGHTPressed,
+                                 cr.behaviors.Rex_DragArrowkey2.prototype.cnds.OnDOWNPressed,
+                                 cr.behaviors.Rex_DragArrowkey2.prototype.cnds.OnLEFTPressed,
+                                 cr.behaviors.Rex_DragArrowkey2.prototype.cnds.OnUPPressed     ];   
+        this.release_handlers = [cr.behaviors.Rex_DragArrowkey2.prototype.cnds.OnRIGHTReleased,
+                                 cr.behaviors.Rex_DragArrowkey2.prototype.cnds.OnDOWNReleased,
+                                 cr.behaviors.Rex_DragArrowkey2.prototype.cnds.OnLEFTReleased,
+                                 cr.behaviors.Rex_DragArrowkey2.prototype.cnds.OnUPReleased    ];             
+	};
+	
+    var RIGHTKEY = 0x1;
+    var DOWNKEY = 0x2;
+    var LEFTKEY = 0x4;
+    var UPKEY = 0x8;
+	behinstProto.tick = function ()
+	{  
+        if ((this.type.touchwrap == null) || (!this.activated) || (!this.is_on_dragging)) 
+            return;
+        
+        var cur_x = this.GetX();
+        var cur_y = this.GetY();
+        var dx = cur_x - this.pre_x;
+        var dy = cur_y - this.pre_y;      
+        
+        if ( Math.sqrt(dx*dx + dy*dy) >= this._sensitivity )
+        {
+            var angle = cr.to_clamped_degrees(Math.atan2(dy,dx));
+            angle -= this._angle_offset;
+            switch (this._directions)
+            {
+            case 0:
+                var key_id = (angle <180)? DOWNKEY:UPKEY;
+                this._keydown(key_id);
+                break;
+            case 1:
+                var key_id = ((angle >90) && (angle <=270))? LEFTKEY:RIGHTKEY;
+                this._keydown(key_id);
+                break;  
+            case 2:
+                var key_id = ((angle>45) && (angle<=135))?  DOWNKEY:
+                             ((angle>135) && (angle<=225))? LEFTKEY:
+                             ((angle>225) && (angle<=315))? UPKEY:RIGHTKEY;                          
+                this._keydown(key_id);
+                break;
+            case 3:
+                var key_id = ((angle>22.5) && (angle<=67.5))?   (DOWNKEY | RIGHTKEY):
+                             ((angle>67.5) && (angle<=112.5))?  DOWNKEY:
+                             ((angle>112.5) && (angle<=157.5))? (DOWNKEY | LEFTKEY):
+                             ((angle>157.5) && (angle<=202.5))? LEFTKEY:
+                             ((angle>202.5) && (angle<=247.5))? (LEFTKEY | UPKEY):
+                             ((angle>247.5) && (angle<=292.5))? UPKEY:
+                             ((angle>292.5) && (angle<=337.5))? (UPKEY | RIGHTKEY): RIGHTKEY;                          
+                this._keydown(key_id);
+                break;                
+            }         
+            this.pre_x = cur_x;
+            this.pre_y = cur_y;                    
+        }
+	}; 
+	
+    var _keyid_list = [RIGHTKEY,DOWNKEY,LEFTKEY,UPKEY];       
+    behinstProto._keydown = function(key_id)
+    {
+        if (this.pre_key_id == key_id)
+            return;
+            
+        var i;
+        for (i=0; i<4; i++)
+        {
+            var is_key_down = ((_keyid_list[i] & key_id) != 0);            
+            if (this.keyMap[i] && (!is_key_down))
+                this.runtime.trigger(this.release_handlers[i], this.inst);
+        }
+        var pressed_any_key = false;
+        for (i=0; i<4; i++)
+        {
+            var is_key_down = ((_keyid_list[i] & key_id) != 0); 
+            if ((!this.keyMap[i]) && is_key_down)
+            {
+                this.runtime.trigger(this.press_handlers[i], this.inst);
+                pressed_any_key = true;
+            }
+            this.keyMap[i] = is_key_down;
+        }         
+        if (pressed_any_key)
+            this.runtime.trigger(cr.behaviors.Rex_DragArrowkey2.prototype.cnds.OnAnyPressed, this.inst);
+        
+        this.pre_key_id = key_id;
+    };
+    
+	behinstProto.on_control_start = function(touch_src)
+	{
+	    this.touch_src = touch_src;
+	    this.is_on_dragging = true;  
+		this.pre_x = this.GetX();
+		this.pre_y = this.GetY();    	     
+	};
+	behinstProto.on_control_end = function()
+	{
+	    this.touch_src = null;
+	    this.is_on_dragging = false;   
+	};	
+	behinstProto.GetX = function()
+	{
+        var touch_obj = this.type.touchwrap;
+        this.type.GetX.call(touch_obj, 
+                            touch_obj.fake_ret, this.touch_src, this.inst.layer.index);
+        return touch_obj.fake_ret.value;          
+	};
+    
+	behinstProto.GetY = function()
+	{
+        var touch_obj = this.type.touchwrap;
+        this.type.GetY.call(touch_obj, 
+                            touch_obj.fake_ret, this.touch_src, this.inst.layer.index);
+        return touch_obj.fake_ret.value;         
+	};  
+	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();    
+   
+	Cnds.prototype.IsUPDown = function()
+	{
+        return this.keyMap[3];
+	};
+	Cnds.prototype.IsDOWNDown = function()
+	{
+        return this.keyMap[1];    
+	};	
+	Cnds.prototype.IsLEFTDown = function()
+	{
+        return this.keyMap[2];
+	};
+	Cnds.prototype.IsRIGHTDown = function()
+	{
+        return this.keyMap[0];
+	};    
+    
+	Cnds.prototype.OnUPPressed = function()
+	{
+        return true;
+	};
+	Cnds.prototype.OnDOWNPressed = function()
+	{
+        return true;    
+	};    
+	Cnds.prototype.OnLEFTPressed = function()
+	{
+        return true;    
+	};
+	Cnds.prototype.OnRIGHTPressed = function()
+	{
+        return true;    
+	};      
+
+	Cnds.prototype.OnAnyPressed = function()
+	{
+        return true;    
+	};
+	
+	Cnds.prototype.OnUPReleased = function()
+	{
+        return true;
+	};
+	Cnds.prototype.OnDOWNReleased = function()
+	{
+        return true;    
+	};  
+	Cnds.prototype.OnLEFTReleased = function()
+	{
+        return true;    
+	};
+	Cnds.prototype.OnRIGHTReleased = function()
+	{
+        return true;    
+	};     
+      
+       
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetActivated = function (s)
+	{
+		this.activated = (s==1);
+	};  
+      
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+}());
