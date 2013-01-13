af537fd1d2875f970250801222873a0bc613e4ff
hua <binhua.tein@gmail.com>
2013-01-13 23:45:36 +0800

    Add miniboard plugin

---
+++ plugins/rex_miniboard/edittime.js
@@ -0,0 +1,151 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Mini board",
+		"id":			"Rex_MiniBoard",
+		"version":		"0.1",
+		"description":	"A board-container to group chess and move them together.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Board",
+		"type":			"world",			// appears in layout
+		"rotatable":	true,
+		"flags":	    pf_position_aces
+	};
+};
+
+////////////////////////////////////////
+// Conditions 
+AddObjectParam("Board", "Board object");
+AddNumberParam("Logic X", "The X index of offset.", 0);
+AddNumberParam("Logic Y", "The Y index of offset.", 0);
+AddCondition(1, 0, "Empty", "Main board", 
+             "Cells on <i>{0}</i> at offset to [<i>{1}</i>,<i>{2}</i>] is empty", "Testing if cells on the board are empty.", "IsEmpty");
+AddCondition(2, cf_not_invertible, "Pick all chess", "SOL", 
+             "Pick all chess on this mini board", "Pick all chess on this mini board.", "PickAllChess");   
+AddObjectParam("Chess", "Chess under mini board");
+AddCondition(3, cf_static | cf_not_invertible, "Pick mini board", "SOL: mini board", 
+          "Pick mini board from <i>{0}</i>", "Pick mini board from chess.", "PickMiniboard");               
+AddObjectParam("Board", "Board object");
+AddNumberParam("Logic X", "The X index of start.", 0);
+AddNumberParam("Logic Y", "The Y index of start.", 0);
+AddNumberParam("Range", "The range of cell from start.", 0);
+AddCondition(10, 0, "Can find empty", "Main board: find empty", 
+             "Can find empty logic index on <i>{0}</i>, start at [<i>{1}</i>,<i>{2}</i>], range to <i>{3}</i>", "Can find empty logic index on the board.", "CanFindEmpty");	
+
+////////////////////////////////////////
+// Actions
+AddObjectParam("Layout", "Layout to transfer logic index to physical position");
+AddAction(1, 0, "Setup layout", "0: Setup", 
+          "Set layout to <i>{0}</i>", 
+          "Setup layout to transfer logic index to physical position.", "SetupLayout");
+AddObjectParam("Chess", "Chess object.");
+AddNumberParam("Logic X", "The X index (0-based) of the chess to set.", 0);
+AddNumberParam("Logic Y", "The Y index (0-based) of the chess to set.", 0);
+AddAnyTypeParam("Logic Z", "The Z index (0-based) of the chess to set. 0 is tile.", 0);
+AddAnyTypeParam("Layer", "Layer name of number.", 0);
+AddAction(2, 0, "Create chess", "Physical: Create", "Create chess <i>{0}</i> to [<i>{1}</i>, <i>{2}</i>, <i>{3}</i>], on layer <i>{4}</i>", 
+          "Create chess on the mini board.", "CreateChess");  	
+AddObjectParam("Board", "Board object");
+AddNumberParam("Logic X", "The X index of offset.", 0);
+AddNumberParam("Logic Y", "The Y index of offset.", 0);
+AddAction(3, 0, "Put", "Main board", 
+             "Put on <i>{0}</i> at offset to [<i>{1}</i>,<i>{2}</i>]", "Put chess on the board.", "PutChess");	
+AddAction(4, 0, "Pull out", "Main board", 
+             "Pull out from main board", "Pull out from main board.", "PullOutChess");
+AddAction(5, 0, "Pick all chess", "SOL", 
+          "Pick all chess on the board", "Pick all chess on the board.", "PickAllChess");              
+////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number | ef_variadic_parameters,
+              "Logic X on main board", "Main board", "LX",
+              "Get logic X on main board. Add second parameter to indicate logic X on mini board.");
+AddExpression(2, ef_return_number | ef_variadic_parameters,
+              "Logic Y on main board", "Main board", "LY",
+              "Get logic Y on main board. Add second parameter to indicate logic X on mini board.");
+AddExpression(10, ef_return_number,
+              "Logic X of empty on main board", "Main board: find empty", "EmptyLX",
+              "Get logic X of empty on main board. Used under 'Condition:Can find empty logic index'");
+AddExpression(11, ef_return_number,
+              "Logic Y of empty on main board", "Main board: find empty", "EmptyLY",
+              "Get logic Y of empty on main board. Used under 'Condition:Can find empty logic index'");
+              
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [			
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	// this.myValue = 0...
+}
+
+IDEInstance.prototype.OnCreate = function()
+{
+    this.instance.SetHotspot(new cr.vector2(0, 0));
+}
+
+IDEInstance.prototype.OnInserted = function()
+{
+}
+
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+	
+// Called to draw self in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+    var quad = this.instance.GetBoundingQuad();
+    renderer.Fill(quad, cr.RGB(255, 130, 122));
+}
+
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
+
+IDEInstance.prototype.OnTextureEdited = function ()
+{
+}
---
+++ plugins/rex_miniboard/runtime.js
@@ -0,0 +1,511 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_MiniBoard = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_MiniBoard.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+		this.board = {};
+		this.items = {};
+        this._insts = {};
+
+        this.layout = null;	
+		this._kicked_chess_inst = null;	
+        this._pre_x = this.x;
+		this._pre_y = this.y;
+		this.main_board = null;
+		this.POX = (-1);
+		this.POY = (-1);
+		
+		this.myDestroyCallback = (function (self) {
+											return function(inst) {
+												self.onInstanceDestroyed(inst);
+											};
+										})(this); 
+        this.runtime.addDestroyCallback(this.myDestroyCallback); 
+		this.runtime.tick2Me(this); 
+
+        this.exp_EmptyLX = (-1);
+        this.exp_EmptyLY = (-1);
+	};
+	
+	instanceProto.onInstanceDestroyed = function (inst)
+	{
+	    var uid=inst.uid;		
+		if (this._insts[uid] == null)
+		    return;
+	    this.remove_item(uid);
+	};    
+    
+	instanceProto.onDestroy = function ()
+	{
+		var uid, inst, insts=this._insts;
+		for (uid in insts)
+		    this.runtime.DestroyInstance(insts[uid]);
+		this.runtime.removeDestroyCallback(this.myDestroyCallback);        	    	
+	};
+	instanceProto.chess_pos_set = function ()
+	{
+	    var dx = this.x - this._pre_x;
+		var dy = this.y - this._pre_y;
+		if ((dx == 0) && (dy == 0))
+		    return;
+			
+		var uid, inst, insts=this._insts;
+		for (uid in insts)
+		{
+		    inst = insts[uid];
+			inst.x += dx;
+			inst.y += dy;
+			inst.set_bbox_changed();
+		}
+        this._pre_x = this.x;
+		this._pre_y = this.y;
+	}; 
+	instanceProto.tick2 = function ()
+	{
+	    this.chess_pos_set();
+	};    
+	
+	instanceProto.draw = function(ctx)
+	{
+	};
+	
+	instanceProto.drawGL = function(glw)
+	{
+	};
+	var _get_uid = function(objs)
+	{
+        var uid;
+	    if (objs == null)
+	        uid = null;
+	    else if (typeof(objs) != "number")
+	    {
+	        var inst = objs.getFirstPicked();
+	        uid = (inst!=null)? inst.uid:null;
+	    }
+	    else
+	        uid = objs;
+            
+        return uid;
+	};
+    instanceProto._get_layer = function(layerparam)
+    {
+        return (typeof layerparam == "number")?
+               this.runtime.getLayerByNumber(layerparam):
+               this.runtime.getLayerByName(layerparam);
+    };
+	instanceProto.xyz2uid = function(x, y, z)
+	{
+	    var tmp = this.board[x];
+		if (tmp != null)
+		{
+		    tmp = tmp[y];
+			if (tmp != null)
+			    return tmp[z];
+		}
+		return null;
+	};
+	instanceProto._put_chess = function(x, y, z, uid)
+	{
+	    var tmp;
+		if (this.board[x] == null)
+		    this.board[x] = {};
+        tmp = this.board[x];
+		if (tmp[y] == null)
+		    tmp[y] = {};
+	    tmp = tmp[y];
+		tmp[z] = uid;
+	};	
+	instanceProto.uid2xyz = function(uid)
+	{
+	    return this.items[uid];
+	};
+	instanceProto.remove_item = function(uid, kicking_notify)
+	{        
+        if (uid == null)
+            return;
+	    
+        var _xyz = this.uid2xyz(uid);
+        if (_xyz == null)
+            return;
+                    
+        if (kicking_notify)
+        {
+            this._kicked_chess_inst = this._insts[uid];
+            //this.runtime.trigger(cr.plugins_.Rex_MiniBoard.prototype.cnds.OnChessKicked, this); 
+        }
+        
+        delete this.items[uid];
+        delete this.board[_xyz.x][_xyz.y][_xyz.z];        
+        delete this._insts[uid];		
+	};
+	instanceProto.add_item = function(inst, _x, _y, _z)
+	{                
+        // inst could be instance(object) or uid(number)
+        if (inst == null)
+            return;
+			
+        var uid = inst.uid;
+        this.remove_item(this.xyz2uid(_x,_y,_z), true);
+		this._put_chess(_x, _y, _z,uid);
+	    this.items[uid] = {x:_x, y:_y, z:_z};
+        this._insts[uid] = inst;
+        
+        inst.extra.rex_miniboard_uid = this.uid;
+        //this.runtime.trigger(cr.plugins_.Rex_MiniBoard.prototype.cnds.OnCollided, this);                                           
+	};
+
+	instanceProto.CreateItem = function(obj_type,x,y,z,_layer)
+	{
+        var layer = this._get_layer(_layer);
+        var inst = this.layout.CreateItem(obj_type,x,y,z,layer);
+        if (!inst)
+            return;
+        
+		this.runtime.isInOnDestroy++;
+		this.runtime.trigger(Object.getPrototypeOf(obj_type.plugin).cnds.OnCreated, inst);
+		this.runtime.isInOnDestroy--;
+        
+        // Pick just this instance
+        obj_type.getCurrentSol().pick_one(inst);
+
+        return inst;
+	};
+	instanceProto.CreateChess = function(obj_type,x,y,z,layer)
+	{
+        if ( (obj_type ==null) || (this.layout == null) )
+            return;
+
+	    var pox_save = this.layout.GetPOX();
+		var poy_save = this.layout.GetPOY();
+		this.layout.SetPOX(this.x);
+		this.layout.SetPOY(this.y);
+        var inst = this.CreateItem(obj_type,x,y,z,layer);
+		if (inst != null)
+	        this.add_item(inst,x,y,z);  
+		this.layout.SetPOX(pox_save);
+		this.layout.SetPOY(poy_save);
+	    return inst;
+	};
+	instanceProto.IsEmpty = function (board_inst, offset_lx, offset_ly)
+	{
+		var board_xmax = board_inst.x_max;
+		var board_ymax = board_inst.y_max;
+		var board = board_inst.board;   
+		var _xyz, x, y, z;
+		var uid, insts = this._insts;
+		for (uid in insts)
+		{
+		    _xyz = this.uid2xyz(uid);
+			x = _xyz.x+offset_lx;
+			y = _xyz.y+offset_ly;
+			z = _xyz.z;
+			if ((x < 0) || (x > board_xmax) || 
+			    (y < 0) || (y > board_ymax) || 
+			    (board[x][y][z] != null))
+			    return false;
+		}
+		return true;
+	}; 	
+	instanceProto.PutChess = function (board_inst, offset_lx, offset_ly)
+	{	 
+	    if (this.main_board != null)
+		    return;
+			
+		var _xyz, x, y, z;
+		var uid, insts = this._insts;
+		for (uid in insts)
+		{
+		    _xyz = this.uid2xyz(uid);
+			x = _xyz.x+offset_lx;
+			y = _xyz.y+offset_ly;
+			z = _xyz.z;			
+			board_inst.add_item(insts[uid], x, y, z);
+		}
+		this.x = board_inst.layout.GetX(offset_lx, offset_ly, 0);
+		this.y = board_inst.layout.GetY(offset_lx, offset_ly, 0);
+		this.chess_pos_set();
+		this.main_board = board_inst;
+		this.POX = offset_lx;
+		this.POY = offset_ly;		
+	};
+	
+	instanceProto.PullOutChess = function ()
+	{	 
+	    if (this.main_board == null)
+		    return;
+        
+		var _xyz, x, y, z;
+		var uid, insts = this._insts;
+		for (uid in insts)
+			this.main_board.remove_item(uid);
+		this.main_board = null;
+		this.POX = (-1);
+		this.POY = (-1);        
+	};
+    
+	instanceProto._pick_all_insts = function ()
+	{	    
+	    var uid, inst, objtype, sol;
+	    var insts=this._insts;
+	    var objtype_name={};	
+	    var has_inst = false;    
+	    for (uid in insts)
+	    {
+	        inst = insts[uid];
+	        objtype = inst.type; 
+	        sol = objtype.getCurrentSol();
+	        if (!(objtype.name in objtype_name))
+	        {
+	            sol.select_all = false;
+	            sol.instances.length = 0;
+	            objtype_name[objtype.name] = true;
+	        }
+	        sol.instances.push(inst);  
+	        has_inst = true;
+	    }
+	    return has_inst;
+	};
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();
+	  
+	Cnds.prototype.IsEmpty = function (board_objs, offset_lx, offset_ly)
+	{
+		if (!board_objs)
+			return; 
+		return this.IsEmpty(board_objs.getFirstPicked(), offset_lx, offset_ly);
+	}; 
+
+	Cnds.prototype.PickAllChess = function ()
+	{
+	    return this._pick_all_insts();
+	};
+    
+    var _uid2inst = {};
+    var _uid2inst_get = function(insts)
+    {
+        var uid;
+        for (uid in _uid2inst)
+            delete _uid2inst[uid];
+        var i, cnt=insts.length, inst;
+        for (i=0; i<cnt; i++)
+        {
+            inst = insts[i];
+            _uid2inst[inst.uid] = inst;
+        }
+        return _uid2inst;
+    };
+    
+	Cnds.prototype.PickMiniboard =function (objtype)
+	{
+		if (!objtype)
+			return; 
+            
+    	var insts = objtype.getCurrentSol().getObjects();        
+    	var cnt = insts.length;
+    	if (cnt == 0)
+            return false;  
+ 
+        var i, miniboard_uid, miniboard_inst;
+	    var runtime = this.runtime;
+	    var container_type = runtime.getCurrentCondition().type;         
+        var sol = container_type.getCurrentSol();
+        sol.select_all = true;
+        var uid2inst = _uid2inst_get(sol.getObjects());
+        sol.select_all = false;
+        sol.instances.length = 0;              
+        for (i=0;i<cnt;i++)
+        {
+            miniboard_uid = insts[i].extra.rex_miniboard_uid;
+            miniboard_inst = uid2inst[miniboard_uid];
+            if (miniboard_inst == null)
+                continue;            
+            sol.instances.push(miniboard_inst);
+            delete uid2inst[miniboard_uid];
+        }
+        var current_event = runtime.getCurrentEventStack().current_event;
+        runtime.pushCopySol(current_event.solModifiers);
+        current_event.retrigger();
+        runtime.popSol(current_event.solModifiers);
+		return false;            
+	}; 
+    
+	Cnds.prototype.CanFindEmpty = function (board_objs, _start_lx, _start_ly, _range)
+	{	
+		if ((!board_objs) || (_range <0))
+			return; 
+            
+        var board_inst = board_objs.getFirstPicked();
+        if (this.IsEmpty(board_inst, _start_lx, _start_ly))
+        {
+            this.exp_EmptyLX = _start_lx;
+            this.exp_EmptyLY = _start_ly;
+            return true;
+        }
+
+        var is_empty;
+        var r, width, x, y, sx, sy, i;
+        var dir, dir_x, dir_y;
+        for (r=1; r<=_range; r++)
+        {
+            width = r*2;
+            for (dir=0; dir<4; dir++)
+            {
+                switch (dir)
+                {
+                case 0:  // left-top
+                    sx = _start_lx - r;
+                    sy = _start_ly - r;          
+                    dir_x = 1; dir_y = 0;
+                break;
+                case 1:  // right-top
+                    sx = _start_lx + r;
+                    sy = _start_ly - r;
+                    dir_x = 0; dir_y = 1;
+                break;
+                case 2:  // right-bottom
+                    sx = _start_lx + r;
+                    sy = _start_ly + r;
+                    dir_x = -1; dir_y = 0;
+                break;                  
+                case 3:  // left-bottom
+                    sx = _start_lx - r;
+                    sy = _start_ly + r;  
+                    dir_x = 0; dir_y = -1;                    
+                break;                  
+                }
+                
+                for (i=0; i<width; i++)
+                {
+                    x = sx +(dir_x*i);
+                    y = sy+(dir_y*i);
+                    is_empty = this.IsEmpty(board_inst, x , y);
+                    if (is_empty)
+                    {
+                        this.exp_EmptyLX = x;
+                        this.exp_EmptyLY = y;
+                        return true;
+                    }
+                }
+            }         
+        }
+        this.exp_EmptyLX = (-1);
+        this.exp_EmptyLY = (-1);
+        return false;        
+	};	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+    
+    Acts.prototype.SetupLayout = function (layout_objs)
+	{   
+        var layout = layout_objs.instances[0];
+        if (layout.check_name == "LAYOUT")
+            this.layout = layout;        
+        else
+            alert ("Mini board should connect to a layout object");
+	};  
+	
+	Acts.prototype.CreateChess = function (obj_type,x,y,z,layer)
+	{ 
+		if (!obj_type)
+			return;	
+	    this.CreateChess(obj_type,x,y,z,layer);        
+	};	
+	
+	Acts.prototype.PutChess = function (board_objs, offset_lx, offset_ly)
+	{	 
+		if (!board_objs)
+			return;
+		this.PutChess(board_objs.getFirstPicked(), offset_lx, offset_ly);
+	};
+	
+	Acts.prototype.PullOutChess = function ()
+	{	 
+		this.PullOutChess();
+	};
+	
+	Acts.prototype.PickAllChess = function ()
+	{	
+        this._pick_all_insts();
+	};	    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+	Exps.prototype.LX = function (ret,_offsetLX)
+	{
+	    var lx;
+		if (this.main_board == null)
+		    lx = (-1);
+		else
+		{
+		    lx = this.POX;
+		    if (_offsetLX != null)
+			    lx += _offsetLX;
+		}
+	    ret.set_int(lx);
+	};
+	Exps.prototype.LY = function (ret,_offsetLY)
+	{
+	    var ly;
+		if (this.main_board == null)
+		    ly = (-1);
+		else
+		{
+		    ly = this.POY;
+		    if (_offsetLY != null)
+			    ly += _offsetLY;
+		}
+	    ret.set_int(ly);
+	};	
+	Exps.prototype.EmptyLX = function (ret)
+	{
+	    ret.set_int(this.exp_EmptyLX);
+	};
+	Exps.prototype.EmptyLY = function (ret)
+	{
+	    ret.set_int(this.exp_EmptyLY);
+	};    
+}());
