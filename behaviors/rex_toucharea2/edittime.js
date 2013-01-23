244b0e21910d71d8b6358d259f06669ba6713540
hua <binhua.tein@gmail.com>
2013-01-23 22:42:34 +0800

    Get touchX, touchY on sprite.

---
+++ behaviors/rex_toucharea2/edittime.js
@@ -0,0 +1,80 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Touch area",
+		"id":			"Rex_TouchArea2",
+		"version":		"0.1",        
+		"description":	"Get touch by tracking touchID.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Input",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On touch start", "Touch", "On touch start", "Triggered when touch input begins.", "OnTouchStart");
+AddCondition(1, cf_trigger, "On touch end", "Touch", "On touch end", "Triggered when touch input ends.", "OnTouchEnd");
+AddCondition(2, 0, "Is in touch", "Touch", "Is in touch", "True if touch is currently in contact with this object.", "IsInTouch");
+//AddCondition(3, cf_trigger, "On touch moving start", "Touch moving", "On touch moving start", "Triggered when touch and moving start.", "OnTouchMovingStart");
+
+//////////////////////////////////////////////////////////////
+// Actions
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "Touch X position", "Touch", "X", 
+              "Get the touch X co-ordinate in this object. Return 0 if not in touch this object.");
+AddExpression(1, ef_return_number, "Touch Y position", "Touch", "Y", 
+              "Get the touch Y co-ordinate in this object. Return 0 if not in touch this object.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    ];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_toucharea2/runtime.js
@@ -0,0 +1,148 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_TouchArea2 = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_TouchArea2.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+        this.touchwrap = null;
+	};
+    
+	behtypeProto.TouchWrapGet = function ()
+	{
+        if (this.touchwrap != null)
+            return;
+            
+        var plugins = this.runtime.types;
+        var name, obj;
+        for (name in plugins)
+        {
+            obj = plugins[name].instances[0];
+            if ((obj != null) && (obj.check_name == "TOUCHWRAP"))
+            {
+                this.touchwrap = obj;
+                break;
+            }
+        }
+        assert2(this.touchwrap, "You need put a Touchwrap object for TouchArea behavior");
+	};  
+    
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+        
+        type.TouchWrapGet();                                 
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+	    this.is_touched = false;
+	    this.cur_touchX = this.inst.x;
+	    this.cur_touchY = this.inst.y;
+	};	
+
+	behinstProto.tick = function ()
+	{  
+	    var touch_obj = this.type.touchwrap;
+        var touch_pts = touch_obj.touches;
+		var cnt=touch_pts.length;
+		
+		var is_touched = false, inst=this.inst;
+	   
+	    if (touch_obj.IsInTouch())
+	    {
+		    var i, touch_pt, tx, ty;
+		    for (i=0; i<cnt; i++)
+		    {
+		        inst.update_bbox();
+		        touch_pt = touch_pts[i];
+                tx = inst.layer.canvasToLayer(touch_pt.x, touch_pt.y, true);
+		    	ty = inst.layer.canvasToLayer(touch_pt.x, touch_pt.y, false);   		    
+		    	if (inst.contains_pt(tx,ty))
+		    	{
+		    	    this.cur_touchX = tx;
+		    	    this.cur_touchY = ty;
+		    	    is_touched = true;
+		    	    break;
+		    	}
+		    }
+	    }
+					
+		if ((!this.is_touched) && is_touched)
+		    this.runtime.trigger(cr.behaviors.Rex_TouchArea2.prototype.cnds.OnTouchStart, inst);
+		else if (this.is_touched && (!is_touched))
+		    this.runtime.trigger(cr.behaviors.Rex_TouchArea2.prototype.cnds.OnTouchEnd, inst);
+        this.is_touched = is_touched;			    
+	}; 
+
+	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();    
+
+	Cnds.prototype.OnTouchStart = function ()
+	{
+		return true;
+	};
+	
+	Cnds.prototype.OnTouchEnd = function ()
+	{
+		return true;
+	};
+	
+	Cnds.prototype.IsInTouch = function ()
+	{
+		return this.is_touched;
+	};   
+    
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+    
+	Exps.prototype.X = function (ret)
+	{
+		ret.set_float(this.cur_touchX);
+	};
+    
+	Exps.prototype.Y = function (ret)
+	{
+		ret.set_float(this.cur_touchY);
+	};    
+}());
