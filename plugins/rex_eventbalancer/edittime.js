36e965d449d330738aef61131f6b0dc8e1de887d
hua <binhua.tein@gmail.com>
2013-02-10 18:37:24 +0800

    Add event balancer to execute events duration many ticks

---
+++ plugins/rex_eventbalancer/edittime.js
@@ -0,0 +1,126 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Event balancer",
+		"id":			"Rex_EventBalancer",
+		"version":		"0.1",   		
+		"description":	"Divide heavy looping into ticks",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Control flow",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1,cf_trigger,"On start","Procedure",
+             "On procedure start",
+             "Triggered when procedure start.","OnStart");
+AddCondition(2,cf_trigger,"On processing","Procedure",
+             "On procedure processing",
+             "Triggered when procedure processing.","OnProcessing");
+AddCondition(3,cf_trigger,"On stop","Procedure",
+             "On procedure stop",
+             "Triggered when procedure stop.","OnStop");  
+AddCondition(4,0 ,"Is processing","Procedure",
+             "Is procedure processing",
+             "Return true if procedure processing.","IsProcessing");
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(1,0,"Start","Procedure",
+          "Start procedure",
+          'Start procedure. It will trigger Condition:"On start", then trigger Condition:"On processing"',"Start");
+AddAction(2,0,"Stop","Procedure",
+          "Stop procedure",
+          'Stop procedure. It will trigger Condition:"On Stop"',"Stop");  
+AddNumberParam("Procressing time", "Procressing time per tick in percentage. A tick time is (1/60), 1 is using full tick time.", 0.5);          
+AddAction(3,0,"Set procressing time","Dynamic mode",
+          "Set procressing time to <i>{0}</i>",
+          "Set procressing time","SetProcressingTime"); 
+AddNumberParam("Repeat count", 'Repeat count of triggering condition:"On processing" each tick.', 10);          
+AddAction(4,0,"Set repeat count","Static mode",
+          "Set repeat count to <i>{0}</i>",
+          "Set repeat count","SetRepeatCount");            	  
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, 
+              "Get procressing time", "Procressing time", "ProcressingTime", 
+              "Procressing time per tick in percentage."); 
+              
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Mode", "Dynamic", 'Mode of trigger condition:"On processing".', 
+                    "Dynamic|Static"),    
+    new cr.Property(ept_section, "Dynamic", "",	"Setting when using dynamic mode."),
+    new cr.Property(ept_float, "Procressing time", 0.5, "Procressing time per tick in percentage. A tick time is (1/60), 1 is using full tick time."),
+    new cr.Property(ept_section, "Static", "",	"Setting when using static mode."),
+    new cr.Property(ept_integer, "Repeat count", 10, 'Repeat count of triggering condition:"On processing" each tick.'),    
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+    if (this.properties["Procressing time"] > 1)
+        this.properties["Procressing time"] = 1;
+    if (this.properties["Procressing time"] < 0.01)
+        this.properties["Procressing time"] = 0.01; 
+    if (this.properties["Repeat count"] < 1)
+        this.properties["Repeat count"] = 1;         
+               
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_eventbalancer/runtime.js
@@ -0,0 +1,146 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_EventBalancer = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_EventBalancer.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this.is_dynamic_mode = (this.properties[0] == 0);
+        this.procressing_time = (1/60)*1000*this.properties[1];
+        this.repeat_count = this.properties[2];
+        this.cmd_stop = false;
+	};   
+    
+    instanceProto.tick = function()
+    {         
+        if (this.is_dynamic_mode)
+            this._run_dynamic_mode()
+        else
+            this._run_static_mode()
+            
+        if (this.cmd_stop)
+            this.runtime.trigger(cr.plugins_.Rex_EventBalancer.prototype.cnds.OnStop, this);
+    }; 
+	
+	instanceProto._run_dynamic_mode = function()
+	{
+        var start_time = Date.now();
+        while ((Date.now() - start_time) <= this.procressing_time)
+        {
+            this.runtime.trigger(cr.plugins_.Rex_EventBalancer.prototype.cnds.OnProcessing, this);
+            if (this.cmd_stop)
+                break;
+        }
+	};  
+	
+	instanceProto._run_static_mode = function()
+	{
+	    var i;
+	    for (i=0; i<this.repeat_count; i++)
+	    {
+            this.runtime.trigger(cr.plugins_.Rex_EventBalancer.prototype.cnds.OnProcessing, this);
+            if (this.cmd_stop)	        
+                break;
+	    }
+	};    
+  
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();      
+
+	Cnds.prototype.OnStart = function()
+	{    
+		return true;
+	};
+
+	Cnds.prototype.OnProcessing = function()
+	{    
+		return true;
+	};    
+
+	Cnds.prototype.OnStop = function()
+	{    
+		return true;
+	};    
+
+	Cnds.prototype.IsProcessing = function()
+	{    
+		return true;
+	};    
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+	Acts.prototype.Start = function()
+	{       
+        this.runtime.tickMe(this);
+        this.cmd_stop = false;
+        this.runtime.trigger(cr.plugins_.Rex_EventBalancer.prototype.cnds.OnStart, this);
+	}; 
+
+	Acts.prototype.Stop = function()
+	{       
+        this.runtime.untickMe(this);
+        this.cmd_stop = true;
+	}; 
+
+	Acts.prototype.SetProcressingTime = function(procressing_time)
+	{       
+        this.procressing_time = cr.clamp(procressing_time, 0.01, 1);
+	};
+
+	Acts.prototype.SetRepeatCount = function(repeat_count)
+	{       
+	    if (repeat_count < 1)
+	        repeat_count = 1;	        
+        this.repeat_count = repeat_count;
+	};	
+	     
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+    
+	Exps.prototype.ProcressingTime = function (ret)
+	{  
+        ret.set_any(this.procressing_time);
+	}; 
+}());
