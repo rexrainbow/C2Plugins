fd4cf8de300fe4f0eb8265bd03276c5d72fb21cf
hua <binhua.tein@gmail.com>
2013-03-11 21:58:57 +0800

    Add pause dt behavior

---
+++ behaviors/rex_pause_dt/edittime.js
@@ -0,0 +1,90 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Pause",
+		"id":			"Rex_PauseDt",
+		"description":	"Pause timescale of this instance",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"General",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On pause", "Event", "On pause", 
+             "Triggered when instance paused", "OnPause");
+AddCondition(1, cf_trigger, "On resume", "Event", "On resume", 
+             "Triggered when instance resumed", "OnResume");
+AddCondition(2, 0, "Is pause", "If", "Is pause", 
+             "Return true if instance is paused", "IsPause");
+AddCondition(3, cf_static | cf_not_invertible, "Pick paused instances", "SOL", "Pick paused instances", 
+             "Pick paused instances", "PickPauseInstances");
+AddCondition(4, cf_static | cf_not_invertible, "Pick activated instances", "SOL", "Pick activated instances", 
+             "Pick activated instances", "PickActivatedInstances");
+             
+//////////////////////////////////////////////////////////////
+// Actions 
+AddAction(0, 0, "Toggle pause", "Pause/Resume", 
+          "Toggle pause","Toggle pause.", "TooglePause");  
+AddComboParamOption("Pause");
+AddComboParamOption("Run");
+AddComboParam("State", "Set puase state to.",0);
+AddAction(1, 0, "Set pause state", "Pause/Resume", 
+          "Set pause state to <i>{0}</i>", "Set the puase state.", "SetState");
+          
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "Previous timescale", "Timescale", "PreTimescale", "Get previous timescale.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [    
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{	
+}
---
+++ behaviors/rex_pause_dt/runtime.js
@@ -0,0 +1,172 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_PauseDt = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_PauseDt.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;       
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{      
+        this.is_pause = false;
+        this.previous_timescale = 0;	     
+	};
+
+	behinstProto.tick = function ()
+	{
+	};  
+    
+	behinstProto._toogle_pause = function (state)
+	{
+        var cur_state = this.is_pause;
+        if (state == cur_state)
+            return;
+    
+        this.is_pause = (!cur_state);
+        var trig_method;
+        if (this.is_pause)
+        {
+            this.previous_timescale = this.inst.my_timescale;
+            this.inst.my_timescale = 0;
+            trig_method = cr.behaviors.Rex_PauseDt.prototype.cnds.OnPause;
+        }
+        else
+        {
+            this.inst.my_timescale = this.previous_timescale;
+            this.previous_timescale = 0;
+            trig_method = cr.behaviors.Rex_PauseDt.prototype.cnds.OnResume;
+        }
+        this.runtime.trigger(trig_method, this.inst);   
+	}; 
+	
+	var this_behavior_get = function (inst)
+	{
+		var i, len;
+		for (i = 0, len = inst.behavior_insts.length; i < len; i++)
+		{
+			if (inst.behavior_insts[i] instanceof behaviorProto.Instance)
+				return inst.behavior_insts[i];
+		}
+		
+		return null;
+	};	
+	 
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	Cnds.prototype.OnPause = function ()
+	{
+		return true;
+	};
+
+	Cnds.prototype.OnResume = function ()
+	{
+		return true;
+	};   
+
+	Cnds.prototype.IsPause = function ()
+	{
+		return this.is_pause;
+	};
+
+	var _inst_is_pause = function(inst)
+	{
+	    var b = this_behavior_get(inst);
+	    return (b != null)? b.is_pause:false;
+	};
+
+	var _pick_paused_instances = function (type, is_pause)
+	{
+	    var sol = type.getCurrentSol();  
+        //sol.select_all = true;   
+        var insts = sol.getObjects();
+        var insts_length = insts.length;
+        var i, inst;
+        sol.instances.length = 0;   // clear contents
+        for (i=0; i < insts_length; i++)
+        {
+           inst = insts[i];
+           if (_inst_is_pause(inst) == is_pause)
+               sol.instances.push(inst);
+        }
+        sol.select_all = false;  
+        type.applySolToContainer();  
+        return  (sol.instances.length >0);          
+	}; 
+	Cnds.prototype.PickPauseInstances = function ()
+	{	    
+	    var cnd = this.runtime.getCurrentCondition();
+		var type = cnd.type;
+		return _pick_paused_instances(type, true);
+	};	
+
+	Cnds.prototype.PickActivatedInstances = function ()
+	{	    
+	    var cnd = this.runtime.getCurrentCondition();
+		var type = cnd.type;
+		return _pick_paused_instances(type, false);
+	};		
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+    Acts.prototype.TooglePause = function ()
+	{
+        this._toogle_pause();       
+	}; 
+
+    Acts.prototype.SetState = function (state)
+	{
+        var is_pause = (state == 0);
+        this._toogle_pause(is_pause);       
+	};     
+    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+    Exps.prototype.PreTimescale = function (ret)
+	{
+	    ret.set_float( this.previous_timescale );
+	};
+    
+}());
