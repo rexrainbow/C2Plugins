6913582fae2a8a18b16c337a25b4f7c7c8a9ed1d
hua <binhua.tein@gmail.com>
2013-03-17 23:16:50 +0800

    Add rotateto behavior

---
+++ behaviors/rex_rotateto/edittime.js
@@ -0,0 +1,167 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"RotateTo",
+		"id":			"Rex_RotateTo",
+		"version":		"0.1",        
+		"description":	"Rotate sprite to specific angle",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Movements",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On hit target angle", "Target", 
+             "On {my} hit angle", 
+			 "Triggered when hit target angle.", 
+			 "OnHitTarget");             
+AddCmpParam("Comparison", "Choose the way to compare the current speed.");
+AddNumberParam("Speed", "The speed, in degree per second, to compare the current speed to.");
+AddCondition(1, 0, "Compare speed", "Speed", 
+             "{my} speed {0} {1}", 
+             "Compare the current speed of the object.", 
+             "CompareSpeed");
+AddCondition(3,	0, "Is rotating", "", "Is {my} rotating", "Is object rotating.", "IsRotating");                
+
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "", 
+          "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's RotateTo behavior.", "SetActivated");
+
+AddNumberParam("Max speed", "Maximum speed, in degree per second.");
+AddAction(1, 0, "Set maximum speed", "Speed", 
+          "Set {my} maximum speed to <i>{0}</i>", 
+          "Set the object's maximum speed.", "SetMaxSpeed");
+
+AddNumberParam("Acceleration", "The acceleration setting, in degree per second per second.");
+AddAction(2, 0, "Set acceleration", "Speed", 
+          "Set {my} acceleration to <i>{0}</i>", 
+          "Set the object's acceleration.", "SetAcceleration");
+
+AddNumberParam("Deceleration", "The deceleration setting, in degrees per second per second.");
+AddAction(3, 0, "Set deceleration", "Speed", 
+          "Set {my} deceleration to <i>{0}</i>", 
+          "Set the object's deceleration.", "SetDeceleration");
+
+AddNumberParam("Angle", "The angle to rotate toward.");
+AddComboParamOption("Anti-Clockwise");
+AddComboParamOption("Clockwise");
+AddComboParamOption("Nearest side");
+AddComboParam("Clockwise", "Rotate clockwise or anti-clockwise.",2);
+AddAction(4, 0, "Set target angle", "Target angle", 
+          "Set {my} target angle to <i>{0}</i>, <i>{1}</i>", 
+          "Set target angle to rotate toward.", 
+          "SetTargetAngle");
+
+AddNumberParam("Current speed", "Current speed, in degree per second.", 180);
+AddAction(5, 0, "Set current speed", "Speed", 
+          "Set {my} current speed to <i>{0}</i>", 
+          "Set the object's Current speed.", "SetCurrentSpeed");
+  
+AddObjectParam("Target", "Target object.");
+AddComboParamOption("Anti-Clockwise");
+AddComboParamOption("Clockwise");
+AddComboParamOption("Nearest side");
+AddComboParam("Clockwise", "Rotate clockwise or anti-clockwise.",2);  
+AddAction(6, 0, "Set target angle to object", "Target angle", 
+          "Set {my} target angle to <i>{0}</i>, <i>{1}</i>", 
+          "Set target angle to rotate toward.", 
+          "SetTargetAngleOnObject");  
+  
+AddNumberParam("dA", "The delta angle to rotate toward, in degree.",0);  
+AddComboParamOption("Anti-Clockwise");
+AddComboParamOption("Clockwise");
+AddComboParamOption("Nearest side");
+AddComboParam("Clockwise", "Rotate clockwise or anti-clockwise.",2); 
+AddAction(7, 0, "Set target angle by delta angle", "Target angle", 
+          "Set {my} target angle by delta <i>{0}</i>,  <i>{1}</i>)", 
+          "Set target angle to rotate toward by delta angle.", 
+          "SetTargetAngleByDeltaAngle");  
+        
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "Get current activated state", "Current", "Activated", 
+              "The current activated state of behavior.");
+AddExpression(1, ef_return_number, "Get current speed", "Current", "Speed", 
+              "The current object speed, in degree per second.");
+AddExpression(2, ef_return_number, "Get max speed", "Setting", "MaxSpeed", 
+              "The maximum speed setting, in degree per second.");
+AddExpression(3, ef_return_number, "Get acceleration", "Setting", "Acc", 
+              "The acceleration setting, in degree per second per second.");
+AddExpression(4, ef_return_number, "Get deceleration", "Setting", "Dec", 
+              "The deceleration setting, in degree per second per second.");
+AddExpression(5, ef_return_number, "Get target angle", "Target", "TargetAngle", 
+              "The X co-ordinate of target position to move toward.");
+              
+         
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),                
+	new cr.Property(ept_float, "Max speed", 180, "Maximum speed, in degree per second."),
+	new cr.Property(ept_float, "Acceleration", 0, 
+                    "Acceleration, in degree per second per second."),
+	new cr.Property(ept_float, "Deceleration", 0, 
+                    "Deceleration, in degree per second per second."),
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Max speed"] < 0)
+		this.properties["Max speed"] = 0;
+		
+	if (this.properties["Acceleration"] < 0)
+		this.properties["Acceleration"] = 0;
+		
+	if (this.properties["Deceleration"] < 0)
+		this.properties["Deceleration"] = 0;
+}
---
+++ behaviors/rex_rotateto/runtime.js
@@ -0,0 +1,270 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_RotateTo = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_RotateTo.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+		
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.activated = (this.properties[0] == 1);
+        this.move = {max:this.properties[1],
+                     acc:this.properties[2],
+                     dec:this.properties[3]};
+        this.target = {angle:0, clockwise:true};  
+        this.is_rotating = false;  
+        this.current_speed = 0;       
+        this.remain_distance = 0;
+        this.is_hit_target = false;
+
+        this.is_my_call = false;
+	};
+
+	behinstProto.tick = function ()
+	{
+        if (this.is_hit_target)
+        {
+            this.is_my_call = true;
+            this.runtime.trigger(cr.behaviors.Rex_RotateTo.prototype.cnds.OnHitTarget, this.inst); 
+            this.is_my_call = false;
+            this.is_hit_target = false;
+        }
+        
+        if ( (!this.activated) || (!this.is_rotating) ) 
+        {
+            return;
+        }
+        
+		var dt = this.runtime.getDt(this.inst);
+        if (dt==0)   // can not move if dt == 0
+            return;
+
+        // assign speed
+        var is_slow_down = false;
+        if (this.move.dec != 0)
+        {
+            // is time to deceleration?                
+            var _speed = this.current_speed;
+            var _distance = (_speed*_speed)/(2*this.move.dec); // (v*v)/(2*a)
+            is_slow_down = (_distance >= this.remain_distance);
+        }
+        var acc = (is_slow_down)? (-this.move.dec):this.move.acc;
+        if (acc != 0)
+        {
+            this.SetCurrentSpeed( this.current_speed + (acc * dt) );    
+        }
+
+		// Apply movement to the object     
+        var distance = this.current_speed * dt;
+        this.remain_distance -= distance;   
+        
+        // is hit to target at next tick?
+        if ( (this.remain_distance <= 0) || (this.current_speed <= 0) )
+        {
+            this.is_rotating = false;
+            this.inst.angle = cr.to_clamped_radians(this.target.angle);
+            this.SetCurrentSpeed(0);
+            this.is_hit_target = true;
+        }
+        else
+        {
+            if (this.target.clockwise)
+                this.inst.angle += cr.to_clamped_radians(distance);
+            else
+                this.inst.angle -= cr.to_clamped_radians(distance);
+        } 
+
+		this.inst.set_bbox_changed();        
+	}; 
+	behinstProto.tick2 = function ()
+	{ 
+    };
+    
+	behinstProto.SetCurrentSpeed = function(speed)
+	{
+        if (speed != null)
+        {
+            this.current_speed = (speed > this.move.max)? 
+                                 this.move.max: speed;
+        }        
+        else if (this.move.acc==0)
+        {
+            this.current_speed = this.move.max;
+        }
+	};
+	
+	behinstProto.SetTargetAngle = function (angle, clockwise_mode)  // in radians
+	{
+        debugger;
+        this.is_rotating = true;
+        var cur_angle_degree = cr.to_degrees(this.inst.angle);
+
+		if (clockwise_mode == 2)
+		{
+		    var dA = angle - cur_angle_degree;
+		    clockwise_mode = (dA <= 180)? 1:0;
+		}
+        this.target.clockwise = (clockwise_mode == 1);
+        if (this.target.clockwise)
+            this.remain_distance = angle - cur_angle_degree;
+        else
+            this.remain_distance = cur_angle_degree - angle;
+        if (this.remain_distance < 0)
+            this.remain_distance += 360;
+        this.target.angle = angle;
+        this.SetCurrentSpeed(null);       
+	}; 
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	Cnds.prototype.OnHitTarget = function ()
+	{
+		return (this.is_my_call);
+	};
+
+	Cnds.prototype.CompareSpeed = function (cmp, s)
+	{
+		return cr.do_cmp(this.current_speed, cmp, s);
+	};   
+
+    Cnds.prototype.OnMoving = function ()  // deprecated
+	{
+		return false;
+	};
+    
+	Cnds.prototype.IsRotating = function ()
+	{
+		return (this.activated && this.is_rotating);
+	};
+   
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetActivated = function (s)
+	{
+		this.activated = (s == 1);
+	};  
+
+	Acts.prototype.SetMaxSpeed = function (s)
+	{
+		this.move.max = s;
+        this.SetCurrentSpeed(null);
+	};      
+    
+	Acts.prototype.SetAcceleration = function (a)
+	{
+		this.move.acc = a;
+        this.SetCurrentSpeed(null);
+	};
+	
+	Acts.prototype.SetDeceleration = function (a)
+	{
+		this.move.dec = a;
+	};
+    
+	Acts.prototype.SetTargetAngle = function (angle, clockwise_mode)
+	{
+        this.SetTargetAngle(angle, clockwise_mode)
+	};
+    
+	Acts.prototype.SetCurrentSpeed = function (s)
+	{
+        this.SetCurrentSpeed(s);
+	}; 
+    
+ 	Acts.prototype.SetTargetAngleOnObject = function (objtype, clockwise_mode)
+	{
+		if (!objtype)
+			return;
+		var inst = objtype.getFirstPicked();
+        if (inst != null)
+        {
+            var angle = Math.atan2(inst.y-this.inst.y , inst.x-this.inst.x);
+            this.SetTargetAngle(angle, clockwise_mode);
+        }
+	};
+    
+ 	Acts.prototype.SetTargetAngleByDeltaAngle = function (dA, clockwise_mode)
+	{
+	    var angle = cr.to_clamped_radians(this.inst.angle + dA);
+        this.SetTargetAngle(angle, clockwise_mode);
+	};    
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+    
+	Exps.prototype.Activated = function (ret)
+	{
+		ret.set_int((this.activated)? 1:0);
+	};    
+    
+	Exps.prototype.Speed = function (ret)
+	{
+		ret.set_float(this.current_speed);
+	};
+    
+	Exps.prototype.MaxSpeed = function (ret)
+	{
+		ret.set_float(this.move.max);
+	}; 
+
+	Exps.prototype.Acc = function (ret)
+	{
+		ret.set_float(this.move.acc);
+	};  
+
+ 	Exps.prototype.Dec = function (ret)
+	{
+		ret.set_float(this.move.dec);
+	}; 
+
+	Exps.prototype.TargetAngle = function (ret)
+	{
+        var x = (this.is_rotating)? this.target.angle:0;
+		ret.set_float(x);
+	};    
+}());
