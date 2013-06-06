c1a9c34fa5cf3f88f74ea7a3866f1a5f53069e57
hua <binhua.tein@gmail.com>
2013-06-07 00:02:15 +0800

    Add platform moveto

---
+++ behaviors/rex_platform_moveto/edittime.js
@@ -0,0 +1,116 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Platform MoveTo",
+		"id":			"Rex_Platform_MoveTo",
+		"version":		"0.1",        
+		"description":	"Move platformer to specific position",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropboxusercontent.com/u/5779181/C2Repo/rex_platform_moveto.html",
+		"category":		"Movements",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_trigger, "On hit target position", "", 
+             "On {my} hit target", 
+			 "Triggered when hit target position.", 
+			 "OnHitTarget");             
+AddCondition(3,	0, "Is moving", "", "Is {my} moving", "Is object moving.", "IsMoving");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "", 
+          "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's MoveTo behavior.", "SetActivated");
+AddNumberParam("X", "The X co-ordinate to move toward.");
+AddAction(1, 0, "Move to X", "MoveTo", 
+          "{my} move to X <i>{0}</i>", 
+          "Move to specific position by X.", 
+          "SetTargetPosX");
+AddObjectParam("Target", "Target object.");
+AddAction(3, 0, "Move to object", "MoveTo", 
+          "{my} move to <i>{0}</i>", 
+          "Move to object.", 
+          "SetTargetPosOnObject");
+AddNumberParam("dX", "The delta X to move toward, in pixel.");
+AddAction(4, 0, "Move to delta X", "MoveTo", 
+          "{my} move to delta X <i>{0}</i>", 
+          "Move to specific position by deltaX.", 
+          "SetTargetPosByDeltaX");                        
+AddNumberParam("Distance", "The destance to move toward, in pixel.");
+AddComboParamOption("Left");
+AddComboParamOption("Right");
+AddComboParam("Direction", "Direction of moving.",1);
+AddAction(6, 0, "Move to distance", "MoveTo", 
+          "{my} move to (distance <i>{0}</i>, <i>{1}</i> side)", 
+          "Move to specific position by distance.", 
+          "SetTargetPosByDistance");
+AddAction(10, 0, "Stop", "Stop", "{my} stop", 
+          "Stop moving.", "Stop");       
+                  
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "Get current activated state", "Current", "Activated", 
+              "The current activated state of behavior.");
+AddExpression(1, ef_return_number, "Get target position X", "Target", "TargetX", 
+              "The X co-ordinate of target position to move toward.");
+AddExpression(2, ef_return_number, "Get target position Y", "Target", "TargetY", 
+              "The Y co-ordinate of target position to move toward.");                
+              
+         
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"), 
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_platform_moveto/runtime.js
@@ -0,0 +1,227 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_Platform_MoveTo = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_Platform_MoveTo.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+		
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+	    this.platform_behavior_inst = null;
+        this.activated = (this.properties[0] == 1);
+        this.target = {"m":0,       // 0: x mode , 1: distance mode
+                       "dir": 0, // 0:left , 1: right
+                       "x":0,
+                       "y":0,
+                       "d":0,
+                       "ds":0 };
+        this.is_my_call = false;
+	};
+	
+	behinstProto._get_platform_behavior_inst = function ()
+    {
+        if (this.platform_behavior_inst != null)
+            return this.platform_behavior_inst;
+            
+        var i = this.type.objtype.getBehaviorIndexByName("Platform");   
+        if (i != (-1))
+        {
+            this.platform_behavior_inst = this.inst.behavior_insts[i];
+            return this.platform_behavior_inst;
+        }
+        else
+            assert2("You should add a physics for gravitation behavior.");
+    };
+    	
+	behinstProto._move = function (dir)   // 0: left , 1: right
+    {
+        var platform_behavior_inst = this._get_platform_behavior_inst();
+        assert2(platform_behavior_inst, "Platform MoveTo: You should add a platform behavior");
+        
+        cr.behaviors.Platform.prototype.acts.SimulateControl.call(platform_behavior_inst, dir);        
+    };
+    
+    behinstProto.tick = function ()
+	{        
+        if ( (!this.activated) || (!this.is_moving) ) 
+        {
+            return;
+        }
+    
+	    var is_hit_target = false;        
+        if (this.target["m"] == 0)       // x mode
+        {
+            if ( ((this.target["dir"] == 1) && (this.inst.x >= this.target["x"])) ||
+                 ((this.target["dir"] == 0) && (this.inst.x <= this.target["x"])) )
+                is_hit_target = true;
+            else
+                this._move(this.target["dir"]); 
+        }
+        else if (this.target["m"] == 1)    // distance mode
+        {
+            var x = this.inst.x;
+            var y = this.inst.y;
+            this.target["ds"] += cr.distanceTo(this.target["x"], this.target["y"], x, y);
+            if (this.target["ds"] >= this.target["d"])
+                is_hit_target = true;
+            else
+            {
+                this._move(this.target["dir"]); 
+                this.target["x"] = x;
+                this.target["y"] = y;
+            }
+        }       
+        
+        if (is_hit_target)
+        {            
+            this.is_my_call = true;
+            this.runtime.trigger(cr.behaviors.Rex_Platform_MoveTo.prototype.cnds.OnHitTarget, this.inst); 
+            this.is_my_call = false;            
+            this.is_moving = false;
+        }   
+	}; 
+
+	behinstProto.SetTargetPos = function (_x)
+	{
+        this.is_moving = true;         
+        this.target["m"] = 0;
+        this.target["dir"] = (_x > this.inst.x)? 1:0;
+        this.target["x"] = _x;
+        this.target["y"] = 0;
+        this.target["d"] = 0;
+        this.target["ds"] = 0;        
+	};
+	
+ 	behinstProto.SetTargetPosByDistance = function (distance, dir)
+	{
+	    this.is_moving = true;
+        this.target["m"] = 1;
+        this.target["dir"] = dir;
+        this.target["x"] = this.inst.x;
+        this.target["y"] = this.inst.y;      
+        this.target["d"] = distance;
+        this.target["ds"] = 0;           
+	};  
+	
+	behinstProto.saveToJSON = function ()
+	{
+		return { "en": this.activated,
+		         "im": this.is_moving,
+		         "t": this.target,
+               };
+	};
+	
+	behinstProto.loadFromJSON = function (o)
+	{  
+		this.activated = o["en"];
+		this.is_moving = o["im"];
+		this.target = o["t"];
+	};	    
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	Cnds.prototype.OnHitTarget = function ()
+	{
+		return (this.is_my_call);
+	};
+
+	Cnds.prototype.IsMoving = function ()
+	{
+		return (this.activated && this.is_moving);
+	};
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetActivated = function (s)
+	{
+		this.activated = (s == 1);
+	};  
+
+	Acts.prototype.SetTargetPosX = function (_x)
+	{
+        this.SetTargetPos(_x)
+	};
+	
+ 	Acts.prototype.SetTargetPosOnObject = function (objtype)
+	{
+		if (!objtype)
+			return;
+		var inst = objtype.getFirstPicked();
+        if (inst != null)
+            this.SetTargetPos(this.inst.x);
+	};
+    
+	Acts.prototype.SetTargetPosByDeltaX = function (_x)
+	{
+        this.SetTargetPos(this.inst.x + _x)
+	};
+
+ 	Acts.prototype.SetTargetPosByDistance = function (distance, dir)
+	{
+        this.SetTargetPosByDistance(distance, dir);
+	};      
+    
+ 	Acts.prototype.Stop = function ()
+	{
+        this.is_moving = false;
+	};   	
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+    
+	Exps.prototype.Activated = function (ret)
+	{
+		ret.set_int((this.activated)? 1:0);
+	};    
+    
+	Exps.prototype.TargetX = function (ret)
+	{
+        var x = (this.is_moving)? this.target["x"]:0;
+		ret.set_float(x);
+	};  
+
+}());
