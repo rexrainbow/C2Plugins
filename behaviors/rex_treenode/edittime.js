72039985e3b4a3b24efd872888c7a20878e54704
hua <binhua.tein@gmail.com>
2013-12-18 01:04:52 +0800

    Add tree node behavior

---
+++ behaviors/rex_treenode/edittime.js
@@ -0,0 +1,144 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Tree node",
+		"id":			"rex_treenode",
+		"version":		"0.1",
+		"description":	"Get parent or children instance(s) from a tree.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_treenode.html",
+		"category":		"General",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(20, 0, "Is root", "Tree", "{my} is root", 
+             "Return true if this node is the root.", "IsRoot");
+AddCondition(21, 0, "Has child", "Tree", "{my} has any child", 
+             "Return true if this node has any child.", "HasChild"); 
+AddCondition(22, 0, "Has sibling", "Tree", "{my} has any sibling", 
+             "Return true if this node has any sibling.", "HasSibling");                       
+//////////////////////////////////////////////////////////////
+// Actions 
+AddObjectParam("Group", "Instance group object");
+AddAction(0, 0, "Set instance group ", "Setup", 
+          "Set {my} instance group object to <i>{0}</i>", 
+          "Set instance group object.", "SetInstanceGroup");  
+AddObjectParam("Object", "Object type."); 
+AddAction(1, 0, "Assign parent", "Parent", 
+          "Assign {my} parent to <i>{0}</i>", 
+          "Assign parent instance.", "AssignParent"); 
+AddObjectParam("Object", "Object type."); 
+AddNumberParam("X", "X co-ordinate.", 0);
+AddNumberParam("Y", "Y co-ordinate.", 0);
+AddAnyTypeParam("Layer", "Layer name of number.", 0);
+AddAction(2, 0, "Create child", "Child", 
+          "Create {my} child <i>{0}</i> at (<i>{1}</i>,<i>{2}</i>) on layer <i>{3}</i>", 
+          "Create child instance.", "CreateChild");
+AddObjectParam("Object", "Object type."); 
+AddAction(3, 0, "Add children", "Child", 
+          "Add {my} children <i>{0}</i>", 
+          "Add children instance.", "AddChildren");
+AddAction(4, 0, "Remove", "Tree", 
+          "Remove {my} from tree", 
+          "Remove this node from tree.", "RemoveFromTree");   
+AddNumberParam("UID", "Parent UID.", -1);
+AddAction(5, 0, "Assign parent by UID", "Parent", 
+          "Assign {my} parent to UID: <i>{0}</i>", 
+          "Assign parent by UID.", "AssignParent");
+AddNumberParam("UID", "Child UID.", -1);
+AddAction(6, 0, "Add children by UID", "Child", 
+          "Add {my} children UID: <i>{0}</i>", 
+          "Add children by UID.", "AddChildren");                                         
+AddStringParam("Group", "Put result in this group", '""');
+AddComboParamOption("children");
+AddComboParamOption("children and grandson");
+AddComboParam("Children type", "Children and grandson.", 0);  
+AddAction(21, 0, "Pick children", "SOL", 
+          "Pick {my} <i>{1}</i> UID into group <i>{0}</i>", 
+          "Pick children.", "PickChildren");
+AddStringParam("Group", "Put result in this group", '""'); 
+AddAction(22, 0, "Pick parent", "SOL", 
+          "Pick {my} parent UID into group <i>{0}</i>",  
+          "Pick parent.", "PickParent");
+AddStringParam("Group", "Put result in this group", '""');
+AddAction(23, 0, "Pick sibling", "SOL", 
+          "Pick {my} sibling UID into group <i>{0}</i>",  
+          "Pick sibling.", "PickSibling"); 
+AddStringParam("Group", "Put result in this group", '""');
+AddAction(24, 0, "Pick root", "SOL", 
+          "Pick {my} root UID into group <i>{0}</i>",  
+          "Pick root.", "PickRoot");           
+               	         
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, "Get children count", "Count", "ChildrenCount", 
+              "Get children count of this node."); 
+AddExpression(2, ef_return_number, "Get sibling count", "Count", "SiblingCount", 
+              "Get sibling count of this node.");                
+AddExpression(11, ef_return_number, "Get parent UID", "Parent", "ParentUID", 
+              "Get parent UID of this node. (-1) means that it does not have parent."); 
+AddExpression(12, ef_return_number, "Get root UID", "Parent", "RootUID", 
+              "Get root UID of this node.");              
+AddExpression(13, ef_return_number, "Get first child UID", "Children", "FirstChildUID", 
+              "Get first child UID of this node. (-1) means that it does not have any child."); 
+AddExpression(14, ef_return_number, "Get last child UID", "Children", "LastChildUID", 
+              "Get last child UID of this node. (-1) means that it does not have any child.");
+AddExpression(15, ef_return_number, "Get first sibling UID", "Sibling", "FirstSiblingUID", 
+              "Get first sibling UID of this node. (-1) means that it does not have any sibling.");          
+AddExpression(16, ef_return_number, "Get last sibling UID", "Sibling", "LastSiblingUID", 
+              "Get last sibling UID of this node. (-1) means that it does not have any sibling.");
+                                                
+ACESDone();
+
+var property_list = [
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// any other properties here, e.g...
+	// this.myValue = 0;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_treenode/runtime.js
@@ -0,0 +1,411 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.rex_treenode = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.rex_treenode.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+        this.group = null;  
+	};
+	
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+	    this.parent = -1;
+	    this.children = [];		    
+	    this.root = this.inst.uid;    
+	};
+	
+	behinstProto.onDestroy = function()
+	{
+		this.NodeRemove();
+	};	
+
+	behinstProto.tick = function ()
+	{
+	};
+		
+	behinstProto.saveToJSON = function ()
+	{
+		return { "p": this.parent,
+		         "c": this.children,
+		         "r": this.root
+		       };
+	};
+	
+	behinstProto.loadFromJSON = function (o)
+	{
+	    this.parent = o["p"];
+	    this.children = o["c"];
+	    this.root = o["r"];
+	};
+	
+	behinstProto._add_child = function (child_uid)
+	{
+	    if (this.children.indexOf(child_uid) == (-1))
+	        this.children.push(child_uid);
+	};	
+	
+	behinstProto.CreateChildInst = function (objtype,x,y,_layer)
+	{
+        var layer = (typeof _layer == "number")?
+                    this.runtime.getLayerByNumber(_layer):
+                    this.runtime.getLayerByName(_layer);  
+        var inst = this.runtime.createInstance(objtype, layer, x, y ); 
+		if (inst == null)
+		    return null;
+		
+		var child_tn = this.TreeNodeGet(inst);
+		assert2(child_tn, "[Tree noe] The child need to have tree node behavior");
+		child_tn.parent = this.inst.uid;
+		child_tn.root = this.root;
+		this._add_child(inst.uid);
+		
+		
+        var sol = inst.type.getCurrentSol();
+        sol.select_all = false;
+		sol.instances.length = 1;
+		sol.instances[0] = inst;
+		
+		// Siblings aren't in instance lists yet, pick them manually
+		var i, len, s;
+		if (inst.is_contained)
+		{
+			for (i = 0, len = inst.siblings.length; i < len; i++)
+			{
+				s = inst.siblings[i];
+				sol = s.type.getCurrentSol();
+				sol.select_all = false;
+				sol.instances.length = 1;
+				sol.instances[0] = s;
+			}
+		}
+        
+		this.runtime.isInOnDestroy++;
+		this.runtime.trigger(Object.getPrototypeOf(inst.type.plugin).cnds.OnCreated, inst);
+		this.runtime.isInOnDestroy--;
+        
+	    return inst;
+	}; 		
+	
+	behinstProto.TreeNodeGet = function(inst)
+	{
+	    if (typeof(inst) == "number")
+	        inst = this.runtime.getObjectByUID(inst);	    
+	    if (inst == null)
+	        return null;
+	    	        
+		var i, len;
+		for (i = 0, len = inst.behavior_insts.length; i < len; i++)
+		{
+			if (inst.behavior_insts[i] instanceof behaviorProto.Instance)
+				return inst.behavior_insts[i];
+		}
+		
+		return null;
+	};	
+	
+	behinstProto.ParentSet = function(parent_inst)
+	{	   
+	    // parent_inst might be null
+	    // parent no changed
+	    var parent_uid = (parent_inst != null)? parent_inst.uid:(-1);
+	    if (parent_uid == this.parent)	       
+	        return;
+	        
+	    // set previous parent node
+	    var pre_parent_tn = this.TreeNodeGet(this.parent);
+	    if (pre_parent_tn != null)
+	        pre_parent_tn._remove_child(this.inst.uid);
+	    
+	    // set new parent node
+		var parent_tn = this.TreeNodeGet(parent_inst);
+		if (parent_tn != null)
+		    parent_tn._add_child(this.inst.uid);
+	    
+	    // set my node	    
+	    this.parent = parent_uid;
+	    this.root = (parent_tn != null)? parent_tn.root:this.inst.uid;
+	};
+	
+	behinstProto.ChildrenAdd = function(children_insts)
+	{	
+	    // children have no tree node behavior
+		var child_tn = this.TreeNodeGet(children_insts[0]);
+		if (child_tn == null)
+		    return;
+		    
+		var i, cnt=children_insts.length, child_inst;
+		for(i=0; i<cnt; i++)
+		{
+		    child_inst = children_insts[i];
+		    child_tn = this.TreeNodeGet(child_inst);
+		    child_tn.ParentSet(this);
+		}
+	};	
+	
+	behinstProto._remove_child = function(child_uid)
+	{	   
+		var i = this.children.indexOf(child_uid);
+		this.children.splice(i, 1);
+	};
+	
+    behinstProto.NodeRemove = function ()
+	{
+	    // reset parent node
+	    var parent_tn = this.TreeNodeGet(this.parent);
+	    if (parent_tn != null)	    
+	        parent_tn._remove_child(this.inst.uid);
+	    
+	    // reset children node
+	    var i, cnt=this.children.length, child_inst, child_tn;
+	    for (i=0; i<cnt; i++)
+	    {
+	        child_tn = this.TreeNodeGet(this.children[i]);
+	        if (child_tn != null)
+	            child_tn.ParentSet(null);
+	    }
+	    
+	    // reset my node
+	    this.parent = -1;
+	    this.children.length = 0;	    
+	    this.root = this.inst.uid;
+	};		
+	
+	var ret_uids = [];
+	var children_uid = [];
+	behinstProto.ChildrenUIDGet = function(has_grandson)
+	{
+	    var child_uid, child_inst, child_tn;
+	    ret_uids.length = 0;
+	    children_uid.push.apply(children_uid, this.children);  
+	    while (children_uid.length >0)
+	    {
+	        child_uid = children_uid.pop();
+	        child_inst = this.runtime.getObjectByUID(child_uid);
+	        if (child_inst == null)
+	            continue;	        
+	        ret_uids.push(child_uid);
+	        if (has_grandson)
+	        {
+	            child_tn = this.TreeNodeGet(child_inst);
+	            children_uid.push.apply(children_uid, child_tn.children);
+	        }
+	    }
+	    children_uid.length = 0;	
+	    return ret_uids;   	    
+	};
+	
+	behinstProto._sibling_get = function ()
+	{
+	    var parent_tn = this.TreeNodeGet(this.parent);
+	    if (parent_tn == null)
+	    {
+	        ret_uids.length = 0;
+	    }
+	    else
+	    {
+	        cr.shallowAssignArray(ret_uids, parent_tn.children);
+		    var i = ret_uids.indexOf(this.inst.uid);
+		    ret_uids.splice(i, 1);	    
+		}
+		return ret_uids;
+	};
+		
+	behinstProto._get_inst = function (objtype)
+	{
+	    var inst;                        
+	    if (objtype == null)
+	        inst = null; 
+	    else if (typeof(objtype) == "number")	    
+	        inst = this.runtime.getObjectByUID(objtype);	
+	    else
+	        inst = objtype.getCurrentSol().instances[0];
+	    return inst;
+	};
+	//////////////////////////////////////
+	// Conditions
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	    
+    Cnds.prototype.IsRoot = function ()
+	{
+		return (this.parent == -1);
+	};
+	
+    Cnds.prototype.HasChild = function ()
+	{
+		return (this.children.length > 0);
+	};
+	
+    Cnds.prototype.HasSibling = function ()
+	{
+	    var parent_tn = this.TreeNodeGet(this.parent);
+	    var cnt = (parent_tn != null)? parent_tn.children.length-1: 0;
+		return (cnt > 0);
+	};	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+    Acts.prototype.SetInstanceGroup = function (group_objs)
+	{
+        var group = group_objs.instances[0];
+        if (group.check_name == "INSTGROUP")
+            this.type.group = group;        
+        else
+            alert ("[Tree noe] This object is not a instance group object.");            
+	};
+
+	
+    Acts.prototype.AssignParent = function (objtype)
+	{
+	    // objtype = type | uid
+        var parent_inst = this._get_inst(objtype);
+        if (parent_inst == null)
+            return;
+            
+        this.ParentSet(parent_inst);
+	}; 
+	
+    Acts.prototype.CreateChild = function (objtype,x,y,layer)
+	{
+        if (objtype == null)
+            return; 
+        this.CreateChildInst(objtype,x,y,layer);
+	}; 
+	
+    Acts.prototype.AddChildren = function (objtype)
+	{
+	    // objtype = type | uid
+        var children_insts = this._get_inst(objtype);
+        if (children_insts == null)
+            return;
+        this.ChildrenAdd(children_insts);
+	};
+	
+    Acts.prototype.RemoveFromTree = function ()
+	{
+	    this.NodeRemove();
+	};	
+	
+	
+    Acts.prototype.PickChildren = function (group_name, has_grandson)
+	{
+        var children_uids = this.ChildrenUIDGet(has_grandson);
+        this.type.group.GetGroup(group_name).SetByUIDList(children_uids);
+        children_uids.length = 0;
+	};	
+	
+    Acts.prototype.PickParent = function (group_name)
+	{
+	    var group = this.type.group.GetGroup(group_name);
+	    group.Clean();
+	    if (this.parent != -1)
+	        group.AddUID(this.parent);
+	};
+	
+    Acts.prototype.PickSibling = function (group_name)
+	{
+	    var children_uids = this._sibling_get();
+	    this.type.group.GetGroup(group_name).SetByUIDList(children_uids);
+	    children_uids.length = 0;
+	};
+		
+    Acts.prototype.PickRoot = function (group_name)
+	{
+	    var group = this.type.group.GetGroup(group_name);
+	    group.Clean();
+	    group.AddUID(this.root);
+	};
+		
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+    
+	Exps.prototype.ChildrenCount = function (ret)
+	{
+		ret.set_int(this.children.length);
+	};
+    
+	Exps.prototype.SiblingCount = function (ret)
+	{
+	    var parent_tn = this.TreeNodeGet(this.parent);
+	    var cnt = (parent_tn != null)? parent_tn.children.length-1: 0;
+		ret.set_int(cnt);
+	};
+	
+	Exps.prototype.ParentUID = function (ret)
+	{
+		ret.set_int(this.parent);
+	};
+	
+	Exps.prototype.RootUID = function (ret)
+	{
+		ret.set_int(this.root);
+	};
+	
+	Exps.prototype.FirstChildUID = function (ret)
+	{
+	    var uid = (this.children.length > 0)? this.children[0]: (-1);	                          
+		ret.set_int(uid);
+	};
+	
+	Exps.prototype.LastChildUID = function (ret)
+	{
+	    var uid = (this.children.length > 0)? this.children[this.children.length-1]: (-1);	                          
+		ret.set_int(uid);
+	};	
+	
+	Exps.prototype.FirstSiblingUID = function (ret)
+	{
+	    var children_uids = this._sibling_get();
+	    var uid = (children_uids.length > 0)? children_uids[0]: (-1);                      
+		ret.set_int(uid);
+	};
+	
+	Exps.prototype.LastSiblingUID = function (ret)
+	{
+	    var children_uids = this._sibling_get();
+	    var uid = (children_uids.length > 0)? children_uids[children_uids.length-1]: (-1);                                  
+		ret.set_int(uid);
+	};	
+}());
