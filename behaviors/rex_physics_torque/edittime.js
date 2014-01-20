719c3fd47b5b1c00366f6ae24ec2c34cb84edaf6
hua <binhua.tein@gmail.com>
2014-01-20 20:20:42 +0800

    Add PID torque behavior

---
+++ behaviors/rex_physics_torque/edittime.js
@@ -0,0 +1,116 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"PID Torque",
+		"id":			"Rex_physics_torque",
+		"description":	"Torque to approach the target speed using by PI controller, standed on physics behavior.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropboxusercontent.com/u/5779181/C2Repo/rex_physics_torque.html",
+		"category":		"Physics helper",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+	 		 
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Activated", "Enable the behavior.",1);
+AddAction(0, 0, "Set activated", "Configure", 
+          "Set {my} activated to <i>{0}</i>", 
+          "Enable the object's PID Torque behavior.", "SetActivated");
+AddNumberParam("Target speed", "Target speed for approach.", 180);
+AddAction(2, 0, "Set target speed", "Configure", "Set {my} target speed to <i>{0}</i>", 
+          "Set target speed.", "SetTargetSpeed");
+AddNumberParam("Max force", "Maximum force.", 300);
+AddAction(3, 0, "Set max force", "Configure", "Set {my} max force to <i>{0}</i>", 
+          "Set maximum force.", "SetMaxForce");
+AddNumberParam("Kp", "Kp of PID controller.", 1);
+AddAction(10, 0, "Set Kp", "PID controller", "Set {my} Kp to <i>{0}</i>", 
+          "Set Kp.", "SetKp");
+AddNumberParam("Ki", "Ki of PID controller.", 1);
+AddAction(11, 0, "Set Ki", "PID controller", "Set {my} Ki to <i>{0}</i>", 
+          "Set Ki.", "SetKi");  
+AddNumberParam("Kd", "Kd of PID controller.", 1);
+AddAction(12, 0, "Set Kd", "PID controller", "Set {my} Kd to <i>{0}</i>", 
+          "Set Kd.", "SetKd");            
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, "Get target speed", "Configure", "TargetSpeed", "Get target speed.");
+AddExpression(2, ef_return_number, "Get max force", "Configure", "MaxForce", "Get max force.");
+AddExpression(3, ef_return_number, "Get current applied force", "State", "CurForce", "Get current applied force.");
+AddExpression(10, ef_return_number, "Get Kp", "PID controller", "Kp", "Get Kp.");
+AddExpression(11, ef_return_number, "Get Ki", "PID controller", "Ki", "Get Ki.");
+AddExpression(12, ef_return_number, "Get Kd", "PID controller", "Kd", "Get Kd.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [  
+    new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),                 
+    new cr.Property(ept_float, "Target speed", 180, "Target speed for approach."),
+	new cr.Property(ept_float, "Max force", 300, "Maximum force."),
+    new cr.Property(ept_float, "PID - Kp", 1, "Kp of PID controller."),
+	new cr.Property(ept_float, "PID - Ki", 0.01, "Ki of PID controller."),
+	new cr.Property(ept_float, "PID - Kd", 0, "Kd of PID controller."),	
+	new cr.Property(ept_float, "PID - Reset-windup", 1, "Reset-windup when percent of error is larger then this value. 0 is ignord reset-windup."),	
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{	
+	if (this.properties["Max force"] < 0)
+		this.properties["Max force"] = Math.abs(this.properties["Max force"]);	
+	if (this.properties["PI - Kp"] < 0)
+		this.properties["PI - Kp"] = Math.abs(this.properties["PI - Kp"]);		
+	if (this.properties["PI - Ki"] < 0)
+		this.properties["PI - Ki"] = Math.abs(this.properties["PI - Ki"]);
+	if (this.properties["PI - Kd"] < 0)
+		this.properties["PI - Kd"] = Math.abs(this.properties["PI - Kd"]);		
+		
+	if (this.properties["PID - Reset-windup"] < 0)
+		this.properties["PID - Reset-windup"] = 0;							
+}
---
+++ behaviors/rex_physics_torque/runtime.js
@@ -0,0 +1,285 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_physics_torque = function(runtime)
+{
+	this.runtime = runtime;
+    this.sources = {};
+};
+
+(function ()
+{
+    var worldScale = 0.02;
+	var behaviorProto = cr.behaviors.Rex_physics_torque.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{     
+	};
+    
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;  
+        this.sources = this.behavior.sources;         
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	var fake_ret = {value:0,
+	                set_any: function(value){this.value=value;},
+	                set_int: function(value){this.value=value;},	 
+                    set_float: function(value){this.value=value;},	                          
+	               };       
+	behinstProto.onCreate = function()
+	{     
+        this.physics_behavior_inst = this._get_physics_behavior_inst();         
+       
+        this.activated = (this.properties[0] == 1);
+        this.target_speed = this.properties[1]; 
+		this.max_force = this.properties[2];
+		
+		var kp = this.properties[3];
+		var ki = this.properties[4];
+		var kd = this.properties[5];
+		var reset_windup_err = this.properties[6];
+		this.pid_ctrl = new cr.behaviors.Rex_physics_torque.PIControllerKlass(kp, ki, kd, reset_windup_err);
+		this.current_applied_force = 0;
+	};
+	
+	behinstProto.onDestroy = function()
+	{
+	}; 	
+
+	behinstProto._get_physics_behavior_inst = function ()
+    {
+        var i = this.type.objtype.getBehaviorIndexByName("Physics");   
+        if (i != (-1))
+            return this.inst.behavior_insts[i];
+        else
+            alert("You should add a physics for gravitation behavior.");
+    };
+  
+	behinstProto.tick = function ()
+	{
+	    var cur_speed = this._angular_velocity_get();
+		var err = (this.target_speed - cur_speed)/Math.abs(this.target_speed);
+		var out = this.pid_ctrl.procress(err);
+		if (this.activated)
+		{
+		    this.current_applied_force = out * this.max_force;
+		    this._apply_torque(this.current_applied_force);
+		}
+		else
+		{
+		    this.current_applied_force = 0;
+		}
+	};
+
+	behinstProto._apply_torque = function (f)
+	{
+       cr.behaviors.Physics.prototype.acts.ApplyTorque.call(this.physics_behavior_inst, f);       
+	};    
+	
+	behinstProto._angular_velocity_get = function ()
+	{
+       cr.behaviors.Physics.prototype.exps.AngularVelocity.call(this.physics_behavior_inst, fake_ret);
+	   return fake_ret.value;
+	};	
+
+	behinstProto.saveToJSON = function ()
+	{
+		return { "en": this.activated,
+		         "ts": this.target_speed,
+		         "mf": this.max_force,
+		         "pid": this.pid_ctrl.saveToJSON(),
+                };
+	};
+	
+	behinstProto.loadFromJSON = function (o)
+	{                  
+	    this.activated = o["en"];
+	    this.target_speed = o["ts"];
+	    this.max_force = o["mf"];
+	    this.pid_ctrl.loadFromJSON(o["pid"]);
+	};        
+	
+	/**BEGIN-PREVIEWONLY**/
+	behinstProto.getDebuggerValues = function (propsections)
+	{
+		propsections.push({
+			"title": this.type.name,
+			"properties": [
+				{"name": "Force", "value": this.current_applied_force},
+				{"name": "PI-Kp", "value": this.pid_ctrl.kp},
+				{"name": "PI-Ki", "value": this.pid_ctrl.ki},
+				{"name": "PI-Kd", "value": this.pid_ctrl.kd},
+				{"name": "Reset-windup", "value": this.pid_ctrl.is_reset_windup},
+			]
+		});
+	};
+	
+	behinstProto.onDebugValueEdited = function (header, name, value)
+	{
+		var a, s;
+		
+		switch (name) 
+		{
+		case "PI-Kp": 
+		    this.pid_ctrl.kp = value; 
+			break;
+		case "PI-Ki":
+		    this.pid_ctrl.ki = value;
+			break;
+		case "PI-Kd":
+		    this.pid_ctrl.ki = value;
+			break;			
+		}
+	};
+	/**END-PREVIEWONLY**/	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetActivated = function (s)
+	{
+		this.activated = (s == 1);
+	};  
+	
+	Acts.prototype.SetTargetSpeed = function (s)
+	{	    
+		this.target_speed = s;
+	}; 
+
+	Acts.prototype.SetMaxForce = function (f)
+	{
+	    if (f < 0)
+		    f = 0;
+		this.max_force = f;
+	};
+	
+	Acts.prototype.SetKp = function (kp)
+	{	    
+		this.pid_ctrl.kp = kp; 
+	}; 	 
+	
+	Acts.prototype.SetKp = function (ki)
+	{	    
+		this.pid_ctrl.ki = ki; 
+	}; 
+	
+	Acts.prototype.SetKd = function (kd)
+	{	    
+		this.pid_ctrl.kd = kd; 
+	}; 	
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+	
+	Exps.prototype.TargetSpeed = function (ret)
+	{
+		ret.set_float(this.target_speed);
+	};
+	
+	Exps.prototype.MaxForce = function (ret)
+	{
+		ret.set_float(this.max_force);
+	};	
+	
+	Exps.prototype.CurForce = function (ret)
+	{
+		ret.set_float(this.current_applied_force);
+	};		
+	
+	Exps.prototype.Kp = function (ret)
+	{
+		ret.set_float(this.pid_ctrl.kp);
+	};
+	
+	Exps.prototype.Ki = function (ret)
+	{
+		ret.set_float(this.pid_ctrl.ki);
+	};	
+	
+	Exps.prototype.Kd = function (ret)
+	{
+		ret.set_float(this.pid_ctrl.kd);
+	};		
+	
+}());
+
+(function ()
+{
+    cr.behaviors.Rex_physics_torque.PIControllerKlass = function(kp, ki, kd, reset_windup_err)
+    {       
+	    this.kp = kp;
+		this.ki = ki;
+		this.kd = kd;
+		this.err_acc = 0;
+		this.err_pre = 0;
+		this.reset_windup_err = reset_windup_err;
+		this.is_reset_windup = false;
+    };
+    var PIControllerKlassProto = cr.behaviors.Rex_physics_torque.PIControllerKlass.prototype;
+	
+	PIControllerKlassProto.procress = function (err)
+	{
+	    this.is_reset_windup = (this.reset_windup_err > 0) &&
+	                           (Math.abs(err) > this.reset_windup_err);
+	    if (this.is_reset_windup)
+	        this.err_acc = 0;
+	    else
+	        this.err_acc += err;
+	    var derr = err - this.err_pre;
+	    this.err_pre = err;
+	    var out = (this.kp * err) + (this.ki * this.err_acc) + (this.kd * derr);
+	    out = cr.clamp(out, -1 ,1);
+        return out;
+	};
+	
+	PIControllerKlassProto.saveToJSON = function ()
+	{
+		return { "kp": this.kp,
+		         "ki": this.ki,
+		         "kd": this.kd,
+		         "ea": this.err_acc,
+		         "ep": this.err_pre
+                };
+	};
+	
+	PIControllerKlassProto.loadFromJSON = function (o)
+	{                  
+	    this.kp = o["kp"];
+	    this.ki = o["ki"];
+	    this.kd = o["kd"];
+	    this.err_acc = o["ea"];
+	    this.err_pre = o["ep"];  
+	}; 	
+}());   
