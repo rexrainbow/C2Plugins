a2c70da5421dc18bf6fbcf207b021f045145d5fc
hua <binhua.tein@gmail.com>
2014-01-23 22:53:08 +0800

    Add CSV to dict plugin

---
+++ plugins/rex_csv2dictionary/edittime.js
@@ -0,0 +1,99 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"CSV to Dictionary",
+		"id":			"Rex_CSV2Dictionary",
+		"version":		"0.1",   		
+		"description":	"Transfer csv string to dictionary object",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_csv2dictionary.html",
+		"category":		"Data & Storage",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddStringParam("Data", "Data in CSV format", "");
+AddCondition(1, cf_looping | cf_not_invertible, "For each cell", "For each cell", 
+             "For each cell in <i>{0}</i>", 
+             "Repeat the event for each cell in the csv table.", "ForEachCell");
+             
+//////////////////////////////////////////////////////////////
+// Actions     
+AddStringParam("Data", "Data in CSV format", "");
+AddObjectParam("Dictionary", "Dictionary object");
+AddAction(1, 0, "Put csv data into dictionary", "CSV to Dictionary", 
+          "Put csv data <i>{0}</i> into dictionary <i>{1}</i>", 
+          "Put csv data into dictionary.", "CSV2Dictionary");
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_string, "Current key", "For Each cell", "CurKey", 
+              "Get the current key in a For Each loop.");
+AddExpression(2, ef_return_any, "Current value", "For Each cell", "CurValue", 
+              "Get the current cell value in a For Each loop.");
+                         
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Eval mode", "No", 'Enable "Eval mode" for parsing value.', "No|Yes"),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_csv2dictionary/runtime.js
@@ -0,0 +1,236 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_CSV2Dictionary = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_CSV2Dictionary.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+	    this.is_eval_mode = (this.properties[0] == 1);
+	    this.exp_CurKey = "";
+	    this.exp_CurValue = "";
+	};
+
+	instanceProto.value_get = function(v)
+	{
+	    if (v == null)
+	        v = 0;
+	    else if (this.is_eval_mode)
+	        v = eval("("+v+")");
+        
+        return v;
+	};
+	
+    // copy from    
+    // http://www.bennadel.com/blog/1504-Ask-Ben-Parsing-CSV-Strings-With-Javascript-Exec-Regular-Expression-Command.htm
+    
+    // This will parse a delimited string into an array of
+    // arrays. The default delimiter is the comma, but this
+    // can be overriden in the second argument.
+    var CSVToArray = function ( strData, strDelimiter ){
+        // Check to see if the delimiter is defined. If not,
+        // then default to comma.
+        strDelimiter = (strDelimiter || ",");
+
+        // Create a regular expression to parse the CSV values.
+        var objPattern = new RegExp(
+                (
+                        // Delimiters.
+                        "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+                        // Quoted fields.
+                        "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+                        // Standard fields.
+                        "([^\"\\" + strDelimiter + "\\r\\n]*))"
+                ),
+                "gi"
+                );
+
+
+        // Create an array to hold our data. Give the array
+        // a default empty first row.
+        var arrData = [[]];
+
+        // Create an array to hold our individual pattern
+        // matching groups.
+        var arrMatches = null;
+
+
+        // Keep looping over the regular expression matches
+        // until we can no longer find a match.
+        while (arrMatches = objPattern.exec( strData )){
+
+                // Get the delimiter that was found.
+                var strMatchedDelimiter = arrMatches[ 1 ];
+
+                // Check to see if the given delimiter has a length
+                // (is not the start of string) and if it matches
+                // field delimiter. If id does not, then we know
+                // that this delimiter is a row delimiter.
+                if (
+                        strMatchedDelimiter.length &&
+                        (strMatchedDelimiter != strDelimiter)
+                        ){
+
+                        // Since we have reached a new row of data,
+                        // add an empty row to our data array.
+                        arrData.push( [] );
+
+                }
+
+
+                // Now that we have our delimiter out of the way,
+                // let's check to see which kind of value we
+                // captured (quoted or unquoted).
+                if (arrMatches[ 2 ]){
+
+                        // We found a quoted value. When we capture
+                        // this value, unescape any double quotes.
+                        var strMatchedValue = arrMatches[ 2 ].replace(
+                                new RegExp( "\"\"", "g" ),
+                                "\""
+                                );
+
+                } else {
+
+                        // We found a non-quoted value.
+                        var strMatchedValue = arrMatches[ 3 ];
+
+                }
+
+
+                // Now that we have our value string, let's add
+                // it to the data array.
+                arrData[ arrData.length - 1 ].push( strMatchedValue );
+        }
+
+        // Return the parsed data.
+        return( arrData );
+    };        
+   
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();      
+    
+	Cnds.prototype.ForEachCell = function (csv_string)
+	{
+	    var table = CSVToArray(csv_string);
+		var i, cnt = table.length;
+		if (cnt == 0)
+		    return false;			
+	    
+        var current_frame = this.runtime.getCurrentEventStack();
+        var current_event = current_frame.current_event;
+		var solModifierAfterCnds = current_frame.isModifierAfterCnds();
+			
+	    var entry, v;
+        if (solModifierAfterCnds)
+        {
+		    for (i=0; i<cnt; i++ )
+	        {
+                this.runtime.pushCopySol(current_event.solModifiers);
+                	            
+	            entry = table[i];
+	            this.exp_CurKey = entry[0];
+	            this.exp_CurValue = this.value_get(entry[1]);
+		    	current_event.retrigger();
+		    	
+		    	this.runtime.popSol(current_event.solModifiers);		    	
+		    }	
+	    }
+	    else
+	    {
+		    for (i=0; i<cnt; i++ )
+	        {         
+	            entry = table[i];
+	            this.exp_CurKey = entry[0];
+	            this.exp_CurValue = this.value_get(entry[1]);
+	                               
+		    	current_event.retrigger();
+		    }	        
+	    }
+
+		return false;
+	};  
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+    Acts.prototype.CSV2Dictionary = function (csv_string, dict_objs)
+	{  
+	    assert2(cr.plugins_.Dictionary, "[CSV2Dictionary] Error:No Dictionary object found.");
+	    	    
+        var dict_obj = dict_objs.getFirstPicked();
+        var is_dict_inst = (dict_obj instanceof cr.plugins_.Dictionary.prototype.Instance);
+        assert2(is_dict_inst, "[CSV2Dictionary] Error:Need an dictionary object.");
+
+        var table = CSVToArray(csv_string);        
+		var i, cnt = table.length;
+		if (cnt == 0)
+		    return;	
+		    
+		var entry, k, v;
+		var add_key = cr.plugins_.Dictionary.prototype.acts.AddKey;
+		for (i=0; i<cnt; i++)
+		{
+		    entry = table[i];
+		    k = entry[0];
+		    v = this.value_get(entry[1]);
+		    add_key.apply(dict_obj, [k, v]);
+		}
+	};
+    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+    
+	Exps.prototype.CurKey = function (ret)
+	{
+		ret.set_string(this.exp_CurKey);
+	};
+
+	Exps.prototype.CurValue = function (ret)
+	{
+		ret.set_any(this.exp_CurValue);
+	};		
+}());
