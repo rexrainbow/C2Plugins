696f01e4aa91ed30f4d52a4f59c11b6bd4797ce3
hua <binhua.tein@gmail.com>
2014-02-05 22:13:56 +0800

    Add replacer behavior

---
+++ behaviors/rex_replacer/edittime.js
@@ -0,0 +1,100 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Replacer",
+		"id":			"Rex_Replacer",
+		"description":	"Replace instacne by fade-out itself, and create the target instance then fade-in it.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_replacer.html",
+		"category":		"General",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On fade-out started", "Fade out", "On {my} fade-out started", 
+             "Triggered when fade-out started", "OnFadeOutStart");
+AddCondition(2, cf_trigger, "On fade-out finished", "Fade out", "On {my} fade-out finished", 
+             "Triggered when fade-out finished", "OnFadeOutFinish");
+AddCondition(3, cf_trigger, "On fade-in started", "Fade in", "On {my} fade-in started", 
+             "Triggered when fade-out started", "OnFadeInStart");
+AddCondition(4, cf_trigger, "On fade-in finished", "Fade in", "On {my} fade-in finished", 
+             "Triggered when fade-in finished", "OnFadeInFinish");
+AddCondition(5, 0, "Is fade-out", "Fade out", "Is {my} fade-out", 
+             "Return true if instance is in fade-out stage", "IsFadeOut");
+AddCondition(6, 0, "Is fade-in", "Fade in", "Is {my} fade-in", 
+             "Return true if instance is in fade-in stage", "IsFadeIn");     
+AddCondition(7, 0, "Is idle", "Idle", "Is {my} idle", 
+             "Return true if instance is in idle stage", "IsIdle");                       
+//////////////////////////////////////////////////////////////
+// Actions 
+AddObjectParam("Target", "Target type of replacing instance.");
+AddAction(1, 0, "Replace instance", "Replace", 
+          "{my} Replace to {0}","Replace instance.", "ReplaceInst"); 
+AddStringParam("Target", "Target type in nickname of replacing instance.", '""');
+AddAction(2, 0, "Replace instance to nickname type", "Replace", 
+          "Replace {my} to nickname: <i>{0}</i>","Replace instance to nickname type.", "ReplaceInst");             
+AddNumberParam("Duration", "Duration of fade-out or fade in, in seconds.");
+AddAction(3, 0, "Set duration", "Configure", "Set {my} fade duration to <i>{0}</i>", "Set the object's fade duration.", "SetDuration");
+ 
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, "Get UID of replacing instance", "UID", "ReplacingInstUID", 
+              "The UID of replacing instanc, return -1 if the replacing does not start.");
+AddExpression(2, ef_return_number, "Get UID of replacing instance", "UID", "ReplacingInstUID", 
+              "The UID of replacing instanc, return -1 if the replacing does not start.");
+              
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [   
+    new cr.Property(ept_float, "Fade duration", 1, "Duration of fade-out or fade-in, in seconds."), 
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{	
+	// Clamp values
+	if (this.properties["Fade duration"] < 0)
+		this.properties["Fade duration"] = 0;    
+}
---
+++ behaviors/rex_replacer/runtime.js
@@ -0,0 +1,349 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_Replacer = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_Replacer.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	    this.nickname_object = null;
+	};
+  
+	behtypeProto._nickname_inst_get = function ()
+	{
+	    if (this.nickname_object != null)
+	    {
+	        return this.nickname_object;
+	    }
+	    
+	    assert2(cr.plugins_.Rex_Nickname, "Replacer behavior: Could not find nickname plugin.");
+
+        var plugins = this.runtime.types;			
+        var name, inst;
+        for (name in plugins)
+        {
+            inst = plugins[name].instances[0];
+            
+            if (inst instanceof cr.plugins_.Rex_Nickname.prototype.Instance)
+            {
+                this.nickname_object = inst;
+                return inst;
+            }                                          
+        }
+        return null;
+	};
+	
+	behtypeProto.Nickname2Type = function (nickname)
+	{
+	    var nickname_inst = this._nickname_inst_get();
+	    if (nickname_inst == null)
+	        return null;
+	        
+	    return nickname_inst.Nickname2Type(nickname);
+	};
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;       
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+    var CMD_IDLE = 0;
+    var CMD_FADEOUT = 1;
+    var CMD_FADEIN = 2;    
+	behinstProto.onCreate = function()
+	{      
+        this.fade_duration = this.properties[0];	     
+        this.fade = null;
+        this.cmd = CMD_IDLE;
+        this.ReplacingInstUID = -1;
+	};
+
+	behinstProto.tick = function ()
+	{
+	    if (this.cmd == CMD_IDLE)
+	        return;
+	        
+	    var dt = this.runtime.getDt(this.inst);
+	    if (dt == 0)
+	        return;
+	        
+	    var is_continue = this.fade.tick(dt);
+	    
+        this.inst.opacity = this.fade.value;
+        this.runtime.redraw = true;
+        
+	    if (!is_continue)
+	    {  
+	        if (this.cmd == CMD_FADEOUT)
+	        {
+	            this.runtime.trigger(cr.behaviors.Rex_Replacer.prototype.cnds.OnFadeOutStart, this.inst);
+	            this.runtime.DestroyInstance(this.inst);
+	        }
+	        else  // CMD_FADEIN
+	        {
+	            this.runtime.trigger(cr.behaviors.Rex_Replacer.prototype.cnds.OnFadeInStart, this.inst);
+	        }
+	        this.cmd = CMD_IDLE;
+	    }
+	};  
+	
+  	behinstProto.fadeout_start = function (duration)
+	{          
+	    if (this.fade == null)
+	    {
+	        this.fade = new cr.behaviors.Rex_Replacer.LinearInterpolationKlass();
+	    }
+	    
+	    this.cmd = CMD_FADEOUT;
+	    this.fade.start(this.inst.opacity, 0, duration);
+	    this.runtime.trigger(cr.behaviors.Rex_Replacer.prototype.cnds.OnFadeOutStart, this.inst);   
+	};	
+	
+  	behinstProto.fadein_start = function (duration)
+	{     
+	    if (this.fade == null)
+	    {
+	        this.fade = new cr.behaviors.Rex_Replacer.LinearInterpolationKlass();
+	    }
+
+	    this.cmd = CMD_FADEIN;
+	    this.fade.start(0, this.inst.opacity, duration);
+	    this.runtime.trigger(cr.behaviors.Rex_Replacer.prototype.cnds.OnFadeInStart, this.inst);  
+	};
+	
+	function GetReplacerBehavior(inst)
+	{
+		var i, len;
+		for (i = 0, len = inst.behavior_insts.length; i < len; i++)
+		{
+			if (inst.behavior_insts[i] instanceof behaviorProto.Instance)
+				return inst.behavior_insts[i];
+		}
+		
+		return null;
+	};
+	
+  	behinstProto.pushback_inst = function (target_inst)
+	{     
+	    var layer_insts = this.inst.layer.instances;
+	    var targetZ = target_inst.get_zindex();
+	    cr.arrayRemove(layer_insts, targetZ);
+        var insertZ = this.inst.get_zindex();
+        layer_insts.splice(insertZ, 0, target_inst);
+        this.inst.layer.zindices_stale = true;				
+		this.runtime.redraw = true;        
+	};
+			
+  	behinstProto.replaceTo = function (objtype)
+	{     
+	    this.fadeout_start(this.fade_duration);
+	    var target_inst = this.create_inst(objtype);
+	    if (target_inst == null)
+	        return;
+	    this.ReplacingInstUID = target_inst.uid;
+	    this.pushback_inst(target_inst);
+	    var replacer_inst = GetReplacerBehavior(target_inst);
+	    if (replacer_inst == null)
+	        return;
+	    replacer_inst.fadein_start(this.fade_duration);     
+	};
+	
+	behinstProto.create_inst = function (objtype)
+	{
+        var inst = this.runtime.createInstance(objtype, 
+                                               this.inst.layer, this.inst.x, this.inst.y ); 
+		if (inst == null)
+		    return null;
+		
+        var sol = inst.type.getCurrentSol();
+        sol.select_all = false;
+		sol.instances.length = 1;
+		sol.instances[0] = inst;
+		
+		// Siblings aren't in instance lists yet, pick them manually
+		var i, len, s;
+		if (inst.is_contained)
+		{
+			for (i = 0, len = inst.siblings.length; i < len; i++)
+			{
+				s = inst.siblings[i];
+				sol = s.type.getCurrentSol();
+				sol.select_all = false;
+				sol.instances.length = 1;
+				sol.instances[0] = s;
+			}
+		}
+        
+		this.runtime.isInOnDestroy++;
+		this.runtime.trigger(Object.getPrototypeOf(inst.type.plugin).cnds.OnCreated, inst);
+		this.runtime.isInOnDestroy--;
+        
+	    return inst;
+	}; 		
+	
+	behinstProto.saveToJSON = function ()
+	{
+		return { "fd": this.fade_duration,
+		         "fade": (this.fade == null)? null: this.fade.saveToJSON(),
+                 "cmd": this.cmd,
+                 "touid": this.ReplacingInstUID };
+	};
+	
+	behinstProto.loadFromJSON = function (o)
+	{
+		this.fade_duration = o["fd"];
+		var fade_obj = o["fade"];
+		if (fade_obj == null)		    
+		    this.fade = null;
+		else
+		{
+		    this.fade = new cr.behaviors.Rex_Replacer.LinearInterpolationKlass();
+		    this.fade.loadFromJSON(fade_obj);
+		}
+		this.cmd = o["cmd"];
+		this.ReplacingInstUID = o["touid"];
+	};	
+	 
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	Cnds.prototype.OnFadeOutStart = function ()
+	{
+		return true;
+	};
+
+	Cnds.prototype.OnFadeOutFinish = function ()
+	{
+		return true;
+	};   
+
+	Cnds.prototype.OnFadeInStart = function ()
+	{
+		return true;
+	};
+
+	Cnds.prototype.OnFadeInFinish = function ()
+	{
+		return true;
+	};
+	
+	Cnds.prototype.IsFadeOut = function ()
+	{
+		return (this.cmd == CMD_FADEOUT);
+	};
+
+	Cnds.prototype.IsFadeIn = function ()
+	{
+		return (this.cmd == CMD_FADEIN);
+	};
+
+	Cnds.prototype.IsIdle = function ()
+	{
+		return (this.cmd == CMD_IDLE);
+	};	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+    Acts.prototype.ReplaceInst = function (objtype)
+	{
+        if (typeof(objtype) == "string")
+        {
+            objtype = this.type.Nickname2Type(objtype);
+        }
+        if (!objtype)
+            return;
+            
+        this.replaceTo(objtype);                           
+	}; 
+	
+    Acts.prototype.SetDuration = function (t)
+	{
+       this.fade_duration = t;                           
+	}; 
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+	Exps.prototype.ReplacingInstUID = function (ret)
+	{
+		ret.set_int(this.ReplacingInstUID);
+	}; 
+}());
+
+(function ()
+{
+    // command queue
+    cr.behaviors.Rex_Replacer.LinearInterpolationKlass = function()
+    {
+    };
+    var LinearInterpolationKlassProto = cr.behaviors.Rex_Replacer.LinearInterpolationKlass.prototype;
+    
+    LinearInterpolationKlassProto.start = function(start, end, duration)
+	{
+        this.start_value = start;
+        this.delta_value = end - start;      
+        this.duration = duration;
+        this.accDt = 0; 
+        this.value = start;           
+	};
+	
+    LinearInterpolationKlassProto.tick = function(dt)
+	{
+         this.accDt += dt;
+         var p = (this.accDt >= this.duration)? 1 : (this.accDt/this.duration); 
+         this.value = (p*this.delta_value) + this.start_value;         
+         return (p < 1);
+	};	
+	
+	LinearInterpolationKlassProto.saveToJSON = function ()
+	{
+		return { "s": this.start_value,
+                 "dv": this.delta_value,
+                 "dt": this.duration,
+                 "accDt": this.accDt,
+                 "v": this.value,
+               };
+	};
+	
+	LinearInterpolationKlassProto.loadFromJSON = function (o)
+	{
+		this.start_value = o["s"];
+		this.delta_value = o["dv"];
+		this.duration = o["dt"];
+		this.accDt = o["accDt"];	
+		this.value = o["v"];				
+	};	
+}());
