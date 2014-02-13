7cef69f707266e38f414177581afed123eacea04
hua <binhua.tein@gmail.com>
2014-02-13 23:10:07 +0800

    Add behavior

---
+++ behaviors/rex_pinoffsetxy/edittime.js
@@ -0,0 +1,86 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Pin offsetXY",
+		"id":			"Rex_pinOffsetXY",
+		"description":	"Stick to another object maintaining a relative offsetXY.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_pinxyoffset.html",
+		"category":		"General",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(0, cf_none, "Is pinned", "", "{my} is pinned", "Object is currently pinned to another object.", "IsPinned");
+             
+//////////////////////////////////////////////////////////////
+// Actions 
+AddObjectParam("Pin to", "Choose the object to pin to.");
+AddNumberParam("Offset x", "Offset x in pixel", 0);
+AddNumberParam("Offset y", "Offset y in pixel", 0);
+AddAction(0, af_none, "Pin to object with offset", "Pin", 
+          "{my} Pin to {0} with offset (<i>{1}</i>, <i>{2}</i>)", 
+          "Pin the object to another object with offset.", "Pin");
+AddObjectParam("Pin to", "Choose the object to pin to.");
+AddAction(1, af_none, "Pin to object", "Pin", 
+          "{my} Pin to {0}", 
+          "Pin the object to another object with current offset.", "Pin");          
+
+AddAction(10, af_none, "Unpin", "Unpin", "{my} Unpin", "Unpin the object.", "Unpin");
+         
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_number, "", "", "PinnedUID", "Get the UID of the object pinned to, or -1 if not pinned.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [    
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{	
+}
---
+++ behaviors/rex_pinoffsetxy/runtime.js
@@ -0,0 +1,191 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_pinOffsetXY = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_pinOffsetXY.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;       
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+		this.pinObject = null;
+		this.pinObjectUid = -1;		// for loading
+		this.pin_offsetx = 0;
+		this.pin_offsety = 0;
+		
+		var self = this;
+		
+		// Need to know if pinned object gets destroyed
+		if (!this.recycled)
+		{
+			this.myDestroyCallback = (function(inst) {
+													self.onInstanceDestroyed(inst);
+												});
+		}
+										
+		this.runtime.addDestroyCallback(this.myDestroyCallback);
+	};
+	
+	behinstProto.saveToJSON = function ()
+	{
+		return {
+			"uid": this.pinObject ? this.pinObject.uid : -1,
+			"offx": this.pin_offsetx,
+			"offy": this.pin_offsety,
+		};
+	};
+	
+	behinstProto.loadFromJSON = function (o)
+	{
+		this.pinObjectUid = o["uid"];		// wait until afterLoad to look up		
+		this.pin_offsetx = o["offx"];
+		this.pin_offsety = o["offy"];
+	};
+	
+	behinstProto.afterLoad = function ()
+	{
+		// Look up the pinned object UID now getObjectByUID is available
+		if (this.pinObjectUid === -1)
+			this.pinObject = null;
+		else
+		{
+			this.pinObject = this.runtime.getObjectByUID(this.pinObjectUid);
+			assert2(this.pinObject, "Failed to find pin object by UID");
+		}
+		
+		this.pinObjectUid = -1;
+	};
+	
+	behinstProto.onInstanceDestroyed = function (inst)
+	{
+		// Pinned object being destroyed
+		if (this.pinObject == inst)
+			this.pinObject = null;
+	};
+	
+	behinstProto.onDestroy = function()
+	{
+		this.pinObject = null;
+		this.runtime.removeDestroyCallback(this.myDestroyCallback);
+	};
+	
+	behinstProto.tick = function ()
+	{
+		// do work in tick2 instead, after events to get latest object position
+	};
+
+	behinstProto.tick2 = function ()
+	{
+		if (!this.pinObject)
+			return;
+		
+		var newx = this.pinObject.x + this.pin_offsetx;
+		var newy = this.pinObject.y + this.pin_offsety;		
+		if (this.inst.x !== newx || this.inst.y !== newy)
+		{
+			this.inst.x = newx;
+			this.inst.y = newy;
+			this.inst.set_bbox_changed();
+		}
+	};
+	
+
+	/**BEGIN-PREVIEWONLY**/
+	behinstProto.getDebuggerValues = function (propsections)
+	{
+		propsections.push({
+			"title": this.type.name,
+			"properties": [
+				{"name": "Is pinned", "value": !!this.pinObject, "readonly": true},
+				{"name": "Pinned UID", "value": this.pinObject ? this.pinObject.uid : 0, "readonly": true}
+			]
+		});
+	};
+	/**END-PREVIEWONLY**/
+		 
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	Cnds.prototype.IsPinned = function ()
+	{
+		return !!this.pinObject;
+	};
+		
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.Pin = function (obj, offset_x, offset_y)
+	{
+		if (!obj)
+			return;
+			
+		var otherinst = obj.getFirstPicked(this.inst);
+		
+		if (!otherinst)
+			return;
+			
+        if (offset_x == null)
+            offset_x = this.inst.x - otherinst.x;
+        if (offset_y == null)
+            offset_y = this.inst.y - otherinst.y;
+                  
+		this.pinObject = otherinst;
+		this.pin_offsetx = offset_x;
+		this.pin_offsety = offset_y;
+	};
+	
+	Acts.prototype.Unpin = function ()
+	{
+		this.pinObject = null;
+	};  
+    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+	
+	Exps.prototype.PinnedUID = function (ret)
+	{
+		ret.set_int(this.pinObject ? this.pinObject.uid : -1);
+	};
+	 
+}());
