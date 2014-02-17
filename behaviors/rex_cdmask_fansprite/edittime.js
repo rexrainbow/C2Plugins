8454c3793c2790509a5fe235c026bfd065cb04bd
hua <binhua.tein@gmail.com>
2014-02-17 22:55:41 +0800

    Add behavior

---
+++ behaviors/rex_cdmask_fansprite/edittime.js
@@ -0,0 +1,87 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"CD Mask w fan sprites",
+		"id":			"Rex_cdmask_fansprite",
+		"description":	"Cool down mask with fan sprites. 9 Fan frames are 256,128,64,32,16,8,4,2,1 degrees",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropboxusercontent.com/u/5779181/C2Repo/rex_cdmask_fansprite.html",
+		"category":		"Effect",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, 0, "Pick fan sprites", "SOL", 
+             "{my} pick fan sprites", "Pick fan sprites to SOL.", "PickFan");
+             
+//////////////////////////////////////////////////////////////
+// Actions
+AddObjectParam("Fan", "Fan object.");
+AddAction(1, 0, "Setup fan", "Setup", 
+          "{my} set fan object to <i>{0}</i>", 
+          "Setup fan object.", "SetupFan");             
+AddNumberParam("percentage", "Cool down percentage, 1-0", 0);
+AddAction(3, 0, "Set percentage", "Mask", 
+          "Set cool down percentage to <i>{0}</i>", 
+          "Set cool down percentage.", "SetCoolDownPercentage"); 
+AddAction(4, 0, "Pick fan sprites", "SOL", 
+          "Pick fan sprites", 
+          "Pick fan sprites.", "PickFan");                 
+ 
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    //new cr.Property(ept_combo, "Z order", "Front", "Z order of mask.", "Front|Back"),    
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_cdmask_fansprite/runtime.js
@@ -0,0 +1,240 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_cdmask_fansprite = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_cdmask_fansprite.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{ 
+        this.fan_type = null;       
+        this.set_anim_frame = cr.plugins_.Sprite.prototype.acts.SetAnimFrame;
+        this.set_anim_speed = cr.plugins_.Sprite.prototype.acts.SetAnimSpeed;
+        this.set_sprite_angle = cr.plugins_.Sprite.prototype.acts.SetAngle;
+        this.set_sprite_visible = cr.plugins_.Sprite.prototype.acts.SetVisible;
+	};
+	
+	behtypeProto.CreateInst = function (x, y, layer, frame_index)
+	{
+	    if (this.fan_type == null)
+	        return null;
+	    var inst = this.runtime.createInstance(this.fan_type, layer, x, y);
+	    if (inst == null)
+	        return null;
+	      
+	    this.set_anim_frame.call(inst, frame_index);  
+	    this.set_anim_speed.call(inst, 0);	    
+	    return inst;
+	};	
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+    
+    var _frame2angle = [256, 128, 64, 32, 16, 8, 4, 2, 1];    
+	behinstProto.onCreate = function()
+	{        
+        this.is_back = (this.properties[0] == 1);
+        this.fan_insts = [-1,-1,-1,-1,-1,-1,-1,-1,-1];  // uid
+	};  
+    
+	behinstProto.onDestroy = function()
+	{
+        var i, cnt=this.fan_insts.length, fan_inst;
+        for (i=0; i<cnt; i++)
+        {
+            fan_inst = this._uid2inst(this.fan_insts[i]);
+            if (fan_inst == null)
+                continue;
+            this.runtime.DestroyInstance(fan_inst);
+        }
+	};  
+	
+	behinstProto.tick = function ()
+	{
+		// do work in tick2 instead, after events to get latest object position
+	};
+
+	behinstProto.tick2 = function ()
+	{
+        this._pin_fan_insts();
+	};
+	 
+	behinstProto._uid2inst = function (uid)
+	{
+	    if (uid == null)
+	        return null;
+	    
+	    return this.runtime.getObjectByUID(uid);
+	};	
+		 
+	behinstProto._pin_fan_insts = function ()
+	{
+        var i, cnt=this.fan_insts.length, fan_inst;
+        var reflash = false;     
+        for (i=0; i<cnt; i++)
+        {
+            fan_inst = this._uid2inst(this.fan_insts[i]);
+            if (fan_inst == null)
+                continue;
+                
+            reflash = false;            
+            if (fan_inst.x != this.inst.x)
+            {
+                fan_inst.x = this.inst.x;
+                reflash = true;
+            }
+            if (fan_inst.y != this.inst.y)
+            {
+                fan_inst.y = this.inst.y;
+                reflash = true;
+            }
+            if (reflash)
+            {
+                fan_inst.set_bbox_changed();
+                fan_inst.runtime.redraw = true; 
+            }
+        }
+	};
+
+
+	behinstProto._cd_mask = function (percentage)
+	{
+	    if (this.type.fan_type == null)
+	        return;
+	        	    
+	    var a = Math.floor(percentage * 360), f2a;
+	    var i, cnt=this.fan_insts.length, j=-1;
+	    var fan_angle=0, fan_inst;
+	    
+	    for (i=0; i<cnt; i++)
+	    {
+	        fan_inst = this._uid2inst(this.fan_insts[i]);	        
+	        if (fan_inst == null)
+	        {
+	            fan_inst = this.type.CreateInst(this.inst.x, this.inst.y, this.inst.layer, i);
+	            if (fan_inst == null)
+	                return;
+	            this.fan_insts[i] = fan_inst.uid;
+	        }
+	        
+	        f2a = _frame2angle[i];
+	        if (a >= f2a)
+	        {	            
+	            a -= f2a;	            
+	            this.type.set_sprite_visible.call(fan_inst, true);
+	            this.type.set_sprite_angle.call(fan_inst, fan_angle);	 
+	            fan_angle += f2a;  
+	        }
+	        else
+	        {
+	            this.type.set_sprite_visible.call(fan_inst, false);
+	        }
+	    }
+	    
+	    if (percentage == 0)     // destroy all fan sprites
+	    {
+	        for (i=0; i<cnt; i++)
+	        {
+	            fan_inst = this._uid2inst(this.fan_insts[i]);
+	            this.fan_insts[i] = -1;
+	            if (fan_inst == null)
+                    continue;
+                this.runtime.DestroyInstance(fan_inst);
+	        }
+	    }	    
+	};	
+	
+	behinstProto._pick_fan_insts = function ()
+	{
+	    if (this.type.fan_type == null)
+	        return false;
+
+        var sol = this.type.fan_type.getCurrentSol(); 
+        sol.instances.length = 0;
+        sol.select_all = false; 
+        var i, cnt=this.fan_insts.length, fan_inst;
+        for (i=0; i<cnt; i++)
+        {
+            fan_inst = this._uid2inst(this.fan_insts[i]);
+            if (fan_inst == null)
+                continue;
+            sol.instances.push(fan_inst);
+        }
+        return (sol.instances.length > 0);
+	};
+	
+	behinstProto.saveToJSON = function ()
+	{    
+		return { "fansid": this.type.fan_type.sid,
+		         };
+	};
+	
+	behinstProto.loadFromJSON = function (o)
+	{   
+	    this.type.fan_type = this.runtime.getObjectTypeBySid(o["fansid"]);
+	};	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	  
+	Cnds.prototype.PickCanvas = function ()
+	{
+		return this._pick_fan_insts(); 
+	};	 
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+    Acts.prototype.SetupFan = function (fan_type)
+	{
+	    this.type.fan_type = fan_type;
+	}; 
+
+	Acts.prototype.SetCoolDownPercentage = function(percentage)
+	{
+	    this._cd_mask(percentage);
+	};
+	
+	Acts.prototype.PickFan = function()
+	{
+        this._pick_fan_insts();      
+	};    
+    
+    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+}());
