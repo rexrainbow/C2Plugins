64e8dbc08d3e032f9d48a7691b058e79443c1711
hua <binhua.tein@gmail.com>
2014-02-18 23:48:13 +0800

    Modified from official function plugin, allow passing string name parameters.

---
+++ plugins/rex_function2/edittime.js
@@ -0,0 +1,116 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Function2",
+		"id":			"Rex_Function2",
+		"version":		"0.1",
+		"description":	"Run other events in an action, like functions in programming languages.",
+		"author":		"Scirra",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_function2.html",
+		"category":		"Control flow",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddStringParam("Name", "The name of the function that is being called.", "\"\"");
+AddCondition(0,	cf_trigger | cf_fast_trigger, "On function", "Function", "On <b>{0}</b>", "Triggered when a function is called.", "OnFunction");
+
+AddAnyTypeParam("Index", "The zero-based index of the parameter to get, or name in string.");
+AddCmpParam("Comparison", "How to compare the function parameter.");
+AddAnyTypeParam("Value", "The value to compare to.");
+AddCondition(1, cf_none, "Compare parameter", "Function", "Parameter {0} {1} {2}", "Compare the value of a parameter in a function call.", "CompareParam");
+
+AddStringParam("Name", "Parameter's name", '""');
+AddAnyTypeParam("Value", "The default value.");
+AddCondition(50, 0, "Declare parameter", "Input parameter", "Declare parameter <i>{0}</i>, default to <i>{1}</i>", "Declare input parameter in name string and it's default value", "DefineParam");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("Name", "The name of the function to call.", "\"\"");
+AddVariadicParams("Parameter {n}", "A parameter to pass for the function call, which can be accessed with Function.Param({n}).");
+AddAction(0, 0, "Call function", "Function", "Call <b>{0}</b> (<i>{...}</i>)", "Call a function, running its 'On function' event.", "CallFunction");
+
+AddAnyTypeParam("Value", "A number or some text to return from the function call.");
+AddAction(1, 0, "Set return value", "Function", "Set return value to <b>{0}</b>", "In an 'On function' event, set the return value.", "SetReturnValue");
+
+AddAnyTypeParam("Name", "Name of parameter", '""');
+AddAnyTypeParam("Value", "Value", "0");
+AddAction(50, 0, "Set parameter", "Input parameter", "Set parameter <i>{0}</i> to <i>{1}</i>", "Set a parameter pass into function.", "SetParameter");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(0, ef_return_any, "", "Function", "ReturnValue", "Get the value set by 'Set return value'.");
+
+AddExpression(1, ef_return_number, "", "Function", "ParamCount", "Get the number of parameters passed to this function.");
+
+AddAnyTypeParam("Index", "The zero-based index of the parameter to get, or name in string.");
+AddExpression(2, ef_return_any, "", "Function", "Param", "Get the value of a parameter passed to the function.");
+
+AddStringParam("Name", "The name of the function to call.");
+AddExpression(3, ef_return_any | ef_variadic_parameters, "", "Function", "Call", "Call a function with parameters and return its return value.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_function2/runtime.js
@@ -0,0 +1,310 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Function2 = function(runtime)
+{
+    this.runtime = runtime;
+};
+
+(function ()
+{
+    var pluginProto = cr.plugins_.Rex_Function2.prototype;
+        
+    /////////////////////////////////////
+    // Object type class
+    pluginProto.Type = function(plugin)
+    {
+        this.plugin = plugin;
+        this.runtime = plugin.runtime;
+    };
+
+    var typeProto = pluginProto.Type.prototype;
+
+    typeProto.onCreate = function()
+    {
+    };
+
+    /////////////////////////////////////
+    // Instance class
+    pluginProto.Instance = function(type)
+    {
+        this.type = type;
+        this.runtime = type.runtime;
+    };
+    
+    var instanceProto = pluginProto.Instance.prototype;
+    
+    var funcStack = [];
+    var funcStackPtr = -1;
+    var isInPreview = false;	// set in onCreate
+    
+    function FuncStackEntry()
+    {
+        this.name = "";
+        this.retVal = 0;
+        this.params = [];
+        this.name_map = {};
+    };
+    
+    function pushFuncStack()
+    {
+        funcStackPtr++;
+        
+        if (funcStackPtr === funcStack.length)
+            funcStack.push(new FuncStackEntry());
+            
+        return funcStack[funcStackPtr];
+    };
+    
+    function getCurrentFuncStack()
+    {
+        if (funcStackPtr < 0)
+            return null;
+            
+        return funcStack[funcStackPtr];
+    };
+    
+    function getOneAboveFuncStack()
+    {
+        if (!funcStack.length)
+            return null;
+        
+        var i = funcStackPtr + 1;
+        
+        if (i >= funcStack.length)
+            i = funcStack.length - 1;
+            
+        return funcStack[i];
+    };
+    
+    function popFuncStack()
+    {
+        assert2(funcStackPtr >= 0, "Popping empty function stack");
+        
+        funcStackPtr--;
+    };
+
+    instanceProto.onCreate = function()
+    {
+        isInPreview = (typeof cr_is_preview !== "undefined");
+        this.name_map = {};
+        this.param_index = 0;
+    };
+    
+    instanceProto.name_map_init = function(fs)
+    {
+        this.param_index = 0;
+        var n;
+        for (n in fs.name_map)
+        {
+            delete fs.name_map[n];
+        }
+        for (n in this.name_map)
+        {
+            fs.name_map[n] = this.name_map[n];
+            delete this.name_map[n];
+        }
+    };
+    //////////////////////////////////////
+    // Conditions
+    function Cnds() {};
+
+    // THIS IS A SPECIAL TRIGGER
+    Cnds.prototype.OnFunction = function (name_)
+    {                
+        var fs = getCurrentFuncStack();
+        
+        if (!fs)
+            return false;
+        
+        return cr.equals_nocase(name_, fs.name);
+    };
+    
+    Cnds.prototype.CompareParam = function (param_index_, cmp_, value_)
+    {
+        var fs = getCurrentFuncStack();
+        
+        if (!fs)
+            return false;
+        
+        var index_;
+        if (typeof(param_index_) == "string")
+        {
+            // fs.name_map[name] is a parameter number index (indexed by name)
+            index_ = fs.name_map[param_index_];
+            if (index_ == null)                            
+                return false;            
+        }
+        else
+            index_ = cr.floor(param_index_);
+        
+        if (index_ < 0 || index_ >= fs.params.length)
+            return false;
+
+        return cr.do_cmp(fs.params[index_], cmp_, value_);
+    };
+    
+    Cnds.prototype.DefineParam = function (name, default_value)
+    {
+        var fs = getCurrentFuncStack();
+        
+        if (!fs)
+            return false;        
+        
+        
+        // fs.name_map[name] is a passing value
+        var value = (fs.name_map[name] != null)? fs.name_map[name] : default_value;
+        // fs.name_map[name] is a number index now
+        fs.name_map[name] = this.param_index;
+        if (this.param_index >= fs.params.length)
+        {
+            fs.params.length = this.param_index + 1;            
+        }
+        if (fs.params[this.param_index] == null)
+            fs.params[this.param_index] = value;
+        this.param_index += 1;
+        return true;
+    };    
+    
+    pluginProto.cnds = new Cnds();
+
+    //////////////////////////////////////
+    // Actions
+    function Acts() {};
+
+    Acts.prototype.CallFunction = function (name_, params_)
+    {
+        var fs = pushFuncStack();
+        fs.name = name_.toLowerCase();
+        fs.retVal = 0;
+        cr.shallowAssignArray(fs.params, params_);
+                
+        this.name_map_init(fs);        
+        // Note: executing fast trigger path based on fs.name
+        var ran = this.runtime.trigger(cr.plugins_.Rex_Function2.prototype.cnds.OnFunction, this, fs.name);
+        
+        // In preview mode, log to the console if nothing was triggered
+        if (isInPreview && !ran)
+        {
+            log("[Construct 2] Rex_Function2 object: called function '" + name_ + "', but no event was triggered. Is the function call spelt incorrectly or no longer used?", "warn");
+        }
+        
+        popFuncStack();
+    };
+    
+    Acts.prototype.SetReturnValue = function (value_)
+    {
+        var fs = getCurrentFuncStack();
+        
+        if (fs)
+            fs.retVal = value_;
+        else
+            log("[Construct 2] Rex_Function2 object: used 'Set return value' when not in a function call", "warn");
+    };
+    
+    Acts.prototype.SetParameter = function (name_, value_)
+    {
+        this.name_map[name_] = value_;
+    };    
+    
+    pluginProto.acts = new Acts();
+
+    //////////////////////////////////////
+    // Expressions
+    function Exps() {};
+
+    Exps.prototype.ReturnValue = function (ret)
+    {
+        // The previous function has already popped - so check one level up the function stack
+        var fs = getOneAboveFuncStack();
+        
+        if (fs)
+            ret.set_any(fs.retVal);
+        else
+            ret.set_int(0);
+    };
+    
+    Exps.prototype.ParamCount = function (ret)
+    {
+        var fs = getCurrentFuncStack();
+        
+        if (fs)
+            ret.set_int(fs.params.length);
+        else
+        {
+            log("[Construct 2] Rex_Function2 object: used 'ParamCount' expression when not in a function call", "warn");
+            ret.set_int(0);
+        }
+    };
+    
+    Exps.prototype.Param = function (ret, param_index_)
+    {
+        var fs = getCurrentFuncStack();
+        if (!fs)
+        {
+            log("[Construct 2] Rex_Function2 object: used 'Param' expression when not in a function call", "warn");
+            ret.set_int(0);
+            return;
+        }
+        
+        var index_;
+        if (typeof(param_index_) == "string")
+        {
+            index_ = fs.name_map[param_index_];
+            if (index_ == null)
+            {
+                log("[Construct 2] Rex_Function2 object: in function '" + fs.name + "', could not find parameter " + param_index_ , "warn");
+                ret.set_int(0);
+                return;
+            }
+        }
+        else
+            index_ = cr.floor(param_index_);
+            
+        if (index_ >= 0 && index_ < fs.params.length)
+        {
+            ret.set_any(fs.params[index_]);
+        }
+        else
+        {
+            log("[Construct 2] Rex_Function2 object: in function '" + fs.name + "', accessed parameter out of bounds (accessed index " + index_ + ", " + fs.params.length + " params available)", "warn");
+            ret.set_int(0);
+        }
+        
+    };
+    
+    Exps.prototype.Call = function (ret, name_)
+    {
+        var fs = pushFuncStack();
+        fs.name = name_.toLowerCase();
+        fs.retVal = 0;
+        
+        // Copy rest of parameters from arguments
+        fs.params.length = 0;
+        var i, len;
+        for (i = 2, len = arguments.length; i < len; i++)
+            fs.params.push(arguments[i]);
+        
+        this.name_map_init(fs); 
+        // Note: executing fast trigger path based on fs.name
+        var ran = this.runtime.trigger(cr.plugins_.Rex_Function2.prototype.cnds.OnFunction, this, fs.name);
+        
+        // In preview mode, log to the console if nothing was triggered
+        if (isInPreview && !ran)
+        {
+            log("[Construct 2] Rex_Function2 object: expression Rex_Function2.Call('" + name_ + "' ...) was used, but no event was triggered. Is the function call spelt incorrectly or no longer used?", "warn");
+        }
+        
+        popFuncStack();
+
+        ret.set_any(fs.retVal);
+    };
+    
+    pluginProto.exps = new Exps();
+
+}());
