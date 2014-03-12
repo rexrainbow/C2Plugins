58623e29ad50cdd06debad212851a915e51b1436
hua <binhua.tein@gmail.com>
2014-03-12 23:23:11 +0800

    Add tilt to angle detector

---
+++ plugins/rex_tilt2angle/edittime.js
@@ -0,0 +1,99 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Tilt to angle",
+		"id":			"Rex_Tilt2Angle",
+		"version":		"0.1",   		
+		"description":	"Get angle from tilt's beta and gamma input.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_tilt2angle.html",
+		"category":		"Input",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("0");
+AddComboParamOption("current angle");
+AddComboParam("ZERO angle", "ZERO angle of Left-Right direction.");
+AddAction(2, 0, "Calibration", "calibration", "Calibration zero angle to <i>{0}</i>", 
+          "Calibration zero angle.", "Calibration");
+          
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, "Zero of Up-Bottom", "Calibration", "ZEROUD", "Get zero of Up-Bottom, in degrees.");
+AddExpression(2, ef_return_number, "Zero of Left-Right", "Calibration", "ZEROLR", "Get zero of Left-Right, in degrees.");
+AddExpression(3, ef_return_number, "Vector length of X axis", "Length", "LengthX", "Get vector length of X axis.");
+AddExpression(4, ef_return_number, "Vector length of Y axis", "Length", "LengthY", "Get vector length of X axis.");
+AddExpression(5, ef_return_number, "Tilt vector angle", "Angle", "Angle", "Tilt angle, in degrees.");
+AddExpression(6, ef_return_number, "Tilt vector length", "Length", "Length", "Tilt vector length.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Sensitivity"] < 0)
+		this.properties["Sensitivity"] = 1;    
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{	
+}
---
+++ plugins/rex_tilt2angle/runtime.js
@@ -0,0 +1,240 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Tilt2Angle = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Tilt2Angle.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{  
+	};
+    
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.runtime.tickMe(this);
+     
+        this.setup_stage = true;
+        this.touchwrap = null;
+        this.GetBeta = null;
+        this.GetGamma = null;        
+        this.degree_ZEROUD = 0;
+        this.degree_ZEROLR = 0;
+        this.degree_diffUD = 0;
+        this.degree_diffLR = 0;
+        this.degree_tiltangle = 0;
+        this.pre_orientation = 0;
+        this.is_any_pressed = false;
+	};
+
+	instanceProto.TouchWrapGet = function ()
+	{  
+        var plugins = this.runtime.types;
+        var name, obj;
+        for (name in plugins)
+        {
+            obj = plugins[name].instances[0];
+            if ((obj != null) && (obj.check_name == "TOUCHWRAP"))
+            {
+                this.touchwrap = obj;
+                this.GetBeta = cr.plugins_.rex_TouchWrap.prototype.exps.Beta;
+                this.GetGamma = cr.plugins_.rex_TouchWrap.prototype.exps.Gamma;                
+                this.touchwrap.HookMe(this);
+                break;
+            }
+        }
+	}; 
+    
+    instanceProto.tick = function()
+    {
+        this._setup();
+        this._tilt2angle();
+    };
+	
+	var orientation_get = function()
+	{
+	    var ret = window["orientation"];
+	    if (ret == null)
+	        ret = 0;
+	    return ret;
+	};
+    
+	instanceProto._beta_get = function ()
+	{
+        var touch_obj = this.touchwrap;
+        this.GetBeta.call(touch_obj, touch_obj.fake_ret);
+        return touch_obj.fake_ret.value;  
+	};  
+    
+	instanceProto._gamma_get = function ()
+	{
+        var touch_obj = this.touchwrap;
+        this.GetGamma.call(touch_obj, touch_obj.fake_ret);
+        return touch_obj.fake_ret.value;  
+	};      
+    
+	instanceProto._setup = function ()
+	{
+        if (!this.setup_stage)
+            return;
+        
+        this.TouchWrapGet();  
+        this.setup_stage = false;
+        if (this.touchwrap == null)
+            assert("Tilt to Angle: please put touchwrap object into project file.");
+        else        
+            this._degree_ZERO_set();        
+	};
+	
+	instanceProto._degree_ZERO_set = function (ZERO_UD, ZERO_LR)
+	{
+        var is_landspcape = (Math.abs(orientation_get()) == 90);  
+        if (ZERO_UD == null)
+        {
+            ZERO_UD = (is_landspcape)?
+                      this._gamma_get():this._beta_get();  
+        }
+        if (ZERO_LR == null)
+        {
+            ZERO_LR = (is_landspcape)?
+                      this._beta_get():this._gamma_get();  
+        }
+        this.degree_ZEROUD = ZERO_UD;               
+        this.degree_ZEROLR = ZERO_LR;  
+	};	
+
+    instanceProto._diffUD_get = function(orientation)
+    {
+        var diff;
+        switch (orientation)
+        {
+        case 0:    // U:b+ , D:b-
+            diff = -this._beta_get() + this.degree_ZEROUD;  
+            break;
+        case 90:   // U:g- , D:g+
+            diff = this._gamma_get() - this.degree_ZEROUD;
+            break;
+        case 180:  // U:b- , D:b+
+            diff = this._beta_get() - this.degree_ZEROUD;  
+            break;
+        case -90:  // U:g+ , D:g-
+            diff = -this._gamma_get() + this.degree_ZEROUD;
+            break;
+        }
+        return diff;
+    };    
+    instanceProto._diffLR_get = function(orientation)
+    {
+        var diff;
+        switch (orientation)
+        {
+        case 0:    // L:g+ , R:g-
+            diff = -this._gamma_get() + this.degree_ZEROLR;
+            break;
+        case 90:   // L:b+ , R:b-
+            diff = -this._beta_get() + this.degree_ZEROLR;            
+            break;
+        case 180:  // L:g- , R:g+
+            diff = this._gamma_get() - this.degree_ZEROLR;
+            break;
+        case -90:  // L:b- , R:b+
+            diff = this._beta_get() - this.degree_ZEROLR;         
+            break;
+        }
+        return diff;
+    };   
+	instanceProto._tilt2angle = function ()
+	{
+        if (this.touchwrap == null)
+            return;
+          
+        var orientation = orientation_get();
+        if (this.pre_orientation != orientation)
+        {
+            this._degree_ZERO_set();
+            this.pre_orientation = orientation;
+        }
+        this.degree_diffUD = this._diffUD_get(orientation);
+        this.degree_diffLR = this._diffLR_get(orientation);   
+        this.degree_tiltangle = cr.to_degrees(cr.angleTo(0, 0, this.degree_diffLR, this.degree_diffUD));
+	}; 
+//////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();    
+
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+	
+    Acts.prototype.Calibration = function (ZERO_UD, ZERO_LR)
+	{	     
+        if (ZERO_UD==1)      // 1 = get current angle
+            ZERO_UD = null;
+        if (ZERO_LR==1)     // 1 = get current angle
+            ZERO_LR = null;
+        this._setup();        
+        this._degree_ZERO_set(ZERO_UD, ZERO_LR);
+	};
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+    
+	Exps.prototype.ZEROUD = function (ret)
+	{
+		ret.set_float(this.degree_ZEROUD);
+	};
+	Exps.prototype.ZEROLR = function (ret)
+	{
+		ret.set_float(this.degree_ZEROLR);
+	};   
+	Exps.prototype.LengthX = function (ret)
+	{
+		ret.set_float(this.degree_diffUD);
+	};
+	Exps.prototype.LengthY = function (ret)
+	{
+		ret.set_float(this.degree_diffLR);
+	};
+	Exps.prototype.Angle = function (ret)
+	{
+		ret.set_float(this.degree_tiltangle);
+	};    
+	Exps.prototype.Length = function (ret)
+	{
+		ret.set_float(cr.distanceTo(0,0, this.degree_diffLR, this.degree_diffUD));
+	};
+}());
