0dc0d373a41945f93bcf262953d8b34d7b69d74c
hua <binhua.tein@gmail.com>
2014-03-24 23:25:31 +0800

    Multi-player helper

---
+++ plugins/rex_MPchat/edittime.js
@@ -0,0 +1,114 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"MP chat",
+		"id":			"Rex_MPchat",
+		"version":		"0.1",        
+		"description":	"Chat message buffer manager",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_mpchat.html",
+		"category":		"Multi-player helper",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Condition
+AddCondition(1, cf_trigger, "On content updated", "Content", "On content updated", 
+             "Triggered when content updated by message received.", "OnContentUpdate");
+AddCondition(2, cf_trigger, "On decorating message", "Decorate", "On decorating message", 
+             "Callback to decorate chat-message.", "OnDecorateMessage");
+//////////////////////////////////////////////////////////////
+// Actions           
+AddStringParam("Message", "The message data to send.");
+AddVariadicParams("Extra data {n}", 
+                  'Extra data, which can be accessed with MPchat.ExtraData({n}), used under callback "Condition:On decorating message".');
+AddAction(1, af_none, "Push message", "Input", 
+          "Push message <i>{0}</i>, with extra data to (<i>{...}</i>)", 
+          "Push a message into buffer.", "PushMsg");
+AddStringParam("Log", "Log.");
+AddAction(2, af_none, "Push log", "Input", "Push log <i>{0}</i>", "Push a log into buffer.", "PushLog");
+AddStringParam("Message", "Decorated result.");
+AddAction(3, af_none, "Set result", "Decorate", "Set decorated result to <i>{0}</i>", 
+          'Set decorated result, used under callback "Condition:On decorating chat message".', "SetDecoratedResult");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_string, "Content of message", "Content", "Content", 
+              'The content of message buffer.');
+AddExpression(2, ef_return_string, "Raw message", "Decorate", "RawMessage", 
+              'Raw message, used under callback "Condition:On decorating message".');
+AddExpression(3, ef_return_string, "Sender Alias", "Decorate", "SenderAlias", 
+              'The alias of sender, used under callback "Condition:On decorating message".');
+AddExpression(4, ef_return_string, "Last message", "Content", "LastMsg", 
+              'The last updated message of current content.');			  
+AddAnyTypeParam("Index", "The zero-based index of the parameter to get, or name in string.");
+AddExpression(5, ef_return_any, "Decorating parameter", "Decorate", "ExtraData", 
+              'Get the value of a parameter, used under callback "Condition:On decorating message".');
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_text, "Tag prefix", "_chat", "Tag prefix for sending and receiving message.", "", "readonly"),
+    new cr.Property(ept_integer, "Buffer length", 100, "Buffer length of message. 0 is infinite."),
+    new cr.Property(ept_combo, "Reorder", "No", "Reorder chat message to match to host.", "No|Yes"),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_MPwrap/runtime.js
@@ -0,0 +1,232 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_MPwrap = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_MPwrap.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+    var isSupported = false;    
+	instanceProto.onCreate = function()
+	{
+        this.multiplayer = null;
+        this.MP_IsHost = null;
+        //this.MP_HostBroadcastMessage = null;        
+        this.MP_SendPeerMessage = null;
+        
+        this.msg_router = new cr.plugins_.Rex_MPwrap.MsgRouterKlass();
+        this._override();
+	};
+    
+    instanceProto._multiplayer_get = function ()
+    {
+        if (this.multiplayer != null)
+            return this.multiplayer;
+
+        assert2(cr.plugins_.Multiplayer, "MPwrap: Can not find multiplayer oject.");
+        var plugins = this.runtime.types;
+        var name, inst;
+        for (name in plugins)
+        {
+            inst = plugins[name].instances[0];
+            if (inst instanceof cr.plugins_.Multiplayer.prototype.Instance)
+            {
+                this.multiplayer = inst;
+                this.MP_IsHost = cr.plugins_.Multiplayer.prototype.cnds.IsHost;                
+                //this.MP_HostBroadcastMessage = cr.plugins_.Multiplayer.prototype.acts.HostBroadcastMessage;
+                this.MP_SendPeerMessage = cr.plugins_.Multiplayer.prototype.acts.SendPeerMessage;
+                
+                isSupported = inst.mp["isSupported"]();
+                this.msg_router.mp = inst.mp;
+                return this.multiplayer;
+            }
+        }
+        assert2(this.multiplayer, "MPwrap: Can not find multiplayer oject.");
+        return null; 
+    };
+    
+    instanceProto._override = function ()
+    {
+        var mp = this._multiplayer_get().mp;
+        var onpeermessage = mp["onpeermessage"];
+        var msg_router = this.msg_router;
+        mp["onpeermessage"] = function (peer, o)
+        {           
+            var is_break = msg_router.route(peer, o);
+            if (!is_break)
+            {
+                onpeermessage.call(mp, peer, o);
+            }
+        };
+    };
+
+    instanceProto.MsgHandlerAdd = function (tag, thisArg, call_back_fn)
+    {
+        this.msg_router.add_handler(tag, thisArg, call_back_fn);
+    };    
+    instanceProto.MsgHandlerRemove = function (tag)
+    {
+        this.msg_router.remove_handler(tag);
+    };        
+
+    instanceProto.IsHost = function ()
+    {
+        var multiplayer_obj = this._multiplayer_get();
+        return this.MP_IsHost.call(multiplayer_obj);
+    }; 
+
+    instanceProto.GetMyAlias = function ()
+    {
+        var mp = this._multiplayer_get().mp;
+        return mp["getMyAlias"]();
+    };     
+
+	function modeToDCType(mode_)
+	{
+		switch (mode_) {
+		case 0:		// reliable ordered
+			return "o";
+		case 1:		// reliable unordered
+			return "r";
+		case 2:		// unreliable
+			return "u";
+		default:
+			return "o";
+		}
+	};
+    
+    // set no_skip_from_id to yes to broadcast to all peers
+    instanceProto.HostBroadcastMessage = function (from_id_, tag_, message_, mode_, no_skip_from_id)
+    {
+		if (!isSupported)
+			return;
+            
+        var mp = this._multiplayer_get().mp;
+        
+        if (mode_ == null)
+            mode_ = 0;
+		
+        var skip;
+        if (no_skip_from_id != true)
+		    skip = mp["getPeerById"](from_id_);
+        else
+            skip = null;
+		
+		var o = {
+			"c": "m",			// command: message
+			"t": tag_,			// tag
+			"f": (from_id_ || mp["getHostID"]()),
+			"m": message_		// content
+		};
+		
+		mp["hostBroadcast"](modeToDCType(mode_), JSON.stringify(o), skip);
+    };     
+    
+    instanceProto.SendMessage = function (peerid_, tag_, message_, mode_)
+    {
+        if (mode_ == null)
+            mode_ = 0;
+        
+        var multiplayer_obj = this._multiplayer_get();
+        this.MP_SendPeerMessage.call(multiplayer_obj, peerid_, tag_, message_, mode_);
+    };
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();    
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+
+}());
+
+(function ()
+{
+    cr.plugins_.Rex_MPwrap.MsgRouterKlass = function()
+    {
+        this.mp = null;
+        this.tag2handler = {};
+    };
+    
+    var MsgRouterKlassProto = cr.plugins_.Rex_MPwrap.MsgRouterKlass.prototype;
+    
+    MsgRouterKlassProto.add_handler = function (tag, thisArg, call_back_fn)
+    {
+        if (!this.tag2handler.hasOwnProperty(tag))
+            this.tag2handler[tag] = new cr.plugins_.Rex_MPwrap._MsgHandlerKlass(thisArg, call_back_fn);
+    };
+    
+    MsgRouterKlassProto.remove_handler = function (tag)
+    {
+        if (this.tag2handler.hasOwnProperty(tag))
+            delete this.tag2handler[tag];
+    };    
+    
+    MsgRouterKlassProto.route = function (peer, o)
+    {
+        var tag = o["t"];
+        var has_handler = this.tag2handler.hasOwnProperty(tag);
+        if (!has_handler)
+            return false;
+                    
+        // allow "f" field to override sender             
+        var fromId = (o["f"])?  o["f"] : peer["id"]; 
+        var fromAlias = this.mp["getAliasFromId"](fromId);
+        var content = o["m"];            
+        this.tag2handler[tag].DoHandle(fromId, fromAlias, content);
+        return true;
+    };
+    
+    // _MsgHandler
+    cr.plugins_.Rex_MPwrap._MsgHandlerKlass = function(thisArg, call_back_fn)
+    {   
+        this.thisArg = thisArg;
+        this.call_back_fn = call_back_fn;
+    };
+    var _MsgHandlerKlassProto = cr.plugins_.Rex_MPwrap._MsgHandlerKlass.prototype;
+
+    _MsgHandlerKlassProto.DoHandle = function(fromId, fromAlias, content)
+    {   
+        this.call_back_fn.call(this.thisArg, fromId, fromAlias, content);
+    };    
+}());
