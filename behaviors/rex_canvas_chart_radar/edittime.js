8d667bf25da0551e2c9519fd96c9e80b4034022c
hua <binhua.tein@gmail.com>
2014-06-15 17:01:54 +0800

    Add radar chart behavior

---
+++ behaviors/rex_canvas_chart_radar/edittime.js
@@ -0,0 +1,168 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Radar chart",
+		"id":			"Rex_canvas_chart_radar",
+		"description":	"Draw a radar chart on the canvas. The api of chart - http://www.chartjs.org/",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_canvas_chart_radar.html",
+		"category":		"Canvas",
+		"flags":		0,
+		"dependency":	"Chart.js"		
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+//AddCondition(1,	cf_trigger, "On drawing finished", "Drawing", 
+//             "On {my} drawing finished", "Triggered when drawing finished.", 
+//             "OnFinished");           
+             
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(1, 0, "Plot", "Plot", 
+          "{my} Plot", 
+          "Plot chart.", "PlotChart");
+AddStringParam("Label", "Label", '"Col"');
+AddAction(2, 0, "Add label", "Chart - label", 
+          "{my} Add label name to <i>{0}</i>", 
+          "Add label.", "Addlabel");
+AddStringParam("Data set", "Data set", '"Row"');
+AddStringParam("Fill color", "Fill color, hex \"#FFA500\", \"rgb(0-255,0-255,0-255)\", \"rgba(0-255,0-255,0-255,0-1)\", \"hsl(0-360,0-100%,0-100%)\", or \"hsla(0-360,0-100%,0-100%,0-1)\" ", '"rgba(220,220,220,0.5)"');
+AddStringParam("Stroke color", "Stroke color, hex \"#FFA500\", \"rgb(0-255,0-255,0-255)\", \"rgba(0-255,0-255,0-255,0-1)\", \"hsl(0-360,0-100%,0-100%)\", or \"hsla(0-360,0-100%,0-100%,0-1)\" ", '"rgba(220,220,220,1)"');
+AddStringParam("Point color", "Point color, hex \"#FFA500\", \"rgb(0-255,0-255,0-255)\", \"rgba(0-255,0-255,0-255,0-1)\", \"hsl(0-360,0-100%,0-100%)\", or \"hsla(0-360,0-100%,0-100%,0-1)\" ", '"rgba(220,220,220,1)"');
+AddStringParam("Point stroke color", "Point stroke color, hex \"#FFA500\", \"rgb(0-255,0-255,0-255)\", \"rgba(0-255,0-255,0-255,0-1)\", \"hsl(0-360,0-100%,0-100%)\", or \"hsla(0-360,0-100%,0-100%,0-1)\" ", '"#fff"');
+AddAction(3, 0, "Add dataset", "Chart - dataset", 
+          "{my} Add dataset, name to <i>{0}</i>, fill color to <i>{1}</i>, stroke color to <i>{2}</i>, point color to <i>{3}</i>, point stroke color to <i>{4}</i>", 
+          "Add dataset name and colors.", "AddDataSetCfg");  
+AddStringParam("Label", "Label", '"Col"');
+AddStringParam("Data set", "Data set", '"Row"');
+AddNumberParam("Value", "Data value.", 0);
+AddAction(4, 0, "Set data", "Chart - data", 
+          "{my} Set data ( <i>{0}</i> , <i>{1}</i> ) to <i>{2}</i>", 
+          "Set data.", "SetData");
+AddAction(11, 0, "Clean all labels", "Chart - label", 
+          "{my} Clean all labels", 
+          "Clean all labels. It will also clean datasets and data.", "CleanLabels");
+AddAction(12, 0,  "Clean all datasets", "Chart - dataset", 
+          "{my} Clean all datasets", 
+          "Clean all datasets. It will also clean data.", "CleanDatasets");
+AddAction(13, 0,  "Clean all data", "Chart - data", 
+          "{my} Clean all data", 
+          "Clean all data.", "CleanData");		  
+//////////////////////////////////////////////////////////////
+// Expressions
+AddStringParam("Label", "Label", '"Col"');
+AddStringParam("Data set", "Data set", '"Row"');
+AddExpression(1, ef_return_number, "Get data", 
+              "Chart - data", "Data", 
+              "Get data.");                 
+                           
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [  
+    // scale overlay
+    new cr.Property(ept_combo, "Scale overlay", "No", "Show the scale above the chart data.", "No|Yes"),
+	
+	// scale override
+    new cr.Property(ept_combo, "Scale override", "No", "Override with a hard coded scale.", "No|Yes"),	
+    new cr.Property(ept_float, "Scale steps", 1, "The number of steps in a hard coded scale, required if Scale override sets to Yes"),	
+    new cr.Property(ept_float, "Scale step width", 1, "The value jump in the hard coded scale, required if Scale override sets to Yes"),	
+    new cr.Property(ept_float, "Scale start value", 1, "The centre starting value, required if Scale override sets to Yes"),	
+	
+	// scale line
+    new cr.Property(ept_combo, "Scale show line", "Yes", "Show lines for each scale point.", "No|Yes"),	
+    new cr.Property(ept_text, "Scale line color", "rgba(0,0,0,.1)", "Colour of the scale line."),
+    new cr.Property(ept_float, "Scale line width", 1, "Pixel width of the scale line."),
+	
+	// scale labels
+    new cr.Property(ept_combo, "Scale show labels", "No", "Show labels on the scale.", "No|Yes"),	
+	new cr.Property(ept_text, "Scale label", "<%=value%>", "Interpolated JS string - can access value."),
+	new cr.Property(ept_text, "Scale font family", "Arial", "Scale label font declaration for the scale label."),	
+    new cr.Property(ept_float, "Scale font size", 12, "Scale label font size in pixels."),	
+    new cr.Property(ept_combo, "Scale font style", "Normal", "Scale label font weight style.", "Normal|Bold|Bolder"),	
+	new cr.Property(ept_text, "Scale font color", "#666", "Scale label font colour."),
+    new cr.Property(ept_combo, "Scale show label backdrop", "Yes", "Show a backdrop to the scale label.", "No|Yes"),
+	
+	// scale backdrop
+    new cr.Property(ept_text, "Scale backdrop color", "rgba(255,255,255,0.75)", "The colour of the label backdrop."),
+    new cr.Property(ept_float, "Scale backdrop padding X", 2, "The backdrop padding above & below the label in pixels."),		
+    new cr.Property(ept_float, "Scale backdrop padding Y", 2, "The backdrop padding above & below the label in pixels."),	
+	
+	// angle line
+    new cr.Property(ept_combo, "Angle show line-out", "Yes", "Show the angle lines out of the radar.", "No|Yes"),
+    new cr.Property(ept_text, "Angle line color ", "rgba(0,0,0,.1)", "Colour of the angle line."),
+    new cr.Property(ept_float, "Angle line width", 1, "Pixel width of the angle line."),
+	
+	// point label
+	new cr.Property(ept_text, "Point label font family", "Arial", "Point label font declaration."),	
+    new cr.Property(ept_float, "Point label font size", 12, "Point label font size in pixels."),	
+    new cr.Property(ept_combo, "Point label font style", "Normal", "Point label font weight.", "Normal|Bold|Bolder"),	
+	new cr.Property(ept_text, "Point label font color ", "#666", "Point label font colour."),
+	
+	// point dot
+    new cr.Property(ept_combo, "Point dot", "Yes", "Show a dot for each point.", "No|Yes"),	
+    new cr.Property(ept_float, "Point dot radius", 3, "Radius of each point dot in pixels."),
+    new cr.Property(ept_float, "Point dot stroke width", 1, "Pixel width of point dot stroke."),
+    
+	// dataset
+	new cr.Property(ept_combo, "Dataset stroke", "Yes", "Show a stroke for datasets.", "No|Yes"),	
+    new cr.Property(ept_float, "Dataset stroke width", 2, "Pixel width of dataset stroke."),
+	new cr.Property(ept_combo, "Dataset fill", "Yes", "Fill the dataset with a colour.", "No|Yes"),			              
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	// Clamp values
+	if (this.properties["Start angle"] < 0)
+		this.properties["Start value"] = 0;  
+	if (this.properties["Start angle"] > 359)
+		this.properties["Start value"] = 359;  
+	if (this.properties["Delta angle"] < -180)
+		this.properties["Delta angle"] = -180;  
+	if (this.properties["Delta angle"] > 180)
+		this.properties["Delta angle"] = 180; 
+	if (this.properties["Duration"] < 0)
+		this.properties["Duration"] = 0;  			 		    
+}
---
+++ behaviors/rex_canvas_chart_radar/runtime.js
@@ -0,0 +1,217 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_canvas_chart_radar = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_canvas_chart_radar.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{  
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+	    this.options = { // scaleOverlay
+		                 "scaleOverlay": (this.properties[0] === 1),
+		                 // scaleOverride
+						 "scaleOverride": (this.properties[1] === 1),
+						 "scaleSteps": (this.properties[1] === 1)? this.properties[2]:null,
+						 "scaleStepWidth": (this.properties[1] === 1)? this.properties[3]:null,
+						 "scaleStartValue": (this.properties[1] === 1)? this.properties[4]:null,	
+                         // scale line
+		                 "scaleShowLine": (this.properties[5] === 1),
+		                 "scaleLineColor": this.properties[6],
+		                 "scaleLineWidth": this.properties[7],
+						 // scale labels
+		                 "scaleShowLabels": (this.properties[8] === 1),
+						 "scaleLabel" : this.properties[9],
+						 "scaleFontFamily" : this.properties[10],
+						 "scaleFontSize" : this.properties[11],
+						 "scaleFontStyle" : get_font_style(this.properties[12]),
+						 "scaleFontColor" : this.properties[13],
+						 "scaleShowLabelBackdrop" : (this.properties[14] === 1),
+						 // scale backdrop
+						 "scaleBackdropColor": this.properties[15],
+						 "scaleBackdropPaddingX" : this.properties[16],
+						 "scaleBackdropPaddingY" : this.properties[17],
+						 // angle line
+						 "angleShowLineOut" : (this.properties[18] === 1),
+						 "angleLineColor" : this.properties[19],
+						 "angleLineWidth" : this.properties[20],
+						 // point label
+						 "pointLabelFontFamily" : this.properties[21],
+						 "pointLabelFontSize" : this.properties[22],
+						 "pointLabelFontStyle" : get_font_style(this.properties[23]),
+						 "pointLabelFontColor" : this.properties[24],
+						 // point dot
+						 "pointDot" : (this.properties[25] === 1),
+						 "pointDotRadius" : this.properties[26],
+						 "pointDotStrokeWidth" : this.properties[27],
+						 // dataset
+						 "datasetStroke" : (this.properties[28] === 1),
+						 "datasetStrokeWidth" : this.properties[29],
+						 "datasetFill" : (this.properties[30] === 1),						 						
+		                 "animation" : false
+		};
+
+	    this.labels = [];
+	    this.datasets = [];
+	    this.datasets_color = [];
+	    this.data = {};
+	};  
+	
+	var get_font_style = function(style)
+	{
+	    return (style == 0)? "normal":
+		       (style == 1)? "bold":
+			                 "bolder";
+	}
+	
+	behinstProto.tick = function ()
+	{
+	};
+	behinstProto.get_data = function (label_name, dataset_name)
+	{
+	    if (!this.data.hasOwnProperty(label_name))
+	        return 0;
+	    var col = this.data[label_name];
+	    if (!col.hasOwnProperty(dataset_name))
+	        return 0; 
+	    return col[dataset_name];
+	};
+	behinstProto.plot = function(data)
+	{
+	    if (this.inst.extra.chartjs == null)
+		    this.inst.extra.chartjs = new window["Chart"](this.inst.ctx);
+	    this.inst.extra.chartjs["Radar"](data, this.options);
+	    this.inst.runtime.redraw = true;  
+	    this.inst.update_tex = true;  	        
+	};  	
+
+	var hashtable_clean = function(o)
+	{
+	    var k;
+		for (k in o)
+		    delete o[k];
+	}
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	  
+//	Cnds.prototype.OnFinished = function ()
+//	{        
+//		return this.is_my_call;
+//	};
+//	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+	
+	var datasets = [];
+	Acts.prototype.PlotChart = function()
+	{
+	    datasets.length = 0;
+	    var i, cnti=this.datasets.length;
+	    var j, cntj=this.labels.length;
+	    var dataset_name;
+	    for (i=0; i<cnti; i++)
+	    {
+	        dataset_name = this.datasets[i]; 
+	        var data = [];
+	        for (j=0; j<cntj; j++)
+	            data.push(this.get_data(this.labels[j], this.datasets[i]));
+	            
+	        datasets.push({"fillColor": this.datasets_color[i][0],
+	                       "strokeColor" : this.datasets_color[i][1],
+        			       "pointColor" : this.datasets_color[i][2],
+        			       "pointStrokeColor" : this.datasets_color[i][3],
+	                       "data" : data
+	                       });
+	    }
+        var data = {
+        	"labels" : this.labels,
+        	"datasets" : datasets
+        }
+	    this.plot(data);
+	}; 
+	Acts.prototype.Addlabel = function(label_name)
+	{   
+	    this.labels.push(label_name);
+	}; 	
+	Acts.prototype.AddDataSetCfg = function(dataset_name, fill_color, stroke_color, point_color, point_stroke_color)
+	{
+	    this.datasets.push(dataset_name);	    
+	    this.datasets_color.push([fill_color, stroke_color, point_color, point_stroke_color]);
+	}; 	
+	Acts.prototype.SetData = function(label_name, dataset_name, value)
+	{
+	    if (!this.data.hasOwnProperty(label_name))
+	        this.data[label_name] = {};
+	    var col = this.data[label_name];
+	    if (!col.hasOwnProperty(dataset_name))
+	        col[dataset_name] = {};	    
+	    col[dataset_name] = value;
+	};
+	Acts.prototype.CleanLabels = function()
+	{
+	    this.labels.length = 0;
+	    this.datasets.length = 0;
+		this.datasets_color.length = 0;		
+		hashtable_clean(this.data);
+	}; 
+	Acts.prototype.CleanDatasets = function()
+	{
+	    this.datasets.length = 0;
+		this.datasets_color.length = 0;
+		hashtable_clean(this.data);
+	}; 	
+	Acts.prototype.CleanData = function()
+	{
+		hashtable_clean(this.data);
+	}; 		
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+    Exps.prototype.StartAngle = function (ret, label_name, dataset_name)
+	{       
+	    ret.set_float(this.get_data(label_name, dataset_name));
+	};
+	
+}());
