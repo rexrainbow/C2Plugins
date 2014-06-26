85f6a23fc7e8c11cdef8809b5fd5f1cea06b83e8
hua <binhua.tein@gmail.com>
2014-06-26 23:20:28 +0800

    Add behavior

---
+++ behaviors/rex_menufsm/edittime.js
@@ -0,0 +1,94 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Menu FSM",
+		"id":			"Rex_menufsm",
+		"description":	"FSM to contorl the behavior of menu.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_menufsm.html",
+		"category":		"General",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On opening", "Event", "On opening", 
+             "Triggered when menu opening", "OnOpening");
+AddCondition(2, cf_trigger, "On opened", "Event", "On opened", 
+             "Triggered when menu opened", "OnOpened");
+AddCondition(3, cf_trigger, "On closing", "Event", "On closing", 
+             "Triggered when menu closing", "OnClosing");
+AddCondition(4, cf_trigger, "On closed", "Event", "On closed", 
+             "Triggered when menu closed", "OnClosed");             
+AddCondition(5, 0, "Is opened", "If", "Is opened", 
+             "Return true if menu is opened", "IsOpened");
+AddCondition(6, 0, "Is closed", "If", "Is closed", 
+             "Return true if menu is opened", "IsClosed");
+             
+//////////////////////////////////////////////////////////////
+// Actions 
+AddAction(1, 0, "Open", "Open/Close", 
+          "Open menu", "Open menu.", "OpenMenu");
+AddAction(2, 0, "Close", "Open/Close", 
+          "Close menu", "Close menu.", "CloseMenu");   
+AddAction(3, 0, "Wait transition event", "Transition", 
+          "Wait transition event", "Wait transition event.", "WaitEvnet");  
+AddAction(4, 0, "Finish transition event", "Transition", 
+          "Finish transition event", "Finish transition event.", "FinishEvnet");
+AddAction(5, 0, "Force finish transition event", "Transition", 
+          "Force finish transition event", "Force finish transition event.", "ForceFinishEvnet");          
+//////////////////////////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [    
+    new cr.Property(ept_combo, "Initial state", "Off", "Set initial state.", "Off|Opened|Closed"),    
+    new cr.Property(ept_combo, "Has transition state", "Yes", "Set yes to have OPENING and CLOSING state.", "No|Yes"),       
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{	
+}
---
+++ behaviors/rex_menufsm/runtime.js
@@ -0,0 +1,214 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_menufsm = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_menufsm.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;       
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+    var STATE_OFF = 0;
+    var STATE_OPENING = 1;
+    var STATE_OPENED = 2;
+    var STATE_CLOSING = 3;
+    var STATE_CLOSED = 4;
+    var CMD_OPEN = 1;
+    var CMD_CLOSE = 2;
+    var CMD_FINISHEDEVENT = 3;
+    var CMD_FORCEFINISHEDEVENT = 4;
+    var STATENAMEMAP = ["Off", "Opening", "Opened", "Closing", "Closed"];
+	behinstProto.onCreate = function()
+	{       
+        this.state = (this.properties[0] == 1)? STATE_OPENED:
+                     (this.properties[0] == 2)? STATE_CLOSED:
+                                                STATE_OFF;
+        this.has_transition_state = (this.properties[1] == 1);                                               
+        this.waiting_event_count = 0;        
+	};
+
+	behinstProto.tick = function ()
+	{
+	};  
+    
+	behinstProto._on_logic = function (cmd)
+	{
+        var pre_state = this.state;
+        switch (this.state)
+        {
+        case STATE_OFF:
+            if (cmd == CMD_OPEN)             
+                this.state = (this.has_transition_state)? STATE_OPENING:STATE_OPENED;
+            else if (cmd == CMD_CLOSE)
+                this.state = (this.has_transition_state)? STATE_CLOSING:STATE_CLOSED;
+        break;
+        case STATE_OPENING:
+            if (cmd == CMD_FINISHEDEVENT)
+            {
+                if (this.waiting_event_count > 0)
+                    this.waiting_event_count -= 1;
+                if (this.waiting_event_count == 0)
+                    this.state = STATE_OPENED;
+            }
+            else if (cmd == CMD_FORCEFINISHEDEVENT)
+            {
+                this.waiting_event_count = 0;
+                this.state = STATE_OPENED;
+            }            
+        break;
+        case STATE_OPENED:
+            if (cmd == CMD_CLOSE)
+                this.state = (this.has_transition_state)? STATE_CLOSING:STATE_CLOSED;
+        break;
+        case STATE_CLOSING:
+            if (cmd == CMD_FINISHEDEVENT)
+            {
+                if (this.waiting_event_count > 0)
+                    this.waiting_event_count -= 1;
+                if (this.waiting_event_count == 0)
+                    this.state = STATE_CLOSED;
+            }
+            else if (cmd == CMD_FORCEFINISHEDEVENT)
+            {
+                this.waiting_event_count = 0;
+                this.state = STATE_CLOSED;
+            }                   
+        break;
+        case STATE_CLOSED:
+            if (cmd == CMD_OPEN)
+                this.state = (this.has_transition_state)? STATE_OPENING:STATE_OPENED;
+        break;   
+        }
+        
+        if (pre_state != this.state)
+        {
+            var handler = (this.state == STATE_OPENING)? cr.behaviors.Rex_menufsm.prototype.cnds.OnOpening:
+                          (this.state == STATE_OPENED)? cr.behaviors.Rex_menufsm.prototype.cnds.OnOpened:
+                          (this.state == STATE_CLOSING)? cr.behaviors.Rex_menufsm.prototype.cnds.OnClosing:
+                          (this.state == STATE_CLOSED)? cr.behaviors.Rex_menufsm.prototype.cnds.OnClosed:
+                                                        null;
+            if (handler != null)
+                this.runtime.trigger(handler, this.inst);      
+        }
+	};    
+
+	behinstProto._inc_waiting_event_count = function ()
+	{
+		if ((this.state == STATE_OPENING) || (this.state == STATE_CLOSING))
+        {
+            this.waiting_event_count += 1;
+        }
+	};
+	behinstProto.saveToJSON = function ()
+	{
+		return { "s": this.state,
+                 "wec": this.waiting_event_count,
+               };
+	};
+	
+	behinstProto.loadFromJSON = function (o)
+	{
+		this.state = o["s"];
+        this.waiting_event_count = o["wec"];
+	};	
+    
+	/**BEGIN-PREVIEWONLY**/    
+	behinstProto.getDebuggerValues = function (propsections)
+	{
+		propsections.push({
+			"title": this.type.name,
+			"properties": [{"name": "State", "value":STATENAMEMAP[this.state]}
+                           ]
+		});
+	};
+	
+	behinstProto.onDebugValueEdited = function (header, name, value)
+	{
+	};
+	/**END-PREVIEWONLY**/	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	Cnds.prototype.OnOpening = function () { return true; };
+	Cnds.prototype.OnOpened = function () { return true; };   
+	Cnds.prototype.OnClosing = function () { return true; };
+	Cnds.prototype.OnClosed = function () { return true; }; 
+    
+	Cnds.prototype.IsOpened = function ()
+	{
+		return (this.state == STATE_OPENED);
+	};
+	Cnds.prototype.IsClosed = function ()
+	{
+		return (this.state == STATE_CLOSED);
+	};
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+    Acts.prototype.OpenMenu = function ()
+	{  
+        this._on_logic(CMD_OPEN);
+	}; 
+
+    Acts.prototype.CloseMenu = function ()
+	{
+        this._on_logic(CMD_CLOSE);
+	};     
+
+    Acts.prototype.WaitEvnet = function ()
+	{
+        this._inc_waiting_event_count();
+	};    
+
+    Acts.prototype.FinishEvnet = function ()
+	{
+        this._on_logic(CMD_FINISHEDEVENT);
+	};
+
+    Acts.prototype.ForceFinishEvnet = function ()
+	{
+        this._on_logic(CMD_FORCEFINISHEDEVENT);
+	};	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+}());
