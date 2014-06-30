ad8eeb0dc24967ab8ff7a839b6d8adc489c66b8b
hua <binhua.tein@gmail.com>
2014-07-01 00:04:54 +0800

    Add uid to property plugin

---
+++ plugins/rex_uid2prop/edittime.js
@@ -0,0 +1,115 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"UID to Properties",
+		"id":			"Rex_UID2Prop",
+		"version":		"0.1",        
+		"description":	"Get properties (x,y,angle,opacity,private variable) by UID.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_uid2prop.html",
+		"category":		"General",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		pf_singleglobal
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("Alias", "An alias to identify this private variable.");
+AddObjectParam("Object", "Object type.");
+AddObjectInstanceVarParam("Instance variable", "Choose the instance variable in the above object.");
+AddAction(1, af_none, "Define alias", "Private variable", 
+          "Define alias <b>{0}</b> refer to {1} variable <b>{2}</b>", 
+          "Define an alias of private variable.", "DefinePrivateVariableAlias");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddNumberParam("UID", "The UID of instance.", 0);
+AddExpression(1, ef_return_number, "Get position X", "Position", "X", "Get instance's position X.");
+AddNumberParam("UID", "The UID of instance.", 0);
+AddExpression(2, ef_return_number, "Get position Y", "Position", "Y", "Get instance's position Y.");
+AddNumberParam("UID", "The UID of instance.", 0);
+AddExpression(3, ef_return_number, "Get angle", "Angle", "Angle", "Get instance's angle.");
+AddNumberParam("UID", "The UID of instance.", 0);
+AddExpression(4, ef_return_number, "Get width", "Size", "Width", "Get instance's width.");
+AddNumberParam("UID", "The UID of instance.", 0);
+AddExpression(5, ef_return_number, "Get height", "Size", "Height", "Get instance's height.");
+AddNumberParam("UID", "The UID of instance.", 0);
+AddExpression(6, ef_return_number, "Get opacity", "Opacity", "Opacity", "Get instance's opacity.");
+AddNumberParam("UID", "The UID of instance.", 0);
+AddExpression(7, ef_return_number, "Get visible", "Visible", "Visible", "Get instance's visible.");
+AddNumberParam("UID", "The UID of instance.", 0);
+AddAnyTypeParam("ImagePoint", "Name or number of image point to get.", 0);
+AddExpression(8, ef_return_number, "Get image point X", "Image point", "ImgptX", "Get instance's image point X.");
+AddNumberParam("UID", "The UID of instance.", 0);
+AddAnyTypeParam("ImagePoint", "Name or number of image point to get.", 0);
+AddExpression(9, ef_return_number, "Get image point Y", "Image point", "ImgptY", "Get instance's image point Y.");
+AddNumberParam("UID", "The UID of instance.", 0);
+AddStringParam("Alias", "An alias to identify this private variable.");
+AddExpression(10, ef_return_any, "Get value of private variable", "Private variable", "PV", "Get value of private variable by alias.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_uid2prop/runtime.js
@@ -0,0 +1,242 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_UID2Prop = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_UID2Prop.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.alias2pv = {}; // {sid:pv_index}
+	};
+    
+	instanceProto.onDestroy = function ()
+	{
+	};
+    
+	instanceProto.add_alias = function (alias_, type_, var_)
+	{
+        var map = this.alias2pv[alias_];
+        if (map == null)
+        {
+            map = {};
+            this.alias2pv[alias_] = map;
+
+        }
+        else
+        {
+            var k;
+            for (k in map)
+                delete map[k];
+        }
+        
+		if (type_.is_family)
+		{
+            var i, cnt=type_.members.length, t;
+			for (i=0; i<cnt; i++)
+			{
+				t = type_.members[i];
+                
+                map[t.sid] = var_ + t.family_var_map[type_.family_index];
+			}
+		}
+		else
+		{
+            map[type_.sid] = var_;
+		}
+	};
+    
+	instanceProto.get_pv = function (inst, alias_)
+	{
+        if (inst == null)
+            return 0;
+        var map = this.alias2pv[alias_];
+        if (map == null)
+            return 0;
+            
+        var pv_index = map[inst.type.sid];
+        if (pv_index == null)
+            return 0;
+            
+        return inst.instance_vars[pv_index];
+	};
+    
+	instanceProto.saveToJSON = function ()
+	{    
+		return { "alias": this.alias2pv,
+		         };
+	};
+	
+	instanceProto.loadFromJSON = function (o)
+	{   
+	    this.alias2pv = o["alias"];
+	};
+    
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();    
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+    
+	Acts.prototype.DefinePrivateVariableAlias = function (alias_, type_, var_)
+	{
+        this.add_alias(alias_, type_, var_);
+	};
+    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+
+    Exps.prototype.X = function (ret, uid)
+	{
+        var inst = this.runtime.getObjectByUID(uid);
+        if ((inst == null) || (inst.x == null))
+        {
+	        ret.set_float(0);
+            return;
+        }
+        
+        ret.set_float(inst.x);
+	};
+
+    Exps.prototype.Y = function (ret, uid)
+	{
+        var inst = this.runtime.getObjectByUID(uid);
+        if ((inst == null) || (inst.y == null))
+        {
+	        ret.set_float(0);
+            return;
+        }
+        
+        ret.set_float(inst.y);
+	};
+
+    Exps.prototype.Angle = function (ret, uid)
+	{
+        var inst = this.runtime.getObjectByUID(uid);
+        if ((inst == null) || (inst.angle == null))
+        {
+	        ret.set_float(0);
+            return;
+        }
+        
+        ret.set_float(cr.to_clamped_degrees(inst.angle));
+	};    
+
+    Exps.prototype.Width = function (ret, uid)
+	{
+        var inst = this.runtime.getObjectByUID(uid);
+        if ((inst == null) || (inst.width == null))
+        {
+	        ret.set_float(0);
+            return;
+        }
+        
+        ret.set_float(inst.width);
+	};    
+
+    Exps.prototype.Height = function (ret, uid)
+	{
+        var inst = this.runtime.getObjectByUID(uid);
+        if ((inst == null) || (inst.height == null))
+        {
+	        ret.set_float(0);
+            return;
+        }
+        
+        ret.set_float(inst.height);
+	};       
+
+    Exps.prototype.Opacity = function (ret, uid)
+	{
+        var inst = this.runtime.getObjectByUID(uid);
+        if ((inst == null) || (inst.opacity == null))
+        {
+	        ret.set_float(0);
+            return;
+        }
+        
+        ret.set_float(inst.opacity);
+	};     
+
+    Exps.prototype.Visible = function (ret, uid)
+	{
+        var inst = this.runtime.getObjectByUID(uid);
+        if ((inst == null) || (inst.visible == null))
+        {
+	        ret.set_float(0);
+            return;
+        }
+        
+        ret.set_float(inst.visible);
+	};      
+    
+    Exps.prototype.ImgptX = function (ret, uid, imgpt)
+	{
+        var inst = this.runtime.getObjectByUID(uid);
+        if ((inst == null) || (inst.getImagePoint == null))
+        {
+	        ret.set_float(0);
+            return;
+        }
+        
+        ret.set_float(inst.getImagePoint(imgpt, true));
+	};
+
+    Exps.prototype.ImgptY = function (ret, uid, imgpt)
+	{
+        var inst = this.runtime.getObjectByUID(uid);
+        if ((inst == null) || (inst.getImagePoint == null))
+        {
+	        ret.set_float(0);
+            return;
+        }
+        
+        ret.set_float(inst.getImagePoint(imgpt, false));
+	};
+
+    Exps.prototype.PV = function (ret, uid, alias)
+	{
+        var inst = this.runtime.getObjectByUID(uid);    
+        ret.set_any(this.get_pv(inst, alias));
+	};    
+}());
