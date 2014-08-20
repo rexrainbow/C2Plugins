ef220fbcda9f6eb942a088504212ea700b78733d
hua <binhua.tein@gmail.com>
2014-08-21 00:56:17 +0800

    Add LJp plugin

---
+++ behaviors/rex_LJpotential/edittime.js
@@ -0,0 +1,146 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"LJ potential",
+		"id":			"Rex_LJ_potential",
+		"description":	"Lennard-Jones potential formula - ( (A/r^n)-(B/r^m) ) for attracting or rejecting objects.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropboxusercontent.com/u/5779181/C2Repo/rex_lj_potential.html",
+		"category":		"AI",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, 0, "Has been attracted", "Target", "{my} has been attracted", 
+             "Target has been attracted by source.", "HasBeenAttracted");
+AddCondition(2, cf_trigger, "Begin attracted", "Target", 
+             "{my} Begin attracted", 
+			 "Triggered when attracted beginning, to get attracted source uid.", 
+			 "BeginAttracted");  
+AddCondition(3, cf_trigger, "Begin attracting", "Source", 
+             "{my} Begin attracting", 
+			 "Triggered when attracting beginning, to get attracted target uid.", 
+			 "BeginAttracting");  	
+AddCondition(4, cf_trigger, "End attracted", "Target", 
+             "{my} End attracted", 
+			 "Triggered when attracted ending, to get attracted source uid.", 
+			 "EndAttracted");  
+AddCondition(5, cf_trigger, "End attracting", "Source", 
+             "{my} End attracting", 
+			 "Triggered when attracting end, to get attracted target uid.", 
+			 "EndAttracting"); 
+AddCondition(6, 0, "Has attracting", "Source", "{my} has attracting", 
+             "Source has attracting target.", "HasAttracting");	
+	 		 
+//////////////////////////////////////////////////////////////
+// Actions         
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Source", "Enable to be source.",1);
+AddAction(1, 0, "Set source enable", "Activated", "Set {my} source enable to <i>{0}</i>", 
+          "Enable to be a gravitation source.", "SetSourceActivated");
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Target", "Enable to be target.",1);
+AddAction(2, 0, "Set target enable", "Activated", "Set {my} target enable to <i>{0}</i>", 
+          "Enable to be a gravitation target.", "SetTargetActivated");
+AddNumberParam("Range", "Sensitivity range.");
+AddAction(4, 0, "Set sensitivity range", "Sensitivity", "Set {my} sensitivity range to <i>{0}</i>", 
+          "Set sensitivity range for source.", "SetRange");
+AddStringParam("Source tag", "Source tag.", '""');
+AddAction(5, 0, "Set source tag", "Tag", "Set {my} source tag to <i>{0}</i>",
+         "Set source tag.", "SetSourceTag");
+AddStringParam("Target tag", "Target tag.", '""');
+AddAction(6, 0, "Set target tag", "Tag", "Set {my} target tag to <i>{0}</i>",
+         "Set target tag.", "SetTargetTag");    
+AddComboParamOption("A");
+AddComboParamOption("n");
+AddComboParamOption("B");
+AddComboParamOption("m");
+AddComboParam("Parameter", "Parameter of LJ potential.", 0);
+AddAction(7, 0, "Set parameter", "LJ potential", "Set {my} LJ potential parameter <i>{0}</i> to <i>{1}</i>", 
+          "Set parameters of LJ potential.", "SetLJParam");          
+AddAction(11, 0, "Update", "Update", "Update {my} output force", 
+          "Update output force.", "UpdateForce");          
+AddAction(12, 0, "Clean", "Update wo tag", "Clean {my} output force to 0", 
+          "Clean output force to 0.", "CleanForce");
+AddObjectParam("Source", "Source for attracting or rejecting.");          
+AddAction(13, 0, "Attracted by source", "Update wo tag", "Attracted {my} by source <i>{0}</i>", 
+          "Attracted by source.", "AttractedBySource"); 
+          
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, "Get source enable", "Enable", "IsSource", "1 is source.");
+AddExpression(2, ef_return_number, "Get target enable", "Enable", "IsTarget", "1 is target.");
+AddExpression(4, ef_return_number, "Get sensitivity range", "Sensitivity", "Range", "Get sensitivity range.");
+AddExpression(5, ef_return_number, "Get attracting source uid", "Source", "SourceUID", "Get attracting source uid.");
+AddExpression(6, ef_return_number, "Get attracted target uid", "Target", "TargetUID", "Get attracted target uid.");
+AddExpression(7, ef_return_string, "Get source tag", "Tag", "SourceTag", "Get source tag.");
+AddExpression(8, ef_return_string, "Get target tag", "Tag", "TargetTag", "Get target tag.");
+AddExpression(11, ef_return_number, "Get angle of force", "Output", "ForceAngle", "Get angle of total attracting force.");
+AddExpression(12, ef_return_number, "Get magnitude of force", "Output", "ForceMagnitude", "Get magnitude of total attracting force.");
+AddExpression(13, ef_return_number, "Get dx of force", "Output", "ForceDx", "Get dx of total attracting force.");
+AddExpression(14, ef_return_number, "Get dy of force", "Output", "ForceDy", "Get dy of total attracting force.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_combo, "Source", "Yes", "Source for attracting or rejecting.", "No|Yes"),     
+    new cr.Property(ept_text, "Source tag", "", "Target will attract or reject to sources which have the same tag."),   
+    new cr.Property(ept_float, "A", 1, "Parameter A, for attracting magnitude. (A/r^n)-(B/r^m)"), 
+    new cr.Property(ept_float, "n", 1, "Parameter n, for attracting declined. (A/r^n)-(B/r^m)"),   
+    new cr.Property(ept_float, "B", 1, "Parameter B, for rejecting magnitude. (A/r^n)-(B/r^m)"), 
+    new cr.Property(ept_float, "m", 1, "Parameter m, for rejecting declined. (A/r^n)-(B/r^m)"),  
+    new cr.Property(ept_float, "Sensitivity range", 0, "Affecting when distance is less then this range. 0 is infinity. Source only."),    
+    new cr.Property(ept_combo, "Target", "No", "Attract or reject to source.", "No|Yes"),    
+    new cr.Property(ept_text, "Target tag", "", "Target will attract or reject to sources which have the same tag."),    
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+	if (this.properties["Gravitation range"] < 0)
+		this.properties["Gravitation range"] = Math.abs(this.properties["Gravitation range"]);		
+}
---
+++ behaviors/rex_LJpotential/runtime.js
@@ -0,0 +1,537 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_LJ_potential = function(runtime)
+{
+	this.runtime = runtime;
+    this.sources = {};
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_LJ_potential.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{     
+	};
+    
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;  
+        this.sources = this.behavior.sources;         
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.source_tag = this.properties[1]; 
+        this.pre_source_tag = null;
+        this.target_tag = this.properties[8];        
+        this._set_source((this.properties[0] == 1));
+        if (!this.recycled)
+        {
+            this.LJ_potential_param = {};
+        }
+        this.LJ_potential_param["A"] = this.properties[2];
+        this.LJ_potential_param["n"] = this.properties[3];
+        this.LJ_potential_param["B"] = this.properties[4];
+        this.LJ_potential_param["m"] = this.properties[5];        
+        
+        this._set_target((this.properties[7] == 1));
+        this._set_range(this.properties[6]);
+
+        this.has_been_attracted = false;
+        this.has_attracting = false;
+        this.attracting_source_uid = (-1);
+        this.attracted_target_uid = (-1)
+        
+        if (!this.recycled)
+        {
+            this.output_force = {};
+        }
+        this.output_force["x"] = 0;
+        this.output_force["y"] = 0;       
+        
+        if (!this.recycled)
+        {        	           
+            this.pre_sources = {};
+            this.pre_targets = {};        
+            this.current_sources = {};
+            this.current_targets = {};
+        }
+	};
+	
+	behinstProto.onDestroy = function()
+	{
+	    this._source_remove();
+	    var uid, has_inst = false;
+	    for (uid in this.sources[this.source_tag])
+	    {
+	        has_inst = true;
+	        break;
+	    }
+	    if (!has_inst)
+	        delete this.sources[this.source_tag];
+			
+	    clean_table(this.pre_sources);
+	    clean_table(this.pre_targets);	
+	    clean_table(this.current_sources);
+	    clean_table(this.current_targets);			
+	}; 	
+	
+	behinstProto._source_append = function()
+	{
+        if (this.pre_source_tag == this.source_tag)
+            return;
+            
+        var uid = this.inst.uid;
+        if ((this.pre_source_tag != null) && (this.pre_source_tag in this.sources))
+        {
+            var sources = this.sources[this.pre_source_tag];
+            if (uid in sources)
+                delete sources[uid];
+        }
+        if (!(this.source_tag in this.sources))
+            this.sources[this.source_tag] = {};
+        this.sources[this.source_tag][uid] = this;
+        this.pre_source_tag = this.source_tag;
+	};
+    
+	behinstProto._source_remove = function()
+	{
+        var uid = this.inst.uid;
+        if (this.source_tag in this.sources)
+        {
+            var sources = this.sources[this.source_tag];
+            if (uid in sources)
+                delete sources[uid];
+        }
+        this.pre_source_tag = null;
+	};    
+    
+	behinstProto._set_source = function(is_source)
+	{        
+        this.is_source = is_source;
+        if (is_source)        
+            this._source_append();     
+        else
+            this._source_remove();
+                                
+	};
+	behinstProto._set_target = function(is_target)
+	{        
+        this.is_target = is_target;
+	};
+	behinstProto._set_range = function(range)
+	{        
+        this.sensitivity_range = range;        
+        this.sensitivity_range_pow2 = range*range;                         
+	};
+		
+	behinstProto.tick = function ()
+	{               
+	};
+    
+	behinstProto.attracted_by_sources = function ()
+	{       
+        if (!this.is_target)
+            return;
+         
+        this.output_force["x"] = 0;
+        this.output_force["y"] = 0;
+        
+        this.has_been_attracted = false;
+        if (!(this.target_tag in this.sources))
+            return;
+            
+        var sources = this.sources[this.target_tag];
+		var my_uid = this.inst.uid;
+        var uid, source_inst, inst;
+        this.has_been_attracted = false;        
+        this.has_attracting = false; 
+        for (uid in sources)
+        {
+            source_inst = sources[uid];
+			inst = source_inst.inst;
+			
+            //We do not want an object to be exerting a gravitational force on itself
+            if (my_uid === inst.uid)
+            {
+                source_inst.has_attracting = false;
+                continue;	
+            }
+
+            if (!this._in_range(inst,  source_inst.sensitivity_range_pow2))
+            {
+                source_inst.has_attracting = false;
+                continue;
+            }
+            
+            this.has_been_attracted = true;     
+            source_inst.has_attracting = true;                   
+            this._accumulate_force(source_inst);
+            this._attracting_target(source_inst, my_uid);
+            this._attracted_by_source(this, uid);
+        }       
+        
+	    this.attracted_end();      
+	};
+    
+	behinstProto.attracted_end = function ()
+	{
+	    this._attracting_target_end();
+	    this._attracted_by_source_end();
+	    	        
+	    copy_table(this.pre_sources, this.current_sources);
+	    clean_table(this.current_sources);
+	    copy_table(this.pre_targets, this.current_targets);
+	    clean_table(this.current_targets);      
+	};    
+
+	behinstProto._attracting_target = function (source_inst, target_uid)
+	{
+	    source_inst.attracted_target_uid = parseInt(target_uid);
+	    var pre_targets = source_inst.pre_targets;
+	    if (!(target_uid in source_inst.pre_targets))
+	        this.runtime.trigger(cr.behaviors.Rex_LJ_potential.prototype.cnds.BeginAttracting, source_inst.inst);  
+	    source_inst.current_targets[target_uid] = true;
+	}; 
+	
+	behinstProto._attracted_by_source = function (target_inst, source_uid)
+	{
+	    this.attracting_source_uid = parseInt(source_uid);
+	    var pre_sources = target_inst.pre_sources;
+	    if (!(source_uid in target_inst.pre_sources))
+    	    this.runtime.trigger(cr.behaviors.Rex_LJ_potential.prototype.cnds.BeginAttracted, target_inst.inst);  
+	    target_inst.current_sources[source_uid] = true; 
+	};	
+	
+	behinstProto._attracting_target_end = function ()
+	{
+	    var uid;
+	    for (uid in this.pre_targets)
+	    {
+	        if (uid in this.current_targets)
+	            continue;
+            this.attracted_target_uid = parseInt(uid);	        
+	        this.runtime.trigger(cr.behaviors.Rex_LJ_potential.prototype.cnds.EndAttracting, this.inst);  
+	    }   	          
+	}; 
+	
+	behinstProto._attracted_by_source_end = function ()
+	{
+	    var uid, source_inst;
+	    for (uid in this.pre_sources)
+	    {
+	        if (uid in this.current_sources)
+	            continue;     
+	        this.attracting_source_uid = parseInt(uid);
+	        this.runtime.trigger(cr.behaviors.Rex_LJ_potential.prototype.cnds.EndAttracted, this.inst);  
+	    }  
+	};		
+	
+	behinstProto._in_range = function (inst1, sensitivity_range_pow2)
+	{
+	   if (sensitivity_range_pow2 == 0)
+	       return true;
+	       
+       var inst0 = this.inst;
+       var dx = inst1.x - inst0.x;
+       var dy = inst1.y - inst0.y;
+       var distance_pow2 = (dx*dx)+(dy*dy);
+       return (distance_pow2 <= sensitivity_range_pow2);
+	}; 
+    
+    // LJ potential
+	behinstProto._accumulate_force = function (source_behavior)
+	{       
+        var dx = source_behavior.inst.x - this.inst.x;
+        var dy = source_behavior.inst.y - this.inst.y;
+        // get force
+        var r = Math.sqrt( (dx*dx) + (dy*dy) );
+        var params = source_behavior.LJ_potential_param;        
+        var fA = _term_value_get(params["A"], params["n"], r);
+        var fB = _term_value_get(params["B"], params["m"], r);
+        var U = fA - fB;
+        // accumulate force
+        var a = Math.atan2(dy, dx);        
+        this.output_force["x"] += U * Math.cos(a);
+        this.output_force["y"] += U * Math.sin(a);
+	};
+    
+    var _term_value_get = function (A, n, r)
+    {
+        var val;
+        if (A == 0)
+            val = 0;
+        else
+        {
+            switch (n)
+            {
+            case 0: val = A;                break;
+            case 1: val = A/r;              break;
+            case 2: val = A/(r*r);          break;
+            case 3: val = A/(r*r*r);        break;
+            default: val = A/Math.pow(r,n); break;
+            }
+        }
+        return val;
+    };
+
+    var clean_table = function (table)
+    {
+        var key;
+        for (key in table)
+            delete table[key];
+    };
+    var copy_table = function (target, source)
+    {
+        clean_table(target);
+        var key;
+        for (key in source)
+            target[key] = source[key];
+    };
+    
+	behinstProto.saveToJSON = function ()
+	{
+		return { "st": this.source_tag, 
+                 "pst": this.pre_source_tag,
+                 "tt": this.target_tag,
+                 "is": this.is_source,
+                 "it": this.is_target, 
+                 "LJparams": this.LJ_potential_param,
+                 "sr": this.sensitivity_range,
+                 "of": this.output_force,
+                };
+	};
+	
+	behinstProto.loadFromJSON = function (o)
+	{            
+        this.source_tag = o["st"]; 
+        this.pre_source_tag = o["pst"];
+        this.target_tag = o["tt"];
+        this.is_source = o["is"];
+        this._set_source(this.is_source);
+        this.is_target = o["it"];
+        this._set_target(this.is_target);
+        this.LJ_potential_param = o["LJparams"];
+        this.sensitivity_range = o["sr"];
+        this._set_range(this.sensitivity_range);
+        this.output_force = o["of"];
+	};        
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	Cnds.prototype.HasBeenAttracted = function ()
+	{
+		return this.has_been_attracted;
+	};
+
+	Cnds.prototype.BeginAttracted = function ()
+	{
+		return true;
+	};	
+
+	Cnds.prototype.BeginAttracting = function ()
+	{
+		return true;
+	};	
+	
+	Cnds.prototype.EndAttracted = function ()
+	{
+		return true;
+	};	
+
+	Cnds.prototype.EndAttracting = function ()
+	{
+		return true;
+	};		
+
+	Cnds.prototype.HasAttracting = function ()
+	{
+		return this.has_attracting;
+	};	
+    
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetSourceActivated = function (s)
+	{
+		this._set_source((s==1));
+	}; 
+
+	Acts.prototype.SetTargetActivated = function (s)
+	{
+		this._set_target((s==1));
+	}; 	
+
+	Acts.prototype.SetRange = function (range)
+	{
+		this._set_range(range);
+	}; 
+
+	Acts.prototype.SetSourceTag = function (tag)
+	{
+		this.source_tag = tag; 
+        this._source_append();
+	}; 		
+
+	Acts.prototype.SetLJParam = function (i, value)
+	{
+        var params = this.LJ_potential_param;        
+        switch (i)
+        {
+        case 0: params["A"] = value; break;
+        case 1: params["n"] = value; break;
+        case 2: params["B"] = value; break;
+        case 3: params["m"] = value; break;        
+        }
+	};
+	Acts.prototype.SetTargetTag = function (tag)
+	{
+		this.target_tag = tag; 
+	}; 	
+
+	Acts.prototype.UpdateForce = function ()
+	{
+        this.attracted_by_sources();
+	}; 	    
+    
+	Acts.prototype.CleanForce = function ()
+	{
+        this.output_force["x"] = 0;
+        this.output_force["y"] = 0;  
+	}; 	    
+
+	function GetThisBehavior(inst)
+	{
+		var i, len;
+		for (i = 0, len = inst.behavior_insts.length; i < len; i++)
+		{
+			if (inst.behavior_insts[i] instanceof behaviorProto.Instance)
+				return inst.behavior_insts[i];
+		}
+		
+		return null;
+	};    
+    
+	Acts.prototype.AttractedBySource = function (objtype)
+	{
+        if (!objtype)
+            return;
+             
+	    var insts = objtype.getCurrentSol().getObjects();
+	    var i, cnt=insts.length, inst, behavior_inst;
+        var my_uid=this.inst.uid;
+        this.has_been_attracted = false; 
+        for (i=0; i<cnt; i++)
+        {
+            inst = insts[i];
+                
+            behavior_inst = GetThisBehavior(insts[i]);
+            if (behavior_inst == null)
+                continue;
+                
+            if (my_uid === inst.uid)
+            {
+                behavior_inst.has_attracting = false;
+                continue;
+            }                
+                
+            if (!this._in_range(inst,  behavior_inst.sensitivity_range_pow2))
+            {
+                behavior_inst.has_attracting = false;   
+                continue;                
+            }
+            
+            this.has_been_attracted = true; 
+            behavior_inst.has_attracting = true;   
+            this._accumulate_force(behavior_inst); 
+        }
+	}; 	    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+	
+	Exps.prototype.IsSource = function (ret)
+	{
+		ret.set_int((this.is_source)? 1:0);
+	};
+	
+	Exps.prototype.IsTarget = function (ret)
+	{
+		ret.set_int((this.is_target)? 1:0);
+	};	
+
+	Exps.prototype.Range = function (ret)
+	{
+		ret.set_float(this.sensitivity_range);
+	};	
+	Exps.prototype.SourceUID = function (ret)
+	{
+		ret.set_int(this.attracting_source_uid);
+	};		
+	Exps.prototype.TargetUID = function (ret)
+	{
+		ret.set_int(this.attracted_target_uid);
+	};	
+	Exps.prototype.SourceTag = function (ret)
+	{
+		ret.set_string(this.source_tag);
+	};		
+	Exps.prototype.TargetTag = function (ret)
+	{
+		ret.set_string(this.target_tag);
+	};	
+    
+	Exps.prototype.ForceAngle = function (ret)
+	{
+        var dx = this.output_force["x"];
+        var dy = this.output_force["y"];    
+        var a = Math.atan2(dy, dx);
+		ret.set_float(cr.to_clamped_degrees(a));
+	};	
+	Exps.prototype.ForceMagnitude = function (ret)
+	{
+        var dx = this.output_force["x"];
+        var dy = this.output_force["y"];    
+        var m = Math.sqrt( (dx*dx) + (dy*dy) );
+		ret.set_float(m);
+	};   
+	Exps.prototype.ForceDx = function (ret)
+	{
+		ret.set_float(this.output_force["x"]);
+	};	
+	Exps.prototype.ForceDy = function (ret)
+	{
+		ret.set_float(this.output_force["y"]);
+	};   
+    
+}());
