8946b041c9ac2c52ef3071a603d9acd1ac7d608e
hua <binhua.tein@gmail.com>
2014-08-22 23:12:40 +0800

    Add line behavior

---
+++ behaviors/rex_line/edittime.js
@@ -0,0 +1,102 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Line",
+		"id":			"Rex_Line",
+		"description":	"Set position, angle, and width of this instance, to be a line.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_line.html",
+		"category":		"General",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions 
+AddNumberParam("Start X", "The X co-ordinate of Start point.");
+AddNumberParam("Start Y", "The Y co-ordinate of Start point.");
+AddNumberParam("End X", "The X co-ordinate of End point.");
+AddNumberParam("End Y", "The Y co-ordinate of End point.");
+AddAction(1, 0, "Line between two points", "Line between", 
+          "{my} from (<i>{0}</i>, <i>{1}</i>) to (<i>{2}</i>, <i>{3}</i>)",
+          "Line between two points.", "LineBtPoints");  
+AddObjectParam("Start", "Start object.");
+AddObjectParam("End", "End object.");
+AddAction(2, 0, "Line between two instances", "Line between", 
+          "{my} from <i>{0}</i> to <i>{1}</i>",
+          "Line between two instances.", "LineBtInsts");  
+AddNumberParam("Start", "UID of Start object.");
+AddNumberParam("End", "UID of End object.");
+AddAction(3, 0, "Line between two instances by UID", "Line between", 
+          "{my} from UID <i>{0}</i> to UID <i>{1}</i>",
+          "Line between two instances by UID.", "LineBtInstsUID"); 
+AddNumberParam("End X", "The X co-ordinate of End point.");
+AddNumberParam("End Y", "The Y co-ordinate of End point.");
+AddAction(4, 0, "Line to point", "Line to", 
+          "{my} to (<i>{0}</i>, <i>{1}</i>)",
+          "Line to point.", "LineToPoint");  
+AddObjectParam("End", "End object.");
+AddAction(5, 0, "Line to instance", "Line to", 
+          "{my} to <i>{0}</i>",
+          "Line to instance.", "LineToInst"); 
+AddNumberParam("End", "UID of End object.");
+AddAction(6, 0, "Line to instance by UID", "Line to", 
+          "{my} to UID <i>{0}</i>",
+          "Line to instance by UID.", "LineToIntUID"); 
+                  
+//////////////////////////////////////////////////////////////
+// Expressions
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [    
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{	
+}
---
+++ behaviors/rex_line/runtime.js
@@ -0,0 +1,144 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_Line = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_Line.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;       
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{      	     
+	};
+
+	behinstProto.tick = function ()
+	{
+	};  
+
+    behinstProto.line_to_points = function (x0, y0, x1, y1)
+    {
+        var dx=x1-x0, dy=y1-y0;
+        var d = Math.sqrt(dx*dx + dy*dy);
+        var a = Math.atan2(dy, dx);
+                
+        var has_update = false;
+        var inst = this.inst;
+        if ((inst.x !== x0) || (inst.y !== y0))
+        {
+        	inst.x = x;
+        	inst.y = y;
+            has_update = true;
+        }
+        
+        if (inst.angle != a)
+        {
+            inst.angle = a;
+            has_update = true;
+        }
+        
+        if (inst.width != d)
+        {
+            inst.width = d;
+            has_update = true;
+        }
+        
+        if (has_update)
+        {
+        	inst.set_bbox_changed();    
+        }
+	}; 	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+		
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+    Acts.prototype.LineBtPoints = function (x0, y0, x1, y1)
+	{
+        this.line_to_points(x0, y0, x1, y1);       
+	}; 
+
+    Acts.prototype.LineBtInsts = function (objtype0, objtype1)
+	{
+        if ((!objtype0) || (!objtype1))
+            return;	    
+        var inst0 = objtype0.getFirstPicked();
+        var inst1 = objtype1.getFirstPicked();
+        this.line_to_points(inst0.x, inst0.y, inst1.x, inst1.y);      
+	};     
+
+    Acts.prototype.LineBtInstsUID = function (uid0, uid1)
+	{
+        var inst0 = this.runtime.getObjectByUID(uid0);
+        var inst1 = this.runtime.getObjectByUID(uid1);
+        if ((!inst0) || (!inst1))
+            return;	    
+        this.line_to_points(inst0.x, inst0.y, inst1.x, inst1.y);  
+	};
+	
+    Acts.prototype.LineToPoint = function (x1, y1)
+	{
+        this.line_to_points(this.inst.x, this.inst.y, x1, y1);       
+	}; 	  
+	
+    Acts.prototype.LineToInst = function (objtype1)
+	{
+        if (!objtype1)
+            return;	    
+        var inst0 = this.inst;
+        var inst1 = objtype1.getFirstPicked();
+        this.line_to_points(inst0.x, inst0.y, inst1.x, inst1.y);      
+	};
+
+    Acts.prototype.LineToInstUID = function (uid1)
+	{
+	    var inst0 = this.inst;
+        var inst1 = this.runtime.getObjectByUID(uid1);
+        if (!inst1)
+            return;	    
+        this.line_to_points(inst0.x, inst0.y, inst1.x, inst1.y);  
+	};	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+}());
