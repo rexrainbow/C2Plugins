202b0e8dacd7e0b441e31756e326d02156eed56a
hua <binhua.tein@gmail.com>
2014-09-19 22:55:17 +0800

    Add plugin

---
+++ behaviors/rex_sprite_animation_loader/edittime.js
@@ -0,0 +1,100 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Animation loader",
+		"id":			"Rex_animation_loader",
+		"version":		"0.1",
+		"description":	"Load animations from URL.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_animation_loader.html",
+		"category":		"General",
+		"flags":		bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On get URL", "URL", "{my} on get URL", 
+             'Triggered when request a URL for loading a frame. Get animation name and frame index by "expression:AnimationName", "expression:FrameIndex"', 
+             "OnGetURL");             
+AddCondition(2, cf_trigger, "On all animations loaded", "Loaded", "{my} on all animations loaded", 
+             "Triggered when all animations loaded.", 
+             "OnAllAnimationLoaded");
+AddCondition(3, cf_trigger, "On frame loaded successful", "Loaded", "{my} on frame loaded successful", 
+             'Triggered when a frame is loaded successful. Get animation name and frame index by "expression:AnimationName", "expression:FrameIndex"', 
+             "OnFrameLoaded"); 
+AddCondition(4, cf_trigger, "On frame loaded failed", "Loaded", "{my} on frame loaded failed", 
+             'Triggered when a frame is loaded failed. Get animation name and frame index by "expression:AnimationName", "expression:FrameIndex"', 
+             "OnFrameLoadedFailed");                          
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("URL", "URL of loaded frame.", '""');
+AddAction(1, 0, "Set URL", "URL", "{my} set URL to <i>{0}</i>", 'Set requested URL. Call this action under "Condition: On get URL"', "SetURL");
+AddComboParamOption("Resize to image size");
+AddComboParamOption("Keep current size");
+AddComboParam("Size", "Whether to resize the sprite to the size of the loaded image, or stretch it to the current size.");
+AddAction(2, 0, "Load all animations", "Animation", "{my} load all animations ({0})", "Load all anmations from URL.", "LoadAllAnimations");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_string, "Request animation name", 
+              "URL", "AnimationName", 
+              'Get request animation name. Use this expression under "Condition: On get URL"');
+AddExpression(2, ef_return_number, "Request frame index", 
+              "URL", "FrameIndex", 
+              'Get request frame index. Use this expression under "Condition: On get URL"');
+AddExpression(3, ef_return_string, "URL of loaded frame", 
+              "Loaded", "FrameURL", 
+              'Get requested url. Use this expression under "Condition: On frame loaded", or "Condition: On frame loaded failed".');
+                   
+ACESDone();
+
+var property_list = [
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// any other properties here, e.g...
+	// this.myValue = 0;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_sprite_animation_loader/runtime.js
@@ -0,0 +1,320 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_animation_loader = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_animation_loader.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+        this.exp_AnimationName = "";
+        this.exp_FrameIndex = 0;
+        this.exp_URL = "";
+	};
+	
+	behinstProto.tick = function ()
+	{
+	};
+    
+	behinstProto.request_url = function (animation_name, frame_index)
+	{
+        this.exp_AnimationName = animation_name;
+        this.exp_FrameIndex = frame_index;
+        this.exp_URL = "";
+        this.runtime.trigger(cr.behaviors.Rex_animation_loader.prototype.cnds.OnGetURL, this.inst);
+        return this.exp_URL;
+	};
+	
+	var event_name_get = function (animation_name, frame_index)
+    {
+        return animation_name+"-"+frame_index.toString();
+    };
+    
+	behinstProto.load_imagee = function (target_frame, animation_name, frame_index, resize_, loader_task)
+	{
+	    var task_key = loader_task.key;
+        loader_task.add_wait( event_name_get(animation_name, frame_index) );
+
+        var url_ = this.request_url(animation_name, frame_index);    
+        var thebehavior = this;
+        
+        // copy from official sprite plugin
+		var img = new Image();
+		var self = this.inst;
+		var curFrame_ = target_frame;
+		
+		img.onload = function ()
+		{
+		    if (!loader_task.is_current_task(task_key))
+		        return;
+		        
+			// If this action was used on multiple instances, they will each try to create a
+			// separate image or texture, which is a waste of memory. So if the same image has
+			// already been loaded, ignore this callback.
+			if (curFrame_.texture_img.src === img.src)
+			{
+				// Still may need to switch to using the image's texture in WebGL renderer
+				if (self.runtime.glwrap && self.curFrame === curFrame_)
+					self.curWebGLTexture = curFrame_.webGL_texture;
+				
+				// Still need to trigger 'On loaded'
+				self.runtime.redraw = true;
+				self.runtime.trigger(cr.plugins_.Sprite.prototype.cnds.OnURLLoaded, self);
+			
+				return;
+			}
+			
+			curFrame_.texture_img = img;
+			curFrame_.offx = 0;
+			curFrame_.offy = 0;
+			curFrame_.width = img.width;
+			curFrame_.height = img.height;
+			curFrame_.spritesheeted = false;
+			curFrame_.datauri = "";
+			
+			// WebGL renderer: need to create texture (canvas2D just draws with img directly)
+			if (self.runtime.glwrap)
+			{
+				if (curFrame_.webGL_texture)
+					self.runtime.glwrap.deleteTexture(curFrame_.webGL_texture);
+					
+				curFrame_.webGL_texture = self.runtime.glwrap.loadTexture(img, false, self.runtime.linearSampling);
+				
+				if (self.curFrame === curFrame_)
+					self.curWebGLTexture = curFrame_.webGL_texture;
+				
+				// Need to update other instance's curWebGLTexture
+				self.type.updateAllCurrentTexture();
+			}
+			
+			// Set size if necessary
+			if (resize_ === 0)		// resize to image size
+			{
+				self.width = img.width;
+				self.height = img.height;
+				self.set_bbox_changed();
+			}
+			
+			self.runtime.redraw = true;
+			self.runtime.trigger(cr.plugins_.Sprite.prototype.cnds.OnURLLoaded, self);
+            
+            
+            thebehavior.on_frame_loaded( animation_name, frame_index, url_, true, loader_task ); 
+              
+		};   // end of img.onload
+		
+        img.onerror = function ()
+        {
+		    if (!loader_task.is_current_task(task_key))
+		        return;
+		                    
+            thebehavior.on_frame_loaded( animation_name, frame_index, url_, false, loader_task );               
+        };
+		
+		if (url_.substr(0, 5) !== "data:")
+			img.crossOrigin = 'anonymous';
+		
+		img.src = url_;
+        // copy from official sprite plugin
+        
+	};  
+
+	behinstProto.on_frame_loaded = function (animation_name, frame_index, url_, is_success, loader_task)
+	{
+        this.exp_AnimationName = animation_name;
+        this.exp_FrameIndex = frame_index; 
+        this.exp_URL = url_;
+        
+        if ( is_success )
+            this.runtime.trigger(cr.behaviors.Rex_animation_loader.prototype.cnds.OnFrameLoaded, this.inst);
+        else
+            this.runtime.trigger(cr.behaviors.Rex_animation_loader.prototype.cnds.OnFrameLoadedFailed, this.inst);
+            
+       loader_task.remove_wait( event_name_get(animation_name, frame_index) );
+       if ( !loader_task.is_waitting() )
+        {
+            this.runtime.trigger(cr.behaviors.Rex_animation_loader.prototype.cnds.OnAllAnimationLoaded, this.inst);
+        }
+	};
+	  
+	//////////////////////////////////////
+	// Conditions
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	    
+	Cnds.prototype.OnGetURL = function ()
+	{  
+		return true;
+	};
+
+	Cnds.prototype.OnAllAnimationLoaded = function ()
+	{  
+		return true;
+	};    
+
+	Cnds.prototype.OnFrameLoaded = function ()
+	{  
+		return true;
+	};    
+
+	Cnds.prototype.OnFrameLoadedFailed = function ()
+	{  
+		return true;
+	};    	
+	    
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+    
+    Acts.prototype.SetURL = function (url_)
+	{
+	    this.exp_URL = url_;
+	}; 
+    
+	Acts.prototype.LoadAllAnimations = function (resize_)
+	{
+	    var loader_task = cr.behaviors.Rex_animation_loader.loader_task_get(this.inst.type.sid.toString());	    
+	    loader_task.reset_task();
+	    
+        var animations=this.inst.type.animations;
+        var i, cnti=animations.length;
+        var j, cntj, frames, target_frame;
+        var animation_name, frame_index;
+        for (i=0; i<cnti; i++)
+        {
+            animation_name = animations[i].name;
+            frames = animations[i].frames;
+            cntj = frames.length;
+            for (j=0; j<cntj; j++)
+            {
+                frame_index = j;
+                target_frame = frames[frame_index];
+                this.load_imagee(target_frame, animation_name, frame_index, resize_, loader_task);
+            }
+        }
+	};
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+    Exps.prototype.AnimationName = function (ret)
+	{     
+	    ret.set_string(this.exp_AnimationName);
+	};
+
+    Exps.prototype.FrameIndex = function (ret)
+	{     
+	    ret.set_int(this.exp_FrameIndex);
+	};	
+
+    Exps.prototype.FrameURL = function (ret)
+	{     
+	    ret.set_string(this.exp_URL);
+	};		
+}());
+
+(function ()
+{
+    var LoaderKlass = function ()
+    {
+        this.key = 0;
+        this.wait_events = {};
+    };
+    
+    var LoaderKlassProto = LoaderKlass.prototype;
+    
+    LoaderKlassProto.reset_task = function ()
+	{
+	    return;
+	    
+	    // TODO
+        this.key += 1;
+        var n;
+        for (n in this.wait_events)
+        {
+            delete this.wait_events[n];
+        }
+	};
+	
+    LoaderKlassProto.is_current_task = function (key)
+	{	
+        return (this.key === key);
+	};	
+	    
+    LoaderKlassProto.add_wait = function (name)
+	{
+        this.wait_events[name] = true;
+	}; 
+    
+	LoaderKlassProto.remove_wait = function (name)
+	{
+        if (!this.wait_events.hasOwnProperty(name))
+            return;
+        
+        delete this.wait_events[name];
+	};  
+	
+    LoaderKlassProto.is_waitting = function (name)
+	{
+        var n, is_waitting=false;
+        for (n in this.wait_events)
+        {
+            is_waitting = true;
+            break;
+        }
+        return is_waitting;
+	};
+	
+	var _sid2task = {};
+	cr.behaviors.Rex_animation_loader.loader_task_get = function (sid)
+	{
+	    if (!_sid2task.hasOwnProperty(sid))
+	    {
+	        _sid2task[sid] = new LoaderKlass;
+	    }
+	    return _sid2task[sid];
+	}; 
+		   
+}());
