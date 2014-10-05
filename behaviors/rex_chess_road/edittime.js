21503e005a812115ffbdd9e8a0cb5b60d8e02b4c
hua <binhua.tein@gmail.com>
2014-10-05 11:19:03 +0800

    Add road behavior

---
+++ behaviors/rex_chess_road/edittime.js
@@ -0,0 +1,80 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Road",
+		"id":			"rex_chess_road",
+		"version":		"0.1",
+		"description":	"Show road by frames automatically.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_chess_road.html",
+		"category":		"Board",
+		"flags":		0	
+						| bf_onlyone
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddComboParamOption("disabled");
+AddComboParamOption("enabled");
+AddComboParam("State", "Whether to enable or disable the behavior.");
+AddAction(0, 0, "Set enabled", "Enable", "Set {my} {0}", "Enable or disable the behavior.", "SetActivated");
+
+//////////////////////////////////////////////////////////////
+// Expressions
+
+ACESDone();
+
+var property_list = [
+    new cr.Property(ept_combo, "Activated", "Yes", "Enable if you wish this to begin at the start of the layout.", "No|Yes"),
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of the behavior in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// any other properties here, e.g...
+	// this.myValue = 0;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_chess_road/runtime.js
@@ -0,0 +1,168 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.rex_chess_road = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.rex_chess_road.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+    function GetThisBehavior(inst)
+	{
+		var i, len;
+		for (i = 0, len = inst.behavior_insts.length; i < len; i++)
+		{
+			if (inst.behavior_insts[i] instanceof behaviorProto.Instance)
+				return inst.behavior_insts[i];
+		}
+		
+		return null;
+	};	
+    
+    
+	behinstProto.onCreate = function()
+	{
+	    this.activated = (this.properties[0]==1);
+	    this.board = null;
+        this.ActSetAnimFrame = (cr.plugins_.Sprite == null)? null:
+                               cr.plugins_.Sprite.prototype.acts.SetAnimFrame;
+	};
+	
+	behinstProto.tick = function ()
+	{
+	    if ((!this.activated) || (!this.ActSetAnimFrame))
+	        return;
+	    
+        var score = this.neighbor_score_get();
+        if (score == null)
+            return null;
+
+        this.ActSetAnimFrame.call(this.inst, score);
+	};
+	
+	behinstProto.neighbor_score_get = function ()
+	{
+	    var board = this._board_get();
+        if (board == null)
+            return null;
+            
+        var xyz = board.uid2xyz(this.inst.uid);
+        var layout = board.GetLayout();
+        var dir, dir_count = layout.GetDirCount();
+        var neighbor_uid, neighbor_inst, binst;
+        var score=0;
+        for (dir=0; dir<dir_count; dir++)
+        {
+            neighbor_uid = board.dir2uid(this.inst.uid, dir, xyz.z);
+            if (neighbor_uid == null)
+                continue;            
+            neighbor_inst = board.uid2inst(neighbor_uid);
+            if (neighbor_inst == null)
+                continue;
+            binst = GetThisBehavior(neighbor_inst);
+            if (!binst.activated)
+                continue;
+            score += (1<<dir);
+        }
+        return score;        
+	};    
+    
+   	behinstProto._board_get = function ()
+	{
+        var _xyz;
+        if (this.board != null)
+        {
+            _xyz = this.board.uid2xyz(this.inst.uid);
+            if (_xyz != null)
+                return this.board;  // find out xyz on board
+            else  // chess no longer at board
+                this.board = null;
+        }
+            
+        var plugins = this.runtime.types;
+        var name, obj;
+        for (name in plugins)
+        {
+            obj = plugins[name].instances[0];
+            if ((obj != null) && (obj.check_name == "BOARD"))
+            {
+                _xyz = obj.uid2xyz(this.inst.uid)
+                if (_xyz != null)
+                { 
+                    this.board = obj;					
+                    return this.board;
+                }
+            }
+        }
+        return null;	
+	};
+		
+	behinstProto.saveToJSON = function ()
+	{
+		return { "en": this.activated
+                };
+	};
+	
+	behinstProto.loadFromJSON = function (o)
+	{
+	    this.activated = o["en"];
+	};
+	//////////////////////////////////////
+	// Conditions
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+	    
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+
+	Acts.prototype.SetActivated = function (s)
+	{
+		this.activated = (s==1);
+	};  
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+	
+}());
