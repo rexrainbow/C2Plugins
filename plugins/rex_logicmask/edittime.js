a667c70e5b97c4890d3168bdfa5f5298f9bd8290
hua <binhua.tein@gmail.com>
2014-11-06 07:52:16 +0800

    Add logic mask plugin

---
+++ plugins/rex_logicmask/edittime.js
@@ -0,0 +1,130 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Logic mask",
+		"id":			"Rex_LogicMask",
+		"version":		"0.1",        
+		"description":	"A cover area of a board object.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_logicmask.html",
+		"category":		"Rex - Board - application",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_looping | cf_not_invertible, "For each enter LXY", "Enter", 
+             "For each enter LXY", 
+             "Repeat the event for each logic position of entered area.", "ForEachEnter");  
+AddCondition(2, cf_looping | cf_not_invertible, "For each exit LXY", "Exit", 
+             "For each exit LXY", 
+             "Repeat the event for each logic position of exited area.", "ForEachExit");   
+AddCondition(3, cf_looping | cf_not_invertible, "For each mask", "Mask area", 
+             "For each mask LXY", 
+             "Repeat the event for each logic position of mask area.", "ForEachMask");  
+AddNumberParam("Logic X", "The X index.", 0);
+AddNumberParam("Logic Y", "The Y index.", 0);
+AddCondition(4, 0, "In mask area", "Mask area", 
+             "[<i>{0}</i>,<i>{1}</i>] is in masked area", 
+             "Testing if point is in masked area.", "IsMaskArea");			 
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(1, 0, "Clean", "Define mask", 
+          "Clean mask", 
+          "Clean mask.", "CleanMask");
+AddNumberParam("Left-top X", "Logic X position of Left-top point related by origin point.", -2);   
+AddNumberParam("Left-top Y", "Logic Y position of Left-top point related by origin point.", -2); 
+AddNumberParam("Width", "Witdh of area.", 5);   
+AddNumberParam("Height", "Height of area.", 5); 
+AddAnyTypeParam("Value", "Filled value. Could be number or (JSON) string.", 1); 
+AddAction(2, 0, "Fill a rectangle", "Define mask", 
+          "Fill a rectangle mask at offset to [<i>{0}</i>, <i>{1}</i>], width to <i>{2}</i>, height to <i>{3}</i>, with value to <i>{4}</i>",
+          "Fill a rectangle mask.", "FillRectangleMask");  
+AddNumberParam("X", "Logic X position related by origin point.", 0);   
+AddNumberParam("Y", "Logic Y position related by origin point.", 0);
+AddAnyTypeParam("Value", "Filled value. Could be number or (JSON) string.", 1); 
+AddAction(3, 0, "Fill a point", "Define mask", 
+          "Fill at [<i>{0}</i>, <i>{1}</i>], with value to <i>{2}</i>",
+          "Fill a point mask.", "FillPointMask");   
+AddNumberParam("X", "Logic X position of origin point.", 0);   
+AddNumberParam("Y", "Logic Y position of origin point.", 0);
+AddAction(11, 0, "Set origin point", "Origin point", 
+          "Set origin point to [<i>{0}</i>, <i>{1}</i>]",
+          "Set origin point.", "SetOrigin");                   
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_number, "Current Logic X ", "For each", "CurLX", 
+              "Current Logic X in a For Each loop.");   			  
+AddExpression(2, ef_return_number, "Current Logic Y ", "For each", "CurLY",
+              "Current Logic Y in a For Each loop."); 
+AddExpression(3, ef_return_any, "Current value of mask", "For each", "CurValue",
+              "Current value of mask in a For Each loop."); 
+AddExpression(11, ef_return_number, "Logic X of Origin point", "Origin", "OX", 
+              "Logic X of Origin point.");   			  
+AddExpression(12, ef_return_number, "Logic Y of Origin point", "Origin", "OY", 
+              "Logic Y of Origin point.");   					  
+                               
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_logicmask/runtime.js
@@ -0,0 +1,252 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_LogicMask = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_LogicMask.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{          
+	    this.origin = [null, null];
+	    this.mask2value = {};  // mask - value
+	    this.mask2board = {};  // mask - board
+	    this.board2mask = {};  // board - mask
+
+	    this.onenter = [];
+	    this.onexit = [];
+	    
+	    this.exp_CurLX = 0;
+	    this.exp_CurLY = 0;
+	    this.exp_CurValue = 0;
+	};
+	
+	var lxy2key = function (x, y)
+	{
+	    return JSON.stringify([x,y]);
+	};
+	var key2lxy = function (k)
+	{
+	    return JSON.parse(k);
+	};					
+	var clean_table = function (o)
+	{
+	    var k;
+		for (k in o)
+		    delete o[k];
+	};
+	
+	instanceProto.set_mask = function (x, y, v)
+	{
+	    var k = lxy2key(x,y);
+	    this.mask2board[k] = null;	 	    
+	    this.mask2value[k] = v;   
+	};
+	
+	instanceProto.clean_mask = function ()
+	{
+	    clean_table(this.mask2value);
+	    clean_table(this.mask2board);	     	   
+	};	
+	
+	var pre_board2mask = {};
+	instanceProto.place_mask = function (x, y)
+	{	  
+	    this.origin[0] = x;
+	    this.origin[1] = y;	    	    
+	    // swap pre_board2mask and this.board2mask
+		var tmp = pre_board2mask;
+		pre_board2mask = this.board2mask;
+		this.board2mask = tmp;
+		
+        var k, lxy, new_place;
+        for (k in this.mask2board)
+        {            
+		    lxy = key2lxy(k);
+			lxy[0] += x;
+            lxy[1] += y;
+            new_place = JSON.stringify(lxy);
+            this.mask2board[k] = new_place;
+            this.board2mask[new_place] = k;
+        }
+        
+        this.onenter.length = 0;
+        this.onexit.length = 0;
+        for (k in this.board2mask)
+        {
+            if (!pre_board2mask.hasOwnProperty(k))
+                this.onenter.push(k);
+        }
+        for (k in pre_board2mask)
+        {
+            if (!this.board2mask.hasOwnProperty(k))
+                this.onexit.push(k);
+        }
+        clean_table(pre_board2mask);
+	};	
+	
+	instanceProto.cond_for_each = function (klist, for_each_key)
+	{
+        var current_frame = this.runtime.getCurrentEventStack();
+        var current_event = current_frame.current_event;
+		var solModifierAfterCnds = current_frame.isModifierAfterCnds();
+		
+		var i, k, lxy;
+        for(i in klist)
+        {
+            if (solModifierAfterCnds)
+            {
+                this.runtime.pushCopySol(current_event.solModifiers);
+            }
+            
+			if (!for_each_key)
+			{
+                k = klist[i];
+		    }
+			else
+			{
+			    k = i;
+			}
+			lxy = key2lxy(k);
+	        this.exp_CurLX = lxy[0];
+	        this.exp_CurLY = lxy[1];
+	        this.exp_CurValue = this.mask2value[ this.board2mask[k] ];
+		    current_event.retrigger();
+		      
+		    if (solModifierAfterCnds)
+		    {
+		        this.runtime.popSol(current_event.solModifiers);
+		    }
+        }
+                    		
+		return false;
+	};	
+	
+	instanceProto.saveToJSON = function ()
+	{
+		return {  };
+	};
+	
+	instanceProto.loadFromJSON = function (o)
+	{
+	};
+	
+	instanceProto.afterLoad = function ()
+	{
+	};	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds(); 
+	   
+	Cnds.prototype.ForEachEnter = function ()
+	{
+        return this.cond_for_each(this.onenter);
+	};		
+	Cnds.prototype.ForEachExit = function ()
+	{	     
+        return this.cond_for_each(this.onexit);
+	};	
+	Cnds.prototype.ForEachMask = function ()
+	{	     
+        return this.cond_for_each(this.board2mask, true);
+	};	
+	Cnds.prototype.IsMaskArea = function (x, y)
+	{	     
+        return this.board2mask.hasOwnProperty(lxy2key(x,y));
+	};	
+    //////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+	
+	Acts.prototype.CleanMask = function ()
+	{
+	    this.clean_mask();
+	};
+	
+	Acts.prototype.FillRectangleMask = function (x, y, w, h, v)
+	{
+	    var i,j, xmax=x+w-1, ymax=y+h-1;
+	    for (i=x; i<=xmax; i++)
+	    {
+	        for (j=y; j<=ymax; j++)
+	        {
+	            this.set_mask(i,j,v);
+	        }
+	    }
+	};	
+	
+	Acts.prototype.FillPointMask = function (x, y, v)
+	{
+	    this.set_mask(x, y, v);
+	};		
+	
+	Acts.prototype.SetOrigin = function (x, y)
+	{
+	    this.place_mask(x, y);
+	};					
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+	Exps.prototype.CurLX = function (ret)
+	{
+		ret.set_int(this.exp_CurLX);
+	}; 	
+	Exps.prototype.CurLY = function (ret)
+	{
+		ret.set_int(this.exp_CurLY);
+	};
+	Exps.prototype.CurValue = function (ret)
+	{
+		ret.set_any(this.exp_CurValue);
+	}; 
+	Exps.prototype.OX = function (ret)
+	{
+		ret.set_int(this.origin[0]);
+	}; 	
+	Exps.prototype.OY = function (ret)
+	{
+		ret.set_int(this.origin[1]);
+	};	
+}());
+
+(function ()
+{   
+ 
+}());
