cdf4211089c16a867d74a75972dda09c3d5e6216
hua <binhua.tein@gmail.com>
2014-11-06 07:53:13 +0800

    Add pixel cache behavior

---
+++ behaviors/rex_canvas_pixelcache/edittime.js
@@ -0,0 +1,106 @@
+﻿function GetBehaviorSettings()
+{
+	return {
+		"name":			"Pixel cache",
+		"id":			"Rex_Canvas_PixelCahce",
+		"description":	"Cache color of pixels.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_canvas_pixelcahce.html",
+		"category":		"Rex - Canvas helper",
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(1, 0, "Cache all", "Cache", 
+          "{my} cache all pixels", 
+		  "Cache color of all pixels.", "CacheArea");
+		  
+AddNumberParam("Left-top X", "X co-ordinate of left-top point.", 0);
+AddNumberParam("Left-top Y", "Y co-ordinate of left-top point.", 0);
+AddNumberParam("Width", "Width.", 0);
+AddNumberParam("Height", "Height.", 0);  
+AddAction(2, 0, "Cache area", "Cache", 
+          "{my} cache pixels at left-top ({0},{1}), width to {2} and height to {3}", 
+          "Cache color at an area.", "CacheArea"); 
+		  
+//////////////////////////////////////////////////////////////
+// Expressions
+AddNumberParam("x", "x position on canvas", "0");
+AddNumberParam("y", "y position on canvas", "0");
+AddExpression(1, ef_return_string, "Get rgba at", "Color", "rgbaAt", "This gives the rgba color at a given position on the canvas.");
+
+AddNumberParam("x", "x position on canvas", "0");
+AddNumberParam("y", "y position on canvas", "0");
+AddExpression(2, ef_return_number, "Get red at", "Color", "redAt", "This gives the red component of the color at a given position on the canvas. (0-255)");
+
+AddNumberParam("x", "x position on canvas", "0");
+AddNumberParam("y", "y position on canvas", "0");
+AddExpression(3, ef_return_number, "Get green at", "Color", "greenAt", "This gives the green component of the color at a given position on the canvas. (0-255)");
+
+AddNumberParam("x", "x position on canvas", "0");
+AddNumberParam("y", "y position on canvas", "0");
+AddExpression(4, ef_return_number, "Get blue at", "Color", "blueAt", "This gives the blue component of the color at a given position on the canvas. (0-255)");
+
+AddNumberParam("x", "x position on canvas", "0");
+AddNumberParam("y", "y position on canvas", "0");
+AddExpression(5, ef_return_number, "Get alpha at", "Color", "alphaAt", "This gives the alpha component of the color at a given position on the canvas. (0.0-1.0)");
+
+AddExpression(11, ef_return_number, "Get left X", "Area", "LeftX", "Get left X of cached area");
+AddExpression(12, ef_return_number, "Get right X", "Area", "RightX", "Get right X of cached area");
+AddExpression(13, ef_return_number, "Get top Y", "Area", "TopY", "Get top Y of cached area");
+AddExpression(14, ef_return_number, "Get bottom Y", "Area", "BottomY", "Get bottom Y of cached area");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [           
+	];
+	
+// Called by IDE when a new behavior type is to be created
+function CreateIDEBehaviorType()
+{
+	return new IDEBehaviorType();
+}
+
+// Class representing a behavior type in the IDE
+function IDEBehaviorType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new behavior instance of this type is to be created
+IDEBehaviorType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
---
+++ behaviors/rex_canvas_pixelcache/runtime.js
@@ -0,0 +1,191 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.behaviors, "cr.behaviors not created");
+
+/////////////////////////////////////
+// Behavior class
+cr.behaviors.Rex_Canvas_PixelCahce = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var behaviorProto = cr.behaviors.Rex_Canvas_PixelCahce.prototype;
+		
+	/////////////////////////////////////
+	// Behavior type class
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	
+	var behtypeProto = behaviorProto.Type.prototype;
+
+	behtypeProto.onCreate = function()
+	{  
+	};
+
+	/////////////////////////////////////
+	// Behavior instance class
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;
+		this.runtime = type.runtime;
+	};
+	
+	var behinstProto = behaviorProto.Instance.prototype;
+
+	behinstProto.onCreate = function()
+	{
+	    this.img_data = null;
+		this.area_lx = 0;
+		this.area_rx = 0;
+		this.area_ty = 0;
+		this.area_by = 0;    	
+	};  
+	
+	behinstProto.tick = function ()
+	{	
+	};
+	
+	behinstProto.cache_area = function (x, y, w, h)
+	{
+	    if (x == null)
+		{
+		    x = 0; 
+			y = 0; 
+			w = this.inst.canvas.width; 
+			h = this.inst.canvas.height;
+	    }
+			
+        this.img_data = this.inst.ctx.getImageData(x, y, w, h);
+		this.area_lx = x;
+		this.area_ty = y;
+		this.area_rx = x+w-1;
+		this.area_by = y+h-1;
+	};	
+
+	behinstProto.point_get = function (x,y)
+	{	
+	    if (this.img_data == null)
+		    this.cache_area();
+        if ( (x < this.area_lx) || (x > this.area_rx) ||
+		     (y < this.area_ty) || (y > this.area_by) )
+			return -1;
+	    
+		x -= this.area_lx;
+		y -= this.area_ty;
+        return ((y*this.img_data.width) + x) * 4;
+	};	
+    behinstProto.get_color = function (i)
+	{
+		var data = this.img_data.data;
+		var val = data[i];
+		if (val == null)
+		{
+		    val = 0;
+	    }
+	    return val;
+	};	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	behaviorProto.cnds = new Cnds();
+
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	behaviorProto.acts = new Acts();
+	
+	Acts.prototype.CacheArea = function (x, y, w, h)
+	{	
+        this.cache_area(x, y, w, h);
+	};
+
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	behaviorProto.exps = new Exps();
+
+	Exps.prototype.rgbaAt = function (ret, x, y)
+	{
+	    var i = this.point_get(x, y);
+		if (i == -1)
+		{
+		    ret.set_string("black");
+			return;
+		}
+		
+		var data = this.img_data.data;
+		ret.set_string("rgba(" + data[i] + "," + data[i+1] + "," + data[i+2] + "," + data[i+3]/255 + ")");
+	};
+
+    Exps.prototype.redAt = function (ret, x, y)
+	{
+		var i = this.point_get(x, y);
+		if (i == -1)
+		{
+		    ret.set_int(0);
+			return;
+		}
+		
+		ret.set_int(this.get_color(i));
+	};
+    Exps.prototype.greenAt = function (ret, x, y)
+	{
+		var i = this.point_get(x, y);
+		if (i == -1)
+		{
+		    ret.set_int(0);
+			return;
+		}
+		
+		ret.set_int(this.get_color(i+1));
+	};
+   Exps.prototype.blueAt = function (ret, x, y)
+	{
+		var i = this.point_get(x, y);
+		if (i == -1)
+		{
+		    ret.set_int(0);
+			return;
+		}
+		
+		ret.set_int(this.get_color(i+2));
+	};
+    Exps.prototype.alphaAt = function (ret, x, y)
+	{
+		var i = this.point_get(x, y);
+		if (i == -1)
+		{
+		    ret.set_int(0);
+			return;
+		}
+		
+		ret.set_int(this.get_color(i+3)*100/255);
+	};	
+	
+    Exps.prototype.LeftX = function (ret)
+	{
+		ret.set_int(this.area_lx);
+	};	
+    Exps.prototype.RightX = function (ret)
+	{
+		ret.set_int(this.area_rx);
+	};
+	Exps.prototype.TopY = function (ret)
+	{
+		ret.set_int(this.area_ty);
+	};	
+    Exps.prototype.BottomY = function (ret)
+	{
+		ret.set_int(this.area_by);
+	};	
+}());
