bf7be812fc139b01519d2b2eeff8d93c670916a9
hua <binhua.tein@gmail.com>
2014-12-03 01:12:34 +0800

    Add firebase related plugin

---
+++ plugins/rex_firebase_authentication/edittime.js
@@ -0,0 +1,219 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Authentication",
+		"id":			"Rex_Firebase_Authentication",
+		"version":		"0.1",   		
+		"description":	"Authentication which is provided by firebase. https://www.firebase.com/",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_firebase_authentication.html",
+		"category":		"Rex - Web - firebase",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0,
+		"dependency":	"firebase.js"
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions	
+
+// email - password  
+AddCondition(1, cf_trigger, "On creating account successfully", "Email & Password - create account", 
+            "On creating account successfully", 
+            "Triggered when creating account successfully.", "EmailPassword_OnCreateAccountSuccessfully");
+            
+AddCondition(2, cf_trigger, "On creating account error", "Email & Password - create account", 
+            "On create account error", 
+            "Triggered when creating account error.", "EmailPassword_OnCreateAccountError");   
+            
+AddCondition(3, cf_trigger, "On changing password successfully", "Email & Password - changing password", 
+            "On changing password successfully", 
+            "Triggered when changing password successfully.", "EmailPassword_OnChangingPasswordSuccessfully");
+            
+AddCondition(4, cf_trigger, "On changing password error", "Email & Password - changing password", 
+            "On changing password error", 
+            "Triggered when changing password error.", "EmailPassword_OnChangingPasswordError");    
+            
+AddCondition(5, cf_trigger, "On sending password result email successfully", "Email & Password - sending password result email", 
+            "On sending password result email successfully", 
+            "Triggered when sending password result email successfully.", "EmailPassword_OnSendPasswordResultEmailSuccessfully");
+            
+AddCondition(6, cf_trigger, "On sending password result email error", "Email & Password - sending password result email", 
+            "On sending password result email error", 
+            "Triggered when sending password result email error.", "EmailPassword_OnSendPasswordResultEmailError");                         
+     
+AddCondition(7, cf_trigger, "On deleting user successfully", "Email & Password - deleting user", 
+            "On deleting user successfully", 
+            "Triggered when deleting user successfully.", "EmailPassword_OnDeleteUserSuccessfully");
+            
+AddCondition(8, cf_trigger, "On deleting user error", "Email & Password - deleting user", 
+            "On sending password result email error", 
+            "Triggered when deleting user error.", "EmailPassword_OnDeleteUserError"); 
+                        
+// general            
+AddCondition(31, cf_trigger, "On login successfully", "General - login", 
+            "On login successfully", 
+            "Triggered when login successfully.", "OnLoginSuccessfully");
+            
+AddCondition(32, cf_trigger, "On login error", "General - login", 
+            "On login error", 
+            "Triggered when login error.", "OnLoginError");
+
+AddCondition(33, cf_trigger, "On logged out", "General - login", 
+            "On logged out", 
+            "Triggered when logged out.", "OnLoggedOut");			
+//////////////////////////////////////////////////////////////
+// Actions 
+  
+// email - password  
+AddStringParam("Email", "User email");
+AddStringParam("Password", "User password");
+AddAction(1, 0, "Create account", "Email & Password", 
+          "Create account with email to <i>{0}</i>, password to <i>{1}</i>", 
+          "Create account with email & password.", "EmailPassword_CreateAccount");
+
+AddStringParam("Email", "User email");
+AddStringParam("Password", "User password");
+AddComboParamOption("default");
+AddComboParamOption("sessionOnly");
+AddComboParamOption("never");
+AddComboParam("Remember", "Persisting type");
+AddAction(2, 0, "Login", "Email & Password", 
+          "Login with email to <i>{0}</i>, password to <i>{1}</i>, persisting type to <i>{2}</i>", 
+          "Login with email & password.", "EmailPassword_Login");
+          
+AddStringParam("Email", "User email");
+AddStringParam("Old password", "User password");
+AddStringParam("New password", "User password");
+AddAction(3, 0, "Change password", "Email & Password", 
+          "Change password with email to <i>{0}</i>, password from old <i>{1}</i> to new <i>{1}</i>", 
+          "Change password.", "EmailPassword_ChangePassword");          
+
+AddStringParam("Email", "User email");
+AddAction(4, 0, "Sending password reset email", "Email & Password", 
+          "Sending password reset email to <i>{0}</i>", 
+          "Sending password reset email", "EmailPassword_SendPasswordResultEmail"); 
+             
+AddStringParam("Email", "User email");
+AddStringParam("Password", "User password");
+AddAction(5, 0, "Delete user", "Email & Password", 
+          "Delete user with email to <i>{0}</i>, password to <i>{1}</i>", 
+          "Delete user with email & password.", "EmailPassword_DeleteUser");  
+          
+// anonymous   
+AddComboParamOption("default");
+AddComboParamOption("sessionOnly");
+AddComboParamOption("never");
+AddComboParam("Remember", "Persisting type");       
+AddAction(11, 0, "Login", "Anonymous", 
+          "Login with anonymous, persisting type to <i>{0}</i>", 
+          "Login with anonymous.", "Anonymous_Login");   
+          
+// facebook/google
+AddComboParamOption("Facebook");
+AddComboParamOption("Twitter");
+AddComboParamOption("Github");
+AddComboParamOption("Google");
+AddComboParam("Provider", "Authentication provider");  
+AddComboParamOption("popup");
+AddComboParamOption("redirect");
+AddComboParam("Type ", "Type of login window");
+AddComboParamOption("default");
+AddComboParamOption("sessionOnly");
+AddComboParamOption("never");
+AddComboParam("Remember ", "Persisting type");
+AddStringParam("scope", "A comma-delimited list of requested extended permissions");
+AddAction(21, 0, "Login", "Authentication provider", 
+          "Login by <i>{0}</i> with <i>{1}</i>, persisting type to <i>{2}</i>, scope to <i>{3}</i>", 
+          "Login by authentication provider.", "ProviderAuthentication_Login");  
+ 
+// general         
+AddAction(31, 0, "Logging out", "General", 
+          "Logging out current account", 
+          "Logging out current account.", "LoggingOut");                           
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_string, "Error code", "Error", "ErrorCode", 
+              "Error code.");
+AddExpression(2, ef_return_string, "Error message", "Error", "ErrorMessage", 
+              "Error message.");     
+AddExpression(3, ef_return_string, "User ID", "General auth data", "UserID", 
+              "A unique user ID, intended as the user's unique key across all providers.");
+AddExpression(4, ef_return_string, "Authentication method", "General auth data", "Provider", 
+              "The authentication method used.");                           
+AddExpression(5, ef_return_string, "Display name", "General auth data", "DisplayName", 
+              "Display name.");
+AddExpression(6, ef_return_string, "User ID from provider", "General auth data", "UserIDFromProvider", 
+              "User ID from provider.");
+AddExpression(7, ef_return_string, "Access token", "General auth data", "AccessToken", 
+              "The FOAuth 2.0 access token granted by provider during user authentication.");						  
+AddExpression(8, ef_return_string, "Cached user profile", "General auth data", "CachedUserProfile", 
+              "Cached user profile from provier.");	
+AddExpression(9, ef_return_string, "Email", "General auth data", "Email", 
+              "The user's primary email address as listed on their profile. Returned only if a valid email address is available, and the email permission was granted by the user. ");	
+AddExpression(10, ef_return_string, "User name", "General auth data", "UserName", 
+              "The user's screen name, handle, or alias. Twitter screen names are unique, but subject to change. .");
+			  
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_text, "Domain", "", "The root location of the Firebase data.")
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_firebase_authentication/runtime.js
@@ -0,0 +1,332 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Firebase_Authentication = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Firebase_Authentication.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	    jsfile_load("firebase.js");
+	};
+	
+	var jsfile_load = function(file_name)
+	{
+	    var scripts=document.getElementsByTagName("script");
+	    var exist=false;
+	    for(var i=0;i<scripts.length;i++)
+	    {
+	    	if(scripts[i].src.indexOf(file_name) != -1)
+	    	{
+	    		exist=true;
+	    		break;
+	    	}
+	    }
+	    if(!exist)
+	    {
+	    	var newScriptTag=document.createElement("script");
+	    	newScriptTag.setAttribute("type","text/javascript");
+	    	newScriptTag.setAttribute("src", file_name);
+	    	document.getElementsByTagName("head")[0].appendChild(newScriptTag);
+	    }
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.rootpath = this.properties[0]; 
+        
+        this.last_error = null;
+        this.last_authData = null;
+        // ref cache
+        this.ref_cache = {}; 
+
+        
+        var self = this;
+        var onAuth_handler = function (authData)
+        {
+            if (authData) 
+            {
+                // user authenticated with Firebase
+                //console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
+            }
+            else 
+            {
+                // user is logged out
+                self.runtime.trigger(cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.OnLoggedOut, self);
+            }
+        };
+        this.get_ref()["onAuth"](onAuth_handler);
+	};
+	
+	instanceProto.get_ref = function(k)
+	{
+        if (k == null)
+            k = "";
+	    if (!this.ref_cache.hasOwnProperty(k))
+	        this.ref_cache[k] = new window["Firebase"](this.rootpath + k);
+	        
+        return this.ref_cache[k];
+	};
+	
+	instanceProto.get_query = function(k)
+	{
+        return new window["Firebase"](this.rootpath + k);
+	};
+	     
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();      
+
+	Cnds.prototype.EmailPassword_OnCreateAccountSuccessfully = function ()
+	{
+	    return true;
+	}; 	
+
+	Cnds.prototype.EmailPassword_OnCreateAccountError = function ()
+	{
+	    return true;
+	}; 
+	
+	Cnds.prototype.EmailPassword_OnChangingPasswordSuccessfully = function ()
+	{
+	    return true;
+	}; 	
+
+	Cnds.prototype.EmailPassword_OnChangingPasswordError = function ()
+	{
+	    return true;
+	}; 	
+	
+	Cnds.prototype.EmailPassword_OnSendPasswordResultEmailSuccessfully = function ()
+	{
+	    return true;
+	}; 	
+
+	Cnds.prototype.EmailPassword_OnSendPasswordResultEmailError = function ()
+	{
+	    return true;
+	}; 	
+	
+	Cnds.prototype.EmailPassword_OnDeleteUserSuccessfully = function ()
+	{
+	    return true;
+	}; 	
+
+	Cnds.prototype.EmailPassword_OnDeleteUserError = function ()
+	{
+	    return true;
+	}; 	
+		
+	Cnds.prototype.OnLoginSuccessfully = function ()
+	{
+	    return true;
+	}; 	
+
+	Cnds.prototype.OnLoginError = function ()
+	{
+	    return true;
+	}; 	
+
+	Cnds.prototype.OnLoggedOut = function ()
+	{
+	    return true;
+	}; 		
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+	
+	var on_handler_get = function(self, success_trig, error_trig)
+	{
+	    var handler = function(error, authData) 
+        {
+            self.last_error = error;
+            self.last_authData = authData;
+            if (error === null) 
+            {
+                // get auth data by expression:UserID, and expression:Provider
+                self.runtime.trigger(success_trig, self);                
+            } 
+            else 
+            {
+                // get error message by expression:ErrorCode and expression:ErrorMessage
+                self.runtime.trigger(error_trig, self);
+            }
+        };
+        return handler;
+    };
+	
+    Acts.prototype.EmailPassword_CreateAccount = function (e_, p_)
+	{
+	    var reg_data = {"email":e_,  "password":p_ };
+	    var handler = on_handler_get(this,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.EmailPassword_OnCreateAccountSuccessfully,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.EmailPassword_OnCreateAccountError);
+	    this.get_ref()["createUser"](reg_data, handler);
+	}; 
+
+    var PRESISTING_TYPE = ["default", "sessionOnly", "never"];
+    Acts.prototype.EmailPassword_Login = function (e_, p_, r_)
+	{
+	    var reg_data = {"email":e_,  "password":p_ };
+	    var handler = on_handler_get(this,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.OnLoginSuccessfully,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.OnLoginError);
+        var d = {"remember":PRESISTING_TYPE[r_]};
+	    this.get_ref()["authWithPassword"](reg_data, handler, d);
+	}; 	
+	
+    Acts.prototype.EmailPassword_ChangePassword = function (e_, old_p_, new_p_)
+	{
+	    var reg_data = {"email":e_,  "oldPassword ":old_p_,  "newPassword":new_p_};
+	    var handler = on_handler_get(this,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.EmailPassword_OnChangingPasswordSuccessfully,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.EmailPassword_OnChangingPasswordError);
+	    this.get_ref()["changePassword"](reg_data, handler);
+	}; 
+	
+    Acts.prototype.EmailPassword_SendPasswordResultEmail = function (e_)
+	{
+	    var reg_data = {"email":e_};
+	    var handler = on_handler_get(this,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.EmailPassword_OnSendPasswordResultEmailSuccessfully,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.EmailPassword_OnSendPasswordResultEmailError);
+	    this.get_ref()["resetPassword"](reg_data, handler);
+	};		
+	
+    Acts.prototype.EmailPassword_DeleteUser = function (e_, p_)
+	{
+	    var reg_data = {"email":e_,  "password":p_};
+	    var handler = on_handler_get(this,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.EmailPassword_OnDeleteUserSuccessfully,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.EmailPassword_OnDeleteUserError);
+	    this.get_ref()["removeUser"](reg_data, handler);
+	};	
+	
+    Acts.prototype.Anonymous_Login = function (r_)
+	{
+	    var handler = on_handler_get(this,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.OnLoginSuccessfully,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.OnLoginError);
+        var d = {"remember":PRESISTING_TYPE[r_]};
+	    this.get_ref()["authAnonymously"](handler, d);
+	};	
+	
+	var PROVIDER_TYPE = ["facebook", "twitter", "github", "google"];
+    Acts.prototype.ProviderAuthentication_Login = function (provider_, t_, r_, scope_)
+	{
+	    var login_fn_name = (t_ == 0)? "authWithOAuthPopup":"authWithOAuthRedirect";	    
+	    var provider = PROVIDER_TYPE[provider_];
+	    var handler = on_handler_get(this,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.OnLoginSuccessfully,
+	                                 cr.plugins_.Rex_Firebase_Authentication.prototype.cnds.OnLoginError);
+        var d = {"remember":PRESISTING_TYPE[r_],
+                 "scopr":scope_};
+	    this.get_ref()[login_fn_name](provider, handler, d);
+	};	
+		
+    Acts.prototype.LoggingOut = function ()
+	{
+	    this.get_ref()["unauth"]();
+	};		
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+	Exps.prototype.ErrorCode = function (ret)
+	{
+	    var val = (!this.last_error)? "": this.last_error.code;    
+		ret.set_string(val);
+	}; 
+	
+	Exps.prototype.ErrorMessage = function (ret)
+	{
+	    var val = (!this.last_error)? "": this.last_error["message"];    
+		ret.set_string(val);
+	}; 	
+	Exps.prototype.UserID = function (ret)
+	{
+	    var val = (!this.last_authData)? "": this.last_authData["uid"];
+		ret.set_string(val);
+	}; 	
+	Exps.prototype.Provider = function (ret)
+	{
+	    var val = (!this.last_authData)? "": this.last_authData["provider"];	    
+		ret.set_string(val);
+	}; 	
+
+    var get_provider_property = function (authData, p)
+    {
+		if (authData == null)
+		    return "";
+	    
+		var provide_type = authData["provider"];
+		var provider_info = authData[provide_type];
+		if (provider_info == null)
+		    return "";
+			
+		var val = provider_info[p];
+		if (val == null)
+		    val = "";
+
+        return val;
+    };
+	Exps.prototype.DisplayName = function (ret)
+	{
+		ret.set_string(get_provider_property(this.last_authData, "displayName"));
+	};
+	Exps.prototype.UserIDFromProvider = function (ret)
+	{
+		ret.set_string(get_provider_property(this.last_authData, "id"));
+	};
+	Exps.prototype.AccessToken = function (ret)
+	{
+		ret.set_string(get_provider_property(this.last_authData, "accessToken"));
+	};	    
+	Exps.prototype.CachedUserProfile = function (ret)
+	{
+        var val = get_provider_property(this.last_authData, "cachedUserProfile");
+        ret.set_string( JSON.stringify(val) );
+	};
+	Exps.prototype.Email = function (ret)
+	{
+		ret.set_string(get_provider_property(this.last_authData, "email"));
+	};		
+	Exps.prototype.UserName = function (ret)
+	{
+		ret.set_string(get_provider_property(this.last_authData, "username"));
+	};		
+}());
