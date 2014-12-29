2c94ca054b508460e131acfabfb8b15d65f11a20
hua <binhua.tein@gmail.com>
2014-12-30 00:20:07 +0800

    Add plugin

---
+++ plugins/rex_firebase_itemfilter/edittime.js
@@ -0,0 +1,143 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Item filter",
+		"id":			"Rex_Firebase_ItemFilter",
+		"version":		"0.1",        
+		"description":	"Query items with condition filters.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_firebase_itemfilter.html",
+		"category":		"Rex - Web - firebase",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0,
+		"dependency":	"firebase.js"
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddStringParam("Tag", "A tag, to distinguish between different save requests.", '"_"');
+AddCondition(1, cf_trigger, "On save complete", "Save", 
+            "On save <i>{0}</i> complete",
+            "Triggered when save current item complete.", "OnSaveComplete");
+
+AddStringParam("Tag", "A tag, to distinguish between different save requests.", '"_"');
+AddCondition(2, cf_trigger, "On save error", "Save", 
+            "On save <i>{0}</i> error",
+            "Triggered when save current item error.", "OnSaveError");
+
+AddStringParam("Tag", "A tag, to distinguish between different save requests.", '"_"');
+AddCondition(3, cf_trigger, "On remove complete", "Remove", 
+            "On remove <i>{0}</i> complete",
+            "Triggered when remove current item complete.", "OnRemoveComplete");
+
+AddStringParam("Tag", "A tag, to distinguish between different save requests.", '"_"');
+AddCondition(4, cf_trigger, "On remove error", "Remove", 
+            "On remove <i>{0}</i> error",
+            "Triggered when remove current item error.", "OnRemoveError"); 
+			
+AddStringParam("Tag", "A tag, to distinguish between different save requests.", '"_"');
+AddCondition(11, cf_trigger, "On request complete", "Request", 
+            "On request <i>{0}</i> complete",
+            "Triggered when request current item complete.", "OnRequestComplete");
+
+AddCondition(12, cf_looping | cf_not_invertible, "For each itemID", "Request", 
+             "For each itemID", 
+             "Repeat the event for each itemID of request result.", "ForEachItemID"); 			
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("Key", "The name of the key.", '""');
+AddAnyTypeParam("Value", "The value to set", 0);
+AddAction(1, 0, "Set value", "Save", 
+          "Set key <i>{0}</i> to value <i>{1}</i> in current item", 
+          "Set value into current item.", "SetValue");
+		  
+AddStringParam("ID", "ID of item.", '""');
+AddStringParam("Tag", "A tag, to distinguish between different save requests.", '"_"');
+AddAction(2, 0, "Save", "Save", 
+          "Save current item with ID <i>{0}</i> (tag <i>{1}</i>)", 
+          "Save current item into server.", "Save"); 
+          
+AddStringParam("ID", "ID of item.", '""');
+AddStringParam("Tag", "A tag, to distinguish between different save requests.", '"_"');
+AddAction(3, 0, "Remove", "Remove", 
+          "Remove item with ID <i>{0}</i> (tag <i>{1}</i>)", 
+          "Remove item from server.", "Remove");           
+
+AddNumberParam("Count", "Count of picked item.", 1);    
+AddStringParam("Tag", "A tag, to distinguish between different save requests.", '"_"');      
+AddAction(11, 0, "Get random items", "Request", 
+          "Request - get <i>{0}</i> random items (tag <i>{1}</i>)", 
+          "Get random items.", "GetRandomItems");                            
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_any, "Get itemID", "For each itemID", "CurItemID", 
+              "Get current itemID in a For Each loop.");
+              
+AddExpression(2, ef_return_string, "Get itemID in JSON", "JSON", "ItemIDToJSON", 
+              "Get itemID in JSON string.");
+              
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_text, "Domain", "", "The root location of the Firebase data."),
+    new cr.Property(ept_text, "Sub domain", "ItemA", "Sub domain for this function."),
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_firebase_itemfilter/runtime.js
@@ -0,0 +1,405 @@
+﻿/*
+Note:
+Each item would is belonged one client. i.e. only one client could add/remove/set the item.
+----
+Structure:
+
+itemIDs\
+    <itemID>: true
+    
+filters\
+    <keys>
+        <itemID>: <value>
+
+keys\
+    <keys>: true
+*/
+
+// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Firebase_ItemFilter = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var input_text = "";
+	var pluginProto = cr.plugins_.Rex_Firebase_ItemFilter.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	    jsfile_load("firebase.js");
+	};
+	
+	var jsfile_load = function(file_name)
+	{
+	    var scripts=document.getElementsByTagName("script");
+	    var exist=false;
+	    for(var i=0;i<scripts.length;i++)
+	    {
+	    	if(scripts[i].src.indexOf(file_name) != -1)
+	    	{
+	    		exist=true;
+	    		break;
+	    	}
+	    }
+	    if(!exist)
+	    {
+	    	var newScriptTag=document.createElement("script");
+	    	newScriptTag.setAttribute("type","text/javascript");
+	    	newScriptTag.setAttribute("src", file_name);
+	    	document.getElementsByTagName("head")[0].appendChild(newScriptTag);
+	    }
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{ 
+	    this.rootpath = this.properties[0] + "/" + this.properties[1] + "/"; 
+        
+        this.save_itme = {};
+        this.trig_tag = null;
+        this.request_itemIDs = {};	
+        
+        this.exp_CurItemID = "";
+	};
+	
+	instanceProto.get_ref = function(k)
+	{
+        if (k == null)
+            k = "";
+
+        return new window["Firebase"](this.rootpath + k + "/");
+	};
+	
+	instanceProto.get_key_ref = function(itemID, key_)
+	{
+        return this.get_ref()["child"]("filters")["child"](key_)["child"](itemID);
+	};
+	
+	instanceProto.get_keyIndex_ref = function()
+	{
+        return this.get_ref()["child"]("keys");
+	};
+	
+	instanceProto.get_itemID_ref = function(itemID)
+	{
+        return this.get_ref("itemIDs")["child"](itemID)
+	};
+
+	var clean_table = function (o)
+	{
+	    var k;
+		for (k in o)
+		    delete o[k];
+	};
+	
+	var retrieve_itemIDs = function (table_in, arr_out)
+	{
+        var itemID;
+        arr_out.length = 0;
+        for (itemID in table_in)
+        {
+            arr_out.push(itemID);
+        }
+	};
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();
+	
+	Cnds.prototype.OnSaveComplete = function (tag_)
+	{
+	    return cr.equals_nocase(tag_, this.trig_tag);
+	}; 
+	Cnds.prototype.OnSaveError = function (tag_)
+	{
+	    return cr.equals_nocase(tag_, this.trig_tag);
+	};
+	
+	Cnds.prototype.OnRemoveComplete = function (tag_)
+	{
+	    return cr.equals_nocase(tag_, this.trig_tag);
+	}; 
+	Cnds.prototype.OnRemoveError = function (tag_)
+	{
+	    return cr.equals_nocase(tag_, this.trig_tag);
+	};	
+	
+	Cnds.prototype.OnRequestComplete = function (tag_)
+	{
+	    return cr.equals_nocase(tag_, this.trig_tag);
+	}; 
+	
+	Cnds.prototype.ForEachItemID = function ()
+	{
+        var current_frame = this.runtime.getCurrentEventStack();
+        var current_event = current_frame.current_event;
+		var solModifierAfterCnds = current_frame.isModifierAfterCnds();
+		
+		var k, o=this.exp_CurHeader;
+		for(k in this.request_itemIDs)
+		{
+            if (solModifierAfterCnds)
+            {
+                this.runtime.pushCopySol(current_event.solModifiers);
+            }
+            
+            this.exp_CurItemID = k;
+            current_event.retrigger();
+            
+		    if (solModifierAfterCnds)
+		    {
+		        this.runtime.popSol(current_event.solModifiers);
+		    }            
+		}
+		    
+        this.exp_CurItemID = "";   		
+		return false;
+	};
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+	
+    Acts.prototype.SetValue = function (key_, value_)
+	{
+		this.save_itme[key_] = value_;
+	};
+	
+    Acts.prototype.Save = function (itemID, tag_)
+	{
+	    var self = this;
+	    
+	    // try add new item
+	    var on_read_itemID = function (snapshot)
+        {
+            if (snapshot.val() == null)  // itemID is not in itemID list, add it
+            {
+                // add itemID to list
+                wait_events += 1;
+	            self.get_itemID_ref(itemID)["set"](true, isDone_handler);
+            }
+            else  // itemID is existed
+            {
+            }
+            isDone_handler();
+        };
+	    // try add new item
+	    
+	    // wait done
+        var wait_events = 0;
+        var has_error = false;	    
+	    var isDone_handler = function(error)
+	    {
+	        has_error |= (error != null);
+	        wait_events -= 1;
+	        if (wait_events == 0)
+	        {	            
+	            // all jobs done
+			    var trig = (!has_error)? cr.plugins_.Rex_Firebase_ItemFilter.prototype.cnds.OnSaveComplete:
+				                         cr.plugins_.Rex_Firebase_ItemFilter.prototype.cnds.OnSaveError;
+                self.trig_tag = tag_;				                         
+				self.runtime.trigger(trig, self); 	   
+				self.trig_tag = null;         
+	        }
+	    };
+	    // wait done
+	    
+	    // try add new itemID into itemID list
+	    wait_events += 1;
+	    // check if itemID is in itemID list
+	    this.get_itemID_ref(itemID)["once"]("value", on_read_itemID);	
+	    for (var k in this.save_itme)
+	    {	        
+	        // add key-value
+	        wait_events += 1;
+	        this.get_key_ref(itemID, k)["set"](this.save_itme[k], isDone_handler);
+	        wait_events += 1;
+	        this.get_keyIndex_ref()["child"](k)["set"](true, isDone_handler);
+	    }
+		clean_table(this.save_itme);	
+	};
+	
+    Acts.prototype.Remove = function (itemID, tag_)
+	{
+	    var self = this;
+	    
+	    // try remove itemID
+	    var on_read_itemID = function (snapshot)
+        {
+            if (snapshot.val() == null)  // itemID is not in itemID list
+            {
+            }
+            else  // itemID is existed, try remove it
+            {
+                // remove itemID from list
+                wait_events += 1;
+	            self.get_itemID_ref(itemID)["remove"](isDone_handler);
+                // read keys list
+                wait_events += 1; 
+                self.get_keyIndex_ref()["once"]("value", on_read_keylist);
+            }           
+            isDone_handler();
+        };
+	    // try remove itemID
+	    
+	    // remove keys
+	    var on_read_keylist = function (snapshot)
+        {   
+            var keys = snapshot.val();
+            if (keys != null)
+            {
+                for(var k in keys)
+                {
+                    wait_events += 1;
+	                self.get_key_ref(itemID, k)["remove"](isDone_handler);
+                }                
+            }
+            isDone_handler();
+        };
+	    // remove keys
+	    
+	    // wait done
+        var wait_events = 0;
+        var has_error = false;	    
+	    var isDone_handler = function(error)
+	    {
+	        has_error |= (error != null);
+	        wait_events -= 1;
+	        if (wait_events == 0)
+	        {	            
+	            // all jobs done
+			    var trig = (!has_error)? cr.plugins_.Rex_Firebase_ItemFilter.prototype.cnds.OnRemoveComplete:
+				                         cr.plugins_.Rex_Firebase_ItemFilter.prototype.cnds.OnRemoveError;
+                self.trig_tag = tag_;				                         
+				self.runtime.trigger(trig, self); 	   
+				self.trig_tag = null;         
+	        }
+	    };
+	    // wait done	    
+	    	    
+	    // try remove itemID from itemID list
+	    wait_events += 1;
+	    // check if itemID is in itemID list
+	    this.get_itemID_ref(itemID)["once"]("value", on_read_itemID);	    
+	};
+
+    Acts.prototype.GetRandomItems = function (pick_count, tag_)
+	{	    
+	    clean_table(this.request_itemIDs);
+	    
+	    var self = this;
+	    var on_read_itemIDs = function (snapshot)
+        {
+            var arr_itemIDs = [];
+            var itemIDs = snapshot.val();
+            if (itemIDs == null)
+            {
+                // pick none
+            }
+            else
+            {
+                retrieve_itemIDs(itemIDs, arr_itemIDs);
+                var cnt = arr_itemIDs.length;
+                
+                if (cnt <= pick_count)
+                {
+                    var i;
+                    for (i=0; i<cnt; i++)
+                        self.request_itemIDs[arr_itemIDs[i]] = true; 
+                }
+                else if ((pick_count/cnt) < 0.5)
+                {
+                    // random number picking
+                    var i, rv, try_pick, itemID;
+                    for (i=0; i<pick_count; i++)
+                    {
+                        try_pick = true;
+                        while (try_pick)
+                        {
+                            rv = Math.floor(Math.random() * cnt);
+                            itemID = arr_itemIDs[rv];
+                            if (!self.request_itemIDs.hasOwnProperty(itemID))
+                            {
+                                self.request_itemIDs[itemID] = true;
+                                try_pick = false;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // shuffle index array picking
+                    _shuffle(arr_itemIDs);
+                    arr_itemIDs.length = pick_count;
+                    var i;
+                    for (i=0; i<pick_count; i++)
+                        self.request_itemIDs[arr_itemIDs[i]] = true; 
+                }
+            } // pick random 
+
+            self.trig_tag = tag_;		            
+		    self.runtime.trigger(cr.plugins_.Rex_Firebase_ItemFilter.prototype.cnds.OnRequestComplete, self); 	   
+			self.trig_tag = null;     
+        };
+	
+
+		this.get_ref("itemIDs")["once"]("value", on_read_itemIDs);
+	};	
+	
+	var _shuffle = function (arr, random_gen)
+	{
+        var i = arr.length, j, temp, random_value;
+        if ( i == 0 ) return;
+        while ( --i ) 
+        {
+		    random_value = (random_gen == null)?
+			               Math.random(): random_gen.random();
+            j = Math.floor( random_value * (i+1) );
+            temp = arr[i]; 
+            arr[i] = arr[j]; 
+            arr[j] = temp;
+        }
+    };		
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+
+    Exps.prototype.CurItemID = function (ret)
+	{
+		ret.set_string(this.exp_CurItemID);
+	};
+    
+	Exps.prototype.ItemIDToJSON = function (ret)
+	{
+		ret.set_string(JSON.stringify(this.request_itemIDs));
+	};	
+}());
