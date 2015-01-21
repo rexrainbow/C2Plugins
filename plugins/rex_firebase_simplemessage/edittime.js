1e7436f0e5fcd2c88728b50cb5320809639f3f39
hua <binhua.tein@gmail.com>
2015-01-21 23:41:35 +0800

    Add simple message plugin

---
+++ plugins/rex_firebase_simplemessage/edittime.js
@@ -0,0 +1,116 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Simple message",
+		"id":			"Rex_Firebase_SimpleMessage",
+		"version":		"0.1",        
+		"description":	"Broadcast message to all clients.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_firebase_simplemessage.html",
+		"category":		"Rex - Web - firebase",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0,
+		"dependency":	"firebase.js"
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On receive", "Message", 
+            "On receive message",
+            "Triggered when received message.", "OnReceivedMessage");                   
+//////////////////////////////////////////////////////////////
+// Actions      
+AddStringParam("UserID", "UserID from authentication.", '""');
+AddStringParam("Name", "Player name.", '""');
+AddAction(1, 0, "Set user", "User info", 
+          "Set user name to <i>{1}</i>, user ID to <i>{0}</i>", 
+          "Set user info.", "SetUserInfo");
+          
+AddAction(11, 0, "Start", "Update", 
+          "Start update", 
+          "Start update.", "StartUpdate");
+          
+AddAction(12, 0, "Stop", "Update", 
+          "Stop update", 
+          "Stop update.", "StopUpdate");  
+
+AddStringParam("Message", "Message. String or JSON string for object.", '""');
+AddAction(21, 0, "Broadcast", "Message", 
+          "Broadcast message: <i>{0}</i>", 
+          "Broadcast message.", "BroadcastMessage");          
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_string, "Lest sender ID", "Message", "LastSenderID", 
+              "Get sender ID of last message.");  
+AddExpression(2, ef_return_string, "Lest sender name", "Message", "LastSenderName", 
+              "Get sender name of last message.");            
+AddExpression(3, ef_return_string, "Lest message", "Message", "LastMessage", 
+              "Get last message.");  
+                            
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_text, "Domain", "", "The root location of the Firebase data."),
+    new cr.Property(ept_text, "Sub domain", "Message", "Sub domain for this function."),
+    new cr.Property(ept_combo, "Message type", "String", "Sent message type, string or JSON object in string.", "String|JSON string"),
+    new cr.Property(ept_combo, "Initial update", "Start", "Whether to initially have the updating starting or stopped.", "Stop|Start")
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_firebase_simplemessage/runtime.js
@@ -0,0 +1,285 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Firebase_SimpleMessage = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var input_text = "";
+	var pluginProto = cr.plugins_.Rex_Firebase_SimpleMessage.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	    jsfile_load("firebase.js");
+	};
+	
+	var jsfile_load = function(file_name)
+	{
+	    var scripts=document.getElementsByTagName("script");
+	    var exist=false;
+	    for(var i=0;i<scripts.length;i++)
+	    {
+	    	if(scripts[i].src.indexOf(file_name) != -1)
+	    	{
+	    		exist=true;
+	    		break;
+	    	}
+	    }
+	    if(!exist)
+	    {
+	    	var newScriptTag=document.createElement("script");
+	    	newScriptTag.setAttribute("type","text/javascript");
+	    	newScriptTag.setAttribute("src", file_name);
+	    	document.getElementsByTagName("head")[0].appendChild(newScriptTag);
+	    }
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{ 
+	    this.rootpath = this.properties[0] + "/" + this.properties[1] + "/";
+        var init_start = (this.properties[3] == 1);
+        
+        this.userID = null;
+        this.userName = null;
+        
+        var message_type = this.properties[2];
+        this.simple_message = this.create_simpleMessage(message_type, this.get_ref());
+        this.exp_LastMessage = null;
+        
+        if (init_start)
+            this.simple_message.StartUpdate(); 
+	};
+	
+	instanceProto.get_ref = function(k)
+	{
+	    if (k == null)
+	        k = "";
+	        
+	    var path;
+	    if (k.substring(0,8) == "https://")
+	        path = k;
+	    else
+	        path = this.rootpath + k + "/";
+	        
+        return new window["Firebase"](path);
+	};
+	
+	
+    instanceProto.create_simpleMessage = function (message_type, ref)
+	{    
+	    var self = this;
+	    var on_received = function(d)
+	    {
+	        self.exp_LastMessage = d;
+            var trig = cr.plugins_.Rex_Firebase_SimpleMessage.prototype.cnds.OnReceivedMessage;
+            self.runtime.trigger(trig, self); 
+	    };
+	    
+	    var simple_message = new window.FirebaseSimpleMessageKlass(message_type);
+	    simple_message.onReceived = on_received;
+        simple_message.SetRef(ref);
+        
+        return simple_message;
+    };
+  
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();
+    
+	Cnds.prototype.OnReceivedMessage = function ()
+	{
+	    return true;
+	};
+		
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+    
+    Acts.prototype.SetUserInfo = function (userID, userName)
+	{	    
+        this.userID = userID;
+        this.userName = userName; 
+	};    
+    Acts.prototype.StartUpdate = function ()
+	{	    
+	    this.simple_message.StartUpdate();   
+	};
+ 
+    Acts.prototype.StopUpdate = function ()
+	{
+	    this.simple_message.StopUpdate();    
+	};
+
+    Acts.prototype.BroadcastMessage = function (message)
+	{
+	    this.simple_message.Send(message, this.userID, this.userName);
+	};       
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+    
+	Exps.prototype.LastSenderID = function (ret)
+	{
+        var senderID = null;
+        if (this.exp_LastMessage != null)
+            senderID = this.exp_LastMessage["senderID"];
+        if (senderID == null)
+            senderID = "";
+	    
+		ret.set_string(senderID);
+	};  
+
+	Exps.prototype.LastSenderName = function (ret)
+	{
+        var senderName = null;
+        if (this.exp_LastMessage != null)
+            senderName = this.exp_LastMessage["senderName"];
+        if (senderName == null)
+            senderName = "";            
+            
+		ret.set_string(senderName);
+	}; 
+
+	Exps.prototype.LastMessage = function (ret)
+	{
+        var message = null;
+        if (this.exp_LastMessage != null)
+            message = this.exp_LastMessage["message"];            
+        if (message == null)
+            message = "";  
+        
+		ret.set_string(message);
+	}; 
+	
+}());
+
+(function ()
+{
+    if (window.FirebaseSimpleMessageKlass != null)
+        return;    
+    
+    var MESSAGE_STRING = 0;
+    var MESSAGE_JSON = 1;
+    var SimpleMessageKlass = function (messageType)
+    {
+        // export
+        this.onReceived = null
+        // export
+                
+        this.messageType = messageType;
+        
+        // internal
+        this.skip_first = true;
+        this.stamp = false;
+        this.ref = null;
+        this.on_read = null;        
+    };
+    
+    var SimpleMessageKlassProto = SimpleMessageKlass.prototype;    
+
+    SimpleMessageKlassProto.SetRef = function (ref)
+    {
+        var is_reading = (this.on_read != null);
+        this.StopUpdate();
+        this.ref = ref;
+        if (is_reading)
+            this.StartUpdate();
+    }; 
+    
+    SimpleMessageKlassProto.Send = function (message, senderID, senderName)
+    {
+        if (this.ref == null)
+            return;
+        
+        if (this.messageType == MESSAGE_JSON)
+            message = JSON.parse(s); 
+        
+        var d = {
+            "message": message,
+            "senderID": senderID,
+            "senderName": senderName,
+            "stamp" : this.stamp,
+        };
+        this.skip_first = false;        
+        this.ref["set"](d);        
+        this.stamp = !this.stamp;
+    };    
+    
+    SimpleMessageKlassProto.StartUpdate = function (ref)
+	{
+        this.StopUpdate();
+        if (ref != null)
+            this.ref = ref; 
+        
+        this.skip_first = true;      // skip previous message
+        
+        var self = this;
+	    var on_update = function (snapshot)
+	    {     
+	        var d = snapshot["val"]();
+            if (self.skip_first)
+            {
+                self.skip_first = false;
+                return;
+            }
+            if (d == null)
+                return;
+
+
+            if (self.messageType == MESSAGE_JSON)
+                d["message"] = JSON.stringify(d["message"]);
+            
+            if (self.onReceived)
+                self.onReceived(d);
+        };
+
+        this.ref["on"]("value", on_update);        
+        this.on_read = on_update;
+        this.ref["onDisconnect"]()["remove"]();
+    };
+
+    SimpleMessageKlassProto.StopUpdate = function ()
+	{
+        if (this.on_read == null)
+            return;
+
+        this.ref["off"]("value", this.on_read);
+        this.on_read = null;             
+        
+        this.ref["remove"]();
+        this.ref["onDisconnect"]()["cancel"]();
+    };  
+        	
+	window.FirebaseSimpleMessageKlass = SimpleMessageKlass;
+}());     
