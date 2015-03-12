2db71a296f6682252e8926fe644c773cb3ec3a0b
hua <binhua.tein@gmail.com>
2015-03-13 00:09:47 +0800

    Add plugin

---
+++ plugins/rex_youtube_player/edittime.js
@@ -0,0 +1,170 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Youtube player",
+		"id":			"rex_youtube_player",
+		"version":		"0.1",
+		"description":	"Play youtube video at iframe.",
+		"author":		"Rex.Rainbow",
+		"help url":		"",
+		"category":		"Web",
+		"type":			"world",			// appears in layout
+		"rotatable":	true,
+		"flags":		pf_position_aces | pf_size_aces | pf_angle_aces
+	};
+};
+
+////////////////////////////////////////
+// Conditions
+							
+AddCondition(0, cf_none, "Is playing", "Video", "Is playing", "True if video is currently playing.", "IsPlaying");
+
+AddCondition(1, cf_none, "Is paused", "Video", "Is paused", "True if video is currently paused.", "IsPaused");
+
+AddCondition(2, cf_none, "Has ended", "Video", "Has ended", "True if video has finished playing.", "HasEnded");
+
+AddCondition(3, cf_none, "Is muted", "Video", "Is muted", "True if video sound is muted.", "IsMuted");
+
+AddComboParamOption("Unstarted");			// 0
+AddComboParamOption("Ended");	            // 1
+AddComboParamOption("Playing");				// 2
+AddComboParamOption("Paused");				// 3
+AddComboParamOption("Buffering");		    // 4
+AddComboParamOption("Video cued");		    // 6
+AddComboParam("Event", "The playback event to check for.");
+AddCondition(4, cf_trigger, "On playback event", "Video", "On {0}", "Triggered when a playback event occurs.", "OnPlaybackEvent");
+
+////////////////////////////////////////
+// Actions
+
+AddStringParam("Video ID", "Video ID to play.");
+AddComboParamOption("No");
+AddComboParamOption("Yes");
+AddComboParam("Play", "Start playing upon buffered.", 1);
+AddAction(0, af_none, "Load video", "Video", 
+          "Load video with ID to <i>{0}</i>, auto play to <i>{1}</i>", 
+          "Load video with ID.", "LoadVideoID");
+
+AddNumberParam("Time", "Playback time in seconds to seek to.");
+AddAction(1, af_none, "Set playback time", "Video", "Set playback time to <b>{0}</b> seconds", "Set the current playback time in seconds.", "SetPlaybackTime");
+
+AddComboParamOption("not looping");
+AddComboParamOption("looping");
+AddComboParam("Mode", "Whether or not the video should loop when it reaches the end.");
+AddAction(2, af_none, "Set looping", "Video", "Set {0}", "Set whether the video loops when it reaches the end.", "SetLooping");
+
+AddComboParamOption("not muted");
+AddComboParamOption("muted");
+AddComboParam("Mode", "Whether or not the audio should be muted.");
+AddAction(3, af_none, "Set muted", "Video", "Set {0}", "Set whether the audio is muted.", "SetMuted");
+
+AddNumberParam("Volume", "The volume of the audio to set, from 0 to 100.", 100);
+AddAction(4, af_none, "Set volume", "Video", "Set volume to <b>{0}</b>", "Set the volume of the accompanying audio.", "SetVolume");
+
+AddAction(5, af_none, "Pause", "Video", "Pause", "Pause the current playback.", "Pause");
+
+AddAction(6, af_none, "Play", "Video", "Play", "Start playing the video if stopped or paused. On mobile, may only work in a user input trigger.", "Play");
+
+////////////////////////////////////////
+// Expressions
+
+AddExpression(0, ef_return_number, "", "Video", "PlaybackTime", "Current playback time in seconds.");
+
+AddExpression(1, ef_return_number, "", "Video", "Duration", "Video duration in seconds, if known.");
+
+AddExpression(2, ef_return_number, "", "Video", "Volume", "Current video volume in dB attenuation.");
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+    new cr.Property(ept_text, "Video ID", "r45t8CgSBeA", "Video ID to play."),
+    new cr.Property(ept_combo, "Autoplay", "Yes", "Start playing upon buffered.", "No|Yes"),
+    //new cr.Property(ept_combo,	"Initial visibility",	"Visible",	"Choose whether the video is visible on startup.", "Invisible|Visible")
+	];
+
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	this.just_inserted = false;
+}
+
+IDEInstance.prototype.OnCreate = function()
+{
+	this.instance.SetHotspot(new cr.vector2(0, 0));
+}
+
+IDEInstance.prototype.OnInserted = function()
+{
+	this.instance.SetSize(new cr.vector2(150, 22));
+}
+
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+	
+// Called to draw self in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+	if (!this.font)
+		this.font = renderer.CreateFont("Arial", 14, false, false);
+		
+	renderer.SetTexture(null);
+	var quad = this.instance.GetBoundingQuad();
+	renderer.Fill(quad, cr.RGB(224, 224, 224));
+	renderer.Outline(quad, cr.RGB(0, 0, 0));
+	
+	cr.quad.prototype.offset.call(quad, 0, 2);
+	var rc = new cr.rect();
+	cr.quad.prototype.bounding_box.call(quad, rc);
+
+	this.font.DrawText("Youtube player",
+						rc,
+						cr.RGB(0, 0, 0),
+						ha_center);
+}
+
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
---
+++ plugins/rex_youtube_player/runtime.js
@@ -0,0 +1,363 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.rex_youtube_player = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var manuallyChanged = false;
+	/////////////////////////////////////
+	var pluginProto = cr.plugins_.rex_youtube_player.prototype;
+
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	    jsfile_load("https://www.youtube.com/iframe_api");
+	};
+	
+	var jsfile_load = function(file_name)
+	{
+	    var scripts=document.getElementsByTagName("script");
+	    var exist=false;	    
+	    for(var i=0;i<scripts.length;i++)
+	    {
+	    	if(scripts[i].src.indexOf(file_name) != -1)
+	    	{
+	    		exist=true;
+	    		break;
+	    	}
+	    }
+	    if(!exist)
+	    {
+	    	var newScriptTag=document.createElement("script");
+	    	newScriptTag.setAttribute("type","text/javascript");
+	    	newScriptTag.setAttribute("src", file_name);
+	    	document.getElementsByTagName("head")[0].appendChild(newScriptTag);
+	    }
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+	    var elemId = this.uid.toString();	    
+		this.elem = document.createElement("div");
+		this.elem.id = elemId;
+		this.elem.setAttribute("id", elemId);
+        this.elem.width = Math.round(this.elem.width);
+		this.elem.height = Math.round(this.elem.height);
+		this.elem.x = Math.round(this.elem.x);
+		this.elem.y = Math.round(this.elem.y);		
+		
+		jQuery(this.elem).appendTo(this.runtime.canvasdiv ? this.runtime.canvasdiv : "body");
+		if (this.properties[2] === 0)		// initially invisible
+		{
+			jQuery(this.elem).hide();
+			this.visible = false;
+			//this.element_hidden = true;
+		}
+					
+
+		this.updatePosition();
+
+		this.runtime.tickMe(this);
+
+        this.is_api_loaded = false;		
+        this.youtube_player = null;
+        this.init_videoId = this.properties[0];
+        this.youtube_state = -1;
+        this.is_autoplay = this.properties[1];
+        //this.show_controls = this.properties[2];
+        
+        var self=this;        
+        window["onYouTubeIframeAPIReady"] = function() 
+        {
+            self.is_api_loaded = true;
+            if (self.init_videoId === "")
+                return;
+                
+            self.create_player(self.init_videoId);
+        };
+	};
+
+	instanceProto.onDestroy = function ()
+	{
+		if (this.youtube_player != null)
+		    this.youtube_player["destroy"]();
+
+		jQuery(this.elem).remove();
+		this.elem = null;
+	};
+
+	instanceProto.tick = function ()
+	{
+		this.updatePosition();
+	};
+
+	instanceProto.updatePosition = function ()
+	{
+		var left = this.layer.layerToCanvas(this.x, this.y, true);
+		var top = this.layer.layerToCanvas(this.x, this.y, false);
+		var right = this.layer.layerToCanvas(this.x + this.width, this.y + this.height, true);
+		var bottom = this.layer.layerToCanvas(this.x + this.width, this.y + this.height, false);
+
+		// Is entirely offscreen or invisible: hide
+		if (!this.visible || !this.layer.visible || right <= 0 || bottom <= 0 || left >= this.runtime.width || top >= this.runtime.height)
+		{
+			jQuery(this.elem).hide();
+			return;
+		}
+
+		// Truncate to canvas size
+		if (left < 1)
+			left = 1;
+		if (top < 1)
+			top = 1;
+		if (right >= this.runtime.width)
+			right = this.runtime.width - 1;
+		if (bottom >= this.runtime.height)
+			bottom = this.runtime.height - 1;
+
+		jQuery(this.elem).show();
+
+		var offx = left + jQuery(this.runtime.canvas).offset().left;
+		var offy = top + jQuery(this.runtime.canvas).offset().top;
+		jQuery(this.elem).offset({left: offx, top: offy});
+		jQuery(this.elem).width(right - left);
+		jQuery(this.elem).height(bottom - top);
+		//rounding position & width to avoid jitter
+		this.elem.width = Math.round(this.elem.width);
+		this.elem.height = Math.round(this.elem.height);
+		this.elem.x = Math.round(this.elem.x);
+		this.elem.y = Math.round(this.elem.y);
+		//	
+	};
+
+	// only called if a layout object
+	instanceProto.draw = function(ctx)
+	{
+	};
+
+	instanceProto.drawGL = function(glw)
+	{
+	};
+	
+	instanceProto.create_player = function (videoId)
+	{
+	    if (!this.is_api_loaded)
+	        return;
+	        
+        if (this.youtube_player != null)
+            return;
+	        
+	    var self = this;
+	    var onPlayerStateChange = function (event)
+	    {
+	        if (event["data"] === self.youtube_state)
+	            return;
+	            	        
+	        self.youtube_state = event["data"];
+	        self.runtime.trigger(cr.plugins_.rex_youtube_player.prototype.cnds.OnPlaybackEvent, self);
+	    };
+	    
+	    var onPlayerReady = function (event)
+	    {
+	        if (self.is_autoplay)
+	            self.youtube_player["playVideo"]();
+	    };
+	
+	    var playerVars = {};
+        this.youtube_player = new window["YT"]["Player"](
+            this.elem.id, 
+            { "height": this.elem.height,
+              "width":  this.elem.width,
+              "videoId": videoId,    
+              "playerVars": playerVars,                      
+              "events": {"onStateChange": onPlayerStateChange,
+                         "onReady": onPlayerReady,
+                        }
+            }
+        );
+	};	
+	
+
+	//////////////////////////////////////
+	// Conditions
+    function Cnds() {};
+	pluginProto.cnds = new Cnds();     
+
+	Cnds.prototype.IsPlaying = function ()
+	{
+		if (this.youtube_player == null)
+		    return false;
+		    	    
+	    return (this.youtube_player["getPlayerState"]() === window["YT"]["PlayerState"]["PLAYING"]);
+	};
+	
+	Cnds.prototype.IsPaused = function ()
+	{
+		if (this.youtube_player == null)
+		    return false;
+		    	    
+	    return (this.youtube_player["getPlayerState"]() === window["YT"]["PlayerState"]["PAUSED"]);
+	};
+	
+	Cnds.prototype.HasEnded = function ()
+	{
+		if (this.youtube_player == null)
+		    return false;
+		    	    
+	    return (this.youtube_player["getPlayerState"]() === window["YT"]["PlayerState"]["ENDED"]);
+	};
+	
+	Cnds.prototype.IsMuted = function ()
+	{
+		if (this.youtube_player == null)
+		    return false;
+		    
+		return this.youtube_player["isMuted"]();
+	};
+	
+	Cnds.prototype.OnPlaybackEvent = function (state)
+	{
+	    var s;
+	    switch (this.youtube_state)
+	    {
+	    case -1:  s=0; break;  //Unstarted
+	    case  0:  s=1; break;  //Ended
+	    case  1:  s=2; break;  //Playing
+	    case  2:  s=3; break;  //Paused	
+	    case  3:  s=4; break;  //Buffering
+	    case  5:  s=5; break;  //Video cued	    	        
+	    }
+		return (s === state);
+	};
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+	Acts.prototype.LoadVideoID = function (videoId, is_autoplay)
+	{	   
+	    this.is_autoplay = is_autoplay;
+	    
+	    if (!this.is_api_loaded)
+	        this.init_videoId = videoId;
+	    else if (this.youtube_player == null)
+		    this.create_player(videoId);
+		else
+		    this.youtube_player["loadVideoById"](videoId);
+	};
+	
+	Acts.prototype.SetPlaybackTime = function (s)
+	{
+		if (this.youtube_player == null)
+		    return;
+		
+		this.youtube_player["seekTo"](s);
+	};
+	
+	Acts.prototype.SetLooping = function (l)
+	{
+		if (this.youtube_player == null)
+		    return;
+		    	    
+	    this.youtube_player["setLoop"](l===1);
+	};
+	
+	Acts.prototype.SetMuted = function (m)
+	{
+		if (this.youtube_player == null)
+		    return;
+		    	    
+	    if (m === 0)
+	        this.youtube_player["unMute"]();
+	    else
+	        this.youtube_player["mute"]();
+	};
+	
+	Acts.prototype.SetVolume = function (v)
+	{
+		if (this.youtube_player == null)
+		    return;
+		    
+		this.youtube_player["setVolume"]( cr.clamp(v, 0, 100) );    
+	};
+	
+	Acts.prototype.Pause = function ()
+	{
+		if (this.youtube_player == null)
+		    return;
+		
+		this.youtube_player["pauseVideo"]();
+	};
+	
+	Acts.prototype.Play = function ()
+	{
+	    if (this.youtube_player == null)
+		    return;
+		
+		this.youtube_player["playVideo"]();
+	};
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+	Exps.prototype.PlaybackTime = function (ret)
+	{
+		if (this.youtube_player == null)
+		    t = 0;
+		else
+		    t = this.youtube_player["getCurrentTime"]();
+			    
+		ret.set_float( t );
+	};
+	
+	Exps.prototype.Duration = function (ret)
+	{
+		if (this.youtube_player == null)
+		    t = 0;
+		else
+		    t = this.youtube_player["getDuration"]();
+			    
+		ret.set_float( t );
+	};
+	
+	Exps.prototype.Volume = function (ret)
+	{
+	    var v;
+		if (this.youtube_player == null)
+		    v = 0;
+		else
+		    v = this.youtube_player["getVolume"]();
+		ret.set_float( v );
+	};
+}());
