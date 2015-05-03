43ee550fc8fcf6c26d8e9f5c281cd29dad2aa700
hua <binhua.tein@gmail.com>
2015-05-03 23:54:27 +0800

    Update

---
+++ plugins/rex_parse_authentication/edittime.js
@@ -0,0 +1,149 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Authentication",
+		"id":			"Rex_Parse_Authentication",
+		"version":		"0.1",        
+		"description":	"Authentication which is provided by parse.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_parse_authentication.html",
+		"category":		"Rex - Web - parse",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0,
+		"dependency":	"parse-1.3.2.min.js"
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On creating account successfully", "User name & Password - create account", 
+            "On creating account successfully", 
+            "Triggered when creating account successfully.", "UsernamePassword_OnCreateAccountSuccessfully");
+            
+AddCondition(2, cf_trigger, "On creating account error", "User name & Password - create account", 
+            "On create account error", 
+            "Triggered when creating account error.", "UsernamePassword_OnCreateAccountError");
+            
+AddCondition(5, cf_trigger, "On sending password result email successfully", "User name & Password - sending password result email", 
+            "On sending password result email successfully", 
+            "Triggered when sending password result email successfully.", "UsernamePassword_OnSendPasswordResultEmailSuccessfully");
+            
+AddCondition(6, cf_trigger, "On sending password result email error", "User name & Password - sending password result email", 
+            "On sending password result email error", 
+            "Triggered when sending password result email error.", "UsernamePassword_OnSendPasswordResultEmailError"); 
+                            
+// general            
+AddCondition(31, cf_trigger, "On login successfully", "General - login", 
+            "On login successfully", 
+            "Triggered when login successfully.", "OnLoginSuccessfully");
+            
+AddCondition(32, cf_trigger, "On login error", "General - login", 
+            "On login error", 
+            "Triggered when login error.", "OnLoginError");                
+//////////////////////////////////////////////////////////////
+// Actions
+AddStringParam("User name", "Unique user name.");
+AddStringParam("Password", "User password");
+AddStringParam("Email", "User email, optional.");
+AddAction(1, 0, "Create account", "User name & Password - sign up", 
+          "Create account with user name to <i>{0}</i>, password to <i>{1}</i>, email to <i>{2}</i>", 
+          "Create account with user name & password.", "UsernamePassword_CreateAccount");
+
+AddStringParam("User name", "Unique user name.");
+AddStringParam("Password", "User password");
+AddAction(2, 0, "Login", "User name & Password - login", 
+          "Login with user name to <i>{0}</i>, password to <i>{1}</i>", 
+          "Login with user name & password.", "UsernamePassword_Login"); 
+       
+AddStringParam("Email", "User email");
+AddAction(4, 0, "Sending password reset email", "User name & Password - reset password",
+          "Sending password reset email to <i>{0}</i>", 
+          "Sending password reset email", "UsernamePassword_SendPasswordResultEmail"); 
+                   
+// general         
+AddAction(31, 0, "Logging out", "General", 
+          "Logging out current account", 
+          "Logging out current account.", "LoggingOut"); 
+          
+
+AddStringParam("User name", "Unique user name.");
+AddStringParam("Password", "User password");
+AddAction(101, 0, "Sign up & Login", "User name & Password - login", 
+          "Sign up & login with user name to <i>{0}</i>, password to <i>{1}</i>", 
+          "Sign up & login with user name & password.", "UsernamePassword_SignUpLogin");                               
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_string, "Error code", "Error", "ErrorCode", 
+              "Error code.");
+AddExpression(2, ef_return_string, "Error message", "Error", "ErrorMessage", 
+              "Error message.");
+AddExpression(3, ef_return_string, "User ID", "General auth data", "UserID", 
+              "Unique user ID (object ID in User class)");
+AddExpression(4, ef_return_string, "User name", "General auth data", "UserName", 
+              "Unique user name.");
+
+                                                                                   
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_text, "Application ID", "", "Application ID"),
+	new cr.Property(ept_text, "Javascript Key", "", "Javascript Key")
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_parse_authentication/runtime.js
@@ -0,0 +1,273 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_Parse_Authentication = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_Parse_Authentication.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	    jsfile_load("parse-1.3.2.min.js");
+	};
+	
+	var jsfile_load = function(file_name)
+	{
+	    var scripts=document.getElementsByTagName("script");
+	    var exist=false;
+	    for(var i=0;i<scripts.length;i++)
+	    {
+	    	if(scripts[i].src.indexOf(file_name) != -1)
+	    	{
+	    		exist=true;
+	    		break;
+	    	}
+	    }
+	    if(!exist)
+	    {
+	    	var newScriptTag=document.createElement("script");
+	    	newScriptTag.setAttribute("type","text/javascript");
+	    	newScriptTag.setAttribute("src", file_name);
+	    	document.getElementsByTagName("head")[0].appendChild(newScriptTag);
+	    }
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{ 
+	    this.file_obj = null;
+	    
+	    if (!window.RexC2IsParseInit)
+	    {
+	        window["Parse"]["initialize"](this.properties[0], this.properties[1]);
+	        window.RexC2IsParseInit = true;
+	    }
+	    
+        this.last_error = null;	 
+        this.current_user = null;   // valid after login
+	};
+		  
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();
+
+	Cnds.prototype.UsernamePassword_OnCreateAccountSuccessfully = function ()
+	{
+	    return true;
+	}; 	
+
+	Cnds.prototype.UsernamePassword_OnCreateAccountError = function ()
+	{
+	    return true;
+	}; 
+
+	Cnds.prototype.UsernamePassword_OnSendPasswordResultEmailSuccessfully = function ()
+	{
+	    return true;
+	}; 	
+
+	Cnds.prototype.UsernamePassword_OnSendPasswordResultEmailError = function ()
+	{
+	    return true;
+	}; 	
+	
+	Cnds.prototype.OnLoginSuccessfully = function ()
+	{
+	    return true;
+	}; 	
+
+	Cnds.prototype.OnLoginError = function ()
+	{
+	    return true;
+	}; 	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+    Acts.prototype.UsernamePassword_CreateAccount = function (n_, p_, e_)
+	{
+	    var self=this;
+	    var on_success = function ()
+	    {
+	        self.runtime.trigger(cr.plugins_.Rex_Parse_Authentication.prototype.cnds.UsernamePassword_OnCreateAccountSuccessfully, self);  
+	    };
+	    
+	    var on_error = function (user, error)
+	    {
+	        debugger;
+	        self.last_error = error;
+            self.runtime.trigger(cr.plugins_.Rex_Parse_Authentication.prototype.cnds.UsernamePassword_OnCreateAccountError, self);          
+	    };
+	    var user = new window["Parse"]["User"]();
+        user["set"]("username", n_);
+        user["set"]("password", p_);
+        
+        if (e_ !== "")
+            user["set"]("email", e_);
+        
+        var handler = {"success":on_success, "error":on_error};
+        user["signUp"](null, handler);
+	}; 
+	
+    Acts.prototype.UsernamePassword_Login = function (n_, p_)
+	{
+	    var self=this;
+	    var on_success = function (user)
+	    {
+	        self.current_user = user;
+	        self.runtime.trigger(cr.plugins_.Rex_Parse_Authentication.prototype.cnds.OnLoginSuccessfully, self);  
+	    };
+	    
+	    var on_error = function (user, error)
+	    {
+	        self.current_user = user;
+	        self.last_error = error;
+            self.runtime.trigger(cr.plugins_.Rex_Parse_Authentication.prototype.cnds.OnLoginError, self);          
+	    };
+	    
+	    var handler = {"success":on_success, "error":on_error};	    
+        window["Parse"]["User"]["logIn"](n_, p_, handler);
+	}; 
+	
+    Acts.prototype.UsernamePassword_SendPasswordResultEmail = function (e_)
+	{
+	    var self=this;
+	    var on_success = function ()
+	    {
+	        self.runtime.trigger(cr.plugins_.Rex_Parse_Authentication.prototype.cnds.UsernamePassword_OnSendPasswordResultEmailSuccessfully, self);  
+	    };
+	    
+	    var on_error = function (error)
+	    {
+	        self.last_error = error;
+            self.runtime.trigger(cr.plugins_.Rex_Parse_Authentication.prototype.cnds.UsernamePassword_OnSendPasswordResultEmailError, self);          
+	    };
+	    
+	    var handler = {"success":on_success, "error":on_error};	    
+        window["Parse"]["User"]["requestPasswordReset"](e_, handler);
+	};	
+	
+    Acts.prototype.LoggingOut = function ()
+	{
+	    window["Parse"]["User"]["logOut"]();
+	};	
+	
+    Acts.prototype.UsernamePassword_SignUpLogin = function (n_, p_)
+	{
+	    var self = this;
+	    
+	    var OnLoginSuccessfully = function(user)
+	    { 	      
+	        self.current_user = user;  
+	        self.runtime.trigger(cr.plugins_.Rex_Parse_Authentication.prototype.cnds.OnLoginSuccessfully, self);
+	    };	
+	    
+	    var OnLoginError = function(user, error)
+	    {     
+	        self.current_user = user;  
+            self.last_error = error;
+	        self.runtime.trigger(cr.plugins_.Rex_Parse_Authentication.prototype.cnds.OnLoginError, self);
+	    };	
+		    
+		// step 3
+	    var login_again = function (username, password)
+	    {	 
+	        var handler = {"success": OnLoginSuccessfully,
+                           "error": OnLoginError};
+                           
+            window["Parse"]["User"]["logIn"](username, password, handler);	   
+        };
+        
+        // step 2
+	    var try_signingUp = function (username, password)
+	    {
+	        var _on_success = function (user)
+	        {
+	            login_again(username, password);
+	        };
+	        
+	        var handler = {"success": _on_success,
+                           "error": OnLoginError};
+	        var user = new window["Parse"]["User"]();
+            user["set"]("username", username);
+            user["set"]("password", password);
+            user["signUp"](null, handler);
+	    };        	
+        
+        // step 1
+	    var try_login = function (username, password)
+	    {	 
+	        var _on_error = function (user, error) 
+	        {
+                try_signingUp(username, password);	            
+	        };
+	        
+	        var handler = {"success": OnLoginSuccessfully,
+                           "error": _on_error};                          
+                           
+            window["Parse"]["User"]["logIn"](username, password, handler);   
+        };            
+        
+        try_login(n_, p_);  
+	};	
+		 
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+	
+	Exps.prototype.ErrorCode = function (ret)
+	{
+	    var val = (!this.last_error)? "": this.last_error["code"];    
+		ret.set_string(val);
+	}; 
+	
+	Exps.prototype.ErrorMessage = function (ret)
+	{
+	    var val = (!this.last_error)? "": this.last_error["message"];    
+		ret.set_string(val);
+	}; 
+	
+	Exps.prototype.UserID = function (ret)
+	{
+	    var val = (!this.current_user)? "": this.current_user["id"];    
+		ret.set_string(val);
+	}; 	
+	
+	Exps.prototype.UserName = function (ret)
+	{
+	    var val = (!this.current_user)? "": this.current_user["get"]("username");    
+		ret.set_string(val);
+	}; 	
+		
+}());
