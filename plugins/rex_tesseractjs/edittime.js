e6703fc2de1eb170be697ad1160bd4258eeb714c
Hua <binhua.tein@gmail.com>
2016-10-26 23:36:56 +0800

    Update

---
+++ plugins/rex_tesseractjs/edittime.js
@@ -0,0 +1,111 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Tesseract",
+		"id":			"Rex_tesseractjs",
+		"version":		"0.1",        
+		"description":	"Pure Javascript OCR for 62 Languages. Reference: https://github.com/naptha/tesseract.js",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://dl.dropbox.com/u/5779181/C2Repo/rex_tesseractjs.html",
+		"category":		"Rex - Text",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0,
+		"dependency":	"tesseract.js"             
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddCondition(1, cf_trigger, "On completed", "Event", "On completed", "Trigger when recognize completed", "OnCompleted");
+AddCondition(2, cf_trigger, "On error", "Event", "On error", "Trigger when recognize error", "OnError");
+AddCondition(3, cf_trigger, "On progress", "Event", "On progress", "Trigger when recognize progress", "OnProgress");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddObjectParam("Image", "Current frame of Sprite, or canvas object.");    
+AddAction(1, 0, "Recognize", "recognize", 
+          "Recognize <i>{0}</i>","Recognize image.", "Recognize");  
+
+AddStringParam("Property", "Property name", '""');
+AddAnyTypeParam("Value", "Value to set", 0);
+AddAction(11, 0, "Add property", "Options", 
+          "Add <i>{0}</i>: <i>{1}</i> to options",
+          "Add property to options.", "AddProperty"); 
+          
+AddStringParam("Language", "Language.", '"eng"');
+AddAction(12, 0, "Set language", "Options", 
+          "Set language to <i>{0}</i>",
+          "Set language.", "SetLanguage");           
+//////////////////////////////////////////////////////////////
+// Expressions
+AddExpression(1, ef_return_string, "Result", "Result", "Result", "Get result text.");
+AddExpression(11, ef_return_string, "Current status", "Progress", "Status", "Get current status.");
+AddExpression(12, ef_return_number, "Current progress", "Progress", "Progress", "Get current progress.");
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	new cr.Property(ept_text, "Lagunage", "eng", "Languages, listed in https://github.com/naptha/tesseract.js/blob/master/docs/tesseract_lang_list.md"),
+    new cr.Property(ept_section, "Custom paths", "",	"Custom path."),   
+	new cr.Property(ept_text, "Core path", "", "A string specifying the location of the tesseract.js-core library."),
+	new cr.Property(ept_text, "Lang path", "", "A string specifying the location of the tesseract language files."),    
+	new cr.Property(ept_text, "Worker path", "", "A string specifying the location of the tesseract.worker.js file."),        
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_tesseractjs/runtime.js
@@ -0,0 +1,186 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_tesseractjs = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_tesseractjs.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+        this.options = {};    
+        if (this.properties[0] !== "")        
+            this.options["lang"] = this.properties[0];
+        
+        this.createOptions = { };
+        if (this.properties[1] !== "")
+            this.createOptions["corePath"] = this.properties[1];
+        if (this.properties[2] !== "")
+            this.createOptions["langPath"] = this.properties[2];        
+        if (this.properties[3] !== "")
+            this.createOptions["workerPath"] = this.properties[3];            
+        
+        this.tesseractObj = null;        
+        this.progress = null;
+        this.error = null;        
+        this.result = null;
+	};
+    
+	instanceProto.onDestroy = function ()
+	{
+	};   
+    
+	instanceProto.GetTesseractObj = function ()
+	{
+        if (this.tesseractObj !== null)
+            return this.tesseractObj;
+        
+        this.tesseractObj = window["Tesseract"]["create"](this.createOptions);
+        return this.tesseractObj;
+	}; 
+    
+    var getImage= function (inst)
+    {        
+        if (!inst)
+            return null;
+        
+        var img;
+        if (inst.canvas)
+            img = inst.canvas;
+        else if (inst.curFrame && inst.curFrame.texture_img)
+            img = inst.curFrame.texture_img;       
+        else
+            img = null;
+        
+        return img;
+    }
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();    
+
+	Cnds.prototype.OnCompleted = function ()
+	{
+		return true;
+	};
+
+	Cnds.prototype.OnError = function ()
+	{
+		return true;
+	};        
+
+	Cnds.prototype.OnProgress = function ()
+	{
+		return true;
+	};      
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+    Acts.prototype.Recognize = function (objs)
+	{
+        if (!objs)
+            return;
+        
+        var inst = objs.getFirstPicked();
+        var img = getImage(inst);
+        
+        var self=this;
+        var onProgress = function(p)
+        {
+            self.progress = p;
+            self.runtime.trigger(cr.plugins_.Rex_tesseractjs.prototype.cnds.OnProgress, self);                 
+        };
+        var onError = function (err)
+        {
+            self.error = err;
+            self.runtime.trigger(cr.plugins_.Rex_tesseractjs.prototype.cnds.OnError, self);            
+        }
+        var onFinished = function(result)
+        {
+            self.result = result;
+            self.runtime.trigger(cr.plugins_.Rex_tesseractjs.prototype.cnds.OnCompleted, self);
+        };
+        var job = this.GetTesseractObj()["recognize"](img, this.options);
+        job["progress"](onProgress);
+        job["catch"](onError);
+        job["then"](onFinished);        
+	}; 
+    
+	Acts.prototype.AddProperty = function (name, value)
+	{       
+        this.options[name] = value;
+	};
+    
+	Acts.prototype.SetLanguage = function (lang)
+	{       
+        this.options["lang"] = lang;
+	};    
+    
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+    
+	Exps.prototype.Result = function (ret)
+	{
+        var result = (!this.result)? "": this.result["text"];        
+		ret.set_string( result );
+	};
+    
+	Exps.prototype.Status = function (ret)
+	{
+        var status = (!this.progress)? "": this.progress["status"];        
+		ret.set_string( status );
+	};	
+    
+	Exps.prototype.Progress = function (ret)
+	{
+        var progress;
+        if (this.progress)
+        {
+            progress = this.progress["progress"];
+            if (progress == null)
+                progress = 1;
+        }
+        else
+            progress = 0;
+        
+		ret.set_float( progress );
+	};	    
+    
+}());
