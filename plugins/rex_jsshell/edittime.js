dd62f57ab9790c5c0becccb31dd4a149f502e123
Hua <binhua.tein@gmail.com>
2017-05-26 16:17:22 +0800

    Update

---
+++ plugins/rex_jsshell/edittime.js
@@ -0,0 +1,143 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"JS Shell",
+		"id":			"Rex_jsshell",
+		"version":		"1.0",        
+		"description":	"Invoke javascript function.",
+		"author":		"Rex.Rainbow",
+		"help url":		"https://rexrainbow.github.io/C2RexDoc/c2rexpluginsACE/plugin_rex_jsshell.html",
+		"category":		"Rex - Logic",
+		"type":			"object",			// not in layout
+		"rotatable":	false,
+		"flags":		0
+	};
+};
+
+//////////////////////////////////////////////////////////////
+// Conditions
+AddStringParam("Tag", "A tag, which can be anything you like, to distinguish between different callback.", "\"\"");
+AddCondition(1, cf_trigger, "Callback", "Callback", 
+            "On {0}",
+            "Callback.", "OnCallback");
+
+//////////////////////////////////////////////////////////////
+// Actions
+AddAction(1, 0, "Invoke", "Invoke", 
+          "Invoke function",
+          "Invoke function.", "Invoke"); 
+
+AddStringParam("Name", "Object name in dot notation", '""');
+AddAction(11, 0, "Set object name", "Prepare - Function name", 
+          "Object : window.{0}",
+          "Set object name.", "SetObjectName"); 
+
+AddStringParam("Name", "Function name in dot notation", '""');
+AddAction(12, 0, "Set function name", "Prepare - Function name", 
+          "Function : {0}",
+          "Set function name.", "SetFunctionName"); 
+ 
+AddAnyTypeParam("Value", "Value to set", 0);
+AddAction(21, 0, "Set value", "Prepare - Parameter", 
+          "Parameter: {0}",
+          "Add parameter.", "AddValue"); 
+
+AddStringParam("JSON", "JSON value to set", '""');
+AddAction(22, 0, "Set JSON", "Prepare - Parameter", 
+          "Parameter: {0}",
+          "Add parameter to JSON string.", "AddJSON"); 
+
+AddComboParamOption("false");
+AddComboParamOption("true");
+AddComboParam("Boolean", "Boolean value.", 0);
+AddAction(23, 0, "Set boolean", "Prepare - Parameter",
+          "Parameter: {0}",
+          "Add parameter to a boolean value.", "AddBoolean");
+
+AddAnyTypeParam("Callback tag", "Callback tag to set", '""');
+AddAction(24, 0, "Set callback", "Prepare - Parameter", 
+          "Parameter: callback {0}",
+          "Add parameter to parameter.", "AddCallback"); 
+
+AddStringParam("URL", "The URL to request.", "\"http://\"");
+AddAnyTypeParam("Success callback tag", "Callback of loading success", '""');
+AddAnyTypeParam("Error callback tag", "Callback of loading failed", '""');
+AddAction(101, 0, "Load API", "Load API", 
+          "Load API from {0} (success {1}, error {2})",
+          "Load API.", "LoadAPI"); 		  
+
+//////////////////////////////////////////////////////////////
+// Expressions
+AddAnyTypeParam("Index", "The zero-based index of the parameter to get, or name in string.");
+//AddStringParam("Property", "Property name in dot notation", '""');
+AddExpression(1, ef_return_any | ef_variadic_parameters, "Get parameter of callback", "Callback", "Param", "Get the value of a parameter passed to the callback.");
+
+AddExpression(2, ef_return_number, "Get parameter count of callback", "Callback", "ParamCount", "Get the number of parameters passed to callback.");
+
+AddAnyTypeParam("Variable name", "Variable name of this tone object", '""');        
+//AddStringParam("Property", "Property name in dot notation", '""');
+AddExpression(21, ef_return_any | ef_variadic_parameters, "Get return value", "Function", "ReturnValue", "Get return value.");
+
+
+
+ACESDone();
+
+// Property grid properties for this plugin
+var property_list = [
+	];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance, this);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+}
+
+// Called by the IDE after all initialization on this instance has been completed
+IDEInstance.prototype.OnCreate = function()
+{
+}
+
+// Called by the IDE after a property has been changed
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+	
+// Called by the IDE to draw this instance in the editor
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// Called by the IDE when the renderer has been released (ie. editor closed)
+// All handles to renderer-created resources (fonts, textures etc) must be dropped.
+// Don't worry about releasing them - the renderer will free them - just null out references.
+IDEInstance.prototype.OnRendererReleased = function()
+{
+}
---
+++ plugins/rex_jsshell/runtime.js
@@ -0,0 +1,250 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+cr.plugins_.Rex_jsshell = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	var pluginProto = cr.plugins_.Rex_jsshell.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+	
+	var typeProto = pluginProto.Type.prototype;
+
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	instanceProto.onCreate = function()
+	{
+		// function call
+		this.objectName = "";
+		this.functionName = "";
+		this.functionParams = [];
+		this.returnValue = null;
+
+        // callback
+        this.callbackTag = "";   
+        this.callbackParams = [];   // callbackParams
+        var self=this;
+        this.getCallback = function(callbackTag)
+        {
+            if (callbackTag == null)
+                return null;
+        
+            var cb = function ()
+            {
+                self.callbackTag = callbackTag;            
+                cr.shallowAssignArray(self.callbackParams, arguments);
+                self.runtime.trigger(cr.plugins_.Rex_jsshell.prototype.cnds.OnCallback, self); 
+            }
+            return cb;
+        };
+	};
+    
+	instanceProto.onDestroy = function ()
+	{
+	};   
+
+    instanceProto.LoadAPI = function(src, onload_, onerror_)
+	{
+	    var scripts=document.getElementsByTagName("script");
+	    var exist=false;
+	    for(var i=0;i<scripts.length;i++)
+	    {
+	    	if(scripts[i].src.indexOf(src) != -1)
+	    	{
+	    		exist=true;
+	    		break;
+	    	}
+	    }
+	    if(!exist)
+	    {
+	    	var newScriptTag=document.createElement("script");
+            newScriptTag["type"] = "text/javascript";
+            newScriptTag["src"] = src;
+            
+            // onLoad callback
+            var self=this;        
+            var onLoad = function()
+            {
+                self.isLoaded = true;
+                if (onload_)
+                    onload_();
+            };
+            var onError = function()
+            {
+                if (onerror_)
+                    onerror_();
+            };        
+            newScriptTag["onload"] = onLoad;
+            newScriptTag["onerror"] = onError;            
+	    	document.getElementsByTagName("head")[0].appendChild(newScriptTag);
+	    }
+	};	
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+	pluginProto.cnds = new Cnds();    
+
+	Cnds.prototype.OnCallback = function (tag)
+	{
+		return cr.equals_nocase(tag, this.callbackTag);
+	};
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+	pluginProto.acts = new Acts();
+
+    Acts.prototype.Invoke = function ()
+	{
+		if (this.functionName === "")
+		    return;
+        
+		var o;
+		if (this.objectName !== "")
+			o = getItemByKey(window, this.objectName);				
+		else
+			o = window;
+
+        var f = getItemByKey(o, this.functionName);
+        this.returnValue = f.apply(o, this.functionParams);
+		this.functionParams = [];
+	}; 
+    
+	Acts.prototype.SetObjectName = function (name)
+	{
+        this.objectName = name;      
+	}; 
+
+    Acts.prototype.SetFunctionName = function (name)
+	{
+        this.functionName = name;      
+	}; 
+
+    Acts.prototype.AddValue = function (v)
+	{
+		this.functionParams.push(v);      
+	};     
+
+    Acts.prototype.AddJSON = function (v)
+	{
+		this.functionParams.push(JSON.parse(v));      
+	};    
+
+    Acts.prototype.AddBoolean = function (v)
+	{
+		this.functionParams.push(v === 1);      
+	}; 
+
+    Acts.prototype.AddCallback = function (callbackTag)
+	{
+		this.functionParams.push( this.getCallback(callbackTag) );      
+	};
+
+    Acts.prototype.LoadAPI = function (src, successTag, errorTag)
+	{
+		this.LoadAPI(src, this.getCallback(successTag), this.getCallback(errorTag));   
+	};
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	pluginProto.exps = new Exps();
+
+	Exps.prototype.Param = function (ret, index, keys, default_value)
+	{             
+        var val = this.callbackParams[index];        
+		ret.set_any( getItemValue(val, keys, default_value) );
+	}; 
+
+	Exps.prototype.ParamCount = function (ret)
+	{
+		ret.set_int( this.callbackParams.length );
+	}; 
+
+	Exps.prototype.ReturnValue = function (ret, keys, default_value)
+	{        
+		ret.set_any( getItemValue(this.returnValue, keys, default_value) );
+	}; 
+
+    
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------    
+    // ------------------------------------------------------------------------   	
+    var getItemByKey = function (item, k)
+	{
+        var v;
+	    if (item == null)
+            v = null;
+        else if ( (k == null) || (k === "") )
+            v = item;
+        else if ((typeof(k) === "number") || (k.indexOf(".") == -1))
+            v = item[k];
+        else
+        {
+            var kList = k.split(".");
+            v = item;
+            var i,cnt=kList.length;
+            for(i=0; i<cnt; i++)
+            {
+                if (typeof(v) !== "object")
+                {
+                    v = null;
+                    break;
+                }
+                    
+                v = v[kList[i]];
+            }
+        }
+		return v;
+	}
+ 	var getItemValue = function (item, k, default_value)
+	{
+        return din( getItemByKey(item, k), default_value);
+	};	    
+    
+    var din = function (d, default_value)
+    {       
+        var o;
+	    if (d === true)
+	        o = 1;
+	    else if (d === false)
+	        o = 0;
+        else if (d == null)
+        {
+            if (default_value != null)
+                o = default_value;
+            else
+                o = 0;
+        }
+        else if (typeof(d) == "object")
+            o = JSON.stringify(d);
+        else
+            o = d;
+	    return o;
+    };    
+}());
